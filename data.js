4ffb158783e1de50e6f580c6a53ad9a2c9ebb4de
GitHub Action <action@github.com>
2025-12-15 13:24:24 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,24 +1,24 @@
 window.LIT_DATA = {
-  "update_time": "2025-12-15 21:07",
+  "update_time": "2025-12-15 21:24",
   "news": [
     {
-      "title": "👉【微信搜狗】点击查看“文学评论”公众号最新文章",
+      "title": "👉【微信搜狗】“文学评论”公众号最新文章 (点击直达)",
       "url": "https://weixin.sogou.com/weixin?type=2&query=文学评论",
       "source": "微信矩阵",
       "time": "实时",
       "category": "voice"
     },
     {
-      "title": "👉【中国社科网】文学理论前沿资讯",
-      "url": "http://lit.cssn.cn/wx/",
-      "source": "CSSN",
+      "title": "👉【知网】“数字人文”最新学术论文 (按时间排序)",
+      "url": "https://scholar.baidu.com/scholar?q=数字人文&sc_ylo=2024&as_ylo=2025&sort=sc_time",
+      "source": "CNKI/学术",
       "time": "实时",
       "category": "meeting"
     },
     {
-      "title": "👉【知网】“数字人文”最新学术论文(点击按时间排序)",
-      "url": "https://scholar.baidu.com/scholar?q=数字人文&sc_ylo=2024&as_ylo=2025",
-      "source": "百度学术",
+      "title": "👉【B站】文学讲座最新视频实录",
+      "url": "https://search.bilibili.com/all?keyword=文学讲座&order=pubdate",
+      "source": "Bilibili",
       "time": "实时",
       "category": "activity"
     }
