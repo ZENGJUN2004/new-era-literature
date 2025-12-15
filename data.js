0c853609c85fd454632a5aa2a368f8ce1bda3504
GitHub Action <action@github.com>
2025-12-15 12:57:40 +0000

    Auto update

---
+++ data.js
@@ -0,0 +1,26 @@
+window.LIT_DATA = {
+  "update_time": "2025-12-15 12:57",
+  "news": [
+    {
+      "title": "👉【微博】中国作家协会 - 官方实时动态",
+      "url": "https://s.weibo.com/weibo?q=中国作协",
+      "source": "微博",
+      "time": "实时",
+      "category": "meeting"
+    },
+    {
+      "title": "👉【抖音】搜索“新书发布会”现场视频",
+      "url": "https://www.douyin.com/search/新书发布会",
+      "source": "抖音",
+      "time": "实时",
+      "category": "activity"
+    },
+    {
+      "title": "👉【小红书】搜索“文学批评”最新笔记",
+      "url": "https://www.xiaohongshu.com/search_result?keyword=文学批评",
+      "source": "小红书",
+      "time": "实时",
+      "category": "voice"
+    }
+  ]
+};
