ab9bd537da3b9b28b56a82750e4ba206fbfb59cd
GitHub Action <action@github.com>
2025-12-16 02:47:57 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2025-12-16 08:52",
+  "update_time": "2025-12-16 10:47",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,6 +21,160 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
+    },
+    {
+      "title": "《当代文学批评》苏州首发|打造融通世界、回应时代的学术新阵地",
+      "url": "http://www.thepaper.cn/newsDetail_forward_31296310",
+      "source": "澎湃新闻",
+      "time": "8月5日",
+      "category": "activity"
+    },
+    {
+      "title": "从现实向未来望去——2024年成都科幻文学报告|文学备忘录·2024...",
+      "url": "https://sichuan.scol.com.cn/ggxw/202502/82899689.html",
+      "source": "四川在线",
+      "time": "2月13日",
+      "category": "voice"
+    },
+    {
+      "title": "文化新观察|中国科幻文学:在科技与人文的交汇处书写未来",
+      "url": "https://baijiahao.baidu.com/s?id=1828833886431349797&wfr=spider&for=pc",
+      "source": "新华社",
+      "time": "4月8日",
+      "category": "voice"
+    },
+    {
+      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
+      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
+      "url": "https://m.jiemian.com/article/3573232.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "我们日常生活的“热梗”“热词”为何频频出自这里?",
+      "url": "http://baijiahao.baidu.com/s?id=1826795388563261260&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "3月17日",
+      "category": "other"
+    },
+    {
+      "title": "新华网文化观察丨文艺创作,AI热下的冷思考",
+      "url": "http://www.xinhuanet.com/20250331/f2482d16b98547bdbeaecf39c11f8a9c/c.html",
+      "source": "新华网",
+      "time": "3月31日",
+      "category": "other"
+    },
+    {
+      "title": "为何赞美莫言不止,批判莫言不息——兼论读者的权利 | 斯文牛",
+      "url": "https://cul.sohu.com/a/886645964_100112507",
+      "source": "搜狐网",
+      "time": "4月20日",
+      "category": "meeting"
+    },
+    {
+      "title": "2024年度“中国人文学术十大热点”评选揭晓",
+      "url": "https://www.thepaper.cn/newsDetail_forward_30796508",
+      "source": "澎湃新闻",
+      "time": "5月10日",
+      "category": "voice"
+    },
+    {
+      "title": "“2024年度中国人文学术十大热点”发布",
+      "url": "https://baijiahao.baidu.com/s?id=1831724372102028136&wfr=spider&for=pc",
+      "source": "中国新闻网",
+      "time": "5月10日",
+      "category": "voice"
+    },
+    {
+      "title": "破圈·融合·共生——中国文学新力量圆桌派观察",
+      "url": "https://cul.sohu.com/a/905265087_121119387",
+      "source": "搜狐网",
+      "time": "6月17日",
+      "category": "other"
+    },
+    {
+      "title": "警惕文学“圈子化”!“鉴抄”风波持续蔓延",
+      "url": "http://baijiahao.baidu.com/s?id=1837115884105061815&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "7月9日",
+      "category": "other"
+    },
+    {
+      "title": "被爆抄袭后闹自杀,文学圈也流行撒泼打滚了?",
+      "url": "http://www.myzaker.com/article/6874ab5d8e9f090a6f7fedfb",
+      "source": "ZAKER资讯",
+      "time": "7月14日",
+      "category": "other"
+    },
+    {
+      "title": "一周文艺: “女性短经典”系列首发,上博回应文创争议,房主任...",
+      "url": "http://www.myzaker.com/article/687470188e9f0902d15812ad",
+      "source": "ZAKER资讯",
+      "time": "7月14日",
+      "category": "activity"
+    },
+    {
+      "title": "青年写作为什么越来越难?",
+      "url": "https://baijiahao.baidu.com/s?id=1847645341173926574&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "11月2日",
+      "category": "other"
+    },
+    {
+      "title": "欢迎来到2025年|这些原创文学新书要来了(一)",
+      "url": "https://baijiahao.baidu.com/s?id=1820106063191905004&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "1月2日",
+      "category": "activity"
+    },
+    {
+      "title": "周末书香活动汇总 | 桃花一簇开无主,可爱深红爱浅红",
+      "url": "https://history.sohu.com/a/877017672_121124807",
+      "source": "搜狐网",
+      "time": "3月28日",
+      "category": "other"
+    },
+    {
+      "title": "新版“文学好书榜”发布首期名单:15种新书入选",
+      "url": "https://baijiahao.baidu.com/s?id=1833523663857662236&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "5月30日",
+      "category": "activity"
+    },
+    {
+      "title": "今年的上海书展,这45本书卖得很不错",
+      "url": "https://baijiahao.baidu.com/s?id=1840975766803422608&wfr=spider&for=pc",
+      "source": "界面新闻",
+      "time": "8月20日",
+      "category": "other"
+    },
+    {
+      "title": "2025年浦东图书馆“你选我读好书榜”揭晓",
+      "url": "https://baijiahao.baidu.com/s?id=1842862636955085729&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "9月10日",
+      "category": "other"
+    },
+    {
+      "title": "一周文艺:啊!好多文学奖揭晓,托宾和鸟鸟对谈",
+      "url": "http://baijiahao.baidu.com/s?id=1843930136359172029&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "9月22日",
+      "category": "other"
+    },
+    {
+      "title": "AI概念短片助力新书发布鲁迅文学奖得主王十月分享“最后一部作品”",
+      "url": "https://baijiahao.baidu.com/s?id=1838777094631934865&wfr=spider&for=pc",
+      "source": "封面新闻",
+      "time": "7月27日",
+      "category": "activity"
     },
     {
       "title": "...期刊与中国非虚构写作生态——以《钟山》《人民文学》《十月...",
