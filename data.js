0d6a77d4fff125baf15ecad07ddf78b972b75647
GitHub Action <action@github.com>
2025-12-17 15:13:36 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2025-12-17 22:13",
+  "update_time": "2025-12-17 23:13",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -20,62 +20,6 @@ window.LIT_DATA = {
       "url": "https://scholar.baidu.com/scholar?q=当代文学&sc_ylo=2024&sort=sc_time",
       "source": "CNKI",
       "time": "实时",
-      "category": "meeting"
-    },
-    {
-      "title": "北大特别策划!戴锦华、杨立华、范晔、贾妍对谈",
-      "url": "https://cul.sohu.com/a/887906710_121948389",
-      "source": "搜狐网",
-      "time": "4月23日",
-      "category": "other"
-    },
-    {
-      "title": "深度提问的回归_访谈_节目_观众",
-      "url": "https://yule.sohu.com/a/934319552_121924583",
-      "source": "搜狐娱乐",
-      "time": "9月12日",
-      "category": "other"
-    },
-    {
-      "title": "艺绽|戴锦华:对世界充满迷恋的学者,内心却住着个“小女孩”",
-      "url": "https://baijiahao.baidu.com/s?id=1846839029337108495&wfr=spider&for=pc",
-      "source": "北京日报客户端",
-      "time": "10月24日",
-      "category": "other"
-    },
-    {
-      "title": "数字化转型“甜头”是什么?这场专场活动上企业现身说法",
-      "url": "https://sichuan.scol.com.cn/ggxw/202512/83168062.html",
-      "source": "四川在线",
-      "time": "12月5日",
-      "category": "other"
-    },
-    {
-      "title": "《当代电影》| 中国女性电影的经验书写与美学政治——戴锦华教授...",
-      "url": "https://yule.sohu.com/a/910374354_121124735",
-      "source": "搜狐娱乐",
-      "time": "7月3日",
-      "category": "other"
-    },
-    {
-      "title": "有名或无名",
-      "url": "https://www.huxiu.com/article/4817646.html",
-      "source": "虎嗅APP",
-      "time": "前天23:00",
-      "category": "other"
-    },
-    {
-      "title": "一周文化讲座|文学批评是“鬼扯”吗?",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32055623",
-      "source": "澎湃新闻",
-      "time": "11月28日",
-      "category": "meeting"
-    },
-    {
-      "title": "傅菲散文集《人间珍贵》研讨会举行",
-      "url": "https://baijiahao.baidu.com/s?id=1851566553005974189&wfr=spider&for=pc",
-      "source": "中工网",
-      "time": "前天16:00",
       "category": "meeting"
     },
     {
@@ -107,13 +51,6 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "《河南文学》杂志2025年第四期目录",
-      "url": "https://cul.sohu.com/a/942674515_120753574",
-      "source": "搜狐网",
-      "time": "10月12日",
-      "category": "activity"
-    },
-    {
       "title": "期刊目录| 《文学评论》2025年第5期目录、摘要及编后记",
       "url": "http://baijiahao.baidu.com/s?id=1844013382652568959&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -140,27 +77,6 @@ window.LIT_DATA = {
       "source": "中工网",
       "time": "10月14日",
       "category": "activity"
-    },
-    {
-      "title": "迟子建谈创作:谁不曾有风雪弥漫的时刻",
-      "url": "https://baijiahao.baidu.com/s?id=1851708707251256709&wfr=spider&for=pc",
-      "source": "京报网",
-      "time": "15小时前",
-      "category": "other"
-    },
-    {
-      "title": "项飙: 何不直接一点,直接回应大家关心的现实话题?",
-      "url": "http://www.thepaper.cn/newsDetail_forward_32164664",
-      "source": "澎湃新闻",
-      "time": "昨天12:50",
-      "category": "other"
-    },
-    {
-      "title": "《为了活下去,我们给自己讲故事》:我们是否有拆解叙事的勇气",
-      "url": "https://baijiahao.baidu.com/s?id=1851620918976850973&wfr=spider&for=pc",
-      "source": "北京日报客户端",
-      "time": "昨天08:00",
-      "category": "other"
     }
   ]
 };
