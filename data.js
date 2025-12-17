5c5eb98b439f0c0c36fc05ac097dc6050c74b48b
GitHub Action <action@github.com>
2025-12-17 12:21:15 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2025-12-17 19:11",
+  "update_time": "2025-12-17 20:21",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,6 +21,62 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
+    },
+    {
+      "title": "十年再出发,戏曲新蕊向未来——2025年中国小剧场戏曲展演盛大启幕!",
+      "url": "https://baijiahao.baidu.com/s?id=1850019584749369252&wfr=spider&for=pc",
+      "source": "乐游上海",
+      "time": "11月28日",
+      "category": "other"
+    },
+    {
+      "title": "兰大学子在2025“理解当代中国”外语能力大赛甘肃赛区决赛中获...",
+      "url": "https://baijiahao.baidu.com/s?id=1847847974150846675&wfr=spider&for=pc",
+      "source": "兰州大学",
+      "time": "11月5日",
+      "category": "other"
+    },
+    {
+      "title": "中国当代文学研究会公布“2024中国文学年度档案”",
+      "url": "https://baijiahao.baidu.com/s?id=1821748323942392845&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "1月20日",
+      "category": "other"
+    },
+    {
+      "title": "文学会客厅丨中国当代文学研究会会长张清华:青年作家要大胆创新创作",
+      "url": "https://baijiahao.baidu.com/s?id=1834441478641970872&wfr=spider&for=pc",
+      "source": "齐鲁壹点",
+      "time": "6月9日",
+      "category": "other"
+    },
+    {
+      "title": "中国作协新时代文学研究中心(浙江大学)揭牌并举行系列研讨会",
+      "url": "https://baijiahao.baidu.com/s?id=1851542053618217548&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "前天11:07",
+      "category": "meeting"
+    },
+    {
+      "title": "如何创造意义感将成为我们时代的母题丨一周新书推荐",
+      "url": "https://baijiahao.baidu.com/s?id=1851541648995957986&wfr=spider&for=pc",
+      "source": "界面新闻",
+      "time": "前天11:00",
+      "category": "activity"
+    },
+    {
+      "title": "湃书单|澎湃新闻编辑们在读的13本书:我们给自己讲故事",
+      "url": "https://baijiahao.baidu.com/s?id=1851564424310443024&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "前天17:02",
+      "category": "other"
+    },
+    {
+      "title": "2025浙江青年文学之星·冬季榜揭晓,系列活动在浙江文学馆举行",
+      "url": "https://baijiahao.baidu.com/s?id=1851538660621479587&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "前天10:13",
+      "category": "other"
     },
     {
       "title": "2025-3《收获》目录及简介 | 5月14日出版",
@@ -51,13 +107,6 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "《河南文学》杂志2025年第四期目录",
-      "url": "https://cul.sohu.com/a/942674515_120753574",
-      "source": "搜狐网",
-      "time": "10月12日",
-      "category": "activity"
-    },
-    {
       "title": "期刊目录| 《文学评论》2025年第5期目录、摘要及编后记",
       "url": "http://baijiahao.baidu.com/s?id=1844013382652568959&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -77,6 +126,34 @@ window.LIT_DATA = {
       "source": "中工网",
       "time": "10月14日",
       "category": "activity"
+    },
+    {
+      "title": "将南昌地域文化从历史深处“唤醒”",
+      "url": "https://jiangxi.jxnews.com.cn/system/2025/12/16/031003722.shtml",
+      "source": "中国江西网",
+      "time": "昨天06:35",
+      "category": "other"
+    },
+    {
+      "title": "凝视一朵百合花",
+      "url": "https://baijiahao.baidu.com/s?id=1851634845774652135&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "昨天11:43",
+      "category": "other"
+    },
+    {
+      "title": "中短篇新作如何探讨人生衰老、历史记忆|新批评",
+      "url": "http://www.thepaper.cn/newsDetail_forward_32175731",
+      "source": "澎湃新闻",
+      "time": "昨天12:50",
+      "category": "meeting"
+    },
+    {
+      "title": "文学IP可以怎么改?这场对谈聊出了不少新思路",
+      "url": "https://baijiahao.baidu.com/s?id=1851633337512614969&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "昨天11:13",
+      "category": "other"
     }
   ]
 };
