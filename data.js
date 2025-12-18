917b30896ab686f6f69cd7dc30dcf184be66f140
GitHub Action <action@github.com>
2025-12-18 05:14:38 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2025-12-18 12:25",
+  "update_time": "2025-12-18 13:14",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -20,41 +20,6 @@ window.LIT_DATA = {
       "url": "https://scholar.baidu.com/scholar?q=当代文学&sc_ylo=2024&sort=sc_time",
       "source": "CNKI",
       "time": "实时",
-      "category": "meeting"
-    },
-    {
-      "title": "2024年度“中国人文学术十大热点”评选揭晓",
-      "url": "https://www.thepaper.cn/newsDetail_forward_30796508",
-      "source": "澎湃新闻",
-      "time": "5月10日",
-      "category": "voice"
-    },
-    {
-      "title": "新华网文化观察丨文艺创作,AI热下的冷思考",
-      "url": "http://www.xinhuanet.com/20250331/f2482d16b98547bdbeaecf39c11f8a9c/c.html",
-      "source": "新华网",
-      "time": "3月31日",
-      "category": "other"
-    },
-    {
-      "title": "警惕文学“圈子化”!“鉴抄”风波持续蔓延",
-      "url": "http://baijiahao.baidu.com/s?id=1837115884105061815&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "7月9日",
-      "category": "other"
-    },
-    {
-      "title": "被爆抄袭后闹自杀,文学圈也流行撒泼打滚了?",
-      "url": "http://www.myzaker.com/article/6874ab5d8e9f090a6f7fedfb",
-      "source": "ZAKER资讯",
-      "time": "7月14日",
-      "category": "other"
-    },
-    {
-      "title": "艺苑论剑|作家“破圈”,走进日常烟火",
-      "url": "https://baijiahao.baidu.com/s?id=1849936311634046300&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "11月27日",
       "category": "meeting"
     },
     {
@@ -86,13 +51,6 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "《河南文学》杂志2025年第四期目录",
-      "url": "https://cul.sohu.com/a/942674515_120753574",
-      "source": "搜狐网",
-      "time": "10月12日",
-      "category": "activity"
-    },
-    {
       "title": "期刊目录| 《文学评论》2025年第5期目录、摘要及编后记",
       "url": "http://baijiahao.baidu.com/s?id=1844013382652568959&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -107,46 +65,11 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "专家研讨胡学文长篇小说《龙凤歌》:  在创新中突破乡土题材的固化...",
-      "url": "http://baijiahao.baidu.com/s?id=1851649917140346075&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "前天15:42",
+      "title": "长篇历史小说《红珊瑚》首发",
+      "url": "https://baijiahao.baidu.com/s?id=1845943816360001304&wfr=spider&for=pc",
+      "source": "中工网",
+      "time": "10月14日",
       "category": "activity"
-    },
-    {
-      "title": "渭南著名作家王旺山最新长篇小说《向阳而生》首发推介会成功举行",
-      "url": "https://baijiahao.baidu.com/s?id=1851808580981526113&wfr=spider&for=pc",
-      "source": "中时在线",
-      "time": "2小时前",
-      "category": "activity"
-    },
-    {
-      "title": "八大盘点捕捉文娱风向标 一起“从从容容”重温2025年|2025名人堂",
-      "url": "https://baijiahao.baidu.com/s?id=1851670457835211640&wfr=spider&for=pc",
-      "source": "封面新闻",
-      "time": "昨天16:57",
-      "category": "other"
-    },
-    {
-      "title": "演员王传君发文宣布退出,发文称:“不小心进入亚健康‘待机模式...",
-      "url": "https://baijiahao.baidu.com/s?id=1851798937603940381&wfr=spider&for=pc",
-      "source": "中原网",
-      "time": "昨天15:19",
-      "category": "other"
-    },
-    {
-      "title": "演员王传君发文宣布退出",
-      "url": "https://baijiahao.baidu.com/s?id=1851752025940043861&wfr=spider&for=pc",
-      "source": "青岛新闻网",
-      "time": "昨天17:19",
-      "category": "other"
-    },
-    {
-      "title": "拓展书写山乡巨变,叶炜推出长篇新作《乡野新风四重奏》",
-      "url": "https://baijiahao.baidu.com/s?id=1851732834771111739&wfr=spider&for=pc",
-      "source": "文汇报",
-      "time": "昨天13:40",
-      "category": "other"
     }
   ]
 };
