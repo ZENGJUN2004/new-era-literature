0e494a7d2b132580f518e4b4783fb35981c62f79
GitHub Action <action@github.com>
2025-12-19 06:17:31 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2025-12-19 13:14",
+  "update_time": "2025-12-19 14:17",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,6 +21,27 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
+    },
+    {
+      "title": "巩义市举行2025杜甫文学之夜",
+      "url": "https://baijiahao.baidu.com/s?id=1851733212132562757&wfr=spider&for=pc",
+      "source": "湘湘带你看社会",
+      "time": "前天13:46",
+      "category": "other"
+    },
+    {
+      "title": "插上“改编”的翅膀 让原著飞得更高更远",
+      "url": "https://www.hangzhou.gov.cn/col/col812270/art/2025/art_55fc19cb780d42b4b9502e054efdf933.html",
+      "source": "杭州市人民政府",
+      "time": "前天09:52",
+      "category": "other"
+    },
+    {
+      "title": "柳建伟河南财大开讲:故乡文化基因如何烙印文学创作",
+      "url": "https://baijiahao.baidu.com/s?id=1851728227671464425&wfr=spider&for=pc",
+      "source": "大象新闻",
+      "time": "前天12:26",
+      "category": "other"
     },
     {
       "title": "2025-3《收获》目录及简介 | 5月14日出版",
@@ -84,6 +105,48 @@ window.LIT_DATA = {
       "source": "工人日报",
       "time": "10月14日",
       "category": "activity"
+    },
+    {
+      "title": "从西关橘香到沽上粤韵,王松长篇小说《橘红》在广州首发",
+      "url": "https://baijiahao.baidu.com/s?id=1847847012189787325&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "11月4日",
+      "category": "activity"
+    },
+    {
+      "title": "作家东西剧作《秘方》在广西南宁首演",
+      "url": "https://baijiahao.baidu.com/s?id=1851820683860587467&wfr=spider&for=pc",
+      "source": "人民资讯",
+      "time": "昨天11:29",
+      "category": "other"
+    },
+    {
+      "title": "柳建伟研究中心在郑州成立,豫籍文艺名家“归根还巢”",
+      "url": "https://baijiahao.baidu.com/s?id=1851816651894761313&wfr=spider&for=pc",
+      "source": "大象新闻",
+      "time": "昨天11:52",
+      "category": "other"
+    },
+    {
+      "title": "作家的故乡——柳建伟“文学名家大讲堂”在郑州开讲",
+      "url": "https://baijiahao.baidu.com/s?id=1851821924484745380&wfr=spider&for=pc",
+      "source": "大象新闻",
+      "time": "昨天13:16",
+      "category": "other"
+    },
+    {
+      "title": "文学如何书写伟大时代?茅盾文学奖得主柳建伟高校开讲",
+      "url": "https://baijiahao.baidu.com/s?id=1851814255112598357&wfr=spider&for=pc",
+      "source": "正观新闻",
+      "time": "昨天11:04",
+      "category": "other"
+    },
+    {
+      "title": "茅盾文学奖得主柳建伟在郑州商学院解析“文学如何书写伟大时代”",
+      "url": "https://baijiahao.baidu.com/s?id=1851837443377446783&wfr=spider&for=pc",
+      "source": "正观新闻",
+      "time": "昨天17:12",
+      "category": "other"
     }
   ]
 };
