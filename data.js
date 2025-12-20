63c4d5160d021054bf11aae896ee93e884179f23
GitHub Action <action@github.com>
2025-12-20 12:18:37 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2025-12-20 19:09",
+  "update_time": "2025-12-20 20:18",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,83 +21,6 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
-    },
-    {
-      "title": "何诗海︱《魏叔子文集》评点与魏禧古文大家地位的生成",
-      "url": "https://cul.sohu.com/a/888806528_121119369",
-      "source": "搜狐网",
-      "time": "4月25日",
-      "category": "other"
-    },
-    {
-      "title": "这十部作品以“啄木之声”,奏响文艺评论强音",
-      "url": "http://baijiahao.baidu.com/s?id=1832698085974034661&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "5月21日",
-      "category": "meeting"
-    },
-    {
-      "title": "文艺评论就是要好处说好,坏处说坏——访中国文艺评论家协会新任...",
-      "url": "https://baijiahao.baidu.com/s?id=1837932558877089122&wfr=spider&for=pc",
-      "source": "光明网",
-      "time": "7月18日",
-      "category": "meeting"
-    },
-    {
-      "title": "国际移民日:华侨华人以主动叙事打破“他者”定义",
-      "url": "https://www.chinanews.com.cn/hr/2025/12-18/10536124.shtml",
-      "source": "中国新闻网",
-      "time": "前天18:43",
-      "category": "other"
-    },
-    {
-      "title": "《超低空飞行》:我们倒退着走进未来",
-      "url": "http://baijiahao.baidu.com/s?id=1851843069316707368&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "前天18:52",
-      "category": "other"
-    },
-    {
-      "title": "夏烈专访:从资深评论家“跨界”特约主编",
-      "url": "https://baijiahao.baidu.com/s?id=1834818706263406472&wfr=spider&for=pc",
-      "source": "浙江日报",
-      "time": "6月14日",
-      "category": "meeting"
-    },
-    {
-      "title": "文学现场·“杭州会议”与“寻根”|“‘寻根’思潮与中国文化主体...",
-      "url": "https://cul.sohu.com/a/894254126_121139852",
-      "source": "搜狐网",
-      "time": "5月12日",
-      "category": "voice"
-    },
-    {
-      "title": "文学现场·“杭州会议”与“寻根”|谢尚发:如何让史料完整地说话...",
-      "url": "https://cul.sohu.com/a/894253674_121139852",
-      "source": "搜狐网",
-      "time": "5月12日",
-      "category": "other"
-    },
-    {
-      "title": "回归大地的文学信仰——罗长江《大地五部曲》评论集分享会发言摘登",
-      "url": "https://baijiahao.baidu.com/s?id=1835085212602265908&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "6月16日",
-      "category": "meeting"
-    },
-    {
-      "title": "思潮与现象|杨鹏鑫:再媒介化的裂隙与角力:《繁花》戏剧、电视剧的...",
-      "url": "https://cul.sohu.com/a/864139199_121139852",
-      "source": "搜狐网",
-      "time": "2月26日",
-      "category": "voice"
-    },
-    {
-      "title": "新大众文艺的“三新”发展进路",
-      "url": "https://baijiahao.baidu.com/s?id=1851843735151918002&wfr=spider&for=pc",
-      "source": "光明网",
-      "time": "前天18:58",
-      "category": "other"
     },
     {
       "title": "2025-3《收获》目录及简介 | 5月14日出版",
@@ -142,13 +65,6 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "获奖感言感动网友,青年作家刘楚昕新书《泥潭》发布",
-      "url": "https://baijiahao.baidu.com/s?id=1836714185649934540&wfr=spider&for=pc",
-      "source": "北京日报客户端",
-      "time": "7月4日",
-      "category": "activity"
-    },
-    {
       "title": "长篇历史小说《红珊瑚》首发",
       "url": "https://baijiahao.baidu.com/s?id=1845939098107077495&wfr=spider&for=pc",
       "source": "工人日报",
@@ -168,48 +84,6 @@ window.LIT_DATA = {
       "source": "中时在线",
       "time": "前天09:44",
       "category": "activity"
-    },
-    {
-      "title": "文化出海“新三样”,如何讲好中国故事?",
-      "url": "http://baijiahao.baidu.com/s?id=1852005554718502078&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "5小时前",
-      "category": "other"
-    },
-    {
-      "title": "独家对话马伯庸:有一种英雄主义来自“平庸的善”",
-      "url": "http://baijiahao.baidu.com/s?id=1851988281193428319&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "9小时前",
-      "category": "other"
-    },
-    {
-      "title": "罗永浩被逼急了!怒怼上海电信:网速问题再不解决就发疯",
-      "url": "https://baijiahao.baidu.com/s?id=1851919221750258208&wfr=spider&for=pc",
-      "source": "雷科技",
-      "time": "昨天15:03",
-      "category": "other"
-    },
-    {
-      "title": "一周文化讲座|一个人类学家的上海漂流",
-      "url": "https://baijiahao.baidu.com/s?id=1851904006951693638&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "昨天10:59",
-      "category": "meeting"
-    },
-    {
-      "title": "“大文学观”视野下散文何为",
-      "url": "https://baijiahao.baidu.com/s?id=1851886405417327120&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "昨天06:21",
-      "category": "other"
-    },
-    {
-      "title": "郜元宝:许多作家的自信心靠长篇撑起来,但这样的自信心很虚假 |...",
-      "url": "http://baijiahao.baidu.com/s?id=1851940175825940009&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "昨天20:35",
-      "category": "other"
     }
   ]
 };
