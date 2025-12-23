a6076594d5bb720ad8567e08c4db3bc9436bb3d3
GitHub Action <action@github.com>
2025-12-23 17:12:53 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2025-12-24 00:14",
+  "update_time": "2025-12-24 01:12",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,104 +21,6 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
-    },
-    {
-      "title": "《当代文学批评》苏州首发|打造融通世界、回应时代的学术新阵地",
-      "url": "http://www.thepaper.cn/newsDetail_forward_31296310",
-      "source": "澎湃新闻",
-      "time": "8月5日",
-      "category": "activity"
-    },
-    {
-      "title": "教育部中国语言文学类专业教学指导委员会2025年工作(扩大)会议在...",
-      "url": "https://news.jlu.edu.cn/info/1306/60198.htm",
-      "source": "吉林大学新闻中心网站",
-      "time": "12月10日",
-      "category": "other"
-    },
-    {
-      "title": "段崇轩:文学研讨会的“变奏曲”|中原作家",
-      "url": "https://cul.sohu.com/a/915929244_475768",
-      "source": "搜狐网",
-      "time": "7月21日",
-      "category": "meeting"
-    },
-    {
-      "title": "新岁丰歌 笔抒乡情——罗倩纪实文学《归禾》作品研讨会综述",
-      "url": "https://cul.sohu.com/a/950583399_121124759",
-      "source": "搜狐网",
-      "time": "11月3日",
-      "category": "meeting"
-    },
-    {
-      "title": "“2025钟子逸论坛暨文学与科学交叉研究”国际研讨会综述",
-      "url": "https://baijiahao.baidu.com/s?id=1849110085809815316&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "11月18日",
-      "category": "meeting"
-    },
-    {
-      "title": "女性文学为何持续破圈?业内人士:以柔情照亮天地,入骨批判见真章",
-      "url": "https://baijiahao.baidu.com/s?id=1835438720752445278&wfr=spider&for=pc",
-      "source": "封面新闻",
-      "time": "6月20日",
-      "category": "other"
-    },
-    {
-      "title": "作家学者共话女性写作",
-      "url": "https://baijiahao.baidu.com/s?id=1849269197483926374&wfr=spider&for=pc",
-      "source": "金台资讯",
-      "time": "11月20日",
-      "category": "voice"
-    },
-    {
-      "title": "小红书与网络公共领域:基于身份认同理论的性别角色再塑分析",
-      "url": "http://www.eeo.com.cn/2025/1222/774424.shtml",
-      "source": "经济观察网",
-      "time": "前天14:26",
-      "category": "meeting"
-    },
-    {
-      "title": "文学花边·书单|2025年,这些华语文学新书要来了(一)",
-      "url": "https://www.thepaper.cn/newsDetail_forward_29821532",
-      "source": "澎湃新闻",
-      "time": "1月2日",
-      "category": "activity"
-    },
-    {
-      "title": "凡“持微火者”,手中皆有光|持微火者·女性文学好书榜2024年度...",
-      "url": "https://www.thepaper.cn/newsDetail_forward_29937191",
-      "source": "澎湃新闻",
-      "time": "1月20日",
-      "category": "other"
-    },
-    {
-      "title": "中英作家聊“她故事”,女性表达应关注更多话题",
-      "url": "https://www.thepaper.cn/newsDetail_forward_31813394",
-      "source": "澎湃新闻",
-      "time": "10月20日",
-      "category": "other"
-    },
-    {
-      "title": "中外译者共话西语文学在华传播与出版历程",
-      "url": "https://baijiahao.baidu.com/s?id=1852212087641651088&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "前天20:36",
-      "category": "activity"
-    },
-    {
-      "title": "家族史与民族史互为映射投照——长篇纪实文学《撕裂的月亮》专家...",
-      "url": "https://baijiahao.baidu.com/s?id=1852166632964554649&wfr=spider&for=pc",
-      "source": "海报新闻",
-      "time": "前天08:34",
-      "category": "other"
-    },
-    {
-      "title": "酒神少年时 | 张象",
-      "url": "http://baijiahao.baidu.com/s?id=1852216301484873902&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "前天21:44",
-      "category": "other"
     },
     {
       "title": "医保商保“双目录”发布 科创板创新药企收获颇丰",
@@ -170,80 +72,17 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "获奖感言感动网友,青年作家刘楚昕新书《泥潭》发布",
-      "url": "https://baijiahao.baidu.com/s?id=1836714185649934540&wfr=spider&for=pc",
-      "source": "北京日报客户端",
-      "time": "7月4日",
+      "title": "长篇历史小说《红珊瑚》首发",
+      "url": "https://baijiahao.baidu.com/s?id=1845939098107077495&wfr=spider&for=pc",
+      "source": "工人日报",
+      "time": "10月14日",
       "category": "activity"
     },
     {
-      "title": "重塑曹禺经典中“未竟之生命”,谈父女两代人笔下“最雷雨”的女性...",
-      "url": "https://baijiahao.baidu.com/s?id=1852153389492719225&wfr=spider&for=pc",
-      "source": "文汇报",
-      "time": "前天05:05",
-      "category": "other"
-    },
-    {
-      "title": "《当代》首发作品入选中国小说学会2025年度好小说",
-      "url": "https://cul.sohu.com/a/967921041_121119370",
-      "source": "搜狐网",
-      "time": "前天08:30",
-      "category": "activity"
-    },
-    {
-      "title": "南平松溪历史文化散文集《一山剑气一溪风》首发式举行",
-      "url": "https://baijiahao.baidu.com/s?id=1852195609400774940&wfr=spider&for=pc",
-      "source": "中国网",
-      "time": "前天15:44",
-      "category": "activity"
-    },
-    {
-      "title": "《寻找金蔷薇》:置身文学现场,照亮心中理想|新书",
-      "url": "https://baijiahao.baidu.com/s?id=1852269374714369061&wfr=spider&for=pc",
-      "source": "湘湘带你看社会",
-      "time": "昨天11:48",
-      "category": "activity"
-    },
-    {
-      "title": "邱兵:“钝感”或许是更高级的先锋_澎湃号·湃客_澎湃新闻-The Paper",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32224556",
-      "source": "澎湃新闻",
-      "time": "昨天15:26",
-      "category": "other"
-    },
-    {
-      "title": "对谈|欧洲骑士文学中对女性的尊崇由阿拉伯世界而来?",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32230070",
-      "source": "澎湃新闻",
-      "time": "昨天15:38",
-      "category": "other"
-    },
-    {
-      "title": "山河血脉丨跨越祁连",
-      "url": "https://baijiahao.baidu.com/s?id=1852268758543647872&wfr=spider&for=pc",
-      "source": "中国军网",
-      "time": "昨天11:39",
-      "category": "other"
-    },
-    {
-      "title": "2025年12月23日 渭南文化旅游资讯微报(组图)",
-      "url": "https://www.ishaanxi.com/c/2025/1223/3583418.shtml",
-      "source": "陕西网",
-      "time": "昨天19:11",
-      "category": "other"
-    },
-    {
-      "title": "丁捷新作《绽放》首发:文学的光在哪里?就在每个人的身边",
-      "url": "https://baijiahao.baidu.com/s?id=1852257177315072816&wfr=spider&for=pc",
-      "source": "文汇报",
-      "time": "昨天08:34",
-      "category": "activity"
-    },
-    {
-      "title": "孙晓玉长篇小说《如此深情的土地》首发式举行",
-      "url": "https://baijiahao.baidu.com/s?id=1852302531623548283&wfr=spider&for=pc",
-      "source": "每日甘肃",
-      "time": "昨天16:45",
+      "title": "从西关橘香到沽上粤韵,王松长篇小说《橘红》在广州首发",
+      "url": "https://baijiahao.baidu.com/s?id=1847847012189787325&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "11月4日",
       "category": "activity"
     }
   ]
