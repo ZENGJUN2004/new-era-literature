5df71c0a9172e199e10dcbeac975bc71881bd6c4
GitHub Action <action@github.com>
2025-12-24 16:13:48 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2025-12-24 23:11",
+  "update_time": "2025-12-25 00:13",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,18 +23,95 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "茅盾文学奖不应\"自相矛盾\"_正义网",
-      "url": "http://www.jcrb.com/culture/jianghu/mdwxj/index.html",
-      "source": "正义网",
-      "time": "",
+      "title": "段崇轩:文学研讨会的“变奏曲”|中原作家",
+      "url": "https://cul.sohu.com/a/915929244_475768",
+      "source": "搜狐网",
+      "time": "7月21日",
+      "category": "meeting"
+    },
+    {
+      "title": "新岁丰歌 笔抒乡情——罗倩纪实文学《归禾》作品研讨会综述",
+      "url": "https://cul.sohu.com/a/950583399_121124759",
+      "source": "搜狐网",
+      "time": "11月3日",
+      "category": "meeting"
+    },
+    {
+      "title": "“2025钟子逸论坛暨文学与科学交叉研究”国际研讨会综述",
+      "url": "https://baijiahao.baidu.com/s?id=1849110085809815316&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "11月18日",
+      "category": "meeting"
+    },
+    {
+      "title": "周末书香活动汇总 | 桃花一簇开无主,可爱深红爱浅红",
+      "url": "https://history.sohu.com/a/877017672_121124807",
+      "source": "搜狐网",
+      "time": "3月28日",
       "category": "other"
     },
     {
-      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
-      "url": "https://m.jiemian.com/article/3573232.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "voice"
+      "title": "www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=98...",
+      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=987051&sid=300",
+      "source": "上观",
+      "time": "9月22日",
+      "category": "other"
+    },
+    {
+      "title": "75岁作家宝刀不老,00后“宝宝”乘风破浪|直击第十次省作代会",
+      "url": "https://www.xhby.net/content/s694aa552e4b04cd42d125b56.html",
+      "source": "新华报业网",
+      "time": "前天22:21",
+      "category": "other"
+    },
+    {
+      "title": "AI概念短片助力新书发布鲁迅文学奖得主王十月分享“最后一部作品”",
+      "url": "https://baijiahao.baidu.com/s?id=1838777094631934865&wfr=spider&for=pc",
+      "source": "封面新闻",
+      "time": "7月27日",
+      "category": "activity"
+    },
+    {
+      "title": "汇积内力 聚焦时代 抒写生活|中国作家“深入生活·扎根人民”系列...",
+      "url": "https://baijiahao.baidu.com/s?id=1850681660617074479&wfr=spider&for=pc",
+      "source": "天眼新闻",
+      "time": "12月5日",
+      "category": "other"
+    },
+    {
+      "title": "《寻找金蔷薇》:置身文学现场,照亮心中理想|新书",
+      "url": "https://baijiahao.baidu.com/s?id=1852269374714369061&wfr=spider&for=pc",
+      "source": "湘湘带你看社会",
+      "time": "前天11:48",
+      "category": "activity"
+    },
+    {
+      "title": "鲁敏新书《不可能死去的人》分享会暨《当代》名家写作课在津举行",
+      "url": "http://app.myzaker.com/news/article.php?pk=693c4a928e9f09654b39dc6c",
+      "source": "ZAKER",
+      "time": "12月12日",
+      "category": "activity"
+    },
+    {
+      "title": "www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=846328",
+      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=846328",
+      "source": "上观",
+      "time": "1月17日",
+      "category": "other"
+    },
+    {
+      "title": "收获",
+      "url": "https://baijiahao.baidu.com/s?id=1846902948359785298&wfr=spider&for=pc",
+      "source": "金台资讯",
+      "time": "10月25日",
+      "category": "other"
+    },
+    {
+      "title": "创新药“跃迁”这一年:从出海爆发到价值兑现",
+      "url": "https://baijiahao.baidu.com/s?id=1852259786230115468&wfr=spider&for=pc",
+      "source": "中国财富网",
+      "time": "前天09:09",
+      "category": "other"
     },
     {
       "title": "2025-3《收获》目录及简介 | 5月14日出版",
@@ -86,11 +163,39 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "余华新作评分遭遇滑铁卢文学“顶流”也需要祛魅丨新闻两点论",
-      "url": "https://baijiahao.baidu.com/s?id=1852292883372204688&wfr=spider&for=pc",
-      "source": "封面新闻",
-      "time": "昨天18:02",
+      "title": "攀高筑峰,文艺苏军交出“时代答卷”",
+      "url": "http://www.js.chinanews.com.cn/news/2025/1224/231653.html",
+      "source": "中国新闻网",
+      "time": "昨天08:18",
+      "category": "other"
+    },
+    {
+      "title": "溯源优秀传统文化核心理念 共话中国式生态法治现代化",
+      "url": "http://www.legaldaily.com.cn/index/content/2025-12/24/content_9311632.html",
+      "source": "法治网",
+      "time": "昨天06:35",
+      "category": "other"
+    },
+    {
+      "title": "贴近时代 书写华章——湖南省作协十年工作综述",
+      "url": "https://baijiahao.baidu.com/s?id=1852344000257413195&wfr=spider&for=pc",
+      "source": "湖南日报",
+      "time": "昨天07:04",
       "category": "meeting"
+    },
+    {
+      "title": "哪种胶原蛋白肽抗衰老效果最好?权威推荐胶原蛋白肽10大品牌,榜首...",
+      "url": "https://baijiahao.baidu.com/s?id=1852349146080963971&wfr=spider&for=pc",
+      "source": "京九晚报",
+      "time": "昨天08:56",
+      "category": "other"
+    },
+    {
+      "title": "《鼓励外商投资产业目录(2025年版)》发布,2026年2月1日起施行",
+      "url": "https://finance.eastmoney.com/a/202512243600675957.html",
+      "source": "东方财富网",
+      "time": "昨天15:32",
+      "category": "activity"
     }
   ]
 };
