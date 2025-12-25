5c68aaab1b05d56e067912ca1bc1c5995acf67b8
GitHub Action <action@github.com>
2025-12-25 04:30:06 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2025-12-25 11:55",
+  "update_time": "2025-12-25 12:30",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,6 +21,41 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
+    },
+    {
+      "title": "段崇轩:文学研讨会的“变奏曲”|中原作家",
+      "url": "https://cul.sohu.com/a/915929244_475768",
+      "source": "搜狐网",
+      "time": "7月21日",
+      "category": "meeting"
+    },
+    {
+      "title": "新岁丰歌 笔抒乡情——罗倩纪实文学《归禾》作品研讨会综述",
+      "url": "https://cul.sohu.com/a/950583399_121124759",
+      "source": "搜狐网",
+      "time": "11月3日",
+      "category": "meeting"
+    },
+    {
+      "title": "“2025钟子逸论坛暨文学与科学交叉研究”国际研讨会综述",
+      "url": "https://baijiahao.baidu.com/s?id=1849110085809815316&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "11月18日",
+      "category": "meeting"
+    },
+    {
+      "title": "www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=846328",
+      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=846328",
+      "source": "上观",
+      "time": "1月17日",
+      "category": "other"
+    },
+    {
+      "title": "收获",
+      "url": "https://baijiahao.baidu.com/s?id=1846902948359785298&wfr=spider&for=pc",
+      "source": "金台资讯",
+      "time": "10月25日",
+      "category": "other"
     },
     {
       "title": "2025-3《收获》目录及简介 | 5月14日出版",
@@ -83,6 +118,55 @@ window.LIT_DATA = {
       "url": "https://baijiahao.baidu.com/s?id=1852302531623548283&wfr=spider&for=pc",
       "source": "每日甘肃",
       "time": "前天16:45",
+      "category": "activity"
+    },
+    {
+      "title": "擎旗奋进践嘱托 “晋江经验”续华章——晋江市宣传思想文化工作综述",
+      "url": "https://baijiahao.baidu.com/s?id=1852449414717226567&wfr=spider&for=pc",
+      "source": "闽南网",
+      "time": "1小时前",
+      "category": "meeting"
+    },
+    {
+      "title": "中原大地披锦绣 绿色发展绘新篇 ——河南省“十四五”林业高质量...",
+      "url": "https://www.henan.gov.cn/2025/12-25/3277983.html",
+      "source": "河南省人民政府门户网站",
+      "time": "5小时前",
+      "category": "other"
+    },
+    {
+      "title": "攀高筑峰,文艺苏军交出“时代答卷”",
+      "url": "http://www.js.chinanews.com.cn/news/2025/1224/231653.html",
+      "source": "中国新闻网",
+      "time": "昨天08:18",
+      "category": "other"
+    },
+    {
+      "title": "溯源优秀传统文化核心理念 共话中国式生态法治现代化",
+      "url": "http://www.legaldaily.com.cn/index/content/2025-12/24/content_9311632.html",
+      "source": "法治网",
+      "time": "昨天06:35",
+      "category": "other"
+    },
+    {
+      "title": "贴近时代 书写华章——湖南省作协十年工作综述",
+      "url": "https://baijiahao.baidu.com/s?id=1852344000257413195&wfr=spider&for=pc",
+      "source": "湖南日报",
+      "time": "昨天07:04",
+      "category": "meeting"
+    },
+    {
+      "title": "哪种胶原蛋白肽抗衰老效果最好?权威推荐胶原蛋白肽10大品牌,榜首...",
+      "url": "https://baijiahao.baidu.com/s?id=1852349146080963971&wfr=spider&for=pc",
+      "source": "京九晚报",
+      "time": "昨天08:56",
+      "category": "other"
+    },
+    {
+      "title": "《鼓励外商投资产业目录(2025年版)》发布,2026年2月1日起施行",
+      "url": "https://finance.eastmoney.com/a/202512243600675957.html",
+      "source": "东方财富网",
+      "time": "昨天15:32",
       "category": "activity"
     }
   ]
