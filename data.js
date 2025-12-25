875c3a4564492884cf8ab2d91c5fc86c594ee7a7
GitHub Action <action@github.com>
2025-12-25 12:20:28 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2025-12-25 19:10",
+  "update_time": "2025-12-25 20:20",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,90 +21,6 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
-    },
-    {
-      "title": "北大特别策划!戴锦华、杨立华、范晔、贾妍对谈",
-      "url": "http://m.bjnews.com.cn/detail/174538146719296.html",
-      "source": "新京报",
-      "time": "4月23日",
-      "category": "other"
-    },
-    {
-      "title": "戴锦华:面对充满危机的世界,人文学是最好的解毒剂",
-      "url": "https://cul.sohu.com/a/948596137_227314",
-      "source": "搜狐网",
-      "time": "10月29日",
-      "category": "other"
-    },
-    {
-      "title": "在“汤一介当代学人讲座”,听戴锦华回忆“时代之女与造就时代”",
-      "url": "https://baijiahao.baidu.com/s?id=1847588402613043445&wfr=spider&for=pc",
-      "source": "中国青年报",
-      "time": "11月1日",
-      "category": "meeting"
-    },
-    {
-      "title": "金鸡电影论坛|戴锦华:我们还未能回答如何应对AI冲击",
-      "url": "https://www.thepaper.cn/newsDetail_forward_31983945",
-      "source": "澎湃新闻",
-      "time": "11月17日",
-      "category": "meeting"
-    },
-    {
-      "title": "戴锦华对话鲁豫:遭遇切·格瓦拉,是我生命里一场精神热恋",
-      "url": "https://www.thepaper.cn/newsDetail_forward_31983989",
-      "source": "澎湃新闻",
-      "time": "11月17日",
-      "category": "other"
-    },
-    {
-      "title": "戴锦华:爱不死,电影不死|金鸡年度演讲全文",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32023832",
-      "source": "澎湃新闻",
-      "time": "11月24日",
-      "category": "other"
-    },
-    {
-      "title": "《当代电影》| 中国女性电影的经验书写与美学政治——戴锦华教授...",
-      "url": "https://yule.sohu.com/a/910374354_121124735",
-      "source": "搜狐娱乐",
-      "time": "7月3日",
-      "category": "other"
-    },
-    {
-      "title": "北京十月文学月这场论坛 探讨青年写作的创新问题",
-      "url": "https://baijiahao.baidu.com/s?id=1847501421602431540&wfr=spider&for=pc",
-      "source": "青瞳视角",
-      "time": "10月31日",
-      "category": "meeting"
-    },
-    {
-      "title": "...期刊与中国非虚构写作生态——以《钟山》《人民文学》《十月...",
-      "url": "https://cul.sohu.com/a/929655837_121124729",
-      "source": "搜狐网",
-      "time": "8月28日",
-      "category": "voice"
-    },
-    {
-      "title": "【贾平凹新作】《消息》首发《十月》杂志,限量版限时抢购!",
-      "url": "https://cul.sohu.com/a/877611717_122354591",
-      "source": "搜狐网",
-      "time": "3月31日",
-      "category": "activity"
-    },
-    {
-      "title": "www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=846328",
-      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=846328",
-      "source": "上观",
-      "time": "1月17日",
-      "category": "other"
-    },
-    {
-      "title": "收获",
-      "url": "https://baijiahao.baidu.com/s?id=1846902948359785298&wfr=spider&for=pc",
-      "source": "金台资讯",
-      "time": "10月25日",
-      "category": "other"
     },
     {
       "title": "2025-3《收获》目录及简介 | 5月14日出版",
@@ -156,39 +72,18 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
+      "title": "从西关橘香到沽上粤韵,王松长篇小说《橘红》在广州首发",
+      "url": "https://baijiahao.baidu.com/s?id=1847847012189787325&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "11月4日",
+      "category": "activity"
+    },
+    {
       "title": "孙晓玉长篇小说《如此深情的土地》首发式举行",
       "url": "https://baijiahao.baidu.com/s?id=1852302531623548283&wfr=spider&for=pc",
       "source": "每日甘肃",
       "time": "前天16:45",
       "category": "activity"
-    },
-    {
-      "title": "2025年,这些文化大家离我们远去",
-      "url": "https://baijiahao.baidu.com/s?id=1852447621147751384&wfr=spider&for=pc",
-      "source": "界面新闻",
-      "time": "8小时前",
-      "category": "other"
-    },
-    {
-      "title": "《初步举证》北大上演,辛芷蕾到场观看,戴锦华教授与主创展开深度...",
-      "url": "https://baijiahao.baidu.com/s?id=1852448080677043218&wfr=spider&for=pc",
-      "source": "北晚在线",
-      "time": "8小时前",
-      "category": "other"
-    },
-    {
-      "title": "中原大地披锦绣 绿色发展绘新篇 ——河南省“十四五”林业高质量...",
-      "url": "https://www.henan.gov.cn/2025/12-25/3277983.html",
-      "source": "河南省人民政府门户网站",
-      "time": "11小时前",
-      "category": "other"
-    },
-    {
-      "title": "哪种胶原蛋白肽抗衰老效果最好?权威推荐胶原蛋白肽10大品牌,榜首...",
-      "url": "https://baijiahao.baidu.com/s?id=1852349146080963971&wfr=spider&for=pc",
-      "source": "京九晚报",
-      "time": "昨天08:56",
-      "category": "other"
     },
     {
       "title": "《鼓励外商投资产业目录(2025年版)》发布,2026年2月1日起施行",
