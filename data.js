e3d2c06dde55da0b65f9ddcea515f9eea085671f
GitHub Action <action@github.com>
2025-12-25 03:55:44 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2025-12-25 10:49",
+  "update_time": "2025-12-25 11:55",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,111 +21,6 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
-    },
-    {
-      "title": "从现实向未来望去——2024年成都科幻文学报告|文学备忘录·2024...",
-      "url": "https://sichuan.scol.com.cn/ggxw/202502/82899689.html",
-      "source": "四川在线",
-      "time": "2月13日",
-      "category": "voice"
-    },
-    {
-      "title": "文化新观察|中国科幻文学:在科技与人文的交汇处书写未来",
-      "url": "https://baijiahao.baidu.com/s?id=1828833886431349797&wfr=spider&for=pc",
-      "source": "新华社",
-      "time": "4月8日",
-      "category": "voice"
-    },
-    {
-      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
-      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
-      "url": "https://m.jiemian.com/article/3573232.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "我们日常生活的“热梗”“热词”为何频频出自这里?",
-      "url": "http://baijiahao.baidu.com/s?id=1826795388563261260&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "3月17日",
-      "category": "other"
-    },
-    {
-      "title": "界面新闻-只服务于独立思考的人群-Jiemian.com",
-      "url": "https://m.jiemian.com/?q=22877",
-      "source": "界面新闻",
-      "time": "",
-      "category": "other"
-    },
-    {
-      "title": "名家共话新消费时代的文学未来——“八桂书香周”文学论坛在邕举行",
-      "url": "http://opinion.gxnews.com.cn/staticpages/20250419/newgx68035f26-21786169.shtml",
-      "source": "广西新闻网",
-      "time": "4月19日",
-      "category": "meeting"
-    },
-    {
-      "title": "2024年度“中国人文学术十大热点”评选揭晓",
-      "url": "https://www.thepaper.cn/newsDetail_forward_30796508",
-      "source": "澎湃新闻",
-      "time": "5月10日",
-      "category": "voice"
-    },
-    {
-      "title": "破圈·融合·共生——中国文学新力量圆桌派观察",
-      "url": "https://cul.sohu.com/a/905265087_121119387",
-      "source": "搜狐网",
-      "time": "6月17日",
-      "category": "other"
-    },
-    {
-      "title": "一周文艺: “女性短经典”系列首发,上博回应文创争议,房主任...",
-      "url": "http://www.myzaker.com/article/687470188e9f0902d15812ad",
-      "source": "ZAKER资讯",
-      "time": "7月14日",
-      "category": "activity"
-    },
-    {
-      "title": "周末书香活动汇总 | 桃花一簇开无主,可爱深红爱浅红",
-      "url": "https://history.sohu.com/a/877017672_121124807",
-      "source": "搜狐网",
-      "time": "3月28日",
-      "category": "other"
-    },
-    {
-      "title": "2025年浦东图书馆“你选我读好书榜”揭晓",
-      "url": "https://baijiahao.baidu.com/s?id=1842862636955085729&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "9月10日",
-      "category": "other"
-    },
-    {
-      "title": "AI概念短片助力新书发布鲁迅文学奖得主王十月分享“最后一部作品”",
-      "url": "https://baijiahao.baidu.com/s?id=1838777094631934865&wfr=spider&for=pc",
-      "source": "封面新闻",
-      "time": "7月27日",
-      "category": "activity"
-    },
-    {
-      "title": "...期刊与中国非虚构写作生态——以《钟山》《人民文学》《十月...",
-      "url": "https://cul.sohu.com/a/929655837_121124729",
-      "source": "搜狐网",
-      "time": "8月28日",
-      "category": "voice"
-    },
-    {
-      "title": "【贾平凹新作】《消息》首发《十月》杂志,限量版限时抢购!",
-      "url": "https://cul.sohu.com/a/877611717_122354591",
-      "source": "搜狐网",
-      "time": "3月31日",
-      "category": "activity"
     },
     {
       "title": "2025-3《收获》目录及简介 | 5月14日出版",
@@ -181,6 +76,13 @@ window.LIT_DATA = {
       "url": "https://baijiahao.baidu.com/s?id=1847847012189787325&wfr=spider&for=pc",
       "source": "钱江晚报",
       "time": "11月4日",
+      "category": "activity"
+    },
+    {
+      "title": "孙晓玉长篇小说《如此深情的土地》首发式举行",
+      "url": "https://baijiahao.baidu.com/s?id=1852302531623548283&wfr=spider&for=pc",
+      "source": "每日甘肃",
+      "time": "前天16:45",
       "category": "activity"
     }
   ]
