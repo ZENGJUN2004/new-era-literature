c85db838f9961a6d273f8f4a2ecdf7233925a21b
GitHub Action <action@github.com>
2025-12-28 08:14:44 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2025-12-28 15:12",
+  "update_time": "2025-12-28 16:14",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,6 +21,41 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
+    },
+    {
+      "title": "【优质期刊推荐】心灵悦读——文学类期刊推荐",
+      "url": "https://cul.sohu.com/a/966341879_121106902",
+      "source": "搜狐网",
+      "time": "12月17日",
+      "category": "other"
+    },
+    {
+      "title": "钱报读书会丨赵德发:脚踏土地,以写作“生万物”",
+      "url": "https://baijiahao.baidu.com/s?id=1852557744575068651&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "前天16:07",
+      "category": "other"
+    },
+    {
+      "title": "就业典型 | 青年残疾诗人许天伦的文学梦:多才谁似许公子 一根手指...",
+      "url": "http://baijiahao.baidu.com/s?id=1852614383932766291&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "前天20:43",
+      "category": "other"
+    },
+    {
+      "title": "www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=846328",
+      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=846328",
+      "source": "上观",
+      "time": "1月17日",
+      "category": "other"
+    },
+    {
+      "title": "智飞生物上榜2025重庆民营企业科技创新指数100强",
+      "url": "https://baijiahao.baidu.com/s?id=1852565198817743996&wfr=spider&for=pc",
+      "source": "金台资讯",
+      "time": "前天18:00",
+      "category": "other"
     },
     {
       "title": "2025-3《收获》目录及简介 | 5月14日出版",
@@ -77,6 +112,62 @@ window.LIT_DATA = {
       "source": "工人日报",
       "time": "10月14日",
       "category": "activity"
+    },
+    {
+      "title": "全国文学报刊出版界2025“新大众文艺联席会议”暨主题研讨会在...",
+      "url": "https://baijiahao.baidu.com/s?id=1852714082642674564&wfr=spider&for=pc",
+      "source": "三秦都市报",
+      "time": "6小时前",
+      "category": "activity"
+    },
+    {
+      "title": "邹胜念:所以,要警惕爱上湖和湖一样的女人 | 纯粹诗歌",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32221584",
+      "source": "澎湃新闻",
+      "time": "昨天18:23",
+      "category": "other"
+    },
+    {
+      "title": "《北京文学》首发作品《富贵如云》《木匠李密》入选中国小说学会...",
+      "url": "https://baijiahao.baidu.com/s?id=1852621357543048413&wfr=spider&for=pc",
+      "source": "北晚在线",
+      "time": "昨天08:46",
+      "category": "activity"
+    },
+    {
+      "title": "第35期“解放书单”上新,在书页里读懂时代与思想",
+      "url": "http://baijiahao.baidu.com/s?id=1852613696054596683&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "昨天07:01",
+      "category": "other"
+    },
+    {
+      "title": "文坛名家新作齐发,谁会“笑”到最后?",
+      "url": "https://baijiahao.baidu.com/s?id=1852609844294664936&wfr=spider&for=pc",
+      "source": "文汇报",
+      "time": "昨天06:00",
+      "category": "other"
+    },
+    {
+      "title": "余华、刘震云、陈彦、范稳最新长篇纷纷亮相,谁会“笑”到最后",
+      "url": "https://baijiahao.baidu.com/s?id=1852637044588301558&wfr=spider&for=pc",
+      "source": "湘湘带你看社会",
+      "time": "昨天13:12",
+      "category": "other"
+    },
+    {
+      "title": "封关后,这一文旅盛会首次来到海南",
+      "url": "http://www.thepaper.cn/newsDetail_forward_32267330",
+      "source": "澎湃新闻",
+      "time": "昨天18:03",
+      "category": "other"
+    },
+    {
+      "title": "公开宣判+新闻发布,“沉浸式”接受禁毒教育,“零距离”感受法律威严",
+      "url": "http://baijiahao.baidu.com/s?id=1852639143350434797&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "昨天12:16",
+      "category": "other"
     }
   ]
 };
