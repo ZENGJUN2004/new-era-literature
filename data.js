e90bfbf8a38b90a3f97fc9e78467de261dacbe91
GitHub Action <action@github.com>
2025-12-28 11:09:22 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2025-12-28 18:11",
+  "update_time": "2025-12-28 19:09",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,41 +21,6 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
-    },
-    {
-      "title": "专家学者在汉共话新文学语言与中外文学语言传统",
-      "url": "https://baijiahao.baidu.com/s?id=1849047423176621578&wfr=spider&for=pc",
-      "source": "极目新闻",
-      "time": "11月17日",
-      "category": "other"
-    },
-    {
-      "title": "资讯 |《学术月刊》2025年第9期目录及摘要",
-      "url": "http://baijiahao.baidu.com/s?id=1843961684968116029&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "9月22日",
-      "category": "activity"
-    },
-    {
-      "title": "上海首位!陈思和获颁中国现代文学学术贡献荣誉奖",
-      "url": "https://baijiahao.baidu.com/s?id=1850841907024718369&wfr=spider&for=pc",
-      "source": "湘湘带你看社会",
-      "time": "12月7日",
-      "category": "other"
-    },
-    {
-      "title": "【优质期刊推荐】心灵悦读——文学类期刊推荐",
-      "url": "https://cul.sohu.com/a/966341879_121106902",
-      "source": "搜狐网",
-      "time": "12月17日",
-      "category": "other"
-    },
-    {
-      "title": "资讯 | 中国现代文学学科建设暨陈思和教授新著分享会在沪举行",
-      "url": "http://baijiahao.baidu.com/s?id=1851022562089198175&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "12月9日",
-      "category": "other"
     },
     {
       "title": "2025-3《收获》目录及简介 | 5月14日出版",
@@ -100,31 +65,10 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "获奖感言感动网友,青年作家刘楚昕新书《泥潭》发布",
-      "url": "https://baijiahao.baidu.com/s?id=1836714185649934540&wfr=spider&for=pc",
-      "source": "北京日报客户端",
-      "time": "7月4日",
-      "category": "activity"
-    },
-    {
-      "title": "硬核书写当代中国人的互联网生活史,石一枫长篇小说《一日顶流...",
-      "url": "https://baijiahao.baidu.com/s?id=1829564387551772566&wfr=spider&for=pc",
-      "source": "扬子晚报",
-      "time": "4月16日",
-      "category": "activity"
-    },
-    {
       "title": "长篇历史小说《红珊瑚》首发",
       "url": "https://baijiahao.baidu.com/s?id=1845939098107077495&wfr=spider&for=pc",
       "source": "工人日报",
       "time": "10月14日",
-      "category": "activity"
-    },
-    {
-      "title": "从西关橘香到沽上粤韵,王松长篇小说《橘红》在广州首发",
-      "url": "https://baijiahao.baidu.com/s?id=1847847012189787325&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "11月4日",
       "category": "activity"
     }
   ]
