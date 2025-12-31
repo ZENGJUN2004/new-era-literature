d0cc96a1f8c079285c83a7dfb727ad9bdf11eeee
GitHub Action <action@github.com>
2025-12-31 16:14:13 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2025-12-31 23:11",
+  "update_time": "2026-01-01 00:14",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,172 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "文学现场·“杭州会议”与“寻根”|谢尚发:如何让史料完整地说话...",
-      "url": "https://cul.sohu.com/a/894253674_121139852",
-      "source": "搜狐网",
-      "time": "5月12日",
-      "category": "other"
-    },
-    {
-      "title": "回归大地的文学信仰——罗长江《大地五部曲》评论集分享会发言摘登",
-      "url": "https://baijiahao.baidu.com/s?id=1835085212602265908&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "6月16日",
-      "category": "meeting"
-    },
-    {
-      "title": "思潮与现象|杨鹏鑫:再媒介化的裂隙与角力:《繁花》戏剧、电视剧的...",
-      "url": "https://cul.sohu.com/a/864139199_121139852",
-      "source": "搜狐网",
-      "time": "2月26日",
-      "category": "voice"
-    },
-    {
-      "title": "“打工文学”谱写了光荣与梦想,记录了改革开放的“心灵史”",
-      "url": "https://cul.sohu.com/a/859820053_161795",
-      "source": "搜狐网",
-      "time": "2月16日",
-      "category": "other"
-    },
-    {
-      "title": "新民艺评|卜翌:诗歌贴近生活,文艺持续生长——露台新现象带来的启示",
-      "url": "http://baijiahao.baidu.com/s?id=1852831417462999939&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "前天16:41",
-      "category": "voice"
-    },
-    {
-      "title": "山西·阳泉第三届刘慈欣故乡科幻文化活动周侧记",
-      "url": "http://www.sx.xinhuanet.com/20251015/fd41e5fae4f8482aa3704671f32112cf/c.html",
-      "source": "新华网山西",
-      "time": "10月15日",
-      "category": "voice"
-    },
-    {
-      "title": "文化新观察|中国科幻文学:在科技与人文的交汇处书写未来",
-      "url": "https://news.ycwb.com/ikinvkotkb/content_53340856.htm",
-      "source": "金羊网",
-      "time": "4月9日",
-      "category": "voice"
-    },
-    {
-      "title": "科幻文学如何圈粉新大众?",
-      "url": "https://baijiahao.baidu.com/s?id=1847391019566076234&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "10月30日",
-      "category": "voice"
-    },
-    {
-      "title": "艺览2025·文学盘点:创作、跨界与回响",
-      "url": "https://baijiahao.baidu.com/s?id=1852829596315967415&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "前天16:12",
-      "category": "other"
-    },
-    {
-      "title": "文化新观察丨中国网络文学发展新趋势",
-      "url": "https://baijiahao.baidu.com/s?id=1836721737874141864&wfr=spider&for=pc",
-      "source": "北京日报客户端",
-      "time": "7月4日",
-      "category": "other"
-    },
-    {
-      "title": "2025-3《收获》目录及简介 | 5月14日出版",
-      "url": "https://cul.sohu.com/a/894856558_638785",
-      "source": "搜狐网",
-      "time": "5月13日",
-      "category": "activity"
-    },
-    {
-      "title": "2025-4《收获》|目录与简介",
-      "url": "https://cul.sohu.com/a/913273206_638785",
-      "source": "搜狐网",
-      "time": "7月12日",
-      "category": "activity"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "《河南文学》杂志2025年第三期目录",
-      "url": "https://cul.sohu.com/a/922955150_120753574",
-      "source": "搜狐网",
-      "time": "8月15日",
-      "category": "activity"
-    },
-    {
-      "title": "《河南文学》杂志2025年第四期目录",
-      "url": "https://cul.sohu.com/a/942674515_120753574",
-      "source": "搜狐网",
-      "time": "10月12日",
-      "category": "activity"
-    },
-    {
-      "title": "期刊目录| 《文学评论》2025年第5期目录、摘要及编后记",
-      "url": "http://baijiahao.baidu.com/s?id=1844013382652568959&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "9月22日",
-      "category": "activity"
-    },
-    {
-      "title": "获奖感言感动网友,青年作家刘楚昕新书《泥潭》发布",
-      "url": "https://baijiahao.baidu.com/s?id=1836714185649934540&wfr=spider&for=pc",
-      "source": "北京日报客户端",
-      "time": "7月4日",
-      "category": "activity"
-    },
-    {
-      "title": "硬核书写当代中国人的互联网生活史,石一枫长篇小说《一日顶流...",
-      "url": "https://baijiahao.baidu.com/s?id=1829564387551772566&wfr=spider&for=pc",
-      "source": "扬子晚报",
-      "time": "4月16日",
-      "category": "activity"
-    },
-    {
-      "title": "长篇历史小说《红珊瑚》首发",
-      "url": "https://baijiahao.baidu.com/s?id=1845939098107077495&wfr=spider&for=pc",
-      "source": "工人日报",
-      "time": "10月14日",
-      "category": "activity"
-    },
-    {
-      "title": "从西关橘香到沽上粤韵,王松长篇小说《橘红》在广州首发",
-      "url": "https://baijiahao.baidu.com/s?id=1847847012189787325&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "11月4日",
-      "category": "activity"
-    },
-    {
-      "title": "“近百年女性作家中,她的文字很淡,气象却是最大的”",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32278547",
-      "source": "澎湃新闻",
-      "time": "10小时前",
-      "category": "other"
-    },
-    {
-      "title": "消失的读者,消失的作家|2025文学盘点",
-      "url": "https://www.jiemian.com/article/13819206.html",
-      "source": "界面新闻",
-      "time": "12小时前",
-      "category": "other"
-    },
-    {
-      "title": "以新的观念直面文学发展新现实",
-      "url": "https://baijiahao.baidu.com/s?id=1852965363742411590&wfr=spider&for=pc",
-      "source": "光明网",
-      "time": "20小时前",
-      "category": "other"
-    },
-    {
-      "title": "北大中文系三代学者共书“百年中国文学与当代文化建设”",
-      "url": "https://baijiahao.baidu.com/s?id=1852927426145413135&wfr=spider&for=pc",
-      "source": "湘湘带你看社会",
-      "time": "昨天18:08",
-      "category": "other"
     }
   ]
 };
