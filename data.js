23a99c1861cfdf3c9b6d5c7c97a1f9b6722fa941
GitHub Action <action@github.com>
2026-01-01 13:28:18 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-01 20:21",
+  "update_time": "2026-01-01 21:28",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,27 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "与简·奥斯丁时空对话 青年如何在平凡生活中汲取成长力量",
-      "url": "https://baijiahao.baidu.com/s?id=1852930961686058129&wfr=spider&for=pc",
-      "source": "青瞳视角",
-      "time": "前天19:03",
-      "category": "other"
-    },
-    {
-      "title": "鲁迅文学奖得主胡学文两地三馆巡回开讲",
-      "url": "https://baijiahao.baidu.com/s?id=1852939516719048594&wfr=spider&for=pc",
-      "source": "大皖新闻",
-      "time": "前天21:18",
-      "category": "other"
-    },
-    {
-      "title": "钱报读书会丨沈苇《亚洲腹地》:111个词条打开新疆的N种方式",
-      "url": "https://baijiahao.baidu.com/s?id=1852921607158057676&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "前天16:33",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -51,17 +30,24 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "22家刊物共答“原创之问”:2026,文学如何捍卫真实?",
-      "url": "https://baijiahao.baidu.com/s?id=1853072821305079854&wfr=spider&for=pc",
+      "title": "2025年,哪些文学事件令你印象深刻?",
+      "url": "https://baijiahao.baidu.com/s?id=1852890507237047777&wfr=spider&for=pc",
       "source": "澎湃新闻",
-      "time": "11小时前",
+      "time": "前天08:20",
       "category": "other"
     },
     {
-      "title": "第五届矛盾文学奖得主阿来做客青城分享阅读经验",
-      "url": "https://baijiahao.baidu.com/s?id=1853013380814197345&wfr=spider&for=pc",
-      "source": "金台资讯",
-      "time": "昨天16:44",
+      "title": "专家研讨新时代长篇小说创作趋势",
+      "url": "https://baijiahao.baidu.com/s?id=1853074185494246261&wfr=spider&for=pc",
+      "source": "人民网",
+      "time": "17小时前",
+      "category": "activity"
+    },
+    {
+      "title": "曹禺“最爱的人”是她?听万方杨扬王宏图共话繁漪",
+      "url": "http://baijiahao.baidu.com/s?id=1853023260552241076&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "昨天19:29",
       "category": "other"
     }
   ]
