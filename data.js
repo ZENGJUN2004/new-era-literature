6c506e450cefa31cf0fedb54a16b964f5f5ef89d
GitHub Action <action@github.com>
2026-01-01 15:11:40 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-01 22:11",
+  "update_time": "2026-01-01 23:11",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,46 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "以影像点亮历史记忆《寻找张纯如》创作研讨会在京举行",
-      "url": "https://baijiahao.baidu.com/s?id=1852900107508224321&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "前天10:54",
-      "category": "meeting"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "22家刊物共答“原创之问”:2026,文学如何捍卫真实?",
-      "url": "https://baijiahao.baidu.com/s?id=1853072821305079854&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "13小时前",
-      "category": "other"
-    },
-    {
-      "title": "褪去标签:“素人写作”的2.0阶段",
-      "url": "http://app.myzaker.com/news/article.php?m=1767171878&pk=6954e59e8e9f0927f03a0acc",
-      "source": "ZAKER",
-      "time": "昨天16:57",
-      "category": "other"
-    },
-    {
-      "title": "2025年12月,我们看了这21本书|小鸟文学·新书",
-      "url": "https://baijiahao.baidu.com/s?id=1853015313691287183&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "昨天17:25",
-      "category": "activity"
-    },
-    {
-      "title": "《上海书评》2025年度衡鉴:十种推荐书揭晓(附现场实录)",
-      "url": "https://baijiahao.baidu.com/s?id=1853017687123605092&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "昨天18:01",
-      "category": "other"
     }
   ]
 };
