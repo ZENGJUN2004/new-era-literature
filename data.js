79adbe953f877a916259d2bcd3282b302d5d0d0f
GitHub Action <action@github.com>
2026-01-03 16:12:26 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-03 23:10",
+  "update_time": "2026-01-04 00:12",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -27,20 +27,6 @@ window.LIT_DATA = {
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "将小说擅自改编摄制成短剧上线传播法院:侵犯被独家授权公司的改编...",
-      "url": "https://baijiahao.baidu.com/s?id=1853072759241479031&wfr=spider&for=pc",
-      "source": "北青网",
-      "time": "前天08:38",
-      "category": "activity"
-    },
-    {
-      "title": "专家研讨新时代长篇小说创作趋势",
-      "url": "https://baijiahao.baidu.com/s?id=1853074185494246261&wfr=spider&for=pc",
-      "source": "人民网",
-      "time": "前天03:46",
       "category": "activity"
     }
   ]
