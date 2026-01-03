1ba0427020ca7016921040dff9861ecc64da7ac4
GitHub Action <action@github.com>
2026-01-03 04:23:29 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-03 11:50",
+  "update_time": "2026-01-03 12:23",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -27,13 +27,6 @@ window.LIT_DATA = {
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "专家研讨新时代长篇小说创作趋势",
-      "url": "https://baijiahao.baidu.com/s?id=1853074185494246261&wfr=spider&for=pc",
-      "source": "人民网",
-      "time": "前天03:46",
       "category": "activity"
     }
   ]
