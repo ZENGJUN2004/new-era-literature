26dd390c18e104deb132d8aa77a2ac30a339b66f
GitHub Action <action@github.com>
2026-01-04 08:15:12 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-04 15:14",
+  "update_time": "2026-01-04 16:15",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,39 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "历史上的今天|1982年12月15日,首届茅盾文学奖授奖仪式举行",
+      "url": "https://m.thepaper.cn/newsDetail_forward_32136733",
+      "source": "澎湃新闻",
+      "time": "",
+      "category": "other"
+    },
+    {
+      "title": "“文字盛宴——茅盾文学奖获奖作品展”",
+      "url": "https://cul.sohu.com/a/971828194_121106991",
+      "source": "搜狐网",
+      "time": "前天16:41",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "你不喜欢的文学人物,反而带给你更多领悟 |作家东西",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32301045",
+      "source": "澎湃新闻",
+      "time": "1小时前",
+      "category": "other"
+    },
+    {
+      "title": "刘亮程:穿透彼岸与此生",
+      "url": "https://www.chinanews.com.cn/cul/2026/01-04/10545593.shtml",
+      "source": "中国新闻网",
+      "time": "2小时前",
+      "category": "other"
     }
   ]
 };
