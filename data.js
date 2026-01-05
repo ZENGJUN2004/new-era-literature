26eb859bc1bbf939661a4920ed85eb263bd8071d
GitHub Action <action@github.com>
2026-01-05 03:07:43 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-05 08:57",
+  "update_time": "2026-01-05 11:07",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,18 +23,32 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
+      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
+      "url": "https://m.jiemian.com/article/3573232.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "“余华文学小卖部”出圈,上海书展撬动书香经济",
+      "url": "https://m.jiemian.com/article/13205978.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "2025年全球各地建筑师的深耕和思考",
-      "url": "https://www.huxiu.com/article/4823126.html",
-      "source": "虎嗅APP",
-      "time": "昨天10:26",
-      "category": "other"
     }
   ]
 };
