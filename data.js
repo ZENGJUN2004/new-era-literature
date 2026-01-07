76988efb838a5fb5239a9e85b60325b68f04232a
GitHub Action <action@github.com>
2026-01-07 03:58:40 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-07 10:52",
+  "update_time": "2026-01-07 11:58",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,74 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "「“艺”周聚焦③」警惕同质化与流量陷阱:让新大众文艺向上生长",
-      "url": "https://baijiahao.baidu.com/s?id=1853465599042170492&wfr=spider&for=pc",
-      "source": "闪电新闻",
-      "time": "前天16:40",
-      "category": "other"
-    },
-    {
-      "title": "文学双周记|22家刊物答原创之问,邓紫棋入选银河奖引热议",
-      "url": "https://baijiahao.baidu.com/s?id=1853442602393722283&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "前天10:35",
-      "category": "other"
-    },
-    {
-      "title": "赵德发长篇小说《大海风》入选中国当代文学研究会年度榜单(2025)",
-      "url": "https://baijiahao.baidu.com/s?id=1853465599041956599&wfr=spider&for=pc",
-      "source": "闪电新闻",
-      "time": "前天16:32",
-      "category": "activity"
-    },
-    {
-      "title": "中国当代文学研究会年度榜单(2025)发布",
-      "url": "https://baijiahao.baidu.com/s?id=1853483055197931621&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "前天21:18",
-      "category": "other"
-    },
-    {
-      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
-      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
-      "url": "https://m.jiemian.com/article/3573232.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "在文学对话中走向文明互鉴",
-      "url": "https://m.thepaper.cn/newsDetail_forward_32316475",
-      "source": "澎湃新闻",
-      "time": "前天09:45",
-      "category": "other"
-    },
-    {
-      "title": "2026年,这些华语文学新书要来了(二)",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32309555",
-      "source": "澎湃新闻",
-      "time": "前天08:12",
-      "category": "activity"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "非虚构写作:走访、调研、访谈……向一切发问",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32323303",
-      "source": "澎湃新闻",
-      "time": "昨天15:39",
-      "category": "voice"
     }
   ]
 };
