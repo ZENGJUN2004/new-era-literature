53d28e9d277e1c5c26880b801c4d4e55aa375923
GitHub Action <action@github.com>
2026-01-09 17:15:37 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-10 00:14",
+  "update_time": "2026-01-10 01:15",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,17 +23,45 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
-      "url": "https://m.jiemian.com/article/3573232.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "voice"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "荐书|经典新读:从历史与传统中寻找解法",
+      "url": "https://baijiahao.baidu.com/s?id=1853731034637824756&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "前天14:59",
+      "category": "other"
+    },
+    {
+      "title": "10元经典名著让读者爱不释手",
+      "url": "https://baijiahao.baidu.com/s?id=1853800399272792052&wfr=spider&for=pc",
+      "source": "金台资讯",
+      "time": "昨天08:37",
+      "category": "other"
+    },
+    {
+      "title": "新蕾出版社:打造专属IP,解锁传统文化传播新方式",
+      "url": "http://news.enorth.com.cn/system/2026/01/09/059061673.shtml",
+      "source": "北方网",
+      "time": "昨天17:12",
+      "category": "activity"
+    },
+    {
+      "title": "爱与接纳编织成长力量,“梦想家”品牌新作《妈妈的百衲被》在京首发",
+      "url": "https://baijiahao.baidu.com/s?id=1853838053713210297&wfr=spider&for=pc",
+      "source": "潇湘晨报",
+      "time": "昨天19:22",
+      "category": "activity"
+    },
+    {
+      "title": "《新传媒时代与新大众文艺的兴起:新大众文艺大家论》新书发布暨...",
+      "url": "https://baijiahao.baidu.com/s?id=1853820120371975482&wfr=spider&for=pc",
+      "source": "三秦都市报",
+      "time": "昨天14:33",
       "category": "activity"
     }
   ]
