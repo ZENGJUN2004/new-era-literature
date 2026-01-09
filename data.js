fd4939b0fed324d879f434d03ffb846d1e97ec76
GitHub Action <action@github.com>
2026-01-09 02:53:15 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-09 08:53",
+  "update_time": "2026-01-09 10:53",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,10 +23,45 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
+      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
+      "url": "https://m.jiemian.com/article/3573232.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "voice"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "荐书|经典新读:从历史与传统中寻找解法",
+      "url": "https://baijiahao.baidu.com/s?id=1853731034637824756&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "昨天14:59",
+      "category": "other"
+    },
+    {
+      "title": "名人作家持续在抖音首发新书《咸的玩笑》《泥潭》等登上最受欢迎榜",
+      "url": "https://baijiahao.baidu.com/s?id=1853791221970800143&wfr=spider&for=pc",
+      "source": "海南特区报",
+      "time": "昨天20:25",
+      "category": "activity"
+    },
+    {
+      "title": "以不同维度回应儿童成长《扎克俄博的召唤》等少儿新书发布",
+      "url": "https://baijiahao.baidu.com/s?id=1853749909240986489&wfr=spider&for=pc",
+      "source": "中国新闻网",
+      "time": "昨天19:59",
       "category": "activity"
     }
   ]
