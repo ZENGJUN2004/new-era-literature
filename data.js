c20fc404c9bc6c4ce34d7670d2b10a840f126a02
GitHub Action <action@github.com>
2026-01-09 10:13:15 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-09 17:18",
+  "update_time": "2026-01-09 18:13",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,10 +23,38 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "从荧屏山水到实地打卡 浙江以“剧游融合”赋能文旅发展",
+      "url": "https://www.jfdaily.com/wx/detail.do?id=1048119",
+      "source": "上观",
+      "time": "前天13:23",
+      "category": "other"
+    },
+    {
+      "title": "德适生物港股IPO获证监会备案 客户已覆盖超400家医疗机构",
+      "url": "https://baijiahao.baidu.com/s?id=1853662397102878471&wfr=spider&for=pc",
+      "source": "证券时报",
+      "time": "前天20:43",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "国产辅酶q10哪个牌子最好 2026品牌排名权威功效测评与选购指南",
+      "url": "https://baijiahao.baidu.com/s?id=1853825015553473951&wfr=spider&for=pc",
+      "source": "中国农科新闻网",
+      "time": "2小时前",
+      "category": "other"
+    },
+    {
+      "title": "新医保目录实施一周:创新药“首单”频现",
+      "url": "https://www.chinanews.com.cn/cj/2026/01-09/10548714.shtml",
+      "source": "中国新闻网",
+      "time": "10小时前",
       "category": "activity"
     }
   ]
