dde698ba51d604b5e8435e292626c7536c261974
GitHub Action <action@github.com>
2026-01-10 17:09:56 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-11 00:12",
+  "update_time": "2026-01-11 01:09",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,41 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "张爱玲:“三十年前的故事还没完——完不了。”",
+      "url": "http://www.thepaper.cn/newsDetail_forward_32337397",
+      "source": "澎湃新闻",
+      "time": "前天12:31",
+      "category": "other"
+    },
+    {
+      "title": "2025春风月榜半年报告:希望这22本佳作为你带来光",
+      "url": "https://baijiahao.baidu.com/s?id=1853802033490694012&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "前天09:47",
+      "category": "other"
+    },
+    {
+      "title": "第一财经年度人文图书|寻找“我们”共同的故事",
+      "url": "https://baijiahao.baidu.com/s?id=1853807002008780901&wfr=spider&for=pc",
+      "source": "第一财经",
+      "time": "前天11:06",
+      "category": "other"
+    },
+    {
+      "title": "王宇平|摩登女郎之辩辨变",
+      "url": "https://baijiahao.baidu.com/s?id=1853810999247971305&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "前天12:10",
+      "category": "other"
+    },
+    {
+      "title": "【专访】文学研究者张莉:远离非黑即白二元对立的思维,去认识和...",
+      "url": "https://m.jiemian.com/article/4103429.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -42,6 +77,20 @@ window.LIT_DATA = {
       "source": "齐鲁壹点",
       "time": "前天10:07",
       "category": "activity"
+    },
+    {
+      "title": "2026北京图书订货会,这些新书和活动值得我们回顾",
+      "url": "https://baijiahao.baidu.com/s?id=1853923961316218683&wfr=spider&for=pc",
+      "source": "新京报",
+      "time": "昨天18:07",
+      "category": "activity"
+    },
+    {
+      "title": "直播预告 | “持微火者·女性文学好书榜”2025年度十大好书即将...",
+      "url": "https://cul.sohu.com/a/974581281_121119377",
+      "source": "搜狐网",
+      "time": "昨天12:33",
+      "category": "other"
     }
   ]
 };
