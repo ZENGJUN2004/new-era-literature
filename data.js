019f73d56d6eef987ae0516842b28dd8da0168a3
GitHub Action <action@github.com>
2026-01-10 20:11:49 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-11 03:08",
+  "update_time": "2026-01-11 04:11",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,13 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "《新传媒时代与新大众文艺的兴起:新大众文艺大家论》新书发布暨...",
-      "url": "https://baijiahao.baidu.com/s?id=1853820120371975482&wfr=spider&for=pc",
-      "source": "三秦都市报",
-      "time": "前天14:33",
-      "category": "activity"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -48,34 +41,6 @@ window.LIT_DATA = {
       "url": "https://baijiahao.baidu.com/s?id=1853803167951408475&wfr=spider&for=pc",
       "source": "齐鲁壹点",
       "time": "前天10:07",
-      "category": "activity"
-    },
-    {
-      "title": "AI迷宫中的心灵坐标 老藤推出长篇小说《自愈者》",
-      "url": "https://baijiahao.baidu.com/s?id=1853929531561008376&wfr=spider&for=pc",
-      "source": "青瞳视角",
-      "time": "昨天19:34",
-      "category": "activity"
-    },
-    {
-      "title": "“清洁女工作家”“烧烤诗人”……“新大众文艺丛书”让更多凡人...",
-      "url": "http://news.enorth.com.cn/system/2026/01/10/059063925.shtml",
-      "source": "北方网",
-      "time": "昨天19:39",
-      "category": "other"
-    },
-    {
-      "title": "老藤长篇小说《自愈者》在京首发 在“AI迷宫”中寻找心灵坐标",
-      "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "昨天22:32",
-      "category": "activity"
-    },
-    {
-      "title": "寻找AI迷宫中的心灵坐标,老藤长篇小说《自愈者》新书发布会在京举行",
-      "url": "https://baijiahao.baidu.com/s?id=1853935548042845532&wfr=spider&for=pc",
-      "source": "潇湘晨报",
-      "time": "昨天21:11",
       "category": "activity"
     }
   ]
