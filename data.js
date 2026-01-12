af3f225a693d13bc10fe326b7bd81f9522a85e47
GitHub Action <action@github.com>
2026-01-12 03:01:09 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-12 08:55",
+  "update_time": "2026-01-12 11:01",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,18 +23,25 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "迎春以诗,“第40届青春诗会诗丛”在京首发",
-      "url": "https://baijiahao.baidu.com/s?id=1853904836037271469&wfr=spider&for=pc",
-      "source": "文汇报",
-      "time": "前天13:03",
-      "category": "activity"
+      "title": "十部作品里的女性故事,汇聚成一支温良智慧的火把",
+      "url": "http://baijiahao.baidu.com/s?id=1726726766072973629&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "other"
     },
     {
-      "title": "作家晓苏在汉病逝 才华与大爱留人间 他根植于乡土是湖北文坛...",
-      "url": "http://news.cnhubei.com/content/2026-01/10/content_19768258.html",
-      "source": "荆楚网新闻",
-      "time": "前天09:39",
-      "category": "other"
+      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
+      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
+      "url": "https://m.jiemian.com/article/3573232.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "voice"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
@@ -51,10 +58,10 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "每一寸土地都充满生机,“新时代山乡巨变创作计划”长篇小说《种粮...",
-      "url": "https://cul.sohu.com/a/974531640_394566",
-      "source": "搜狐网",
-      "time": "前天09:31",
+      "title": "2026北京图书订货会,这些新书和活动值得我们回顾",
+      "url": "https://baijiahao.baidu.com/s?id=1853923961316218683&wfr=spider&for=pc",
+      "source": "新京报",
+      "time": "前天18:07",
       "category": "activity"
     },
     {
@@ -72,10 +79,31 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
+      "title": "热辣湘书 乘“数”而上——2026北京图书订货会中南传媒“马”力全开",
+      "url": "https://baijiahao.baidu.com/s?id=1854023147430130991&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "1小时前",
+      "category": "other"
+    },
+    {
       "title": "有趣且珍贵的“图书目录”",
       "url": "https://baijiahao.baidu.com/s?id=1853971690354413321&wfr=spider&for=pc",
       "source": "新浪财经",
       "time": "昨天06:46",
+      "category": "activity"
+    },
+    {
+      "title": "2026北京图书订货会,哪些新书被推荐?",
+      "url": "https://baijiahao.baidu.com/s?id=1854019087056703520&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "昨天19:18",
+      "category": "activity"
+    },
+    {
+      "title": "每一寸土地都充满生机,“新时代山乡巨变创作计划”长篇小说《种粮...",
+      "url": "https://cul.sohu.com/a/974772531_121119389",
+      "source": "搜狐网",
+      "time": "昨天08:11",
       "category": "activity"
     }
   ]
