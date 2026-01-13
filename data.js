9a409a191417d0cf3d2d701cbd675c0f8492c557
GitHub Action <action@github.com>
2026-01-13 06:18:52 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-13 13:18",
+  "update_time": "2026-01-13 14:18",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,13 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "让更多的文字被看见,“新大众文艺丛书”集结他们笔下的微光和身边...",
-      "url": "https://baijiahao.baidu.com/s?id=1854014276271539255&wfr=spider&for=pc",
-      "source": "京报网",
-      "time": "前天17:48",
-      "category": "other"
-    },
-    {
       "title": "有趣且珍贵的“图书目录”",
       "url": "https://baijiahao.baidu.com/s?id=1853971690354413321&wfr=spider&for=pc",
       "source": "新浪财经",
@@ -42,20 +35,6 @@ window.LIT_DATA = {
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "《中国蒙古文学70年》首发仪式举行",
-      "url": "https://baijiahao.baidu.com/s?id=1854101064960801066&wfr=spider&for=pc",
-      "source": "湘湘带你看社会",
-      "time": "昨天17:02",
-      "category": "activity"
-    },
-    {
-      "title": "第五届爱不落下论坛在京召开",
-      "url": "http://ex.chinadaily.com.cn/exchange/partners/82/rss/channel/cn/columns/6ldgif/stories/WS69648fcea310942cc499ae84.html",
-      "source": "中国日报网",
-      "time": "昨天14:05",
-      "category": "meeting"
     },
     {
       "title": "关注AI时代的精神困境 老藤长篇小说《自愈者》首发",
