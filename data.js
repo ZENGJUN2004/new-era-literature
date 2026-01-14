d6d5ad56188d6dcbc3729386cce5c97ba3ef537d
GitHub Action <action@github.com>
2026-01-14 07:17:28 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-14 14:19",
+  "update_time": "2026-01-14 15:17",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,20 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "张炜《陶渊明的遗产》|无眠的尊严(三):表达方式",
+      "url": "https://baijiahao.baidu.com/s?id=1854098080578419985&wfr=spider&for=pc",
+      "source": "齐鲁壹点",
+      "time": "前天10:44",
+      "category": "other"
+    },
+    {
+      "title": "线上展览 || 文字盛宴——茅盾文学奖获奖作品展",
+      "url": "https://cul.sohu.com/a/975325434_121106902",
+      "source": "搜狐网",
+      "time": "前天20:01",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -34,6 +48,62 @@ window.LIT_DATA = {
       "url": "https://baijiahao.baidu.com/s?id=1854099054361085706&wfr=spider&for=pc",
       "source": "湖南日报",
       "time": "前天16:00",
+      "category": "activity"
+    },
+    {
+      "title": "梁晓声评《大船》:大船破巨浪,国旗迎风扬",
+      "url": "https://baijiahao.baidu.com/s?id=1854272539183690192&wfr=spider&for=pc",
+      "source": "青瞳视角",
+      "time": "49分钟前",
+      "category": "other"
+    },
+    {
+      "title": "文坛新人杨沫的《青春之歌》被激烈批评时,光明日报刊发茅盾的长篇...",
+      "url": "https://baijiahao.baidu.com/s?id=1854264395902397416&wfr=spider&for=pc",
+      "source": "光明日报",
+      "time": "5小时前",
+      "category": "meeting"
+    },
+    {
+      "title": "「文艺经典的光明印记」茅盾这样当“园丁”",
+      "url": "https://baijiahao.baidu.com/s?id=1854229726313604026&wfr=spider&for=pc",
+      "source": "光明网",
+      "time": "12小时前",
+      "category": "other"
+    },
+    {
+      "title": "毕飞宇:要想“手高”,必须首先“眼高”",
+      "url": "https://cul.sohu.com/a/975366061_121124715",
+      "source": "搜狐网",
+      "time": "昨天05:00",
+      "category": "other"
+    },
+    {
+      "title": "首届全国著名诗人双峰诗会举行 以诗歌为媒弘扬湖湘文化",
+      "url": "https://baijiahao.baidu.com/s?id=1854184241574516789&wfr=spider&for=pc",
+      "source": "央广网",
+      "time": "昨天15:04",
+      "category": "other"
+    },
+    {
+      "title": "家国天下 富厚双峰——首届全国著名诗人双峰诗会举行",
+      "url": "https://baijiahao.baidu.com/s?id=1854194180457436012&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "昨天17:37",
+      "category": "other"
+    },
+    {
+      "title": "阿来新作《东坡在人间》亮相北京图书订货会",
+      "url": "http://www.xinhuanet.com/book/20260113/4058937a6099493a976803d1016a1da9/c.html",
+      "source": "新华网",
+      "time": "昨天11:05",
+      "category": "other"
+    },
+    {
+      "title": "重磅首发!昭通长篇儿童小说《背篼里的图书馆》新鲜出炉!",
+      "url": "https://cul.sohu.com/a/975448241_121106902",
+      "source": "搜狐网",
+      "time": "昨天09:52",
       "category": "activity"
     }
   ]
