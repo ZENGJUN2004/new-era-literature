197ad5de6462e8614d2e3e27a1f4a0ea874614d3
GitHub Action <action@github.com>
2026-01-15 23:11:53 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-16 06:12",
+  "update_time": "2026-01-16 07:11",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,60 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "秦地书香“绽”京华——陕西出版展团亮相北京图书订货会侧记",
-      "url": "http://news.cnwest.com/sxxw/a/2026/01/14/23276627.html",
-      "source": "西部网",
-      "time": "前天08:53",
-      "category": "activity"
-    },
-    {
-      "title": "夜读丨温馨的意味",
-      "url": "https://baijiahao.baidu.com/s?id=1854283265577383871&wfr=spider&for=pc",
-      "source": "闪电新闻",
-      "time": "前天17:17",
-      "category": "other"
-    },
-    {
-      "title": "文坛新人杨沫的《青春之歌》被激烈批评时,光明日报刊发茅盾的长篇...",
-      "url": "https://baijiahao.baidu.com/s?id=1854264395902397416&wfr=spider&for=pc",
-      "source": "光明日报",
-      "time": "前天10:15",
-      "category": "meeting"
-    },
-    {
-      "title": "梁晓声评《大船》:大船破巨浪,国旗迎风扬",
-      "url": "https://baijiahao.baidu.com/s?id=1854272539183690192&wfr=spider&for=pc",
-      "source": "青瞳视角",
-      "time": "前天14:27",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "麦家带倪萍回杭州老家,边走边吃的这道美食火了!“我要把它推荐上...",
-      "url": "https://baijiahao.baidu.com/s?id=1854367868447660156&wfr=spider&for=pc",
-      "source": "杭州日报",
-      "time": "昨天14:54",
-      "category": "other"
-    },
-    {
-      "title": "直播预告|来新湖南,看李敬泽毕飞宇梁鸿共话如何在文学里“从心...",
-      "url": "https://baijiahao.baidu.com/s?id=1854365056970471651&wfr=spider&for=pc",
-      "source": "湖南日报",
-      "time": "昨天14:28",
-      "category": "other"
-    },
-    {
-      "title": "43年前的那束光穿越而来,从“文学原乡”照进“空港简阳”",
-      "url": "https://baijiahao.baidu.com/s?id=1854367729725029744&wfr=spider&for=pc",
-      "source": "金台资讯",
-      "time": "昨天15:10",
-      "category": "other"
     }
   ]
 };
