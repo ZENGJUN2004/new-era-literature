9c7d47af331de1132a21c468c76a7d527ba8c9bb
GitHub Action <action@github.com>
2026-01-16 20:12:34 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-17 03:13",
+  "update_time": "2026-01-17 04:12",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,46 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "【专访】文学研究者张莉:远离非黑即白二元对立的思维,去认识和...",
-      "url": "https://m.jiemian.com/article/4103429.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "other"
-    },
-    {
-      "title": "跨界融合,素人崛起,AI冲击……2025中国文坛盘点",
-      "url": "https://baijiahao.baidu.com/s?id=1854363764122606894&wfr=spider&for=pc",
-      "source": "人民日报",
-      "time": "前天14:37",
-      "category": "other"
-    },
-    {
-      "title": "活动丨“女人怎么活?跟天活!” | 年度文学小说《沧城》对谈分享会",
-      "url": "https://cul.sohu.com/a/976320961_121117462",
-      "source": "搜狐网",
-      "time": "前天08:30",
-      "category": "activity"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "2026年,这些新书值得我们期待(上)",
-      "url": "https://baijiahao.baidu.com/s?id=1854440677511475404&wfr=spider&for=pc",
-      "source": "界面新闻",
-      "time": "昨天10:59",
-      "category": "activity"
-    },
-    {
-      "title": "2025文学报年度好书榜揭晓",
-      "url": "https://www.jfdaily.com/news/detail?id=1052392",
-      "source": "上观",
-      "time": "昨天04:00",
-      "category": "other"
     }
   ]
 };
