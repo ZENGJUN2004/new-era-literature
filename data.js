6c27fbbc311b9683d60c963b6a60ca910adc2ccc
GitHub Action <action@github.com>
2026-01-16 12:22:04 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-16 19:11",
+  "update_time": "2026-01-16 20:22",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,13 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "晚潮|探花识草的快乐——评《草木岁华》",
+      "url": "https://baijiahao.baidu.com/s?id=1854281757795145061&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "前天16:53",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -37,18 +44,60 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "洞天寻隐·学林纪|越南黎初帝王的道教信仰",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32373309",
-      "source": "澎湃新闻",
-      "time": "7小时前",
+      "title": "国图“文津阅新”上新——李敬泽《引回风:随笔自选集》新书分享会...",
+      "url": "https://baijiahao.baidu.com/s?id=1854465500014698539&wfr=spider&for=pc",
+      "source": "工人日报",
+      "time": "2小时前",
+      "category": "activity"
+    },
+    {
+      "title": "中国作协副主席李敬泽新书分享会举办,毕飞宇、梁鸿现场聊写作",
+      "url": "https://www.bjnews.com.cn/detail/1768557048129605.html",
+      "source": "新京报",
+      "time": "2小时前",
+      "category": "activity"
+    },
+    {
+      "title": "第一财经年度教育图书|AI时代中国教育需要一场全民思考",
+      "url": "https://baijiahao.baidu.com/s?id=1854443698124486149&wfr=spider&for=pc",
+      "source": "第一财经",
+      "time": "8小时前",
       "category": "other"
     },
     {
-      "title": "当一个科幻小说家患上「老年痴呆」",
-      "url": "https://baijiahao.baidu.com/s?id=1854396038979954697&wfr=spider&for=pc",
-      "source": "人物",
-      "time": "10小时前",
+      "title": "2026年,这些新书值得我们期待(上)",
+      "url": "https://baijiahao.baidu.com/s?id=1854440677511475404&wfr=spider&for=pc",
+      "source": "界面新闻",
+      "time": "9小时前",
       "category": "activity"
+    },
+    {
+      "title": "一周文化讲座|在过度修饰的时代素颜出门",
+      "url": "https://baijiahao.baidu.com/s?id=1854440741048282190&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "9小时前",
+      "category": "meeting"
+    },
+    {
+      "title": "直播预告|来新湖南,看李敬泽毕飞宇梁鸿共话如何在文学里“从心...",
+      "url": "https://baijiahao.baidu.com/s?id=1854365056970471651&wfr=spider&for=pc",
+      "source": "湖南日报",
+      "time": "昨天14:28",
+      "category": "other"
+    },
+    {
+      "title": "真善美的三重奏:文学批评的时代使命",
+      "url": "http://baijiahao.baidu.com/s?id=1854380601623728583&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "昨天19:05",
+      "category": "meeting"
+    },
+    {
+      "title": "《江南器物志》:从物出发,书写江南民间生活长卷",
+      "url": "https://baijiahao.baidu.com/s?id=1854366313329670100&wfr=spider&for=pc",
+      "source": "中国青年报",
+      "time": "昨天15:18",
+      "category": "other"
     }
   ]
 };
