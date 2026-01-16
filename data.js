1a0eb7c5531878956be93b611cf3a4d9de8b4f41
GitHub Action <action@github.com>
2026-01-16 07:16:46 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-16 14:19",
+  "update_time": "2026-01-16 15:16",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,27 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "茅盾文学奖53部获奖作品中已有34部改编成影视作品——严肃文学...",
-      "url": "http://app.myzaker.com/article/6966fa351bc8e0385c000002",
-      "source": "ZAKER",
-      "time": "前天10:06",
-      "category": "other"
-    },
-    {
-      "title": "夜读丨温馨的意味",
-      "url": "https://baijiahao.baidu.com/s?id=1854283265577383871&wfr=spider&for=pc",
-      "source": "闪电新闻",
-      "time": "前天17:17",
-      "category": "other"
-    },
-    {
-      "title": "梁晓声评《大船》:大船破巨浪,国旗迎风扬",
-      "url": "https://baijiahao.baidu.com/s?id=1854272539183690192&wfr=spider&for=pc",
-      "source": "青瞳视角",
-      "time": "前天14:27",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -51,46 +30,11 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "开年读好书: 寻一场高质量的“远行”",
-      "url": "http://news.10jqka.com.cn/20260116/c674068307.shtml",
-      "source": "同花顺财经",
-      "time": "2小时前",
-      "category": "other"
-    },
-    {
-      "title": "一周文化讲座|在过度修饰的时代素颜出门",
-      "url": "https://baijiahao.baidu.com/s?id=1854440741048282190&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "3小时前",
-      "category": "meeting"
-    },
-    {
-      "title": "新书《从夹缝到通途》深度对话“70后”作家群 获文学界高度评价",
-      "url": "http://news.10jqka.com.cn/20260116/c674061296.shtml",
-      "source": "同花顺财经",
-      "time": "5小时前",
+      "title": "【盘州发布】一长篇小说发布,讲述盘州的真人真事",
+      "url": "https://cul.sohu.com/a/976077723_121123794",
+      "source": "搜狐网",
+      "time": "前天19:03",
       "category": "activity"
-    },
-    {
-      "title": "直播预告|来新湖南,看李敬泽毕飞宇梁鸿共话如何在文学里“从心...",
-      "url": "https://baijiahao.baidu.com/s?id=1854365056970471651&wfr=spider&for=pc",
-      "source": "湖南日报",
-      "time": "昨天14:28",
-      "category": "other"
-    },
-    {
-      "title": "范稳新作《青云梯》:直达梦想的百年火车,壮怀激烈的家国情怀",
-      "url": "http://baijiahao.baidu.com/s?id=1854385055224321979&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "昨天20:16",
-      "category": "other"
-    },
-    {
-      "title": "43年前的那束光穿越而来,从“文学原乡”照进“空港简阳”",
-      "url": "https://baijiahao.baidu.com/s?id=1854367729725029744&wfr=spider&for=pc",
-      "source": "金台资讯",
-      "time": "昨天15:10",
-      "category": "other"
     }
   ]
 };
