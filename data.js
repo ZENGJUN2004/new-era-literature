c8d45bc862b2625e503ecbfc1527d7e1d269d2f1
GitHub Action <action@github.com>
2026-01-16 09:17:12 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-16 16:16",
+  "update_time": "2026-01-16 17:17",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,13 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "80多年前美军机迫降咸祥棉花地!徐锦庚新书重温生死大营救",
-      "url": "http://baijiahao.baidu.com/s?id=1854289689545509247&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "前天18:55",
-      "category": "activity"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -41,62 +34,6 @@ window.LIT_DATA = {
       "url": "https://cul.sohu.com/a/976077723_121123794",
       "source": "搜狐网",
       "time": "前天19:03",
-      "category": "activity"
-    },
-    {
-      "title": "错失李娟后 编辑力推《逐水船》",
-      "url": "https://baijiahao.baidu.com/s?id=1854447944982362758&wfr=spider&for=pc",
-      "source": "青瞳视角",
-      "time": "3小时前",
-      "category": "other"
-    },
-    {
-      "title": "开年读好书: 寻一场高质量的“远行”",
-      "url": "http://news.10jqka.com.cn/20260116/c674068307.shtml",
-      "source": "同花顺财经",
-      "time": "4小时前",
-      "category": "other"
-    },
-    {
-      "title": "新书《从夹缝到通途》深度对话“70后”作家群 获文学界高度评价",
-      "url": "http://news.10jqka.com.cn/20260116/c674061296.shtml",
-      "source": "同花顺财经",
-      "time": "7小时前",
-      "category": "activity"
-    },
-    {
-      "title": "直播预告|来新湖南,看李敬泽毕飞宇梁鸿共话如何在文学里“从心...",
-      "url": "https://baijiahao.baidu.com/s?id=1854365056970471651&wfr=spider&for=pc",
-      "source": "湖南日报",
-      "time": "昨天14:28",
-      "category": "other"
-    },
-    {
-      "title": "从乡土叙事到海洋史诗的文学地理突围——赵德发访谈录",
-      "url": "https://baijiahao.baidu.com/s?id=1854386825780878553&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "昨天20:44",
-      "category": "other"
-    },
-    {
-      "title": "用脚步丈量了浙江50多个村庄,纪红建出版长篇报告文学《村道》",
-      "url": "https://baijiahao.baidu.com/s?id=1854386542422333243&wfr=spider&for=pc",
-      "source": "湖南日报",
-      "time": "昨天20:09",
-      "category": "activity"
-    },
-    {
-      "title": "《阅山河》:以山河为镜,在行走中读懂中国",
-      "url": "http://cn.chinadaily.com.cn/a/202601/15/WS6968a938a310942cc499b699.html",
-      "source": "中国日报网",
-      "time": "昨天16:40",
-      "category": "other"
-    },
-    {
-      "title": "鲁奖得主杨庆祥新书《必将有人重写南方》,揭秘现象级文学批评概念...",
-      "url": "http://news.enorth.com.cn/system/2026/01/15/059076038.shtml",
-      "source": "北方网",
-      "time": "昨天10:28",
       "category": "activity"
     }
   ]
