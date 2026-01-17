5bfdbfe42d3d80109cd260c659aa505d740c11e5
GitHub Action <action@github.com>
2026-01-17 12:18:48 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-17 19:09",
+  "update_time": "2026-01-17 20:18",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,10 +23,66 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "直播预告|来新湖南,看李敬泽毕飞宇梁鸿共话如何在文学里“从心...",
+      "url": "https://baijiahao.baidu.com/s?id=1854365056970471651&wfr=spider&for=pc",
+      "source": "湖南日报",
+      "time": "前天14:28",
+      "category": "other"
+    },
+    {
+      "title": "从乡土叙事到海洋史诗的文学地理突围——赵德发访谈录",
+      "url": "https://baijiahao.baidu.com/s?id=1854386825780878553&wfr=spider&for=pc",
+      "source": "齐鲁壹点",
+      "time": "前天20:44",
+      "category": "other"
+    },
+    {
+      "title": "用脚步丈量了浙江50多个村庄,纪红建出版长篇报告文学《村道》",
+      "url": "https://baijiahao.baidu.com/s?id=1854386542422333243&wfr=spider&for=pc",
+      "source": "湖南日报",
+      "time": "前天20:09",
+      "category": "activity"
+    },
+    {
+      "title": "鲁奖得主杨庆祥新书《必将有人重写南方》,揭秘现象级文学批评概念...",
+      "url": "http://news.enorth.com.cn/system/2026/01/15/059076038.shtml",
+      "source": "北方网",
+      "time": "前天10:28",
+      "category": "activity"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "以龙港为原型的浙产剧《小城大事》热播,龙港人这么说",
+      "url": "https://baijiahao.baidu.com/s?id=1854463673621964666&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "昨天17:04",
+      "category": "other"
+    },
+    {
+      "title": "开年读好书: 寻一场高质量的“远行”",
+      "url": "http://news.10jqka.com.cn/20260116/c674068307.shtml",
+      "source": "同花顺财经",
+      "time": "昨天12:04",
+      "category": "other"
+    },
+    {
+      "title": "错失李娟后 编辑力推《逐水船》",
+      "url": "https://baijiahao.baidu.com/s?id=1854447944982362758&wfr=spider&for=pc",
+      "source": "青瞳视角",
+      "time": "昨天12:55",
+      "category": "other"
+    },
+    {
+      "title": "新书《从夹缝到通途》深度对话“70后”作家群 获文学界高度评价",
+      "url": "http://news.10jqka.com.cn/20260116/c674061296.shtml",
+      "source": "同花顺财经",
+      "time": "昨天08:27",
       "category": "activity"
     }
   ]
