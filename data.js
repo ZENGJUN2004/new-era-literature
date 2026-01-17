4b2fed1b11fd9408aead148be5c646ce1a82955a
GitHub Action <action@github.com>
2026-01-17 07:12:06 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-17 14:16",
+  "update_time": "2026-01-17 15:12",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,46 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "2026年国内旅游必打卡路线推荐:十大热门景点深度评测",
+      "url": "https://www.pcauto.com.cn/hj/article/3210037.html",
+      "source": "太平洋汽车",
+      "time": "前天23:52",
+      "category": "other"
+    },
+    {
+      "title": "新岁品乐,读美文重温“梁祝”_澎湃号·湃客_澎湃新闻-The Paper",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32379271",
+      "source": "澎湃新闻",
+      "time": "前天15:12",
+      "category": "other"
+    },
+    {
+      "title": "大型历史文献纪录片《信仰的力量》研讨会--党史频道--人民网",
+      "url": "http://dangshi.people.com.cn/GB/151935/406058/index.html",
+      "source": "人民网",
+      "time": "",
+      "category": "meeting"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "「闽南掌故」弘一法师的惠安缘",
+      "url": "https://baijiahao.baidu.com/s?id=1854433444137465735&wfr=spider&for=pc",
+      "source": "泉州网",
+      "time": "昨天09:04",
+      "category": "other"
+    },
+    {
+      "title": "一周文化讲座|在过度修饰的时代素颜出门",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32392255",
+      "source": "澎湃新闻",
+      "time": "昨天11:00",
+      "category": "meeting"
     }
   ]
 };
