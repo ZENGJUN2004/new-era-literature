eb4ce99f1d3a56752ae453c15a8a05fecbd4837c
GitHub Action <action@github.com>
2026-01-19 14:18:48 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-19 21:38",
+  "update_time": "2026-01-19 22:18",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -27,6 +27,48 @@ window.LIT_DATA = {
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "冯骥才新书发布:五大道是一座富矿",
+      "url": "http://news.enorth.com.cn/system/2026/01/17/059082305.shtml",
+      "source": "北方网",
+      "time": "前天15:39",
+      "category": "activity"
+    },
+    {
+      "title": "长篇儿童小说《额尔齐斯河少年》首发分享会在池举办",
+      "url": "http://ah.anhuinews.com/cz/rwcz/wh/202601/t20260119_9044034.html",
+      "source": "中安在线网站",
+      "time": "12小时前",
+      "category": "activity"
+    },
+    {
+      "title": "文学未来亦是此时此刻 2025文学报年度好书发布",
+      "url": "https://baijiahao.baidu.com/s?id=1854656439089462568&wfr=spider&for=pc",
+      "source": "看看新闻Knews",
+      "time": "昨天20:10",
+      "category": "other"
+    },
+    {
+      "title": "文汇视讯|文学未来亦是此时此刻!文学报发布年度好书榜",
+      "url": "http://baijiahao.baidu.com/s?id=1854644091910869808&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "昨天16:53",
+      "category": "other"
+    },
+    {
+      "title": "张存金:《尚舜天机》新书发布会发言",
+      "url": "https://baijiahao.baidu.com/s?id=1854634960638442770&wfr=spider&for=pc",
+      "source": "大众日报",
+      "time": "昨天14:28",
+      "category": "activity"
+    },
+    {
+      "title": "文学报年度好书线下发布,属于写作、阅读和出版的“此时此刻”具象...",
+      "url": "http://baijiahao.baidu.com/s?id=1854646905904345767&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "昨天17:38",
       "category": "activity"
     }
   ]
