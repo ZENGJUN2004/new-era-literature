dc01c1934af6a2d4607df3206402af60c2014b23
GitHub Action <action@github.com>
2026-01-20 09:23:49 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-20 16:18",
+  "update_time": "2026-01-20 17:23",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,67 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "文学报年度好书线下发布,属于写作、阅读和出版的“此时此刻”具象...",
+      "url": "http://baijiahao.baidu.com/s?id=1854646905904345767&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "前天17:38",
+      "category": "activity"
+    },
+    {
+      "title": "文学未来亦是此时此刻 2025文学报年度好书发布",
+      "url": "https://baijiahao.baidu.com/s?id=1854656439089462568&wfr=spider&for=pc",
+      "source": "看看新闻Knews",
+      "time": "前天20:10",
+      "category": "other"
+    },
+    {
+      "title": "赵德发长篇小说《大海风》入选中国当代文学研究会年度榜单",
+      "url": "https://baijiahao.baidu.com/s?id=1853457293501650014&wfr=spider&for=pc",
+      "source": "齐鲁壹点",
+      "time": "1月5日",
+      "category": "activity"
+    },
+    {
+      "title": "中国当代文学研究会年度榜单(2025)发布",
+      "url": "https://baijiahao.baidu.com/s?id=1853483055197931621&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "1月5日",
+      "category": "other"
+    },
+    {
+      "title": "文学报年度好书发布,中国作协副主席阎晶明:文学的转型是必然的",
+      "url": "https://www.jfdaily.com/wx/detail.do?id=1053628",
+      "source": "上观",
+      "time": "前天14:51",
+      "category": "other"
+    },
+    {
+      "title": "2025文学报年度好书榜揭晓 新大众文艺重点在“新”",
+      "url": "https://baijiahao.baidu.com/s?id=1854696435006784770&wfr=spider&for=pc",
+      "source": "新民晚报",
+      "time": "前天19:06",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "“中国小说年度风云榜”在京正式启动",
+      "url": "https://baijiahao.baidu.com/s?id=1854818522315788227&wfr=spider&for=pc",
+      "source": "中国新闻网",
+      "time": "2小时前",
+      "category": "activity"
+    },
+    {
+      "title": "2025文学报年度好书榜揭晓",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32399401",
+      "source": "澎湃新闻",
+      "time": "4小时前",
+      "category": "other"
     }
   ]
 };
