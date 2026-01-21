907030892fb13272ab865c61df8b12041f644ffb
GitHub Action <action@github.com>
2026-01-21 04:02:08 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-21 10:54",
+  "update_time": "2026-01-21 12:02",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,31 +23,24 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "知乎盐选小说创作与投稿 & 学员作品讲评辅导丨1月25日周日在线直播",
-      "url": "https://cul.sohu.com/a/977592411_121124780",
-      "source": "搜狐网",
-      "time": "前天13:01",
-      "category": "activity"
+      "title": "大型历史文献纪录片《信仰的力量》研讨会--党史频道--人民网",
+      "url": "http://dangshi.people.com.cn/GB/151935/406058/index.html",
+      "source": "人民网",
+      "time": "",
+      "category": "meeting"
     },
     {
-      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
-      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "",
-      "category": "voice"
+      "title": "“邯郸银行杯”少年向上·美好校园 全市中小学生征文活动启事",
+      "url": "https://baijiahao.baidu.com/s?id=1854709170418802016&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "前天10:08",
+      "category": "other"
     },
     {
-      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
-      "url": "https://m.jiemian.com/article/3573232.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "界面新闻-只服务于独立思考的人群-Jiemian.com",
-      "url": "https://m.jiemian.com/?q=19386",
-      "source": "界面新闻",
-      "time": "",
+      "title": "总有微光照亮——王十月新作分享会在北京图书订货会举行",
+      "url": "https://baijiahao.baidu.com/s?id=1853916380221632429&wfr=spider&for=pc",
+      "source": "三秦都市报",
+      "time": "1月10日",
       "category": "other"
     },
     {
@@ -56,6 +49,27 @@ window.LIT_DATA = {
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "专门组建冰鞋营,清代为何重视冰嬉?",
+      "url": "https://baijiahao.baidu.com/s?id=1854889270915960874&wfr=spider&for=pc",
+      "source": "北京日报客户端",
+      "time": "2小时前",
+      "category": "other"
+    },
+    {
+      "title": "中国当代文学研究会2025年度榜单",
+      "url": "https://baijiahao.baidu.com/s?id=1854879539663024017&wfr=spider&for=pc",
+      "source": "青瞳视角",
+      "time": "4小时前",
+      "category": "other"
+    },
+    {
+      "title": "“邯郸银行杯”少年向上·美好校园征文活动正式启动",
+      "url": "https://baijiahao.baidu.com/s?id=1854794559308735910&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "昨天08:45",
+      "category": "other"
     }
   ]
 };
