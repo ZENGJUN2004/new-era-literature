04f5b353cdaeca0a5edb645796bed457b8105f5d
GitHub Action <action@github.com>
2026-01-21 14:20:57 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-21 21:38",
+  "update_time": "2026-01-21 22:20",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -28,6 +28,69 @@ window.LIT_DATA = {
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "在海影节泰国馆,窥见作者电影与类型片共同未来",
+      "url": "https://www.huxiu.com/article/4828525.html",
+      "source": "虎嗅APP",
+      "time": "1小时前",
+      "category": "other"
+    },
+    {
+      "title": "张翎新作《赤道风语——东非漫行散记》:风起赤道,语落人心",
+      "url": "https://baijiahao.baidu.com/s?id=1854930246925513178&wfr=spider&for=pc",
+      "source": "青瞳视角",
+      "time": "1小时前",
+      "category": "other"
+    },
+    {
+      "title": "项飙丨年轻一代被社交媒体全面包围了",
+      "url": "http://www.thepaper.cn/newsDetail_forward_32406860",
+      "source": "澎湃新闻",
+      "time": "8小时前",
+      "category": "other"
+    },
+    {
+      "title": "新大众文艺|王瑛:60岁,刚刚迎来文学之路上升期",
+      "url": "https://baijiahao.baidu.com/s?id=1854887955432311359&wfr=spider&for=pc",
+      "source": "南方网",
+      "time": "12小时前",
+      "category": "other"
+    },
+    {
+      "title": "世界第79名,预示了AI时代99%的人生",
+      "url": "https://www.huxiu.com/article/4828222.html",
+      "source": "虎嗅APP",
+      "time": "昨天22:24",
+      "category": "other"
+    },
+    {
+      "title": "真善美的三重奏:文学批评的时代使命|新批评",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32417549",
+      "source": "澎湃新闻",
+      "time": "昨天13:10",
+      "category": "meeting"
+    },
+    {
+      "title": "“她走过无数人间”——萧红与中国当代文学",
+      "url": "http://cn.chinadaily.com.cn/a/202601/20/WS696f69f9a310942cc499c1a8.html",
+      "source": "中国日报网",
+      "time": "昨天19:41",
+      "category": "other"
+    },
+    {
+      "title": "写厂史|北京加气混凝土厂史_澎湃号·湃客_澎湃新闻-The Paper",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32393396",
+      "source": "澎湃新闻",
+      "time": "昨天13:10",
+      "category": "other"
+    },
+    {
+      "title": "写村史|一个社会工作者的“漂流”与坚守",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32382171",
+      "source": "澎湃新闻",
+      "time": "昨天13:10",
+      "category": "other"
     },
     {
       "title": "抵达辽阔远方的多声部抗战叙事——长篇小说《地啸》首发实录",
