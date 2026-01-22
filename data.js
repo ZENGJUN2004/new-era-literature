619f068502ea5120560d4b9e113620f6a8b3d13f
GitHub Action <action@github.com>
2026-01-22 11:15:41 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-22 18:17",
+  "update_time": "2026-01-22 19:15",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,27 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "来过,写过,活过,皆是存在的拓印",
-      "url": "http://www.thepaper.cn/newsDetail_forward_32407577",
-      "source": "澎湃新闻",
-      "time": "前天16:00",
-      "category": "other"
-    },
-    {
-      "title": "世界第79名,预示了AI时代99%的人生",
-      "url": "https://www.huxiu.com/article/4828222.html",
-      "source": "虎嗅APP",
-      "time": "前天22:24",
-      "category": "other"
-    },
-    {
-      "title": "“她走过无数人间”——萧红与中国当代文学",
-      "url": "http://cn.chinadaily.com.cn/a/202601/20/WS696f69f9a310942cc499c1a8.html",
-      "source": "中国日报网",
-      "time": "前天19:41",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -56,55 +35,6 @@ window.LIT_DATA = {
       "source": "齐鲁壹点",
       "time": "前天17:01",
       "category": "activity"
-    },
-    {
-      "title": "深度书写非洲 张翎《赤道风语——东非漫行散记》新书发布",
-      "url": "https://baijiahao.baidu.com/s?id=1855009426425362877&wfr=spider&for=pc",
-      "source": "中国新闻网",
-      "time": "37分钟前",
-      "category": "activity"
-    },
-    {
-      "title": "没上过一天班,却用异人异事写透人际往来",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32427343",
-      "source": "澎湃新闻",
-      "time": "6小时前",
-      "category": "other"
-    },
-    {
-      "title": "在北京,读懂“新大众文艺”",
-      "url": "https://baijiahao.baidu.com/s?id=1854892994256159088&wfr=spider&for=pc",
-      "source": "首都广电",
-      "time": "10小时前",
-      "category": "other"
-    },
-    {
-      "title": "新大众文艺|王瑛:60岁,刚刚迎来文学之路上升期",
-      "url": "https://baijiahao.baidu.com/s?id=1854887955432311359&wfr=spider&for=pc",
-      "source": "南方网",
-      "time": "昨天09:30",
-      "category": "other"
-    },
-    {
-      "title": "在海影节泰国馆,窥见作者电影与类型片共同未来",
-      "url": "https://www.huxiu.com/article/4828525.html",
-      "source": "虎嗅APP",
-      "time": "昨天20:27",
-      "category": "other"
-    },
-    {
-      "title": "张翎新作《赤道风语——东非漫行散记》:风起赤道,语落人心",
-      "url": "https://baijiahao.baidu.com/s?id=1854930246925513178&wfr=spider&for=pc",
-      "source": "青瞳视角",
-      "time": "昨天20:40",
-      "category": "other"
-    },
-    {
-      "title": "项飙丨年轻一代被社交媒体全面包围了",
-      "url": "http://www.thepaper.cn/newsDetail_forward_32406860",
-      "source": "澎湃新闻",
-      "time": "昨天14:09",
-      "category": "other"
     }
   ]
 };
