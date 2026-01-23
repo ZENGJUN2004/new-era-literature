c245de3012371df43a8e6f1f008e34e83da22ab1
GitHub Action <action@github.com>
2026-01-23 20:14:12 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-24 03:17",
+  "update_time": "2026-01-24 04:14",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -28,41 +28,6 @@ window.LIT_DATA = {
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "刘亮程:每一个被我们塑造的鲜活的文学人物,都在给人、给现实、给...",
-      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1055950&sid=11",
-      "source": "上观",
-      "time": "前天16:55",
-      "category": "other"
-    },
-    {
-      "title": "2026北京图书订货会,这些新书和活动值得我们回顾",
-      "url": "https://www.bjnews.com.cn/detail/1768039641169386.html",
-      "source": "新京报网",
-      "time": "1月10日",
-      "category": "activity"
-    },
-    {
-      "title": "2026年文学上新:长篇扩容、非虚构升温、技术语境探人心",
-      "url": "http://baijiahao.baidu.com/s?id=1853512046747090629&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "1月6日",
-      "category": "voice"
-    },
-    {
-      "title": "新书速览|曹军庆《时间之雾》:一部县城生活档案",
-      "url": "https://baijiahao.baidu.com/s?id=1854988078296745268&wfr=spider&for=pc",
-      "source": "极目新闻",
-      "time": "前天12:00",
-      "category": "activity"
-    },
-    {
-      "title": "刘亮程:写作者内心有太阳,“在文学中给人以长命”|演讲",
-      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1055950",
-      "source": "上观",
-      "time": "前天16:55",
-      "category": "other"
     }
   ]
 };
