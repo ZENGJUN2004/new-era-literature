5db0d3caedf82e26701ac266ea778aaabe1bff36
GitHub Action <action@github.com>
2026-01-24 06:17:22 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-24 13:14",
+  "update_time": "2026-01-24 14:17",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,53 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "乡关何处|千年老家寻访记(之八)轻车重访流芳排",
-      "url": "https://baijiahao.baidu.com/s?id=1855000790108958407&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "前天15:20",
-      "category": "other"
-    },
-    {
-      "title": "岛屿叙事与时代女性生活——读杨怡芬小说《鱼尾纹》",
-      "url": "http://baijiahao.baidu.com/s?id=1855011580216438467&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "前天18:14",
-      "category": "activity"
-    },
-    {
-      "title": "李公明|一周书记:像汉娜·阿伦特那样去思考与……反抗",
-      "url": "https://baijiahao.baidu.com/s?id=1854988634846709347&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "前天12:09",
-      "category": "other"
-    },
-    {
-      "title": "智能共生:AI 人机协作的艺术新前沿 ▸文艺家大讲堂◂",
-      "url": "http://www.thepaper.cn/newsDetail_forward_32446233",
-      "source": "澎湃新闻",
-      "time": "前天16:50",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "「环时深度」格陵兰文化叙事,西方与本土的“冷暖博弈”",
-      "url": "https://baijiahao.baidu.com/s?id=1855148848604700209&wfr=spider&for=pc",
-      "source": "环球网",
-      "time": "6小时前",
-      "category": "other"
-    },
-    {
-      "title": "雅昌专稿 | 一场书法展背后:邹传安艺术的湘博研究与展示实践",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32443264",
-      "source": "澎湃新闻",
-      "time": "昨天12:12",
-      "category": "other"
     },
     {
       "title": "一周文化讲座|在乙游里读《源氏物语》,用长视频播马尔克斯_北京...",
