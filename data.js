a848ac74ac5dcef63586aae5cb52483bef4c4fce
GitHub Action <action@github.com>
2026-01-24 03:55:20 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-24 10:49",
+  "update_time": "2026-01-24 11:55",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,88 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "十部作品里的女性故事,汇聚成一支温良智慧的火把",
-      "url": "http://baijiahao.baidu.com/s?id=1726726766072973629&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "",
-      "category": "other"
-    },
-    {
-      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
-      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
-      "url": "https://m.jiemian.com/article/3573232.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
-      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1050469&sid=200",
-      "source": "上观",
-      "time": "1月12日",
-      "category": "activity"
-    },
-    {
-      "title": "大型历史文献纪录片《信仰的力量》研讨会--党史频道--人民网",
-      "url": "http://dangshi.people.com.cn/GB/151935/406058/index.html",
-      "source": "人民网",
-      "time": "",
-      "category": "meeting"
-    },
-    {
-      "title": "“邯郸银行杯”少年向上·美好校园征文活动正式启动",
-      "url": "https://baijiahao.baidu.com/s?id=1855008322409453551&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "前天17:23",
-      "category": "other"
-    },
-    {
-      "title": "总有微光照亮——王十月新作分享会在北京图书订货会举行",
-      "url": "https://baijiahao.baidu.com/s?id=1853916380221632429&wfr=spider&for=pc",
-      "source": "三秦都市报",
-      "time": "1月10日",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "掌握新闻报道写作的非虚构力量,开启你的写作新篇章!",
-      "url": "https://cul.sohu.com/a/979065823_122528565",
-      "source": "搜狐网",
-      "time": "昨天08:06",
-      "category": "voice"
-    },
-    {
-      "title": "世界新秩序的起点与终点,达沃斯最后的日子",
-      "url": "https://www.huxiu.com/article/4829265.html",
-      "source": "虎嗅APP",
-      "time": "昨天22:38",
-      "category": "other"
-    },
-    {
-      "title": "你还记得2014年马年豫园灯会的“灯王”吗?",
-      "url": "http://baijiahao.baidu.com/s?id=1855138486111161444&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "昨天22:01",
-      "category": "other"
-    },
-    {
-      "title": "《战争与和平》中的导演课 | 徐皓峰:海明威与《千字文》(选读)",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32440449",
-      "source": "澎湃新闻",
-      "time": "昨天12:09",
-      "category": "other"
     },
     {
       "title": "一周文化讲座|在乙游里读《源氏物语》,用长视频播马尔克斯_北京...",
