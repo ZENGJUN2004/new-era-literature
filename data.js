74514fbd6d46b557fdebfc53982b9e3ca76f2cff
GitHub Action <action@github.com>
2026-01-25 12:20:44 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-25 19:10",
+  "update_time": "2026-01-25 20:20",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,6 +21,41 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
+    },
+    {
+      "title": "世界新秩序的起点与终点,达沃斯最后的日子",
+      "url": "https://www.huxiu.com/article/4829265.html",
+      "source": "虎嗅APP",
+      "time": "前天22:38",
+      "category": "other"
+    },
+    {
+      "title": "你还记得2014年马年豫园灯会的“灯王”吗?",
+      "url": "http://baijiahao.baidu.com/s?id=1855138486111161444&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "前天22:01",
+      "category": "other"
+    },
+    {
+      "title": "大型历史文献纪录片《信仰的力量》研讨会--党史频道--人民网",
+      "url": "http://dangshi.people.com.cn/GB/151935/406058/index.html",
+      "source": "人民网",
+      "time": "",
+      "category": "meeting"
+    },
+    {
+      "title": "总有微光照亮——王十月新作分享会在北京图书订货会举行",
+      "url": "https://baijiahao.baidu.com/s?id=1853916380221632429&wfr=spider&for=pc",
+      "source": "三秦都市报",
+      "time": "1月10日",
+      "category": "other"
+    },
+    {
+      "title": "《十月》年度转载盘点 | 2025年,我们被看见",
+      "url": "https://cul.sohu.com/a/979313879_121124729",
+      "source": "搜狐网",
+      "time": "前天17:11",
+      "category": "other"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
