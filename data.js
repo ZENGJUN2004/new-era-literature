6721256ccf64d34228a508ede4f025ec20415af2
GitHub Action <action@github.com>
2026-01-25 00:59:14 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-25 07:11",
+  "update_time": "2026-01-25 08:59",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,10 +23,38 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "2026北京图书订货会,这些新书和活动值得我们回顾",
+      "url": "https://www.bjnews.com.cn/detail/1768039641169386.html",
+      "source": "新京报网",
+      "time": "1月10日",
+      "category": "activity"
+    },
+    {
+      "title": "新文科背景下地方高校育人创新:以“三力”传红色基因以共生促区域...",
+      "url": "https://www.xhby.net/content/s6972e034e4b0317791b009d8.html",
+      "source": "新华报业网",
+      "time": "前天10:43",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "清流映故道,文心照山河!84岁冯骥才再出新书,聚焦天津五大道",
+      "url": "https://baijiahao.baidu.com/s?id=1855092883709057808&wfr=spider&for=pc",
+      "source": "极目新闻",
+      "time": "前天15:46",
+      "category": "activity"
+    },
+    {
+      "title": "老藤长篇小说《自愈者》新书发布会举办",
+      "url": "https://baijiahao.baidu.com/s?id=1854092227377307964&wfr=spider&for=pc",
+      "source": "工人日报",
+      "time": "1月12日",
       "category": "activity"
     },
     {
@@ -35,6 +63,27 @@ window.LIT_DATA = {
       "source": "搜狐网",
       "time": "前天11:03",
       "category": "meeting"
+    },
+    {
+      "title": "文汇X七猫X澎湃共建“智融出版”,《星箭之盾》打头阵",
+      "url": "https://baijiahao.baidu.com/s?id=1855193684582421351&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "昨天18:28",
+      "category": "activity"
+    },
+    {
+      "title": "《芒砀古国——多元文明淬炼下的永城王庄遗址》新书发布",
+      "url": "https://baijiahao.baidu.com/s?id=1855183932209399306&wfr=spider&for=pc",
+      "source": "中国日报网",
+      "time": "昨天14:49",
+      "category": "activity"
+    },
+    {
+      "title": "考古发现纪实文学《芒砀古国》新书在河南郑州发布",
+      "url": "https://baijiahao.baidu.com/s?id=1855168583040111960&wfr=spider&for=pc",
+      "source": "中国新闻网",
+      "time": "昨天11:48",
+      "category": "activity"
     }
   ]
 };
