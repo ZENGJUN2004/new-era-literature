eb0e2cd009ff1195d7153117ca6134815144d0f8
GitHub Action <action@github.com>
2026-01-25 19:10:02 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-26 02:16",
+  "update_time": "2026-01-26 03:10",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -28,41 +28,6 @@ window.LIT_DATA = {
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "文汇X七猫X澎湃共建“智融出版”,《星箭之盾》打头阵",
-      "url": "https://baijiahao.baidu.com/s?id=1855193684582421351&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "前天18:28",
-      "category": "activity"
-    },
-    {
-      "title": "老藤长篇小说《自愈者》新书发布会举办",
-      "url": "https://baijiahao.baidu.com/s?id=1854092227377307964&wfr=spider&for=pc",
-      "source": "工人日报",
-      "time": "1月12日",
-      "category": "activity"
-    },
-    {
-      "title": "考古发现纪实文学《芒砀古国》新书在河南郑州发布",
-      "url": "https://baijiahao.baidu.com/s?id=1855168583040111960&wfr=spider&for=pc",
-      "source": "中国新闻网",
-      "time": "前天11:48",
-      "category": "activity"
-    },
-    {
-      "title": "《问道吉线G331》新书发布会在长春举行",
-      "url": "https://baijiahao.baidu.com/s?id=1855285858000007823&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "昨天18:54",
-      "category": "activity"
-    },
-    {
-      "title": "20余万字讲述“新观察”",
-      "url": "https://baijiahao.baidu.com/s?id=1855264669707944800&wfr=spider&for=pc",
-      "source": "湘湘带你看社会",
-      "time": "昨天13:17",
-      "category": "other"
     }
   ]
 };
