81869dac97bc01c641adda93250edb28d8c18195
GitHub Action <action@github.com>
2026-01-27 10:16:47 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-27 17:22",
+  "update_time": "2026-01-27 18:16",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,20 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "严肃文学,舞台依然广阔(金台随笔)",
+      "url": "http://sn.people.com.cn/n2/2026/0114/c186331-41471880.html",
+      "source": "人民网",
+      "time": "1月14日",
+      "category": "other"
+    },
+    {
+      "title": "徐光耀和他的《平原烈火》",
+      "url": "https://paper.people.com.cn/rmzk/pc/content/202601/12/content_30134230.html",
+      "source": "人民网",
+      "time": "1月12日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -30,11 +44,60 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "赵江长篇小说《大业清晨》在西咸首发",
-      "url": "https://www.sanqin.com/2026-01/25/content_11497045.html",
-      "source": "三秦网",
-      "time": "前天10:59",
-      "category": "activity"
+      "title": "钱报读书会预告丨李敬泽X翟业军X沈苇X萧耳:西湖畔,听风起",
+      "url": "https://baijiahao.baidu.com/s?id=1855460530894792785&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "1小时前",
+      "category": "other"
+    },
+    {
+      "title": "于苍洱文脉间深耕,听大理故事花开有声",
+      "url": "https://baijiahao.baidu.com/s?id=1855451402284417490&wfr=spider&for=pc",
+      "source": "央广网",
+      "time": "3小时前",
+      "category": "other"
+    },
+    {
+      "title": "吕厚量|女神、娼妓、妻子与女奴:古典时代的女性生活",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32415806",
+      "source": "澎湃新闻",
+      "time": "5小时前",
+      "category": "other"
+    },
+    {
+      "title": "上海文艺的高光与深流|舞台剧的在地性与国际化",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32412234",
+      "source": "澎湃新闻",
+      "time": "7小时前",
+      "category": "other"
+    },
+    {
+      "title": "2025年文学研究发展报告",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32463879",
+      "source": "澎湃新闻",
+      "time": "昨天11:38",
+      "category": "other"
+    },
+    {
+      "title": "当AI遇上网络文学——AI时代的网络文学批评",
+      "url": "https://baijiahao.baidu.com/s?id=1855342540300190576&wfr=spider&for=pc",
+      "source": "光明网",
+      "time": "昨天09:53",
+      "category": "meeting"
+    },
+    {
+      "title": "一场跨越九百年的“文学考古”",
+      "url": "https://baijiahao.baidu.com/s?id=1855340685625163638&wfr=spider&for=pc",
+      "source": "金台资讯",
+      "time": "昨天08:48",
+      "category": "other"
+    },
+    {
+      "title": "新宁“文化圈粉”动能澎湃",
+      "url": "https://baijiahao.baidu.com/s?id=1855371597721452895&wfr=spider&for=pc",
+      "source": "金台资讯",
+      "time": "昨天17:24",
+      "category": "other"
     }
   ]
 };
