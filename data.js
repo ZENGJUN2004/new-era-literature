dcc6fbc9cc392d670ab68d66b40a635109c94d79
GitHub Action <action@github.com>
2026-01-27 15:21:05 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-27 22:21",
+  "update_time": "2026-01-27 23:21",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,74 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "枝叶关情,文化服务民生服务体现担当 广东省出版集团不平凡的...",
-      "url": "https://www.time-weekly.com/post/326624",
-      "source": "时代在线",
-      "time": "1月6日",
-      "category": "activity"
-    },
-    {
-      "title": "南方周末2025年度十大好书",
-      "url": "https://cul.sohu.com/a/979858456_120883008",
-      "source": "搜狐网",
-      "time": "前天14:54",
-      "category": "other"
-    },
-    {
-      "title": "【专访】文学研究者张莉:远离非黑即白二元对立的思维,去认识和...",
-      "url": "https://m.jiemian.com/article/4103429.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "other"
-    },
-    {
-      "title": "2025文学报年度好书榜揭晓",
-      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1052392&sid=300",
-      "source": "上观",
-      "time": "1月16日",
-      "category": "other"
-    },
-    {
-      "title": "汇聚女性力量,持微火者·女性文学好书榜发布",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32376524",
-      "source": "澎湃新闻",
-      "time": "1月14日",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "2026迎来玛丽莲·梦露100周年诞辰,出版界掀起文学热潮",
-      "url": "https://baijiahao.baidu.com/s?id=1855474118876583541&wfr=spider&for=pc",
-      "source": "新京报",
-      "time": "1小时前",
-      "category": "activity"
-    },
-    {
-      "title": "吕厚量|女神、娼妓、妻子与女奴:古典时代的女性生活",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32415806",
-      "source": "澎湃新闻",
-      "time": "9小时前",
-      "category": "other"
-    },
-    {
-      "title": "蒋在《外面天气怎么样》:“她”文学中的心灵“潮湿”",
-      "url": "https://baijiahao.baidu.com/s?id=1855437416346888503&wfr=spider&for=pc",
-      "source": "中国青年网",
-      "time": "11小时前",
-      "category": "other"
-    },
-    {
-      "title": "她走过无数人间,萧红为何影响一代又一代写作者和读者",
-      "url": "https://cul.sohu.com/a/980302163_121124779",
-      "source": "搜狐网",
-      "time": "昨天19:32",
-      "category": "other"
     }
   ]
 };
