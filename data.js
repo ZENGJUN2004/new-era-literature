49018b2f6c5bb9ee1a220c8529946a138ec14889
GitHub Action <action@github.com>
2026-01-27 13:39:40 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-27 20:23",
+  "update_time": "2026-01-27 21:39",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,10 +23,52 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "抢先看!译林社2026年度新书书单发布(人文社科篇)_智慧_清代_技艺",
+      "url": "https://cul.sohu.com/a/973609255_235706",
+      "source": "搜狐网",
+      "time": "1月7日",
+      "category": "activity"
+    },
+    {
+      "title": "直播预告|来新湖南,看李敬泽毕飞宇梁鸿共话如何在文学里“从心...",
+      "url": "https://baijiahao.baidu.com/s?id=1854365056970471651&wfr=spider&for=pc",
+      "source": "湖南日报",
+      "time": "1月15日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "人民城市人民建,浙产剧《小城大事》收官看哭观众",
+      "url": "https://baijiahao.baidu.com/s?id=1855453329457003489&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "6小时前",
+      "category": "other"
+    },
+    {
+      "title": "《小城大事》热播,《中国农民城》作者朱晓军说“他们要造一个天堂”",
+      "url": "https://baijiahao.baidu.com/s?id=1855440848271251811&wfr=spider&for=pc",
+      "source": "浙江日报",
+      "time": "9小时前",
+      "category": "other"
+    },
+    {
+      "title": "《我在人间种过花》:诗心与岁月的深沉沉淀",
+      "url": "https://baijiahao.baidu.com/s?id=1855366247204307607&wfr=spider&for=pc",
+      "source": "中国青年报",
+      "time": "昨天16:12",
+      "category": "other"
+    },
+    {
+      "title": "林莉诗集《我们所热爱的生活》在北京首发研讨",
+      "url": "https://baijiahao.baidu.com/s?id=1855361100468542440&wfr=spider&for=pc",
+      "source": "中国日报网",
+      "time": "昨天13:43",
       "category": "activity"
     }
   ]
