35e832f92017fb7e04964851cd4f0c33f9ac2d2f
GitHub Action <action@github.com>
2026-01-27 03:01:02 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-27 08:56",
+  "update_time": "2026-01-27 11:01",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,67 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "大众写科幻,带来了什么?",
+      "url": "https://baijiahao.baidu.com/s?id=1853890395730591225&wfr=spider&for=pc",
+      "source": "湘湘带你看社会",
+      "time": "1月10日",
+      "category": "voice"
+    },
+    {
+      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
+      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
+      "url": "https://m.jiemian.com/article/3573232.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
+      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1050469",
+      "source": "上观",
+      "time": "1月12日",
+      "category": "activity"
+    },
+    {
+      "title": "2026年,这些新书值得我们期待(上)",
+      "url": "https://baijiahao.baidu.com/s?id=1854440677511475404&wfr=spider&for=pc",
+      "source": "界面新闻",
+      "time": "1月16日",
+      "category": "activity"
+    },
+    {
+      "title": "成为《人民文学》头条的报告文学,需要历练什么样的脚力与笔力?",
+      "url": "http://baijiahao.baidu.com/s?id=1726635185081314797&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "新刊速递|《中国作家》2026年第2期目录,精彩纷呈!",
+      "url": "https://cul.sohu.com/a/979765507_121119387",
+      "source": "搜狐网",
+      "time": "前天08:50",
+      "category": "activity"
+    },
+    {
+      "title": "AI生成博士论文靠谱吗?精选9款一键生成论文的软件亲测,知网查重稳...",
+      "url": "https://baijiahao.baidu.com/s?id=1855362783522710092&wfr=spider&for=pc",
+      "source": "周口融媒体",
+      "time": "昨天15:17",
+      "category": "meeting"
     }
   ]
 };
