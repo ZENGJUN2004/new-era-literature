74222e3400a7835fac6c61c12f088385a836c91d
GitHub Action <action@github.com>
2026-01-27 12:23:45 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-27 19:15",
+  "update_time": "2026-01-27 20:23",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,53 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "严肃文学,舞台依然广阔(金台随笔)",
-      "url": "http://sn.people.com.cn/n2/2026/0114/c186331-41471880.html",
-      "source": "人民网",
-      "time": "1月14日",
-      "category": "other"
-    },
-    {
-      "title": "徐光耀和他的《平原烈火》",
-      "url": "https://paper.people.com.cn/rmzk/pc/content/202601/12/content_30134230.html",
-      "source": "人民网",
-      "time": "1月12日",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "钱报读书会预告丨李敬泽X翟业军X沈苇X萧耳:西湖畔,听风起",
-      "url": "https://baijiahao.baidu.com/s?id=1855460530894792785&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "2小时前",
-      "category": "other"
-    },
-    {
-      "title": "于苍洱文脉间深耕,听大理故事花开有声",
-      "url": "https://baijiahao.baidu.com/s?id=1855451402284417490&wfr=spider&for=pc",
-      "source": "央广网",
-      "time": "4小时前",
-      "category": "other"
-    },
-    {
-      "title": "新宁“文化圈粉”动能澎湃",
-      "url": "https://baijiahao.baidu.com/s?id=1855371597721452895&wfr=spider&for=pc",
-      "source": "金台资讯",
-      "time": "昨天17:24",
-      "category": "other"
-    },
-    {
-      "title": "一场跨越九百年的“文学考古”",
-      "url": "https://baijiahao.baidu.com/s?id=1855340685625163638&wfr=spider&for=pc",
-      "source": "金台资讯",
-      "time": "昨天08:48",
-      "category": "other"
     }
   ]
 };
