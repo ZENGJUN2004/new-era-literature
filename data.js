614f0cbf971a0b2874e36dc70e96862175ef9d3e
GitHub Action <action@github.com>
2026-01-28 17:24:54 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-29 00:23",
+  "update_time": "2026-01-29 01:24",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,46 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "【钛晨报】国家医保局:医保药品目录调整专家评审工作已结束;把...",
+      "url": "https://www.tmtpost.com/6283071.html",
+      "source": "钛媒体官方网站",
+      "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "行业景气度回暖叠加政策利好 逾50家生物医药A股公司2025年业绩预盈",
+      "url": "https://www.stcn.com/article/detail/3613263.html",
+      "source": "证券时报网",
+      "time": "前天07:40",
+      "category": "other"
+    },
+    {
+      "title": "宁夏通报22起农业综合行政执法典型案例:剑指种子造假、私屠滥宰...",
+      "url": "https://baijiahao.baidu.com/s?id=1855451536527939387&wfr=spider&for=pc",
+      "source": "中国质量万里行",
+      "time": "前天14:47",
+      "category": "other"
+    },
+    {
+      "title": "中国尚无尼帕病毒输入性病例,沸水加热15分钟可灭活",
+      "url": "https://baijiahao.baidu.com/s?id=1855475134694492955&wfr=spider&for=pc",
+      "source": "第一财经",
+      "time": "前天21:02",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "哪个品牌的辅酶q10效果好?辅酶q10公认品牌排行榜TOP10出炉",
+      "url": "https://baijiahao.baidu.com/s?id=1855546337598046245&wfr=spider&for=pc",
+      "source": "溧阳融媒体中心",
+      "time": "昨天15:54",
+      "category": "voice"
     }
   ]
 };
