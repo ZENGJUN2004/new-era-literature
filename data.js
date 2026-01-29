500dd830b12d01dc4e5277de65b6d0272e7d823a
GitHub Action <action@github.com>
2026-01-29 15:25:06 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-29 22:31",
+  "update_time": "2026-01-29 23:25",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,39 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "中国当代文学研究会年度榜单(2025)发布",
-      "url": "https://baijiahao.baidu.com/s?id=1853483055197931621&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "1月5日",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "丛新强|《回响》中的“回响”是怎样形成的",
-      "url": "https://baijiahao.baidu.com/s?id=1855620452135806488&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "11小时前",
-      "category": "other"
-    },
-    {
-      "title": "法院里来了一群大作家——“知名作家看法院”创作采风活动安徽启幕",
-      "url": "https://baijiahao.baidu.com/s?id=1855524448773772659&wfr=spider&for=pc",
-      "source": "最高人民法院",
-      "time": "昨天10:06",
-      "category": "other"
-    },
-    {
-      "title": "《人民法院报》:法院里来了一群大作家 “知名作家看法院”创作...",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32486695",
-      "source": "澎湃新闻",
-      "time": "昨天18:43",
-      "category": "other"
     }
   ]
 };
