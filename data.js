ed57b647564f1d41ae87883a144fe45cee2ab2b1
GitHub Action <action@github.com>
2026-01-29 13:52:13 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-29 20:29",
+  "update_time": "2026-01-29 21:52",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,53 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "《全民阅读促进条例》颁布 为全民阅读提供条件保障",
+      "url": "http://yn.people.com.cn/n2/2026/0114/c372453-41471599.html",
+      "source": "人民网",
+      "time": "1月14日",
+      "category": "other"
+    },
+    {
+      "title": "省两会上,师院人这样建言|刘大泯委员:加强乡土文学研究并筹建中国...",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32474433",
+      "source": "澎湃新闻",
+      "time": "前天13:59",
+      "category": "other"
+    },
+    {
+      "title": "严肃文学,舞台依然广阔(金台随笔)",
+      "url": "http://sn.people.com.cn/n2/2026/0114/c186331-41471880.html",
+      "source": "人民网",
+      "time": "1月14日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "从“大河”到“人间”:阿来近期的非虚构书写",
+      "url": "https://baijiahao.baidu.com/s?id=1855641686249388111&wfr=spider&for=pc",
+      "source": "湘湘带你看社会",
+      "time": "4小时前",
+      "category": "voice"
+    },
+    {
+      "title": "清欢自在书间:2025年下半年过目难忘的六本新书",
+      "url": "https://baijiahao.baidu.com/s?id=1855529682553627268&wfr=spider&for=pc",
+      "source": "齐鲁壹点",
+      "time": "昨天11:29",
+      "category": "activity"
+    },
+    {
+      "title": "太平间里睡觉,拔上万颗牙,自嘲“认字不多”、却靠一本书赚了1550...",
+      "url": "https://baijiahao.baidu.com/s?id=1855562279680447930&wfr=spider&for=pc",
+      "source": "中国网资讯",
+      "time": "昨天20:08",
+      "category": "other"
     }
   ]
 };
