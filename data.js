94390ca2d64391628a6f41edea8c6aa875e6e1f8
GitHub Action <action@github.com>
2026-01-29 05:01:21 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-29 11:27",
+  "update_time": "2026-01-29 13:01",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,95 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "大众写科幻,带来了什么?",
-      "url": "https://baijiahao.baidu.com/s?id=1853890395730591225&wfr=spider&for=pc",
-      "source": "湘湘带你看社会",
-      "time": "1月10日",
-      "category": "voice"
-    },
-    {
-      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
-      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
-      "url": "https://m.jiemian.com/article/3573232.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
-      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1050469&sid=200",
-      "source": "上观",
-      "time": "1月12日",
-      "category": "activity"
-    },
-    {
-      "title": "《小城大事》热播,《中国农民城》作者朱晓军说“他们要造一个天堂”",
-      "url": "https://baijiahao.baidu.com/s?id=1855440848271251811&wfr=spider&for=pc",
-      "source": "浙江日报",
-      "time": "前天12:31",
-      "category": "other"
-    },
-    {
-      "title": "人民城市人民建,浙产剧《小城大事》收官看哭观众",
-      "url": "https://baijiahao.baidu.com/s?id=1855453329457003489&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "前天15:13",
-      "category": "other"
-    },
-    {
-      "title": "钱报读书会预告丨李敬泽X翟业军X沈苇X萧耳:西湖畔,听风起",
-      "url": "https://baijiahao.baidu.com/s?id=1855460530894792785&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "前天17:10",
-      "category": "other"
-    },
-    {
-      "title": "成为《人民文学》头条的报告文学,需要历练什么样的脚力与笔力?",
-      "url": "http://baijiahao.baidu.com/s?id=1726635185081314797&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "铭刻时光的温度与重量 2025年12月“中南好书”发布",
-      "url": "https://baijiahao.baidu.com/s?id=1855619225571083209&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "14分钟前",
-      "category": "other"
-    },
-    {
-      "title": "沈念:写作是生命体验的深刻共鸣 | 生态文化访谈录",
-      "url": "https://www.cenews.com.cn/news.html?aid=1765835",
-      "source": "中国环境网",
-      "time": "3小时前",
-      "category": "other"
-    },
-    {
-      "title": "和家人一起读了六年新闻,我们有什么收获?",
-      "url": "https://www.infzm.com/contents/313672",
-      "source": "南方周末",
-      "time": "昨天13:01",
-      "category": "other"
-    },
-    {
-      "title": "《候鸟故事集》研讨会举行:共探新大众文艺的“在地性与流动性”",
-      "url": "https://www.bjnews.com.cn/detail/1769600912168127.html",
-      "source": "新京报",
-      "time": "昨天19:48",
-      "category": "meeting"
     }
   ]
 };
