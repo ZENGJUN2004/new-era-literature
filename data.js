ce5bb07f1562d383ee01110f125013a34993f8d9
GitHub Action <action@github.com>
2026-01-29 20:18:12 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-30 03:24",
+  "update_time": "2026-01-30 04:18",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,60 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "文学报年度好书线下发布,属于写作、阅读和出版的“此时此刻”具象...",
-      "url": "https://baijiahao.baidu.com/s?id=1854646887153738304&wfr=spider&for=pc",
-      "source": "文汇报",
-      "time": "1月18日",
-      "category": "activity"
-    },
-    {
-      "title": "2025文学报年度好书榜揭晓 新大众文艺重点在“新”",
-      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1053759",
-      "source": "上观",
-      "time": "1月18日",
-      "category": "other"
-    },
-    {
-      "title": "赵德发长篇小说《大海风》入选中国当代文学研究会年度榜单",
-      "url": "https://baijiahao.baidu.com/s?id=1853457293501650014&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "1月5日",
-      "category": "activity"
-    },
-    {
-      "title": "中国当代文学研究会年度榜单(2025)发布",
-      "url": "https://baijiahao.baidu.com/s?id=1853483055197931621&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "1月5日",
-      "category": "other"
-    },
-    {
-      "title": "广角|科幻文学距离“大众化”还有多远?",
-      "url": "https://culture.ycwb.com/ikimvkjtjj/content_53902483.htm",
-      "source": "金羊网",
-      "time": "1月11日",
-      "category": "voice"
-    },
-    {
-      "title": "迈向跨界融合的大文学时代 2025年中国文坛一瞥",
-      "url": "http://cul.china.com.cn/2026-01/16/content_43336454.htm",
-      "source": "中国网",
-      "time": "1月16日",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "山东省图书馆发布阅读报告,《明朝那些事儿》等书领跑借阅榜",
-      "url": "https://baijiahao.baidu.com/s?id=1855645620719002291&wfr=spider&for=pc",
-      "source": "大众日报",
-      "time": "昨天18:12",
-      "category": "other"
     }
   ]
 };
