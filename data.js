9e841f504217db857db81bd9e5b2db6430d4b0ec
GitHub Action <action@github.com>
2026-01-30 13:47:21 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-01-30 20:29",
+  "update_time": "2026-01-30 21:47",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -30,27 +30,6 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "盘州本土作家创作的长篇历史纪实小说《乌蒙铁壁》新书发布",
-      "url": "https://baijiahao.baidu.com/s?id=1854571604814312489&wfr=spider&for=pc",
-      "source": "天眼新闻",
-      "time": "1月17日",
-      "category": "activity"
-    },
-    {
-      "title": "寻找AI迷宫中的心灵坐标,老藤长篇小说《自愈者》新书发布会在京举行",
-      "url": "https://baijiahao.baidu.com/s?id=1853935548042845532&wfr=spider&for=pc",
-      "source": "潇湘晨报",
-      "time": "1月10日",
-      "category": "activity"
-    },
-    {
-      "title": "全书没有一个“盲”字,却让盲童拥有了逆风飞翔的力量!",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32452615",
-      "source": "澎湃新闻",
-      "time": "前天12:03",
-      "category": "other"
-    },
-    {
       "title": "每一寸土地都充满生机,“新时代山乡巨变创作计划”长篇小说《种粮...",
       "url": "https://cul.sohu.com/a/976114324_232127",
       "source": "搜狐网",
@@ -58,17 +37,10 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "一周文化讲座|艺术与争议:解谜“张大千敦煌考察之行”",
-      "url": "https://baijiahao.baidu.com/s?id=1855709792246548123&wfr=spider&for=pc",
+      "title": "为青年学者提供更多展示机会,多家学术期刊宣布扩版扩容",
+      "url": "https://baijiahao.baidu.com/s?id=1855605068411231409&wfr=spider&for=pc",
       "source": "澎湃新闻",
-      "time": "9小时前",
-      "category": "voice"
-    },
-    {
-      "title": "“拖把”和“抹布”的启示",
-      "url": "http://baijiahao.baidu.com/s?id=1855617194161500769&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "昨天10:40",
+      "time": "昨天07:26",
       "category": "other"
     },
     {
@@ -77,20 +49,6 @@ window.LIT_DATA = {
       "source": "生活日报",
       "time": "昨天17:15",
       "category": "other"
-    },
-    {
-      "title": "70岁那年,贾平凹决定去“流浪”",
-      "url": "http://www.thepaper.cn/newsDetail_forward_32488941",
-      "source": "澎湃新闻",
-      "time": "昨天13:05",
-      "category": "other"
-    },
-    {
-      "title": "《失落之城:楼兰四千年》新书首发,重塑丝路枢纽鲜活叙事",
-      "url": "https://baijiahao.baidu.com/s?id=1855634640636180492&wfr=spider&for=pc",
-      "source": "工人日报",
-      "time": "昨天15:18",
-      "category": "activity"
     }
   ]
 };
