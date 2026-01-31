e834992e44d17ab6caae2afaf12dae094bb5505f
GitHub Action <action@github.com>
2026-01-31 18:17:34 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-01 01:15",
+  "update_time": "2026-02-01 02:17",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,34 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "浙产剧《小城大事》热播 导演孙皓揭秘创作幕后",
-      "url": "http://news.10jqka.com.cn/20260130/c674439260.shtml",
-      "source": "同花顺财经",
-      "time": "前天17:56",
-      "category": "other"
-    },
-    {
-      "title": "《生命树》等近25部剧角逐2026开年档:“首爆”仍然缺失?",
-      "url": "https://www.tmtpost.com/7860732.html",
-      "source": "钛媒体官方网站",
-      "time": "前天08:11",
-      "category": "other"
-    },
-    {
-      "title": "读书会|王春林:2025中国小说:一个人的小说排行榜",
-      "url": "https://baijiahao.baidu.com/s?id=1853627163032581096&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "1月7日",
-      "category": "activity"
-    },
-    {
-      "title": "新书|在文字中探寻民族文化根脉,“文学共同体书系·中国当代多...",
-      "url": "https://baijiahao.baidu.com/s?id=1854720583649548752&wfr=spider&for=pc",
-      "source": "扬子晚报",
-      "time": "1月19日",
-      "category": "activity"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -63,13 +35,6 @@ window.LIT_DATA = {
       "source": "搜狐网",
       "time": "1月14日",
       "category": "activity"
-    },
-    {
-      "title": "长城流泪的女孩和阅山河的军旅作家:共享一种壮怀激烈",
-      "url": "https://baijiahao.baidu.com/s?id=1855804227474332202&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "昨天12:12",
-      "category": "other"
     }
   ]
 };
