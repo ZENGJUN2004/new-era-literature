1081200aa92c5f44a495902e543878e80f11f016
GitHub Action <action@github.com>
2026-02-03 08:25:35 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-03 15:36",
+  "update_time": "2026-02-03 16:25",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,34 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "《生命树》等近25部剧角逐2026开年档:“首爆”仍然缺失?",
+      "url": "https://baijiahao.baidu.com/s?id=1855882963276949469&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "前天08:28",
+      "category": "other"
+    },
+    {
+      "title": "读书会|王春林:2025中国小说:一个人的小说排行榜",
+      "url": "https://baijiahao.baidu.com/s?id=1853627163032581096&wfr=spider&for=pc",
+      "source": "齐鲁壹点",
+      "time": "1月7日",
+      "category": "activity"
+    },
+    {
+      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
+      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1050469",
+      "source": "上观",
+      "time": "1月12日",
+      "category": "activity"
+    },
+    {
+      "title": "新书|在文字中探寻民族文化根脉,“文学共同体书系·中国当代多...",
+      "url": "https://baijiahao.baidu.com/s?id=1854720583649548752&wfr=spider&for=pc",
+      "source": "扬子晚报",
+      "time": "1月19日",
+      "category": "activity"
+    },
+    {
       "title": "国家发展改革委、商务部发布《鼓励外商投资产业目录(2025年版)》",
       "url": "https://m.thepaper.cn/newsDetail_forward_32368121",
       "source": "澎湃新闻",
@@ -42,6 +70,13 @@ window.LIT_DATA = {
       "source": "搜狐网",
       "time": "1月14日",
       "category": "activity"
+    },
+    {
+      "title": "...封笔之作,珍妮特·温特森重述《一千零一夜》|1月世界文学热点",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32512151",
+      "source": "澎湃新闻",
+      "time": "昨天13:08",
+      "category": "voice"
     },
     {
       "title": "冉学东长篇小说《上寨》首发仪式暨作品讨论会在西安举行",
