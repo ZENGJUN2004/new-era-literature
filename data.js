a2a69b00e940a28ec60d4252d62550bd216f5a96
GitHub Action <action@github.com>
2026-02-04 11:26:09 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-04 18:28",
+  "update_time": "2026-02-04 19:26",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,41 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "【专访】文学研究者张莉:远离非黑即白二元对立的思维,去认识和...",
-      "url": "https://m.jiemian.com/article/4103429.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "other"
-    },
-    {
-      "title": "2025文学报年度好书榜揭晓",
-      "url": "https://m.thepaper.cn/newsDetail_forward_32399401",
-      "source": "澎湃新闻",
-      "time": "1月20日",
-      "category": "other"
-    },
-    {
-      "title": "迈向跨界融合的“大文学”时代",
-      "url": "https://www.news.cn/book/20260120/d3fa6fd5f93048fc88a2d3be4ef48224/c.html",
-      "source": "新华网",
-      "time": "1月20日",
-      "category": "other"
-    },
-    {
-      "title": "汇聚女性力量,持微火者·女性文学好书榜发布",
-      "url": "https://m.thepaper.cn/newsDetail_forward_32376524",
-      "source": "澎湃新闻",
-      "time": "1月14日",
-      "category": "other"
-    },
-    {
-      "title": "【钛晨报】国家医保局:医保药品目录调整专家评审工作已结束;把...",
-      "url": "https://www.tmtpost.com/6283071.html",
-      "source": "钛媒体官方网站",
-      "time": "",
-      "category": "activity"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -76,34 +41,6 @@ window.LIT_DATA = {
       "url": "https://baijiahao.baidu.com/s?id=1855993505774569048&wfr=spider&for=pc",
       "source": "三秦都市报",
       "time": "前天14:19",
-      "category": "activity"
-    },
-    {
-      "title": "我省再添经典名方转化成果济川煎颗粒获批上市",
-      "url": "http://news.10jqka.com.cn/20260204/c674545394.shtml",
-      "source": "同花顺财经",
-      "time": "2小时前",
-      "category": "activity"
-    },
-    {
-      "title": "辅酶q10哪个牌子好?好的辅酶q10品牌,检测合格率行业领先",
-      "url": "https://baijiahao.baidu.com/s?id=1856175306705983977&wfr=spider&for=pc",
-      "source": "荆门新闻网",
-      "time": "3小时前",
-      "category": "other"
-    },
-    {
-      "title": "李清照赵明诚20年合力收藏,两版国宝《金石录》首次同展",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32516377",
-      "source": "澎湃新闻",
-      "time": "昨天08:44",
-      "category": "other"
-    },
-    {
-      "title": "人民文学出版社推出《经典常谈(全新校订版)》",
-      "url": "https://baijiahao.baidu.com/s?id=1856101088686185340&wfr=spider&for=pc",
-      "source": "北京日报客户端",
-      "time": "昨天18:51",
       "category": "activity"
     }
   ]
