a1db17f47a600aa26ae01c50d3a072d0fccb9709
GitHub Action <action@github.com>
2026-02-04 08:28:12 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-04 15:37",
+  "update_time": "2026-02-04 16:28",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,10 +23,17 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "钱报读书会|海飞《剧院》开启“迷城”系列,发掘南方县城的秘密",
-      "url": "https://baijiahao.baidu.com/s?id=1856001116947451615&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "前天16:20",
+      "title": "莫言:大师都是等人死了以后追加的",
+      "url": "https://yule.sohu.com/a/979184148_122389433",
+      "source": "搜狐娱乐",
+      "time": "1月23日",
+      "category": "other"
+    },
+    {
+      "title": "阎晶明:新大众文艺,每个人都可以是写作者",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32410550",
+      "source": "澎湃新闻",
+      "time": "1月18日",
       "category": "other"
     },
     {
@@ -42,55 +49,6 @@ window.LIT_DATA = {
       "source": "三秦都市报",
       "time": "前天14:19",
       "category": "activity"
-    },
-    {
-      "title": "城市更新,焕新别样“城市烟火”丨新民述评",
-      "url": "http://baijiahao.baidu.com/s?id=1856174728156380745&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "1小时前",
-      "category": "other"
-    },
-    {
-      "title": "一场法治与文学的深度对话|“知名作家看法院”走进浙江台州",
-      "url": "http://www.thepaper.cn/newsDetail_forward_32531685",
-      "source": "澎湃新闻",
-      "time": "2小时前",
-      "category": "other"
-    },
-    {
-      "title": "...奋进新征程强大动能——2025年南昌市宣传思想文化工作综述",
-      "url": "https://baijiahao.baidu.com/s?id=1856162684063374111&wfr=spider&for=pc",
-      "source": "南昌发布",
-      "time": "4小时前",
-      "category": "meeting"
-    },
-    {
-      "title": "作家与好书直播间等你!立春之夜,共赴3小时文学漫游",
-      "url": "http://www.thepaper.cn/newsDetail_forward_32527799",
-      "source": "澎湃新闻",
-      "time": "4小时前",
-      "category": "other"
-    },
-    {
-      "title": "专访|刚刚下线的“钱弘佐”吴昊宸:我也在跟着《太平年》学历史",
-      "url": "https://baijiahao.baidu.com/s?id=1856156861392488663&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "5小时前",
-      "category": "other"
-    },
-    {
-      "title": "作家陈彦:当代文学像一曲交响乐",
-      "url": "http://www.thepaper.cn/newsDetail_forward_32522404",
-      "source": "澎湃新闻",
-      "time": "昨天15:40",
-      "category": "other"
-    },
-    {
-      "title": "2026年第2期《党建》刊发中国作家协会党组文章",
-      "url": "https://baijiahao.baidu.com/s?id=1856080410343804553&wfr=spider&for=pc",
-      "source": "党建网",
-      "time": "昨天10:04",
-      "category": "other"
     }
   ]
 };
