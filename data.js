17868cc493b82b059e81068a1d8a49c7093f8078
GitHub Action <action@github.com>
2026-02-05 08:29:59 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-05 15:43",
+  "update_time": "2026-02-05 16:29",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,46 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "“西游”IP为何长盛不衰?",
-      "url": "https://www.bjnews.com.cn/detail/1770077284189189.html",
-      "source": "新京报网",
-      "time": "前天08:07",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "李敬泽做客西湖讲坛谈“情感考古”:在文学中探寻中国人情感的源头",
-      "url": "https://baijiahao.baidu.com/s?id=1856159539287434750&wfr=spider&for=pc",
-      "source": "浙江日报",
-      "time": "昨天10:20",
-      "category": "other"
-    },
-    {
-      "title": "西湖讲坛首讲:李敬泽谈“情感考古”,在文学中探寻中国人情感的源头",
-      "url": "https://baijiahao.baidu.com/s?id=1856178408755433363&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "昨天15:20",
-      "category": "other"
-    },
-    {
-      "title": "「新语境下散文何为」挖掘散文更为广袤深厚的可能性",
-      "url": "https://baijiahao.baidu.com/s?id=1856144813616926411&wfr=spider&for=pc",
-      "source": "光明网",
-      "time": "昨天03:45",
-      "category": "other"
-    },
-    {
-      "title": "2025收获文学榜背后,创作有哪些新走向?",
-      "url": "https://baijiahao.baidu.com/s?id=1856139608343207044&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "昨天05:04",
-      "category": "other"
     }
   ]
 };
