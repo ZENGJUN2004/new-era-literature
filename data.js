69da432b013ff6bb40b5cdc6aecb90fb67bb2867
GitHub Action <action@github.com>
2026-02-05 22:16:35 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-06 05:19",
+  "update_time": "2026-02-06 06:16",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,39 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "两会好声音|汤素兰:建议恢复毛泽东文学奖、湖南省青年文学奖",
+      "url": "https://baijiahao.baidu.com/s?id=1856189106319101679&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "前天18:07",
+      "category": "other"
+    },
+    {
+      "title": "克林顿夫妇将出庭作证,特朗普发声",
+      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1063023&sid=11",
+      "source": "上观",
+      "time": "前天14:26",
+      "category": "other"
+    },
+    {
+      "title": "马斯克遭传唤,X平台发声",
+      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1062959&sid=11",
+      "source": "上观",
+      "time": "前天12:36",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "中华吟诵的抢救整理与研究",
+      "url": "https://baijiahao.baidu.com/s?id=1856235410189076642&wfr=spider&for=pc",
+      "source": "光明网",
+      "time": "昨天03:35",
+      "category": "other"
     }
   ]
 };
