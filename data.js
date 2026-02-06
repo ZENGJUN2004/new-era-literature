290287edf63abb292fc63a9ef7a6308abcefc9df
GitHub Action <action@github.com>
2026-02-06 01:00:51 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-06 07:15",
+  "update_time": "2026-02-06 09:00",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,46 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "中国当代文学研究会2025年度榜单",
-      "url": "https://baijiahao.baidu.com/s?id=1854879539663024017&wfr=spider&for=pc",
-      "source": "青瞳视角",
-      "time": "1月21日",
-      "category": "other"
-    },
-    {
-      "title": "2025文学报年度好书榜揭晓 新大众文艺重点在“新”",
-      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1053759",
-      "source": "上观",
-      "time": "1月18日",
-      "category": "other"
-    },
-    {
-      "title": "全勇先《秘密》荣登2025收获文学榜中篇小说榜榜首",
-      "url": "http://app.myzaker.com/news/article.php?pk=698329db8e9f097c384bd31e&m=1770205423",
-      "source": "ZAKER",
-      "time": "前天19:10",
-      "category": "activity"
-    },
-    {
-      "title": "《收获》文学榜揭晓,榜单映射中国文坛迎来“丰年”",
-      "url": "https://baijiahao.baidu.com/s?id=1856239939715495964&wfr=spider&for=pc",
-      "source": "中工网",
-      "time": "前天17:11",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "温州市图书馆2025年度阅读榜单发布 这些书你看过几本?",
-      "url": "https://news.66wz.com/system/2026/02/05/105724244.shtml",
-      "source": "温州新闻网",
-      "time": "昨天10:42",
-      "category": "other"
     }
   ]
 };
