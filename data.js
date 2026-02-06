aa9af5b69b18909ae6d5666bab80f59aa94deb58
GitHub Action <action@github.com>
2026-02-06 09:32:52 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-06 16:29",
+  "update_time": "2026-02-06 17:32",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,39 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "《收获》文学榜揭晓,榜单映射中国文坛迎来“丰年”",
-      "url": "https://baijiahao.baidu.com/s?id=1856184355598057672&wfr=spider&for=pc",
-      "source": "新华网",
-      "time": "前天16:55",
+      "title": "【城事】上海黄金市场:年节加持,消费热度不减",
+      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1063093&sid=11",
+      "source": "上观",
+      "time": "前天19:12",
       "category": "other"
+    },
+    {
+      "title": "2025收获文学榜背后,创作有哪些新走向?",
+      "url": "https://baijiahao.baidu.com/s?id=1856139608343207044&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "前天05:04",
+      "category": "other"
+    },
+    {
+      "title": "媒体眼中海大的2025年!",
+      "url": "https://baijiahao.baidu.com/s?id=1856159417055495057&wfr=spider&for=pc",
+      "source": "中国海洋大学",
+      "time": "前天03:14",
+      "category": "other"
+    },
+    {
+      "title": "大型历史文献纪录片《信仰的力量》研讨会--党史频道--人民网",
+      "url": "http://dangshi.people.com.cn/GB/151935/406058/index.html",
+      "source": "人民网",
+      "time": "",
+      "category": "meeting"
+    },
+    {
+      "title": "全勇先《秘密》荣登2025收获文学榜中篇小说榜榜首",
+      "url": "http://app.myzaker.com/news/article.php?pk=698329db8e9f097c384bd31e&m=1770205423",
+      "source": "ZAKER",
+      "time": "前天19:10",
+      "category": "activity"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
@@ -35,6 +63,76 @@ window.LIT_DATA = {
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "论文发表16个月即得诺奖,这一领域究竟有何魅力?",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32548217",
+      "source": "澎湃新闻",
+      "time": "1小时前",
+      "category": "meeting"
+    },
+    {
+      "title": "海洋文学作家许晨登上“雪龙”号深入采访|长篇报告文学《龙舞南北...",
+      "url": "https://baijiahao.baidu.com/s?id=1856364480066399926&wfr=spider&for=pc",
+      "source": "闪电新闻",
+      "time": "1小时前",
+      "category": "other"
+    },
+    {
+      "title": "2025收获文学榜揭晓,比“大小年”更重要的是写作者的恒心如一",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32535353",
+      "source": "澎湃新闻",
+      "time": "4小时前",
+      "category": "other"
+    },
+    {
+      "title": "一周文化讲座|太平有年_翻书党_澎湃新闻-The Paper",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32540871",
+      "source": "澎湃新闻",
+      "time": "6小时前",
+      "category": "meeting"
+    },
+    {
+      "title": "在沙溪,十平方米食堂的别处人生",
+      "url": "http://www.thepaper.cn/newsDetail_forward_32530915",
+      "source": "澎湃新闻",
+      "time": "昨天12:50",
+      "category": "other"
+    },
+    {
+      "title": "换了多份工作后,她在挖掘写作冷门历史中找回自我",
+      "url": "https://www.infzm.com/contents/314217",
+      "source": "南方周末",
+      "time": "昨天10:00",
+      "category": "other"
+    },
+    {
+      "title": "不敢「偏轨」的技校老师,用写作对抗庸常",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32481073",
+      "source": "澎湃新闻",
+      "time": "昨天12:50",
+      "category": "other"
+    },
+    {
+      "title": "新媒介时代青年写作的可持续性,亟待构建新的文学生态",
+      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1063734&sid=11",
+      "source": "上观",
+      "time": "昨天19:31",
+      "category": "other"
+    },
+    {
+      "title": "从黄河到苏东坡,阿来的非虚构书写有哪些变化|新批评",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32527151",
+      "source": "澎湃新闻",
+      "time": "昨天12:50",
+      "category": "voice"
+    },
+    {
+      "title": "馆员荐书 | 石图期刊(28):这份宝藏期刊清单收好喽!",
+      "url": "https://cul.sohu.com/a/983886929_121106842",
+      "source": "搜狐网",
+      "time": "昨天07:30",
+      "category": "other"
     }
   ]
 };
