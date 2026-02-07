75880c396076de02a0a7b016ce9837c6f4fe5ca5
GitHub Action <action@github.com>
2026-02-07 05:00:35 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-07 11:24",
+  "update_time": "2026-02-07 13:00",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,62 +21,6 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
-    },
-    {
-      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
-      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
-      "url": "https://m.jiemian.com/article/3573232.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
-      "url": "https://www.jfdaily.com/wx/detail.do?id=1050469",
-      "source": "上观",
-      "time": "1月12日",
-      "category": "activity"
-    },
-    {
-      "title": "青年作家正陷入孤独写作,文学共同体是有效路径吗?",
-      "url": "http://baijiahao.baidu.com/s?id=1856284693444131491&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "前天19:30",
-      "category": "other"
-    },
-    {
-      "title": "青年写作真问题:不是经验少,而是太怕“不够老”?",
-      "url": "http://baijiahao.baidu.com/s?id=1856285322738965085&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "前天19:40",
-      "category": "other"
-    },
-    {
-      "title": "2026年,这些华语文学新书要来了(一)",
-      "url": "https://baijiahao.baidu.com/s?id=1853356377637123634&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "1月4日",
-      "category": "activity"
-    },
-    {
-      "title": "2026年,这些新书值得我们期待(上)",
-      "url": "https://baijiahao.baidu.com/s?id=1854440677511475404&wfr=spider&for=pc",
-      "source": "界面新闻",
-      "time": "1月16日",
-      "category": "activity"
-    },
-    {
-      "title": "2025文学报年度好书榜揭晓 新大众文艺重点在“新”",
-      "url": "https://baijiahao.baidu.com/s?id=1854696435006784770&wfr=spider&for=pc",
-      "source": "新民晚报",
-      "time": "1月18日",
-      "category": "other"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
