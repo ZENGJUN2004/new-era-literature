ba50ff83b57a92146d0ea91e62a384ed11d5fb81
GitHub Action <action@github.com>
2026-02-07 17:17:53 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-08 00:16",
+  "update_time": "2026-02-08 01:17",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,39 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "论文发表16个月即得诺奖,这一领域究竟有何魅力?",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32548217",
-      "source": "澎湃新闻",
-      "time": "前天15:47",
-      "category": "meeting"
-    },
-    {
-      "title": "大型历史文献纪录片《信仰的力量》研讨会--党史频道--人民网",
-      "url": "http://dangshi.people.com.cn/GB/151935/406058/index.html",
-      "source": "人民网",
-      "time": "",
-      "category": "meeting"
-    },
-    {
-      "title": "海洋文学作家许晨登上“雪龙”号深入采访|长篇报告文学《龙舞南北...",
-      "url": "https://baijiahao.baidu.com/s?id=1856364480066399926&wfr=spider&for=pc",
-      "source": "闪电新闻",
-      "time": "前天16:30",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "《悬崖》编剧全勇先为哈尔滨城市记忆“提灯”",
-      "url": "https://baijiahao.baidu.com/s?id=1856432367591892844&wfr=spider&for=pc",
-      "source": "央广网",
-      "time": "昨天10:37",
-      "category": "other"
     }
   ]
 };
