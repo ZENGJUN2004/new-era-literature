5b5c5934fc1591a14a0a1f1b969854b7fb52ad33
GitHub Action <action@github.com>
2026-02-08 10:15:37 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-08 17:21",
+  "update_time": "2026-02-08 18:15",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,32 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "从28个字到五千行长诗,伊人长篇叙事诗《望台遗梦》新书发布研讨在...",
+      "url": "https://www.jfdaily.com/news/detail?id=1063746",
+      "source": "上观",
+      "time": "前天06:00",
+      "category": "activity"
+    },
+    {
+      "title": "【专访】文学研究者张莉:远离非黑即白二元对立的思维,去认识和...",
+      "url": "https://m.jiemian.com/article/4103429.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "《女神蒙上眼》开创律政剧女性题材新赛道",
+      "url": "https://baijiahao.baidu.com/s?id=1856551242215745244&wfr=spider&for=pc",
+      "source": "北晚在线",
+      "time": "20分钟前",
+      "category": "other"
     }
   ]
 };
