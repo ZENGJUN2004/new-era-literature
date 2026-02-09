e9343f8ad5a7a89dcb97b476564db2875f654a20
GitHub Action <action@github.com>
2026-02-09 15:43:57 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-09 22:07",
+  "update_time": "2026-02-09 23:43",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,94 +23,17 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "【钛晨报】国家医保局:医保药品目录调整专家评审工作已结束;把...",
-      "url": "https://www.tmtpost.com/6283071.html",
-      "source": "钛媒体官方网站",
+      "title": "鼓励外商投资产业目录(2025年版)",
+      "url": "http://www.mofcom.gov.cn/zfxxgk/zc/gz/art/2025/art_960476d904144787afc9f6fc004cd549.html",
+      "source": "中华人民共和国商务部",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "2026-1《收获》| 中篇:秦苏岚(赵刚)",
-      "url": "https://cul.sohu.com/a/978582246_638785",
-      "source": "搜狐网",
-      "time": "1月21日",
-      "category": "other"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "盘州本土作家创作的长篇历史纪实小说《乌蒙铁壁》新书发布",
-      "url": "https://baijiahao.baidu.com/s?id=1854571604814312489&wfr=spider&for=pc",
-      "source": "天眼新闻",
-      "time": "1月17日",
-      "category": "activity"
-    },
-    {
-      "title": "长篇纪实文学《烽火》新书发布暨座谈会在德州举行",
-      "url": "https://baijiahao.baidu.com/s?id=1854984906459423165&wfr=spider&for=pc",
-      "source": "大众日报",
-      "time": "1月22日",
-      "category": "activity"
-    },
-    {
-      "title": "深度书写非洲 张翎《赤道风语——东非漫行散记》新书发布",
-      "url": "https://baijiahao.baidu.com/s?id=1855009426425362877&wfr=spider&for=pc",
-      "source": "中国新闻网",
-      "time": "1月22日",
-      "category": "activity"
-    },
-    {
-      "title": "《百年骑楼》新书发布系统反映海口气韵与百年城市变迁",
-      "url": "https://baijiahao.baidu.com/s?id=1856620227254872390&wfr=spider&for=pc",
-      "source": "湘湘带你看社会",
-      "time": "9小时前",
-      "category": "activity"
-    },
-    {
-      "title": "央媒看四川丨告别“糊涂账”,盘活“三资”棋——成都探索基层产权...",
-      "url": "https://baijiahao.baidu.com/s?id=1856615311732177087&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "11小时前",
-      "category": "other"
-    },
-    {
-      "title": "《青城梨园撷英》新书发布会在呼和浩特举行",
-      "url": "https://baijiahao.baidu.com/s?id=1856608272935335132&wfr=spider&for=pc",
-      "source": "湘湘带你看社会",
-      "time": "12小时前",
-      "category": "activity"
-    },
-    {
-      "title": "河北文学精品创作竞绽芳华",
-      "url": "https://baijiahao.baidu.com/s?id=1856608715087494022&wfr=spider&for=pc",
-      "source": "金台资讯",
-      "time": "13小时前",
-      "category": "other"
-    },
-    {
-      "title": "自给率提升至50% 高出全国16个百分点——科技保障“油瓶子”",
-      "url": "https://baijiahao.baidu.com/s?id=1856569708072341696&wfr=spider&for=pc",
-      "source": "天眼新闻",
-      "time": "昨天21:49",
-      "category": "other"
-    },
-    {
-      "title": "“人,救回来了!”曾缴毒10余公斤的他以热血点亮希望",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32564147",
-      "source": "澎湃新闻",
-      "time": "昨天22:01",
-      "category": "other"
-    },
-    {
-      "title": "翰墨生香,诗心共映|本土作家许良(吉伦巴雅尔)新书发布会暨作品...",
-      "url": "https://cul.sohu.com/a/985142059_121106854",
-      "source": "搜狐网",
-      "time": "昨天16:59",
       "category": "activity"
     },
     {
