d368d2e43c60535f79821ec59290bb2bc9cee27c
GitHub Action <action@github.com>
2026-02-10 09:48:31 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-10 16:38",
+  "update_time": "2026-02-10 17:48",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -34,69 +34,6 @@ window.LIT_DATA = {
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "长篇纪实文学《烽火》新书发布暨座谈会在德州举行",
-      "url": "https://baijiahao.baidu.com/s?id=1854984906459423165&wfr=spider&for=pc",
-      "source": "大众日报",
-      "time": "1月22日",
-      "category": "activity"
-    },
-    {
-      "title": "盘州本土作家创作的长篇历史纪实小说《乌蒙铁壁》新书发布",
-      "url": "https://baijiahao.baidu.com/s?id=1854571604814312489&wfr=spider&for=pc",
-      "source": "天眼新闻",
-      "time": "1月17日",
-      "category": "activity"
-    },
-    {
-      "title": "翰墨生香,诗心共映|本土作家许良(吉伦巴雅尔)新书发布会暨作品...",
-      "url": "https://cul.sohu.com/a/985142059_121106854",
-      "source": "搜狐网",
-      "time": "前天16:59",
-      "category": "activity"
-    },
-    {
-      "title": "深度书写非洲 张翎《赤道风语——东非漫行散记》新书发布",
-      "url": "https://baijiahao.baidu.com/s?id=1855009426425362877&wfr=spider&for=pc",
-      "source": "中国新闻网",
-      "time": "1月22日",
-      "category": "activity"
-    },
-    {
-      "title": "从乡村少年到茅奖作家:一本书读懂徐则臣的“北上”人生",
-      "url": "https://baijiahao.baidu.com/s?id=1856720830348876388&wfr=spider&for=pc",
-      "source": "封面新闻",
-      "time": "1小时前",
-      "category": "other"
-    },
-    {
-      "title": "向死而生:当我们学会告别,才真正开始活着",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32519409",
-      "source": "澎湃新闻",
-      "time": "昨天13:10",
-      "category": "other"
-    },
-    {
-      "title": "河北文学精品创作竞绽芳华",
-      "url": "https://baijiahao.baidu.com/s?id=1856608715087494022&wfr=spider&for=pc",
-      "source": "金台资讯",
-      "time": "昨天08:56",
-      "category": "other"
-    },
-    {
-      "title": "《青城梨园撷英》新书发布会在呼和浩特举行",
-      "url": "https://baijiahao.baidu.com/s?id=1856608272935335132&wfr=spider&for=pc",
-      "source": "湘湘带你看社会",
-      "time": "昨天09:13",
-      "category": "activity"
-    },
-    {
-      "title": "《百年骑楼》新书发布系统反映海口气韵与百年城市变迁",
-      "url": "https://baijiahao.baidu.com/s?id=1856620227254872390&wfr=spider&for=pc",
-      "source": "湘湘带你看社会",
-      "time": "昨天12:23",
       "category": "activity"
     }
   ]
