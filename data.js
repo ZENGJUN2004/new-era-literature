436663e603daa0c02fe7898657005aeb9a95aa57
GitHub Action <action@github.com>
2026-02-11 16:52:14 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-11 23:48",
+  "update_time": "2026-02-12 00:52",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,9 +23,23 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "【钛晨报】国家医保局:医保药品目录调整专家评审工作已结束;把...",
-      "url": "https://www.tmtpost.com/6283071.html",
-      "source": "钛媒体官方网站",
+      "title": "在绩效与优秀之外,人的情感如何安放?|再谈“吴谢宇案”",
+      "url": "https://baijiahao.baidu.com/s?id=1856704973542710076&wfr=spider&for=pc",
+      "source": "新京报",
+      "time": "前天10:50",
+      "category": "other"
+    },
+    {
+      "title": "她走过无数人间,萧红为何影响一代代写作者和读者",
+      "url": "https://www.bjnews.com.cn/detail/1770683515189912.html",
+      "source": "新京报网",
+      "time": "前天08:31",
+      "category": "other"
+    },
+    {
+      "title": "鼓励外商投资产业目录(2025年版)",
+      "url": "http://www.mofcom.gov.cn/zfxxgk/zc/gz/art/2025/art_960476d904144787afc9f6fc004cd549.html",
+      "source": "中华人民共和国商务部",
       "time": "",
       "category": "activity"
     },
@@ -37,11 +51,18 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "千幅禁毒警示春联走进千家万户",
-      "url": "http://www.thepaper.cn/newsDetail_forward_32585624",
-      "source": "澎湃新闻",
-      "time": "5小时前",
-      "category": "other"
+      "title": "赛博现实主义,或数字时代的小说家——读马亿小说",
+      "url": "https://baijiahao.baidu.com/s?id=1856793590203368804&wfr=spider&for=pc",
+      "source": "光明网",
+      "time": "昨天10:00",
+      "category": "activity"
+    },
+    {
+      "title": "肖辉跃生态文学作品《海岸线呼吸》研讨会在京举行",
+      "url": "https://www.cenews.com.cn/news.html?aid=1768594",
+      "source": "中国环境网",
+      "time": "昨天16:52",
+      "category": "meeting"
     }
   ]
 };
