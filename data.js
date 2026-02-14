b58ab4ce32ac481beb30bce93d3e5ba58e893329
GitHub Action <action@github.com>
2026-02-14 06:31:11 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-14 13:08",
+  "update_time": "2026-02-14 14:31",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -34,6 +34,48 @@ window.LIT_DATA = {
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "寻找AI迷宫中的心灵坐标,老藤长篇小说《自愈者》新书发布会在京举行",
+      "url": "https://baijiahao.baidu.com/s?id=1853935548042845532&wfr=spider&for=pc",
+      "source": "潇湘晨报",
+      "time": "1月10日",
+      "category": "activity"
+    },
+    {
+      "title": "长篇纪实文学《烽火》新书发布暨座谈会在德州举行",
+      "url": "https://baijiahao.baidu.com/s?id=1855004406851169830&wfr=spider&for=pc",
+      "source": "闪电新闻",
+      "time": "1月22日",
+      "category": "activity"
+    },
+    {
+      "title": "深度书写非洲 张翎《赤道风语——东非漫行散记》新书发布",
+      "url": "https://baijiahao.baidu.com/s?id=1855009426425362877&wfr=spider&for=pc",
+      "source": "中国新闻网",
+      "time": "1月22日",
+      "category": "activity"
+    },
+    {
+      "title": "回望自南到北的人生路,“茅奖”作家徐则臣新书《我要从南走到北...",
+      "url": "https://news.dayoo.com/gzrbrmt/202602/12/170636_54928190.htm",
+      "source": "大洋网",
+      "time": "前天16:57",
+      "category": "activity"
+    },
+    {
+      "title": "《岭上行:长丰六十年发展纪实》新书发布会在合肥举行",
+      "url": "https://baijiahao.baidu.com/s?id=1857058836719048711&wfr=spider&for=pc",
+      "source": "大皖新闻",
+      "time": "5小时前",
+      "category": "activity"
+    },
+    {
+      "title": "《我要从南走到北》新书发布会在京举办",
+      "url": "https://baijiahao.baidu.com/s?id=1856979438005159133&wfr=spider&for=pc",
+      "source": "中国青年网",
+      "time": "昨天11:15",
       "category": "activity"
     }
   ]
