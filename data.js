361d504f489ae15c3905f577978d89f097c0c64f
GitHub Action <action@github.com>
2026-02-17 14:00:04 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-17 20:34",
+  "update_time": "2026-02-17 22:00",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,46 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "纪念茅盾先生诞辰130周年:茅盾的最后馈赠",
-      "url": "https://baijiahao.baidu.com/s?id=1855683209800924801&wfr=spider&for=pc",
-      "source": "文汇报",
-      "time": "1月30日",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "宋遂良《复旦求学日记》及臧杰新作《清气》出版",
-      "url": "https://baijiahao.baidu.com/s?id=1857345320755326748&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "8小时前",
-      "category": "activity"
-    },
-    {
-      "title": "茅盾文学奖得主梁晓声亲临!2026新春·海淀书香庙会名家荐读暨新书...",
-      "url": "https://cul.sohu.com/a/987868728_121106842",
-      "source": "搜狐网",
-      "time": "昨天15:09",
-      "category": "activity"
-    },
-    {
-      "title": "著名作家徐则臣,回家啦!",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32625151",
-      "source": "澎湃新闻",
-      "time": "昨天20:33",
-      "category": "other"
-    },
-    {
-      "title": "著名作家徐则臣回乡开讲 分享文学之路与运河情缘",
-      "url": "https://stock.10jqka.com.cn/20260216/c674832562.shtml",
-      "source": "同花顺",
-      "time": "昨天20:37",
-      "category": "other"
     }
   ]
 };
