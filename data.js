0fe9ba62f95b6862b63fa2f4b03422c564f95c97
GitHub Action <action@github.com>
2026-02-18 14:44:55 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-18 22:00",
+  "update_time": "2026-02-18 22:44",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,60 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "四川省十四届人大五次会议“代表通道”集体采访活动(第二场)",
+      "url": "http://www.scio.gov.cn/xwfb/dfxwfb/gssfbh/sc_13848/202602/t20260210_949661.html",
+      "source": "中华人民共和国国务院新闻办公室",
+      "time": "2月4日",
+      "category": "other"
+    },
+    {
+      "title": "搭建作家与读者的桥梁",
+      "url": "https://baijiahao.baidu.com/s?id=1855999891281456393&wfr=spider&for=pc",
+      "source": "中国青年网",
+      "time": "2月2日",
+      "category": "other"
+    },
+    {
+      "title": "来一场穿越时空的对话",
+      "url": "http://m.cnhubei.com/content/2026-02/06/content_19813290.html",
+      "source": "荆楚网",
+      "time": "2月6日",
+      "category": "other"
+    },
+    {
+      "title": "守正彰美 立言开新",
+      "url": "https://hbrb.hebnews.cn/pad/paper/c/202602/06/content_297805.html",
+      "source": "河北日报",
+      "time": "2月6日",
+      "category": "other"
+    },
+    {
+      "title": "两会好声音|汤素兰:建议恢复毛泽东文学奖、湖南省青年文学奖",
+      "url": "https://baijiahao.baidu.com/s?id=1856189106319101679&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "2月4日",
+      "category": "other"
+    },
+    {
+      "title": "“拖把”和“抹布”的启示",
+      "url": "https://baijiahao.baidu.com/s?id=1855617412563728167&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "1月29日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "品质优于价格?来看看2026年02月睢宁睢宁县4000-5000元/㎡的高性价...",
+      "url": "http://news.jssn.fang.com/2015-08-31/54332786.htm",
+      "source": "房天下",
+      "time": "9小时前",
+      "category": "other"
     }
   ]
 };
