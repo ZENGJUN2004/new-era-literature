1ebbed4241475592d71d31855e1b2aa91a06f064
GitHub Action <action@github.com>
2026-02-18 11:31:17 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-18 18:32",
+  "update_time": "2026-02-18 19:31",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,32 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "人民文学出版社致歉,《经典常谈》校订已完成",
-      "url": "https://baijiahao.baidu.com/s?id=1856136464442507613&wfr=spider&for=pc",
-      "source": "光明日报",
-      "time": "2月3日",
-      "category": "activity"
-    },
-    {
-      "title": "文学双周记|《呼啸山庄》《蝇王》《纯真博物馆》改编齐亮相",
-      "url": "https://baijiahao.baidu.com/s?id=1857249405029297946&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "前天10:11",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "郭玉洁:亿万个孤独的自我|年度阅读",
-      "url": "https://baijiahao.baidu.com/s?id=1857431855820724815&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "7小时前",
-      "category": "other"
     }
   ]
 };
