7667f5065b719004e745d13da43bcc63007c9301
GitHub Action <action@github.com>
2026-02-18 14:00:49 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-18 20:34",
+  "update_time": "2026-02-18 22:00",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,80 +23,10 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "2026年,这些华语文学新书要来了(一)",
-      "url": "https://baijiahao.baidu.com/s?id=1853356377637123634&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "1月4日",
-      "category": "activity"
-    },
-    {
-      "title": "2025春风月榜半年报告:希望这22本佳作为你带来光",
-      "url": "https://baijiahao.baidu.com/s?id=1853802033490694012&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "1月9日",
-      "category": "other"
-    },
-    {
-      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
-      "url": "https://www.jfdaily.com/wx/detail.do?id=1050469",
-      "source": "上观",
-      "time": "1月12日",
-      "category": "activity"
-    },
-    {
-      "title": "2026年,这些新书值得我们期待(上)",
-      "url": "https://www.jiemian.com/article/13888715.html",
-      "source": "界面新闻",
-      "time": "1月16日",
-      "category": "activity"
-    },
-    {
-      "title": "苏童毕飞宇吴真全勇先,2025收获文学榜揭晓,榜首作家这样说",
-      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1062375",
-      "source": "上观",
-      "time": "2月3日",
-      "category": "other"
-    },
-    {
-      "title": "纪念茅盾先生诞辰130周年:茅盾的最后馈赠",
-      "url": "https://baijiahao.baidu.com/s?id=1855683209800924801&wfr=spider&for=pc",
-      "source": "文汇报",
-      "time": "1月30日",
-      "category": "other"
-    },
-    {
-      "title": "茅盾文学奖得主梁晓声亲临!2026新春·海淀书香庙会名家荐读暨新书...",
-      "url": "https://cul.sohu.com/a/987868728_121106842",
-      "source": "搜狐网",
-      "time": "前天15:09",
-      "category": "activity"
-    },
-    {
-      "title": "著名作家徐则臣,回家啦!",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32625151",
-      "source": "澎湃新闻",
-      "time": "前天20:33",
-      "category": "other"
-    },
-    {
-      "title": "著名作家徐则臣回乡开讲 分享文学之路与运河情缘",
-      "url": "https://stock.10jqka.com.cn/20260216/c674832562.shtml",
-      "source": "同花顺",
-      "time": "前天20:37",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "宋遂良《复旦求学日记》及臧杰新作《清气》出版",
-      "url": "https://baijiahao.baidu.com/s?id=1857345320755326748&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "昨天12:28",
       "category": "activity"
     }
   ]
