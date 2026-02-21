532c8c04048d9595ea246164115fd0691b040047
GitHub Action <action@github.com>
2026-02-21 03:24:25 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-21 09:01",
+  "update_time": "2026-02-21 11:24",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,6 +21,90 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
+    },
+    {
+      "title": "《小说评论》年度论坛:聚焦“现象级文本与文学史书写”",
+      "url": "http://baijiahao.baidu.com/s?id=1751357796338047975&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "十部作品里的女性故事,汇聚成一支温良智慧的火把",
+      "url": "http://baijiahao.baidu.com/s?id=1726726766072973629&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "other"
+    },
+    {
+      "title": "大众写科幻,带来了什么?",
+      "url": "https://baijiahao.baidu.com/s?id=1853890395730591225&wfr=spider&for=pc",
+      "source": "湘湘带你看社会",
+      "time": "1月10日",
+      "category": "voice"
+    },
+    {
+      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
+      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "茅盾文学奖不应\"自相矛盾\"_正义网",
+      "url": "http://www.jcrb.com/culture/jianghu/mdwxj/index.html",
+      "source": "正义网",
+      "time": "",
+      "category": "other"
+    },
+    {
+      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
+      "url": "https://m.jiemian.com/article/3573232.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "2025春风月榜半年报告:希望这22本佳作为你带来光",
+      "url": "https://baijiahao.baidu.com/s?id=1853802033490694012&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "1月9日",
+      "category": "other"
+    },
+    {
+      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
+      "url": "https://www.jfdaily.com/wx/detail.do?id=1050469",
+      "source": "上观",
+      "time": "1月12日",
+      "category": "activity"
+    },
+    {
+      "title": "2026年,这些新书值得我们期待(上)",
+      "url": "https://baijiahao.baidu.com/s?id=1854440677511475404&wfr=spider&for=pc",
+      "source": "界面新闻",
+      "time": "1月16日",
+      "category": "activity"
+    },
+    {
+      "title": "2025文学报年度好书榜揭晓 新大众文艺重点在“新”",
+      "url": "https://baijiahao.baidu.com/s?id=1854696435006784770&wfr=spider&for=pc",
+      "source": "新民晚报",
+      "time": "1月18日",
+      "category": "other"
+    },
+    {
+      "title": "苏童毕飞宇吴真全勇先,2025收获文学榜揭晓,榜首作家这样说",
+      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1062375",
+      "source": "上观",
+      "time": "2月3日",
+      "category": "other"
+    },
+    {
+      "title": "成为《人民文学》头条的报告文学,需要历练什么样的脚力与笔力?",
+      "url": "http://baijiahao.baidu.com/s?id=1726635185081314797&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "other"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
