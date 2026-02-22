ae3c5a279c758d1b2c75ec26d1b5602bf8ec2bc8
GitHub Action <action@github.com>
2026-02-22 22:13:35 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-23 05:14",
+  "update_time": "2026-02-23 06:13",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -20,6 +20,27 @@ window.LIT_DATA = {
       "url": "https://scholar.baidu.com/scholar?q=当代文学&sc_ylo=2024&sort=sc_time",
       "source": "CNKI",
       "time": "实时",
+      "category": "meeting"
+    },
+    {
+      "title": "不做洪流漂浮者,新大众文艺的扎根与生长",
+      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1065688&sid=300",
+      "source": "上观",
+      "time": "2月12日",
+      "category": "other"
+    },
+    {
+      "title": "两会好声音|汤素兰:建议恢复毛泽东文学奖、湖南省青年文学奖",
+      "url": "https://baijiahao.baidu.com/s?id=1856189106319101679&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "2月4日",
+      "category": "other"
+    },
+    {
+      "title": "上海艺术评论丨新大众文艺,评论为何?",
+      "url": "https://cul.sohu.com/a/986822220_121119390",
+      "source": "搜狐网",
+      "time": "2月12日",
       "category": "meeting"
     },
     {
