d4e5d6e68c8aafb5aeff3e5788bd39351483effc
GitHub Action <action@github.com>
2026-02-26 01:02:22 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-26 07:20",
+  "update_time": "2026-02-26 09:02",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,6 +21,27 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
+    },
+    {
+      "title": "媒体眼中海大的2025年!",
+      "url": "https://baijiahao.baidu.com/s?id=1856159417055495057&wfr=spider&for=pc",
+      "source": "中国海洋大学",
+      "time": "2月4日",
+      "category": "other"
+    },
+    {
+      "title": "大型历史文献纪录片《信仰的力量》研讨会--党史频道--人民网",
+      "url": "http://dangshi.people.com.cn/GB/151935/406058/index.html",
+      "source": "人民网",
+      "time": "",
+      "category": "meeting"
+    },
+    {
+      "title": "新刊|《十月·长篇小说》2026年第1期目录",
+      "url": "https://cul.sohu.com/a/986312585_121124729",
+      "source": "搜狐网",
+      "time": "2月11日",
+      "category": "activity"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
