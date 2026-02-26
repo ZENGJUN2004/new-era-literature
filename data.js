d5607ef320f8ef774b83449e721e367451efad79
GitHub Action <action@github.com>
2026-02-26 05:50:10 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-26 12:06",
+  "update_time": "2026-02-26 13:50",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,60 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "人民文学出版社致歉,《经典常谈》校订已完成",
-      "url": "https://baijiahao.baidu.com/s?id=1856136464442507613&wfr=spider&for=pc",
-      "source": "光明日报",
-      "time": "2月3日",
-      "category": "activity"
-    },
-    {
-      "title": "诗评丨凌之鹤:游侠李立的行吟诗学与精神远征",
-      "url": "https://baijiahao.baidu.com/s?id=1857995728869330221&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "前天16:46",
-      "category": "other"
-    },
-    {
-      "title": "文学归根处,中原起新声——豫籍文艺名家“归根还巢”两年记",
-      "url": "https://baijiahao.baidu.com/s?id=1857988241311349076&wfr=spider&for=pc",
-      "source": "正观新闻",
-      "time": "前天14:41",
-      "category": "other"
-    },
-    {
-      "title": "向葡语国家传播中国文学《人民文学》葡文版首刊正式发行",
-      "url": "https://baijiahao.baidu.com/s?id=1857935327681773426&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "前天00:46",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "为黄永玉的木刻全集补阙,黑妮见到欣喜非常 | 刘心武",
-      "url": "http://baijiahao.baidu.com/s?id=1858153848180300898&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "1小时前",
-      "category": "other"
-    },
-    {
-      "title": "大理州作协主席、文化杂志社二级文学创作李达伟——笔耕不辍 记录...",
-      "url": "https://baijiahao.baidu.com/s?id=1858157136595993928&wfr=spider&for=pc",
-      "source": "云南日报",
-      "time": "1小时前",
-      "category": "other"
-    },
-    {
-      "title": "张清华:他用镜像对镜像、心像对心像的方式寻找到汉语美妙的前世与...",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32644129",
-      "source": "澎湃新闻",
-      "time": "昨天15:14",
-      "category": "other"
     },
     {
       "title": "财经下午茶20260225 | 上海发布楼市“沪七条”",
