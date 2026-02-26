e2ff06bf0c2e0e476e3500eb776de8bb6b6da78c
GitHub Action <action@github.com>
2026-02-26 07:43:39 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-26 14:45",
+  "update_time": "2026-02-26 15:43",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,6 +21,20 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
+    },
+    {
+      "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
+      "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "财经下午茶20260225 | 上海发布楼市“沪七条”",
+      "url": "https://stock.10jqka.com.cn/20260225/c674902343.shtml",
+      "source": "同花顺",
+      "time": "昨天16:54",
+      "category": "other"
     }
   ]
 };
