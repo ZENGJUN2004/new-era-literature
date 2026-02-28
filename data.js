a566d82afc8533d83e5e6f3f0a58ff2da6714c63
GitHub Action <action@github.com>
2026-02-28 03:06:06 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-28 08:56",
+  "update_time": "2026-02-28 11:06",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,97 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "大众写科幻,带来了什么?",
+      "url": "http://baijiahao.baidu.com/s?id=1853855131427137219&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "1月9日",
+      "category": "voice"
+    },
+    {
+      "title": "如何把一个想法写成畅销书?美国顶尖非虚构作家的写作课",
+      "url": "https://www.lifeweek.com.cn/article/13495",
+      "source": "三联生活周刊",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
+      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
+      "url": "https://m.jiemian.com/article/3573232.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "这一场文学跨年,作家带大家一起重温文学故乡,述说文学跋涉",
+      "url": "https://www.takefoto.cn/news/2026/01/02/11500412.shtml",
+      "source": "北晚在线",
+      "time": "1月2日",
+      "category": "other"
+    },
+    {
+      "title": "2026,文学大师开始在评论区“营业”",
+      "url": "https://www.chinaz.com/2026/0127/1733176.shtml",
+      "source": "站长之家",
+      "time": "2月11日",
+      "category": "meeting"
+    },
+    {
+      "title": "热门电视剧背后的创作经验,反复写过的题材还能创意新写?",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32493004",
+      "source": "澎湃新闻",
+      "time": "2月5日",
+      "category": "other"
+    },
+    {
+      "title": "职业剧再不努力,就要被网文卷到了",
+      "url": "https://m.jiemian.com/article/8010089.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "other"
+    },
+    {
+      "title": "2026年,这些华语文学新书要来了(一)",
+      "url": "https://baijiahao.baidu.com/s?id=1853356377637123634&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "1月4日",
+      "category": "activity"
+    },
+    {
+      "title": "2025春风月榜半年报告:希望这22本佳作为你带来光",
+      "url": "https://baijiahao.baidu.com/s?id=1853802033490694012&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "1月9日",
+      "category": "other"
+    },
+    {
+      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
+      "url": "https://www.jfdaily.com/wx/detail.do?id=1050469",
+      "source": "上观",
+      "time": "1月12日",
+      "category": "activity"
+    },
+    {
+      "title": "2026年,这些新书值得我们期待(上)",
+      "url": "https://baijiahao.baidu.com/s?id=1854440677511475404&wfr=spider&for=pc",
+      "source": "界面新闻",
+      "time": "1月16日",
+      "category": "activity"
+    },
+    {
+      "title": "成为《人民文学》头条的报告文学,需要历练什么样的脚力与笔力?",
+      "url": "http://baijiahao.baidu.com/s?id=1726635185081314797&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "other"
+    },
+    {
       "title": "AITO问界在阿联酋收获200台首批订单",
       "url": "http://news.10jqka.com.cn/20260226/c674931181.shtml",
       "source": "同花顺财经",
@@ -41,6 +132,55 @@ window.LIT_DATA = {
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "新加坡著名慈善家、作家卓顺发《太阳出来了,露水不见了》新书发布",
+      "url": "https://cul.sohu.com/a/976352761_121192997",
+      "source": "搜狐网",
+      "time": "1月15日",
+      "category": "activity"
+    },
+    {
+      "title": "「滨州摄影师的2025」王圣歧:光影随行追逐四季",
+      "url": "https://baijiahao.baidu.com/s?id=1858158236939047973&wfr=spider&for=pc",
+      "source": "身边24小时",
+      "time": "前天11:49",
+      "category": "other"
+    },
+    {
+      "title": "《百年骑楼》新书发布系统反映海口气韵与百年城市变迁",
+      "url": "https://baijiahao.baidu.com/s?id=1856620227254872390&wfr=spider&for=pc",
+      "source": "湘湘带你看社会",
+      "time": "2月9日",
+      "category": "activity"
+    },
+    {
+      "title": "徐则臣《我要从南走到北》新书发布",
+      "url": "https://baijiahao.baidu.com/s?id=1856807777518009503&wfr=spider&for=pc",
+      "source": "中国新闻网",
+      "time": "2月11日",
+      "category": "activity"
+    },
+    {
+      "title": "呈现70后成长足迹",
+      "url": "https://baijiahao.baidu.com/s?id=1858328828873105188&wfr=spider&for=pc",
+      "source": "人民网",
+      "time": "8小时前",
+      "category": "other"
+    },
+    {
+      "title": "诗坛“顶流”出圈背后,还原真实东坡",
+      "url": "https://www.jfdaily.com/news/detail?id=1072412",
+      "source": "上观",
+      "time": "昨天09:41",
+      "category": "other"
+    },
+    {
+      "title": "《食野中国》出版:一场关于“中国风土”的世界对话",
+      "url": "https://baijiahao.baidu.com/s?id=1858246474532675835&wfr=spider&for=pc",
+      "source": "青瞳视角",
+      "time": "昨天11:10",
       "category": "activity"
     }
   ]
