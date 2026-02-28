de7fb1d2770479b40cf4305b2e6831bea9baa932
GitHub Action <action@github.com>
2026-02-28 19:13:31 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-01 02:17",
+  "update_time": "2026-03-01 03:13",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,38 +23,10 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "真善美的三重奏:文学批评的时代使命",
-      "url": "http://baijiahao.baidu.com/s?id=1854380601623728583&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "1月15日",
-      "category": "meeting"
-    },
-    {
-      "title": "伦敦电视节官宣:超级剧集《瓦猫》启动 打造东方人文剧集",
-      "url": "https://ent.ynet.com/2026/02/27/3988828t1254.html",
-      "source": "北青网",
-      "time": "前天",
-      "category": "other"
-    },
-    {
-      "title": "《刘醒龙文学年谱》出版",
-      "url": "http://news.cnhubei.com/content/2026-02/27/content_19841961.html",
-      "source": "荆楚网新闻",
-      "time": "前天08:57",
-      "category": "activity"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "《阅山河》出版发行_读书_教育频道_云南网",
-      "url": "http://edu.yunnan.cn/system/2026/02/28/033892993.shtml",
-      "source": "云南网",
-      "time": "昨天10:54",
       "category": "activity"
     }
   ]
