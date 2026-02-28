c6b87a09fd9eb50039e723a0e4efe5154ec8cf22
GitHub Action <action@github.com>
2026-02-28 10:13:11 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-02-28 17:16",
+  "update_time": "2026-02-28 18:13",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,20 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "茅盾文学奖得主徐则臣受聘为连云港“读城”推荐官",
-      "url": "https://baijiahao.baidu.com/s?id=1857112032497137778&wfr=spider&for=pc",
-      "source": "金台资讯",
-      "time": "2月14日",
-      "category": "other"
-    },
-    {
-      "title": "梁晓声:幸福是手握扎实的技能,过好自己的生活",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32663472",
-      "source": "澎湃新闻",
-      "time": "前天17:30",
-      "category": "other"
-    },
-    {
       "title": "AITO问界在阿联酋收获200台首批订单",
       "url": "http://news.10jqka.com.cn/20260226/c674931181.shtml",
       "source": "同花顺财经",
@@ -56,48 +42,6 @@ window.LIT_DATA = {
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "“知名作家看法院”采风作品:“婚姻三部曲”的台前幕后",
-      "url": "https://baijiahao.baidu.com/s?id=1858352369613750146&wfr=spider&for=pc",
-      "source": "光明网",
-      "time": "2小时前",
-      "category": "other"
-    },
-    {
-      "title": "“新华荐书”第三十期推荐图书发布",
-      "url": "https://baijiahao.baidu.com/s?id=1858292474933536667&wfr=spider&for=pc",
-      "source": "安徽经济网",
-      "time": "昨天23:23",
-      "category": "other"
-    },
-    {
-      "title": "“登超”火了!阿来谈登山:摒弃“征服”的旧观念,做带着思考的行路者",
-      "url": "https://baijiahao.baidu.com/s?id=1858251804681814087&wfr=spider&for=pc",
-      "source": "封面新闻",
-      "time": "昨天11:16",
-      "category": "other"
-    },
-    {
-      "title": "《刘醒龙文学年谱》出版",
-      "url": "http://news.cnhubei.com/content/2026-02/27/content_19841961.html",
-      "source": "荆楚网新闻",
-      "time": "昨天08:57",
-      "category": "activity"
-    },
-    {
-      "title": "梁晓声:AI不能代替写作",
-      "url": "https://baijiahao.baidu.com/s?id=1858204394194634263&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "昨天00:03",
-      "category": "other"
-    },
-    {
-      "title": "茅奖作家李洱《花腔》荣获瑞典“2025年度好书奖”",
-      "url": "https://baijiahao.baidu.com/s?id=1858271148173977540&wfr=spider&for=pc",
-      "source": "封面新闻",
-      "time": "昨天17:43",
-      "category": "other"
     }
   ]
 };
