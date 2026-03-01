b6bee628266ae0e2698833504aa767bb36b8df14
GitHub Action <action@github.com>
2026-03-01 23:13:58 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-02 06:13",
+  "update_time": "2026-03-02 07:13",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,6 +21,34 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
+    },
+    {
+      "title": "观察者网·观学院直播厅【思想者说】第21期:从中俄美三国元首同日...",
+      "url": "https://www.guancha.cn/huangjing/2026_02_12_806901.shtml",
+      "source": "观察者网",
+      "time": "2月12日",
+      "category": "other"
+    },
+    {
+      "title": "观察者网·观学院直播厅「思想者说」第21期:给日本松链子,这是...",
+      "url": "https://baijiahao.baidu.com/s?id=1856886008341810036&wfr=spider&for=pc",
+      "source": "观察者网",
+      "time": "2月12日",
+      "category": "other"
+    },
+    {
+      "title": "女性为何写作?她们用文字打破边界",
+      "url": "http://baijiahao.baidu.com/s?id=1855001128564567122&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "1月22日",
+      "category": "other"
+    },
+    {
+      "title": "【专访】文学研究者张莉:远离非黑即白二元对立的思维,去认识和...",
+      "url": "https://m.jiemian.com/article/4103429.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "other"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
