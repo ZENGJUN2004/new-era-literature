660690a32467502b933356f3e52d4bda382cc755
GitHub Action <action@github.com>
2026-03-01 18:18:21 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-02 01:14",
+  "update_time": "2026-03-02 02:18",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,27 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "媒体眼中海大的2025年!",
+      "url": "https://baijiahao.baidu.com/s?id=1856159417055495057&wfr=spider&for=pc",
+      "source": "中国海洋大学",
+      "time": "2月4日",
+      "category": "other"
+    },
+    {
+      "title": "大型历史文献纪录片《信仰的力量》研讨会--党史频道--人民网",
+      "url": "http://dangshi.people.com.cn/GB/151935/406058/index.html",
+      "source": "人民网",
+      "time": "",
+      "category": "meeting"
+    },
+    {
+      "title": "新刊|《十月·长篇小说》2026年第1期目录",
+      "url": "https://cul.sohu.com/a/986312585_121124729",
+      "source": "搜狐网",
+      "time": "2月11日",
+      "category": "activity"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -30,10 +51,10 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "超越地方小说的文学野心 | 方柏林评《东岸纪事》",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32669896",
-      "source": "澎湃新闻",
-      "time": "前天13:49",
+      "title": "北京出版集团每周新书新作 | 新岁启阅",
+      "url": "https://cul.sohu.com/a/991253052_121124729",
+      "source": "搜狐网",
+      "time": "昨天11:38",
       "category": "activity"
     }
   ]
