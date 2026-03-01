6a58d6d2cd38ff01f94adeb5959fd089604b0260
GitHub Action <action@github.com>
2026-03-01 05:44:01 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-01 12:12",
+  "update_time": "2026-03-01 13:44",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,45 +23,10 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "【钛晨报】国家医保局:医保药品目录调整专家评审工作已结束;把...",
-      "url": "https://www.tmtpost.com/6283071.html",
-      "source": "钛媒体官方网站",
-      "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "定格寒假的美好瞬间",
-      "url": "https://m.gmw.cn/2026-02/27/content_38614076.htm",
-      "source": "手机光明网",
-      "time": "前天10:28",
-      "category": "other"
-    },
-    {
-      "title": "普陀区两家文化企业上榜!2025年度上海市“中华文化国际传播...",
-      "url": "https://baijiahao.baidu.com/s?id=1858265444059376804&wfr=spider&for=pc",
-      "source": "新闻晨报",
-      "time": "前天16:13",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "昔日中国心血管药王,重新支棱起来了",
-      "url": "https://baijiahao.baidu.com/s?id=1858357124876933852&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "昨天16:31",
-      "category": "other"
-    },
-    {
-      "title": "连载19年,《某科学的超电磁炮》漫画正式官宣完结",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32667275",
-      "source": "澎湃新闻",
-      "time": "昨天07:37",
       "category": "activity"
     }
   ]
