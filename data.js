9c2c25c284f0a734052d7f4aa043445b27bae15c
GitHub Action <action@github.com>
2026-03-01 14:15:41 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-01 21:33",
+  "update_time": "2026-03-01 22:15",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,53 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "女性为何写作?她们用文字打破边界",
-      "url": "http://baijiahao.baidu.com/s?id=1855001128564567122&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "1月22日",
-      "category": "other"
-    },
-    {
-      "title": "【专访】文学研究者张莉:远离非黑即白二元对立的思维,去认识和...",
-      "url": "https://m.jiemian.com/article/4103429.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "other"
-    },
-    {
-      "title": "人民文学出版社致歉,《经典常谈》校订已完成",
-      "url": "https://baijiahao.baidu.com/s?id=1856136464442507613&wfr=spider&for=pc",
-      "source": "光明日报",
-      "time": "2月3日",
-      "category": "activity"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "报刊门市部春节无休,姜师傅马年有了新烦恼,杂志从“卖不掉”到...",
-      "url": "https://baijiahao.baidu.com/s?id=1858464851638627855&wfr=spider&for=pc",
-      "source": "新闻晨报",
-      "time": "28分钟前",
-      "category": "other"
-    },
-    {
-      "title": "书伴人生",
-      "url": "https://baijiahao.baidu.com/s?id=1858362812924660555&wfr=spider&for=pc",
-      "source": "长沙晚报掌上长沙",
-      "time": "昨天18:01",
-      "category": "other"
-    },
-    {
-      "title": "“新春走基层——春天送你一首诗”系列活动在铜川启动",
-      "url": "http://news.cnwest.com/bwyc/a/2026/02/28/23306279.html",
-      "source": "西部网",
-      "time": "昨天21:01",
-      "category": "other"
     }
   ]
 };
