608ed8ce8a5e99c8459a0262c8421973d939c040
GitHub Action <action@github.com>
2026-03-02 08:31:46 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-02 15:42",
+  "update_time": "2026-03-02 16:31",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,10 +23,24 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "成为《人民文学》头条的报告文学,需要历练什么样的脚力与笔力?",
-      "url": "http://baijiahao.baidu.com/s?id=1726635185081314797&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
+      "title": "《收获》文学榜揭晓,榜单映射中国文坛迎来“丰年”",
+      "url": "https://baijiahao.baidu.com/s?id=1856184355598057672&wfr=spider&for=pc",
+      "source": "新华网",
+      "time": "2月4日",
+      "category": "other"
+    },
+    {
+      "title": "职业剧再不努力,就要被网文卷到了",
+      "url": "https://m.jiemian.com/article/8010089.html",
+      "source": "界面新闻",
       "time": "",
+      "category": "other"
+    },
+    {
+      "title": "茅盾文学奖得主徐则臣受聘为连云港“读城”推荐官",
+      "url": "https://baijiahao.baidu.com/s?id=1857112032497137778&wfr=spider&for=pc",
+      "source": "金台资讯",
+      "time": "2月14日",
       "category": "other"
     },
     {
@@ -35,6 +49,20 @@ window.LIT_DATA = {
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "开学读好书 京东图书开工开学季精选爆款好书享领券满300减120",
+      "url": "http://d.youth.cn/newtech/202603/t20260302_16534568.htm",
+      "source": "中国青年网",
+      "time": "6小时前",
+      "category": "other"
+    },
+    {
+      "title": "2026春风悦读榜揭晓:年度图书100,你读过哪些?",
+      "url": "https://cul.sohu.com/a/991182646_121956422",
+      "source": "搜狐网",
+      "time": "昨天07:13",
+      "category": "other"
     }
   ]
 };
