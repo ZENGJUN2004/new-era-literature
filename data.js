b959108e343a4c52bcfc81300d51fe58a6eb191f
GitHub Action <action@github.com>
2026-03-03 14:36:17 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-03 21:51",
+  "update_time": "2026-03-03 22:36",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,46 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "【钛晨报】国家医保局:医保药品目录调整专家评审工作已结束;把...",
-      "url": "https://www.tmtpost.com/6283071.html",
-      "source": "钛媒体官方网站",
-      "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "网红百万医疗险狂卷创新药,但商保目录为何却首战失利?",
-      "url": "https://www.jiemian.com/article/14055763.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "activity"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "金华历史经典产业收获两个全省第一",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32690926",
-      "source": "澎湃新闻",
-      "time": "12小时前",
-      "category": "other"
-    },
-    {
-      "title": "文学IP改编浪潮下,谁有潜力跑出下一匹黑马?",
-      "url": "http://app.myzaker.com/news/article.php?pk=69a5562bb15ec0479721201e",
-      "source": "ZAKER",
-      "time": "昨天16:53",
-      "category": "other"
-    },
-    {
-      "title": "国家重点治理三大湖泊中首个水质达到Ⅲ类!江苏发布新一轮太湖综合...",
-      "url": "https://baijiahao.baidu.com/s?id=1858544500875755008&wfr=spider&for=pc",
-      "source": "扬子晚报",
-      "time": "昨天17:37",
-      "category": "other"
     }
   ]
 };
