2b29b872868ab136fa87086825c00e4a28cd0402
GitHub Action <action@github.com>
2026-03-03 13:51:34 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-03 20:31",
+  "update_time": "2026-03-03 21:51",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,18 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "大文学观视野下的文学创作——鲁院河北班学员研讨选编",
-      "url": "http://app.myzaker.com/news/article.php?pk=69a1191e8e9f09031d680887",
-      "source": "ZAKER",
+      "title": "【钛晨报】国家医保局:医保药品目录调整专家评审工作已结束;把...",
+      "url": "https://www.tmtpost.com/6283071.html",
+      "source": "钛媒体官方网站",
       "time": "",
-      "category": "meeting"
+      "category": "activity"
+    },
+    {
+      "title": "网红百万医疗险狂卷创新药,但商保目录为何却首战失利?",
+      "url": "https://www.jiemian.com/article/14055763.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "activity"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
@@ -35,6 +42,27 @@ window.LIT_DATA = {
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "金华历史经典产业收获两个全省第一",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32690926",
+      "source": "澎湃新闻",
+      "time": "12小时前",
+      "category": "other"
+    },
+    {
+      "title": "文学IP改编浪潮下,谁有潜力跑出下一匹黑马?",
+      "url": "http://app.myzaker.com/news/article.php?pk=69a5562bb15ec0479721201e",
+      "source": "ZAKER",
+      "time": "昨天16:53",
+      "category": "other"
+    },
+    {
+      "title": "国家重点治理三大湖泊中首个水质达到Ⅲ类!江苏发布新一轮太湖综合...",
+      "url": "https://baijiahao.baidu.com/s?id=1858544500875755008&wfr=spider&for=pc",
+      "source": "扬子晚报",
+      "time": "昨天17:37",
+      "category": "other"
     }
   ]
 };
