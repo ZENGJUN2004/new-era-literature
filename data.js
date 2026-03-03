257da32c354a451eaca9ddf36d98e8d5aa646146
GitHub Action <action@github.com>
2026-03-03 11:24:25 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-03 18:28",
+  "update_time": "2026-03-03 19:24",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,34 +21,6 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
-    },
-    {
-      "title": "《收获》文学榜揭晓,榜单映射中国文坛迎来“丰年”",
-      "url": "https://baijiahao.baidu.com/s?id=1856184355598057672&wfr=spider&for=pc",
-      "source": "新华网",
-      "time": "2月4日",
-      "category": "other"
-    },
-    {
-      "title": "茅盾文学奖不应\"自相矛盾\"_正义网",
-      "url": "http://www.jcrb.com/culture/jianghu/mdwxj/index.html",
-      "source": "正义网",
-      "time": "",
-      "category": "other"
-    },
-    {
-      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
-      "url": "https://m.jiemian.com/article/3573232.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "86岁作家杨本芬“抄袭”争议未平,新作《母女》上架,出品方果麦...",
-      "url": "https://baijiahao.baidu.com/s?id=1858450325951921253&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "前天17:12",
-      "category": "voice"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
