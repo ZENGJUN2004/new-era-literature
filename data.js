b4a42c3429aca4ea41157c07ac66caf20a9a2b69
GitHub Action <action@github.com>
2026-03-03 09:31:21 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-03 16:28",
+  "update_time": "2026-03-03 17:31",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,39 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "中国当代文学研究会年度榜单(2025)发布",
+      "url": "http://www.thepaper.cn/newsDetail_forward_32320691",
+      "source": "澎湃新闻",
+      "time": "1月5日",
+      "category": "other"
+    },
+    {
+      "title": "长篇报告文学《与鹤一起飞》作品研讨会在京举办",
+      "url": "https://www.cenews.com.cn/news.html?aid=1770829",
+      "source": "中国环境网",
+      "time": "前天20:55",
+      "category": "meeting"
+    },
+    {
+      "title": "由想象到现实,机器人花了半个世纪的时间",
+      "url": "https://m.jiemian.com/article/3308356.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "让地方日常叙事透出科幻微光",
+      "url": "https://baijiahao.baidu.com/s?id=1858501070754666125&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "昨天06:38",
+      "category": "voice"
     }
   ]
 };
