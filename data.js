dec79fb25cdfcd822da5ba40ccbd77e199481034
GitHub Action <action@github.com>
2026-03-03 10:28:16 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-03 17:31",
+  "update_time": "2026-03-03 18:28",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,25 +23,32 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "中国当代文学研究会年度榜单(2025)发布",
-      "url": "http://www.thepaper.cn/newsDetail_forward_32320691",
-      "source": "澎湃新闻",
-      "time": "1月5日",
+      "title": "《收获》文学榜揭晓,榜单映射中国文坛迎来“丰年”",
+      "url": "https://baijiahao.baidu.com/s?id=1856184355598057672&wfr=spider&for=pc",
+      "source": "新华网",
+      "time": "2月4日",
       "category": "other"
     },
     {
-      "title": "长篇报告文学《与鹤一起飞》作品研讨会在京举办",
-      "url": "https://www.cenews.com.cn/news.html?aid=1770829",
-      "source": "中国环境网",
-      "time": "前天20:55",
-      "category": "meeting"
-    },
-    {
-      "title": "由想象到现实,机器人花了半个世纪的时间",
-      "url": "https://m.jiemian.com/article/3308356.html",
-      "source": "界面新闻",
+      "title": "茅盾文学奖不应\"自相矛盾\"_正义网",
+      "url": "http://www.jcrb.com/culture/jianghu/mdwxj/index.html",
+      "source": "正义网",
       "time": "",
       "category": "other"
+    },
+    {
+      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
+      "url": "https://m.jiemian.com/article/3573232.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "86岁作家杨本芬“抄袭”争议未平,新作《母女》上架,出品方果麦...",
+      "url": "https://baijiahao.baidu.com/s?id=1858450325951921253&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "前天17:12",
+      "category": "voice"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
@@ -49,13 +56,6 @@ window.LIT_DATA = {
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "让地方日常叙事透出科幻微光",
-      "url": "https://baijiahao.baidu.com/s?id=1858501070754666125&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "昨天06:38",
-      "category": "voice"
     }
   ]
 };
