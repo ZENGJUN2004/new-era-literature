e72bf5cfb0c6bec949f67b53d6af0a451f059552
GitHub Action <action@github.com>
2026-03-04 11:22:20 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-04 18:24",
+  "update_time": "2026-03-04 19:22",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,53 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "《繁花》出海,游本昌的“爷叔”、辛芷蕾的“李李”征服法国观众",
-      "url": "http://baijiahao.baidu.com/s?id=1858549464967485641&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "前天19:27",
-      "category": "other"
-    },
-    {
-      "title": "1997年,陈忠实当选作协主席后去省里开会,一位官员傲慢地问:“怎么...",
-      "url": "https://baijiahao.baidu.com/s?id=1858544020505995772&wfr=spider&for=pc",
-      "source": "海峡导报",
-      "time": "前天22:19",
-      "category": "other"
-    },
-    {
-      "title": "茅盾文学奖得主徐则臣受聘为连云港“读城”推荐官",
-      "url": "https://baijiahao.baidu.com/s?id=1857112032497137778&wfr=spider&for=pc",
-      "source": "金台资讯",
-      "time": "2月14日",
-      "category": "other"
-    },
-    {
-      "title": "...科学网]中国人民大学教授刘震云获第十五届意大利国际南北文学奖",
-      "url": "https://news.ruc.edu.cn/2028310563231342593.html",
-      "source": "中国人民大学新闻网",
-      "time": "前天",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "文娱新论|年代剧刷屏,为何观众还喊剧荒?",
-      "url": "https://baijiahao.baidu.com/s?id=1858712641287535177&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "3小时前",
-      "category": "meeting"
-    },
-    {
-      "title": "《繁花》在欧洲流媒体平台上线",
-      "url": "https://baijiahao.baidu.com/s?id=1858618773890926036&wfr=spider&for=pc",
-      "source": "中工网",
-      "time": "昨天10:18",
-      "category": "other"
     }
   ]
 };
