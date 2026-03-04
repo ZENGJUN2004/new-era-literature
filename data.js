9c9498022176dfb84eb58e7632678ecc1f57f404
GitHub Action <action@github.com>
2026-03-04 22:20:32 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-05 05:23",
+  "update_time": "2026-03-05 06:20",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,46 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "人民文学出版社致歉,《经典常谈》校订已完成",
-      "url": "https://baijiahao.baidu.com/s?id=1856136464442507613&wfr=spider&for=pc",
-      "source": "光明日报",
-      "time": "2月3日",
-      "category": "activity"
-    },
-    {
-      "title": "“新春走基层—春天送你一首诗”系列活动走进陕西铜川",
-      "url": "https://cul.sohu.com/a/992168729_121119377",
-      "source": "搜狐网",
-      "time": "前天20:34",
-      "category": "other"
-    },
-    {
-      "title": "沮水文苑丨袁英民:陕西快板《人民文学耀州行》",
-      "url": "https://cul.sohu.com/a/991826855_121124746",
-      "source": "搜狐网",
-      "time": "前天06:02",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "全国政协常委迟子建:所有的阅读方式都是对知识的抵达,值得尊重",
-      "url": "https://baijiahao.baidu.com/s?id=1858719623263414305&wfr=spider&for=pc",
-      "source": "千龙网",
-      "time": "昨天16:32",
-      "category": "other"
-    },
-    {
-      "title": "专访全国政协常委迟子建:所有的阅读方式都是对知识的抵达,值得尊重",
-      "url": "http://news.10jqka.com.cn/20260304/c675069899.shtml",
-      "source": "同花顺财经",
-      "time": "昨天16:16",
-      "category": "other"
     }
   ]
 };
