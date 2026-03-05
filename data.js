3cd4d7cf18aa982c59796618fd188ebe131e65be
GitHub Action <action@github.com>
2026-03-05 21:23:37 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-06 04:29",
+  "update_time": "2026-03-06 05:23",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,27 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "媒体眼中海大的2025年!",
+      "url": "https://baijiahao.baidu.com/s?id=1856159417055495057&wfr=spider&for=pc",
+      "source": "中国海洋大学",
+      "time": "2月4日",
+      "category": "other"
+    },
+    {
+      "title": "算法参战:硅谷最后的良心,倒在周五下午5点01分",
+      "url": "https://baijiahao.baidu.com/s?id=1858717195495893391&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "前天15:54",
+      "category": "other"
+    },
+    {
+      "title": "大型历史文献纪录片《信仰的力量》研讨会--党史频道--人民网",
+      "url": "http://dangshi.people.com.cn/GB/151935/406058/index.html",
+      "source": "人民网",
+      "time": "",
+      "category": "meeting"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -30,11 +51,18 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
-      "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "1月10日",
+      "title": "王啸峰:魔音耳机 | 《花城》短篇小说",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32703704",
+      "source": "澎湃新闻",
+      "time": "昨天13:40",
       "category": "activity"
+    },
+    {
+      "title": "赵燕飞散文集《脉与络》研讨会在京举行",
+      "url": "https://baijiahao.baidu.com/s?id=1858786753550146846&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "昨天10:14",
+      "category": "meeting"
     }
   ]
 };
