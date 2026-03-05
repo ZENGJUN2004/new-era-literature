e223ee2ee59791afb6c3c88b4ab66f673cd7bf81
GitHub Action <action@github.com>
2026-03-05 08:27:58 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-05 15:35",
+  "update_time": "2026-03-05 16:27",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,39 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "中国当代文学研究会年度榜单(2025)发布",
-      "url": "http://www.thepaper.cn/newsDetail_forward_32317986",
-      "source": "澎湃新闻",
-      "time": "1月5日",
-      "category": "other"
-    },
-    {
-      "title": "《准点开讲》全民思政课:3月7日,黄春校长分享“关乎成长的十八件...",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32693002",
-      "source": "澎湃新闻",
-      "time": "前天14:39",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "要想写作,就先生活——柳青手稿中的人生境界|纪念柳青110周年诞辰",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32694236",
-      "source": "澎湃新闻",
-      "time": "昨天15:06",
-      "category": "other"
-    },
-    {
-      "title": "“写作就是去当街溜子,遇见有情有义的人” | 作家孙睿",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32691631",
-      "source": "澎湃新闻",
-      "time": "昨天16:12",
-      "category": "other"
     }
   ]
 };
