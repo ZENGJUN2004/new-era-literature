e1b33c77bdddf210f75524faaaf8df76bf0800e9
GitHub Action <action@github.com>
2026-03-05 23:43:44 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-06 06:20",
+  "update_time": "2026-03-06 07:43",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,46 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "茅盾文学奖不应\"自相矛盾\"_正义网",
-      "url": "http://www.jcrb.com/culture/jianghu/mdwxj/index.html",
-      "source": "正义网",
-      "time": "",
-      "category": "other"
-    },
-    {
-      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
-      "url": "https://m.jiemian.com/article/3573232.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "voice"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
-      "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "1月10日",
-      "category": "activity"
-    },
-    {
-      "title": "创新是作家活力的展现",
-      "url": "https://baijiahao.baidu.com/s?id=1858832198542749955&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "昨天22:22",
-      "category": "other"
-    },
-    {
-      "title": "她靠“老辈子文学”火成顶流作家,被锤抄袭后还有多少大佬在隐身?",
-      "url": "https://www.huxiu.com/article/4839505.html",
-      "source": "虎嗅APP",
-      "time": "昨天22:44",
-      "category": "other"
     }
   ]
 };
