6b2b87a95163091521177421977dbb5b11e555f8
GitHub Action <action@github.com>
2026-03-05 14:38:09 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-05 21:54",
+  "update_time": "2026-03-05 22:38",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,66 +23,10 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "由想象到现实,机器人花了半个世纪的时间",
-      "url": "https://m.jiemian.com/article/3308356.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "新加坡著名慈善家、作家卓顺发《太阳出来了,露水不见了》新书发布",
-      "url": "https://cul.sohu.com/a/976352761_121192997",
-      "source": "搜狐网",
-      "time": "1月15日",
-      "category": "activity"
-    },
-    {
-      "title": "盘州本土作家创作的长篇历史纪实小说《乌蒙铁壁》新书发布",
-      "url": "https://baijiahao.baidu.com/s?id=1854571604814312489&wfr=spider&for=pc",
-      "source": "天眼新闻",
-      "time": "1月17日",
-      "category": "activity"
-    },
-    {
-      "title": "上话改编《82年生的金智英》梁咏琪领衔,话剧《我们成为的她》7月...",
-      "url": "https://baijiahao.baidu.com/s?id=1858805252678642421&wfr=spider&for=pc",
-      "source": "新闻晨报",
-      "time": "6小时前",
-      "category": "other"
-    },
-    {
-      "title": "经典唤新 激活文化市场“一池春水”",
-      "url": "https://baijiahao.baidu.com/s?id=1858789237977220474&wfr=spider&for=pc",
-      "source": "光明网",
-      "time": "10小时前",
-      "category": "other"
-    },
-    {
-      "title": "知名作家新作上市读者排队抢购",
-      "url": "https://cul.sohu.com/a/992757833_122279996",
-      "source": "搜狐网",
-      "time": "10小时前",
-      "category": "activity"
-    },
-    {
-      "title": "“写作就是去当街溜子,遇见有情有义的人” | 作家孙睿",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32691631",
-      "source": "澎湃新闻",
-      "time": "昨天16:12",
-      "category": "other"
-    },
-    {
-      "title": "“巴金小说系列·文库本”发布",
-      "url": "https://baijiahao.baidu.com/s?id=1858689023048209639&wfr=spider&for=pc",
-      "source": "金台资讯",
-      "time": "昨天07:56",
       "category": "activity"
     }
   ]
