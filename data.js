61b7fff0be958eb713433c0c3420cef20d7d051f
GitHub Action <action@github.com>
2026-03-06 07:32:08 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-06 14:34",
+  "update_time": "2026-03-06 15:32",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,109 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "两会好声音|汤素兰:建议恢复毛泽东文学奖、湖南省青年文学奖",
-      "url": "https://baijiahao.baidu.com/s?id=1856189106319101679&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "2月4日",
-      "category": "other"
-    },
-    {
-      "title": "女性为何写作?她们用文字打破边界",
-      "url": "http://baijiahao.baidu.com/s?id=1855001128564567122&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "1月22日",
-      "category": "other"
-    },
-    {
-      "title": "以小说笔法打捞日常的微光:从《脉与络》看散文创作",
-      "url": "http://www.thepaper.cn/newsDetail_forward_32691831",
-      "source": "澎湃新闻",
-      "time": "前天15:27",
-      "category": "activity"
-    },
-    {
-      "title": "豆瓣阅读和晋江文学城,开年卖出了什么IP?",
-      "url": "https://www.huxiu.com/article/4838848.html",
-      "source": "虎嗅APP",
-      "time": "前天10:57",
-      "category": "other"
-    },
-    {
-      "title": "迈向跨界融合的“大文学”时代",
-      "url": "http://www.xinhuanet.com/book/20260120/d3fa6fd5f93048fc88a2d3be4ef48224/c.html",
-      "source": "新华网",
-      "time": "1月20日",
-      "category": "other"
-    },
-    {
-      "title": "媒体眼中海大的2025年!",
-      "url": "https://baijiahao.baidu.com/s?id=1856159417055495057&wfr=spider&for=pc",
-      "source": "中国海洋大学",
-      "time": "2月4日",
-      "category": "other"
-    },
-    {
-      "title": "算法参战:硅谷最后的良心,倒在周五下午5点01分",
-      "url": "https://baijiahao.baidu.com/s?id=1858717195495893391&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "前天15:54",
-      "category": "other"
-    },
-    {
-      "title": "大型历史文献纪录片《信仰的力量》研讨会--党史频道--人民网",
-      "url": "http://dangshi.people.com.cn/GB/151935/406058/index.html",
-      "source": "人民网",
-      "time": "",
-      "category": "meeting"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "一周文化讲座|寻找女性灵魂深处的力量",
-      "url": "https://baijiahao.baidu.com/s?id=1858880338874296385&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "3小时前",
-      "category": "meeting"
-    },
-    {
-      "title": "他打开了流量密码,却拒绝了洪水般的流量|纪念罗曼·罗兰",
-      "url": "https://www.yicai.com/news/103073213.html",
-      "source": "第一财经",
-      "time": "4小时前",
-      "category": "other"
-    },
-    {
-      "title": "新书预告丨“我要讲的就是那些让所有女性都噤若寒蝉的事情”",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32702506",
-      "source": "澎湃新闻",
-      "time": "昨天13:43",
-      "category": "activity"
-    },
-    {
-      "title": "她如何写作“学界小说”?|专访苏枕书",
-      "url": "https://baijiahao.baidu.com/s?id=1858805975635583938&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "昨天15:02",
-      "category": "activity"
-    },
-    {
-      "title": "王啸峰:魔音耳机 | 《花城》短篇小说",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32703704",
-      "source": "澎湃新闻",
-      "time": "昨天13:40",
-      "category": "activity"
-    },
-    {
-      "title": "赵燕飞散文集《脉与络》研讨会在京举行",
-      "url": "https://baijiahao.baidu.com/s?id=1858786753550146846&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "昨天10:14",
-      "category": "meeting"
     }
   ]
 };
