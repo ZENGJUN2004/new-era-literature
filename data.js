584c30e5848028251bb5602921d46658e68d5996
GitHub Action <action@github.com>
2026-03-06 03:59:42 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-06 09:08",
+  "update_time": "2026-03-06 11:59",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,67 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "女性为何写作?她们用文字打破边界",
+      "url": "http://baijiahao.baidu.com/s?id=1855001128564567122&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "1月22日",
+      "category": "other"
+    },
+    {
+      "title": "以小说笔法打捞日常的微光:从《脉与络》看散文创作",
+      "url": "http://www.thepaper.cn/newsDetail_forward_32691831",
+      "source": "澎湃新闻",
+      "time": "前天15:27",
+      "category": "activity"
+    },
+    {
+      "title": "豆瓣阅读和晋江文学城,开年卖出了什么IP?",
+      "url": "https://www.huxiu.com/article/4838848.html",
+      "source": "虎嗅APP",
+      "time": "前天10:57",
+      "category": "other"
+    },
+    {
+      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
+      "url": "https://m.jiemian.com/article/3573232.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "voice"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "一周文化讲座|寻找女性灵魂深处的力量",
+      "url": "https://baijiahao.baidu.com/s?id=1858880338874296385&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "53分钟前",
+      "category": "meeting"
+    },
+    {
+      "title": "新书预告丨“我要讲的就是那些让所有女性都噤若寒蝉的事情”",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32702506",
+      "source": "澎湃新闻",
+      "time": "昨天13:43",
+      "category": "activity"
+    },
+    {
+      "title": "她如何写作“学界小说”?|专访苏枕书",
+      "url": "https://baijiahao.baidu.com/s?id=1858805975635583938&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "昨天15:02",
+      "category": "activity"
+    },
+    {
+      "title": "扫码阅读 | 三八妇女节,致敬每一位自在生长的女性",
+      "url": "http://www.thepaper.cn/newsDetail_forward_32708180",
+      "source": "澎湃新闻",
+      "time": "昨天17:28",
+      "category": "other"
     }
   ]
 };
