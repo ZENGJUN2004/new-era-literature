573c1d6310b1d1ceb399a5216050e3ccdcaa9414
GitHub Action <action@github.com>
2026-03-06 15:26:42 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-06 22:28",
+  "update_time": "2026-03-06 23:26",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,10 +23,10 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "流氓作家“贾平凹再次语出惊人:“什么同事、同学、朋友,都是人生...",
-      "url": "https://baijiahao.baidu.com/s?id=1858745440662664018&wfr=spider&for=pc",
-      "source": "台海网",
-      "time": "前天23:23",
+      "title": "她说|戴锦华:做现实主义者,求不可能之事",
+      "url": "https://baijiahao.baidu.com/s?id=1855465476132764191&wfr=spider&for=pc",
+      "source": "新闻晨报",
+      "time": "1月27日",
       "category": "other"
     },
     {
@@ -35,34 +35,6 @@ window.LIT_DATA = {
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "茅奖作家乔叶划重点,这张“春风女性书单”致敬她们的表达",
-      "url": "https://baijiahao.baidu.com/s?id=1858905379786849994&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "4小时前",
-      "category": "other"
-    },
-    {
-      "title": "从西游文脉到海河联运!连云港、淮安共赴“双城之约”",
-      "url": "https://baijiahao.baidu.com/s?id=1858881848622414677&wfr=spider&for=pc",
-      "source": "上游新闻",
-      "time": "12小时前",
-      "category": "other"
-    },
-    {
-      "title": "年代剧刷屏,为何观众还喊剧荒?",
-      "url": "https://baijiahao.baidu.com/s?id=1858788672425258954&wfr=spider&for=pc",
-      "source": "正观新闻",
-      "time": "昨天10:10",
-      "category": "other"
-    },
-    {
-      "title": "创新是作家活力的展现",
-      "url": "http://baijiahao.baidu.com/s?id=1858831786824095181&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "昨天22:15",
-      "category": "other"
     }
   ]
 };
