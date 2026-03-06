03764a5b5d88651b99b3b416bdd29fab6b815cdb
GitHub Action <action@github.com>
2026-03-06 11:20:29 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-06 18:24",
+  "update_time": "2026-03-06 19:20",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,41 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "文学双周记|22家刊物答原创之问,邓紫棋入选银河奖引热议",
-      "url": "https://baijiahao.baidu.com/s?id=1853442602393722283&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "1月5日",
-      "category": "other"
-    },
-    {
-      "title": "赵德发长篇小说《大海风》入选中国当代文学研究会年度榜单(2025)",
-      "url": "https://baijiahao.baidu.com/s?id=1853465599041956599&wfr=spider&for=pc",
-      "source": "闪电新闻",
-      "time": "1月5日",
-      "category": "activity"
-    },
-    {
-      "title": "媒体眼中海大的2025年!",
-      "url": "https://baijiahao.baidu.com/s?id=1856159417055495057&wfr=spider&for=pc",
-      "source": "中国海洋大学",
-      "time": "2月4日",
-      "category": "other"
-    },
-    {
-      "title": "算法参战:硅谷最后的良心,倒在周五下午5点01分",
-      "url": "https://baijiahao.baidu.com/s?id=1858717195495893391&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "前天15:54",
-      "category": "other"
-    },
-    {
-      "title": "大型历史文献纪录片《信仰的力量》研讨会--党史频道--人民网",
-      "url": "http://dangshi.people.com.cn/GB/151935/406058/index.html",
-      "source": "人民网",
-      "time": "",
-      "category": "meeting"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -70,20 +35,6 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
-    },
-    {
-      "title": "王啸峰:魔音耳机 | 《花城》短篇小说",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32703704",
-      "source": "澎湃新闻",
-      "time": "昨天13:40",
-      "category": "activity"
-    },
-    {
-      "title": "赵燕飞散文集《脉与络》研讨会在京举行",
-      "url": "https://baijiahao.baidu.com/s?id=1858786753550146846&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "昨天10:14",
-      "category": "meeting"
     }
   ]
 };
