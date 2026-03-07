da6ff59a9b2646689995f9af5d826ded10b3ad27
GitHub Action <action@github.com>
2026-03-07 14:14:53 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-07 21:32",
+  "update_time": "2026-03-07 22:14",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,67 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "名社集萃丨人民文学出版社:生产人民的文学",
+      "url": "https://cul.sohu.com/a/992721208_121106908",
+      "source": "搜狐网",
+      "time": "前天10:18",
+      "category": "activity"
+    },
+    {
+      "title": "“知名作家看法院”采风作品:春天里的法度与温度",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32709100",
+      "source": "澎湃新闻",
+      "time": "前天18:37",
+      "category": "other"
+    },
+    {
+      "title": "创新是作家活力的展现",
+      "url": "https://baijiahao.baidu.com/s?id=1858831769060869523&wfr=spider&for=pc",
+      "source": "文汇报",
+      "time": "前天22:15",
+      "category": "other"
+    },
+    {
+      "title": "向葡语国家传播中国文学《人民文学》葡文版首刊正式发行",
+      "url": "https://baijiahao.baidu.com/s?id=1857935327681773426&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "2月24日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
+      "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "1月10日",
+      "category": "activity"
+    },
+    {
+      "title": "当女性拿起笔,科幻终于有了“身体”|翻翻书·送书",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32705824",
+      "source": "澎湃新闻",
+      "time": "昨天17:03",
+      "category": "voice"
+    },
+    {
+      "title": "阅读的美丽瞬间(文思)",
+      "url": "https://baijiahao.baidu.com/s?id=1858893330478854532&wfr=spider&for=pc",
+      "source": "中国青年网",
+      "time": "昨天14:29",
+      "category": "other"
+    },
+    {
+      "title": "东西问丨作家梁鸿:为何说“中国在梁庄”?",
+      "url": "https://baijiahao.baidu.com/s?id=1858918088453739862&wfr=spider&for=pc",
+      "source": "中国新闻网",
+      "time": "昨天21:04",
+      "category": "other"
     }
   ]
 };
