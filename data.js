38e66b9798410b56c2aebdf3c244d3dee96b82d5
GitHub Action <action@github.com>
2026-03-08 08:18:13 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-08 15:24",
+  "update_time": "2026-03-08 16:18",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,20 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "女性为何写作?她们用文字打破边界",
+      "url": "https://baijiahao.baidu.com/s?id=1855001128564567122&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "1月22日",
+      "category": "other"
+    },
+    {
+      "title": "一周文化讲座|寻找女性灵魂深处的力量",
+      "url": "https://baijiahao.baidu.com/s?id=1858880338874296385&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "前天11:05",
+      "category": "meeting"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -35,6 +49,27 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
+    },
+    {
+      "title": "武汉“名家论坛”妇女节专题开讲:从法理解读《红楼梦》中的女性命运",
+      "url": "https://baijiahao.baidu.com/s?id=1859067572575798771&wfr=spider&for=pc",
+      "source": "九派新闻",
+      "time": "3小时前",
+      "category": "meeting"
+    },
+    {
+      "title": "“女性爽片”的出现,到底是解药还是麻药?|编辑部聊天室",
+      "url": "https://baijiahao.baidu.com/s?id=1859068705682201721&wfr=spider&for=pc",
+      "source": "界面新闻",
+      "time": "3小时前",
+      "category": "other"
+    },
+    {
+      "title": "上海宋庆龄基金会女性创造力分享会暨《上场:女性艺术家访谈录...",
+      "url": "https://baijiahao.baidu.com/s?id=1858976472615716019&wfr=spider&for=pc",
+      "source": "东方网",
+      "time": "昨天09:12",
+      "category": "other"
     }
   ]
 };
