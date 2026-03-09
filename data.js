645d11012a3c965dca04f8d70245bf03082e439c
GitHub Action <action@github.com>
2026-03-09 12:33:33 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-09 19:27",
+  "update_time": "2026-03-09 20:33",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,34 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "故乡对文学意味着什么?离开才能拥有故乡?",
+      "url": "http://baijiahao.baidu.com/s?id=1858936642021052760&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "前天02:01",
+      "category": "other"
+    },
+    {
+      "title": "茅盾文学奖得主徐则臣受聘为连云港“读城”推荐官",
+      "url": "https://baijiahao.baidu.com/s?id=1857112032497137778&wfr=spider&for=pc",
+      "source": "金台资讯",
+      "time": "2月14日",
+      "category": "other"
+    },
+    {
+      "title": "【钛晨报】国家医保局:医保药品目录调整专家评审工作已结束;把...",
+      "url": "https://www.tmtpost.com/6283071.html",
+      "source": "钛媒体官方网站",
+      "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "微短剧凭啥跨越千亿门槛?",
+      "url": "https://baijiahao.baidu.com/s?id=1859012929188057481&wfr=spider&for=pc",
+      "source": "人民政协网",
+      "time": "前天22:14",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -35,6 +63,48 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
+    },
+    {
+      "title": "面对重复和模仿,作家的创新何以如此之难",
+      "url": "https://www.huxiu.com/article/4840442.html",
+      "source": "虎嗅APP",
+      "time": "2小时前",
+      "category": "other"
+    },
+    {
+      "title": "《白鹿原》-陈忠实",
+      "url": "https://baijiahao.baidu.com/s?id=1859170572731186029&wfr=spider&for=pc",
+      "source": "大象新闻",
+      "time": "4小时前",
+      "category": "other"
+    },
+    {
+      "title": "文学照进现实!茅盾文学奖作品经典场景“忘忧茶庄”落地临安",
+      "url": "https://baijiahao.baidu.com/s?id=1859165127156452724&wfr=spider&for=pc",
+      "source": "浙江日报",
+      "time": "5小时前",
+      "category": "other"
+    },
+    {
+      "title": "大型话剧《平凡的世界》本月在福州上演",
+      "url": "https://baijiahao.baidu.com/s?id=1859151563526138243&wfr=spider&for=pc",
+      "source": "东南网",
+      "time": "9小时前",
+      "category": "other"
+    },
+    {
+      "title": "“一片叶子”落地钱王故里,茅盾文学奖得主王旭烽把小说里的忘忧...",
+      "url": "http://news.china.com.cn/2026-03/09/content_118371353.shtml",
+      "source": "中国网新闻中心",
+      "time": "10小时前",
+      "category": "activity"
+    },
+    {
+      "title": "如何真正回归大众视野?三位戏剧人给出他们的回答",
+      "url": "https://baijiahao.baidu.com/s?id=1859062539887379030&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "昨天11:20",
+      "category": "other"
     }
   ]
 };
