b7bcafef17f377a31860bc9734749afa95e42699
GitHub Action <action@github.com>
2026-03-09 08:31:48 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-09 15:43",
+  "update_time": "2026-03-09 16:31",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,60 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "文学双周记|22家刊物答原创之问,邓紫棋入选银河奖引热议",
-      "url": "https://baijiahao.baidu.com/s?id=1853442602393722283&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "1月5日",
-      "category": "other"
-    },
-    {
-      "title": "茅盾文学奖得主徐则臣受聘为连云港“读城”推荐官",
-      "url": "https://baijiahao.baidu.com/s?id=1857112032497137778&wfr=spider&for=pc",
-      "source": "金台资讯",
-      "time": "2月14日",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
-      "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "1月10日",
-      "category": "activity"
-    },
-    {
-      "title": "文学照进现实!茅盾文学奖作品经典场景“忘忧茶庄”落地临安",
-      "url": "https://baijiahao.baidu.com/s?id=1859165127156452724&wfr=spider&for=pc",
-      "source": "浙江日报",
-      "time": "1小时前",
-      "category": "other"
-    },
-    {
-      "title": "大型话剧《平凡的世界》本月在福州上演",
-      "url": "https://baijiahao.baidu.com/s?id=1859151563526138243&wfr=spider&for=pc",
-      "source": "东南网",
-      "time": "5小时前",
-      "category": "other"
-    },
-    {
-      "title": "“一片叶子”落地钱王故里,茅盾文学奖得主王旭烽把小说里的忘忧...",
-      "url": "http://news.china.com.cn/2026-03/09/content_118371353.shtml",
-      "source": "中国网新闻中心",
-      "time": "6小时前",
-      "category": "activity"
-    },
-    {
-      "title": "如何真正回归大众视野?三位戏剧人给出他们的回答",
-      "url": "https://baijiahao.baidu.com/s?id=1859062539887379030&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "昨天11:20",
-      "category": "other"
     }
   ]
 };
