f7dc7cf086af998aa368d035e3f60b0bcdaa2a25
GitHub Action <action@github.com>
2026-03-09 15:45:44 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-09 22:44",
+  "update_time": "2026-03-09 23:45",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -35,27 +35,6 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
-    },
-    {
-      "title": "陈丹燕:上海给了我很多,我想为它做些什么",
-      "url": "https://www.jfdaily.com/news/detail?id=1078073",
-      "source": "上观",
-      "time": "昨天14:49",
-      "category": "other"
-    },
-    {
-      "title": "从农村儿媳到大学教授 作家黄灯:在“普通”里看见光亮|她自有光",
-      "url": "https://baijiahao.baidu.com/s?id=1859053322390914170&wfr=spider&for=pc",
-      "source": "封面新闻",
-      "time": "昨天08:56",
-      "category": "other"
-    },
-    {
-      "title": "全民“养龙虾”时代,我们为什么写作?",
-      "url": "https://baijiahao.baidu.com/s?id=1859084748767619126&wfr=spider&for=pc",
-      "source": "新京报",
-      "time": "昨天17:16",
-      "category": "other"
     }
   ]
 };
