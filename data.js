e3fb2db6afc41b216cb3c0942c9e79f92bec94c2
GitHub Action <action@github.com>
2026-03-09 10:34:37 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-09 17:36",
+  "update_time": "2026-03-09 18:34",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,13 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "《人民文学》葡文版首刊正式发行",
+      "url": "https://baijiahao.baidu.com/s?id=1856707364268271148&wfr=spider&for=pc",
+      "source": "海外网",
+      "time": "2月10日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -35,6 +42,20 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
+    },
+    {
+      "title": "面对重复和模仿,作家的创新何以如此之难",
+      "url": "https://www.huxiu.com/article/4840442.html",
+      "source": "虎嗅APP",
+      "time": "53分钟前",
+      "category": "other"
+    },
+    {
+      "title": "张莉来津对谈张楚,两位鲁奖得主带领读者重读萧红,感知“她走过无...",
+      "url": "http://news.enorth.com.cn/system/2026/03/09/059209041.shtml",
+      "source": "北方网",
+      "time": "8小时前",
+      "category": "other"
     }
   ]
 };
