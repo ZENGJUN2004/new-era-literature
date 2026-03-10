767e7ce6d49c0cac374abb4b8d296b24dfe43a68
GitHub Action <action@github.com>
2026-03-10 13:54:52 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-10 20:33",
+  "update_time": "2026-03-10 21:54",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,27 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "今天我们过节,靠劳动做自己的王,做自己的神",
+      "url": "https://www.huxiu.com/article/4840215.html",
+      "source": "虎嗅APP",
+      "time": "前天22:54",
+      "category": "other"
+    },
+    {
+      "title": "“女性爽片”的出现,到底是解药还是麻药?|编辑部聊天室",
+      "url": "https://baijiahao.baidu.com/s?id=1859068705682201721&wfr=spider&for=pc",
+      "source": "界面新闻",
+      "time": "前天12:58",
+      "category": "other"
+    },
+    {
+      "title": "妇女节快乐 | 一份女性作家的书单",
+      "url": "https://cul.sohu.com/a/993825431_121119369",
+      "source": "搜狐网",
+      "time": "前天10:07",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -35,6 +56,55 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
+    },
+    {
+      "title": "《苔丝》珍藏版阅读分享会:在变动的生活中守护美好、纯粹与坚韧",
+      "url": "https://baijiahao.baidu.com/s?id=1859264626454273349&wfr=spider&for=pc",
+      "source": "浙江日报",
+      "time": "4小时前",
+      "category": "other"
+    },
+    {
+      "title": "杨焄|白雪公主的中国行_上海书评_澎湃新闻-The Paper",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32726966",
+      "source": "澎湃新闻",
+      "time": "11小时前",
+      "category": "other"
+    },
+    {
+      "title": "梁咏琪首登内地话剧舞台!《我们成为的她》3月20日全网开票",
+      "url": "http://www.thepaper.cn/newsDetail_forward_32732655",
+      "source": "澎湃新闻",
+      "time": "昨天07:53",
+      "category": "other"
+    },
+    {
+      "title": "回应历史叩问 唱响时代清音(文化只眼)",
+      "url": "http://ent.people.com.cn/n1/2026/0309/c1012-40677875.html",
+      "source": "人民网",
+      "time": "昨天08:52",
+      "category": "other"
+    },
+    {
+      "title": "宋代美学的哲学根基与生活意蕴_澎湃号·湃客_澎湃新闻-The Paper",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32731378",
+      "source": "澎湃新闻",
+      "time": "昨天12:09",
+      "category": "other"
+    },
+    {
+      "title": "戴锦华讲电影:关于“性别”与“凝视”,你想知道的都在这",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32730938",
+      "source": "澎湃新闻",
+      "time": "昨天12:44",
+      "category": "other"
+    },
+    {
+      "title": "当写作的女人拿回自己的名字_澎湃号·湃客_澎湃新闻-The Paper",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32728918",
+      "source": "澎湃新闻",
+      "time": "昨天12:44",
+      "category": "other"
     }
   ]
 };
