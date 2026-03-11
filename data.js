c8444bc2048d69791d3e4cfc8c371ab3c54e6324
GitHub Action <action@github.com>
2026-03-11 10:28:15 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-11 17:32",
+  "update_time": "2026-03-11 18:28",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,27 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "媒体眼中海大的2025年!",
+      "url": "https://baijiahao.baidu.com/s?id=1856159417055495057&wfr=spider&for=pc",
+      "source": "中国海洋大学",
+      "time": "2月4日",
+      "category": "other"
+    },
+    {
+      "title": "大型历史文献纪录片《信仰的力量》研讨会--党史频道--人民网",
+      "url": "http://dangshi.people.com.cn/GB/151935/406058/index.html",
+      "source": "人民网",
+      "time": "",
+      "category": "meeting"
+    },
+    {
+      "title": "2026年儿童杂志红黑榜!这些订阅了三年以上的,才是真宝藏",
+      "url": "https://learning.sohu.com/a/990940752_121124723",
+      "source": "搜狐网",
+      "time": "2月28日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -34,6 +55,27 @@ window.LIT_DATA = {
       "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
       "source": "红网",
       "time": "1月10日",
+      "category": "activity"
+    },
+    {
+      "title": "赵涛:资水流深——廖静仁印象",
+      "url": "https://baijiahao.baidu.com/s?id=1859334864955812536&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "6小时前",
+      "category": "other"
+    },
+    {
+      "title": "美文欣赏|浸峪村有棵千年夫妻树",
+      "url": "https://baijiahao.baidu.com/s?id=1859267923842472315&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "昨天17:45",
+      "category": "other"
+    },
+    {
+      "title": "新刊|《十月》2026年第2期目录",
+      "url": "https://cul.sohu.com/a/994860834_121124729",
+      "source": "搜狐网",
+      "time": "昨天18:59",
       "category": "activity"
     }
   ]
