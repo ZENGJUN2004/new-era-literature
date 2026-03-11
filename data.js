93e78007d4f63e05e8f74306e3f2be7eae0ac28e
GitHub Action <action@github.com>
2026-03-11 20:23:09 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-12 03:35",
+  "update_time": "2026-03-12 04:23",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,67 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "凭借“独到的哲思和幽默”,刘震云成为首位获意大利国际南北文学奖...",
-      "url": "https://www.jfdaily.com/wx/detail.do?id=1071489",
-      "source": "上观",
-      "time": "2月23日",
-      "category": "other"
-    },
-    {
-      "title": "四川唯一!内江一项目获中国作协重点扶持",
-      "url": "https://baijiahao.baidu.com/s?id=1859285363386558116&wfr=spider&for=pc",
-      "source": "最内江",
-      "time": "前天19:06",
-      "category": "other"
-    },
-    {
-      "title": "独家对话马伯庸:每一部作品都是清零后重新开始",
-      "url": "https://baijiahao.baidu.com/s?id=1859232755073096936&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "前天08:27",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
-      "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "1月10日",
-      "category": "activity"
-    },
-    {
-      "title": "梁晓声谈《我的山与海》原著:为了弥补《人世间》的心结",
-      "url": "https://baijiahao.baidu.com/s?id=1859349748317551305&wfr=spider&for=pc",
-      "source": "红星新闻",
-      "time": "昨天15:28",
-      "category": "other"
-    },
-    {
-      "title": "作家麦家作品《解密》《暗算》入列“企鹅当代经典”",
-      "url": "https://baijiahao.baidu.com/s?id=1859370001902608466&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "昨天20:47",
-      "category": "other"
-    },
-    {
-      "title": "麦家作品译介展亮相伦敦书展",
-      "url": "https://baijiahao.baidu.com/s?id=1859361699265329631&wfr=spider&for=pc",
-      "source": "红星新闻",
-      "time": "昨天18:38",
-      "category": "other"
-    },
-    {
-      "title": "跟随作家阿来,走近更真实、温暖、辽阔的苏东坡",
-      "url": "https://baijiahao.baidu.com/s?id=1859353205103773233&wfr=spider&for=pc",
-      "source": "大众日报",
-      "time": "昨天16:23",
-      "category": "other"
     }
   ]
 };
