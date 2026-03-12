42348a1fb0c26e0b427d690f486d5f17720498b8
GitHub Action <action@github.com>
2026-03-12 03:29:13 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-12 08:57",
+  "update_time": "2026-03-12 11:29",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,6 +21,104 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
+    },
+    {
+      "title": "大众写科幻,带来了什么?",
+      "url": "http://baijiahao.baidu.com/s?id=1853855131427137219&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "1月9日",
+      "category": "voice"
+    },
+    {
+      "title": "如何把一个想法写成畅销书?美国顶尖非虚构作家的写作课",
+      "url": "https://www.lifeweek.com.cn/article/13495",
+      "source": "三联生活周刊",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
+      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
+      "url": "https://m.jiemian.com/article/3573232.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "这一场文学跨年,作家带大家一起重温文学故乡,述说文学跋涉",
+      "url": "https://www.takefoto.cn/news/2026/01/02/11500412.shtml",
+      "source": "北晚在线",
+      "time": "1月2日",
+      "category": "other"
+    },
+    {
+      "title": "文学年景 精彩纷呈——2025年文学阅读",
+      "url": "https://news.gmw.cn/2026-01/17/content_38542176.htm",
+      "source": "光明网新闻中心",
+      "time": "1月17日",
+      "category": "other"
+    },
+    {
+      "title": "迈向跨界融合的“大文学”时代",
+      "url": "http://www.xinhuanet.com/book/20260120/d3fa6fd5f93048fc88a2d3be4ef48224/c.html",
+      "source": "新华网",
+      "time": "1月20日",
+      "category": "other"
+    },
+    {
+      "title": "范迁:聚光灯外的文学潜行者",
+      "url": "https://cul.sohu.com/a/994487188_121124717",
+      "source": "搜狐网",
+      "time": "前天08:03",
+      "category": "other"
+    },
+    {
+      "title": "2026年,这些华语文学新书要来了(一)",
+      "url": "https://baijiahao.baidu.com/s?id=1853356377637123634&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "1月4日",
+      "category": "activity"
+    },
+    {
+      "title": "一周文艺:“文学·中国”盛典举办,《中国奇谭2》上线,邓紫棋入...",
+      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1046912&sid=200",
+      "source": "上观",
+      "time": "1月5日",
+      "category": "other"
+    },
+    {
+      "title": "2025春风月榜半年报告:希望这22本佳作为你带来光",
+      "url": "https://baijiahao.baidu.com/s?id=1853802033490694012&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "1月9日",
+      "category": "other"
+    },
+    {
+      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
+      "url": "https://www.jfdaily.com/wx/detail.do?id=1050469",
+      "source": "上观",
+      "time": "1月12日",
+      "category": "activity"
+    },
+    {
+      "title": "2026年,这些新书值得我们期待(上)",
+      "url": "https://baijiahao.baidu.com/s?id=1854440677511475404&wfr=spider&for=pc",
+      "source": "界面新闻",
+      "time": "1月16日",
+      "category": "activity"
+    },
+    {
+      "title": "成为《人民文学》头条的报告文学,需要历练什么样的脚力与笔力?",
+      "url": "http://baijiahao.baidu.com/s?id=1726635185081314797&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "other"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
