ef0110f84a34d5071e612ced3a49fd26a34b8a83
GitHub Action <action@github.com>
2026-03-12 07:39:04 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-12 14:39",
+  "update_time": "2026-03-12 15:39",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -30,59 +30,10 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "西安发现明秦藩王府门正官员碑 填补历史研究空白",
-      "url": "https://www.chinanews.com.cn/cul/2026/03-10/10584634.shtml",
-      "source": "中国新闻网",
-      "time": "前天18:19",
-      "category": "other"
-    },
-    {
-      "title": "丹青红棉映羊城60余位画家定格英雄花与广州地标之美",
-      "url": "https://www.chinanews.com.cn/cul/2026/03-10/10584662.shtml",
-      "source": "中国新闻网",
-      "time": "前天19:22",
-      "category": "other"
-    },
-    {
-      "title": "儿童打击乐音乐剧《花木兰》融传统文化与现代艺术讲述中国故事",
-      "url": "https://www.chinanews.com.cn/cul/2026/03-10/10584674.shtml",
-      "source": "中国新闻网",
-      "time": "前天19:38",
-      "category": "other"
-    },
-    {
-      "title": "《如轼:苏东坡的后半生》挖掘苏轼与“小人物”的温暖交往",
-      "url": "https://www.chinanews.com.cn/cul/2026/03-10/10584670.shtml",
-      "source": "中国新闻网",
-      "time": "前天19:39",
-      "category": "other"
-    },
-    {
-      "title": "小说精选| 魏子洋:小说《枣花窑》,当代青年作家的现实主义力作",
-      "url": "https://hea.china.com/articles/20260310/202603101821885.html",
-      "source": "中华网",
-      "time": "前天16:09",
-      "category": "activity"
-    },
-    {
-      "title": "刘楚昕《泥潭》亲笔签名版图书正式发售 限量珍藏开启阅读新体验",
-      "url": "https://cul.sohu.com/a/995248841_121123525",
-      "source": "搜狐网",
-      "time": "昨天19:05",
-      "category": "other"
-    },
-    {
-      "title": "《和平》:当普通人遭遇战争丨新书",
-      "url": "https://baijiahao.baidu.com/s?id=1859358993204516897&wfr=spider&for=pc",
-      "source": "湘湘带你看社会",
-      "time": "昨天17:54",
-      "category": "activity"
-    },
-    {
-      "title": "铜川作家陈继清长篇小说《村雁》出版 聚焦乡村脱贫振兴",
-      "url": "http://news.10jqka.com.cn/20260311/c675210532.shtml",
-      "source": "同花顺财经",
-      "time": "昨天09:28",
+      "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
+      "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "1月10日",
       "category": "activity"
     }
   ]
