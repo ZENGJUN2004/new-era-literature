e7c9b0149ff6e3cec9491bc461ecf4d70850b485
GitHub Action <action@github.com>
2026-03-13 08:27:23 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-13 15:37",
+  "update_time": "2026-03-13 16:27",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -30,11 +30,46 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
-      "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "1月10日",
-      "category": "activity"
+      "title": "“鉴抄”风波中,不愿“沉默”的人",
+      "url": "https://baijiahao.baidu.com/s?id=1859417412512389465&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "昨天09:15",
+      "category": "other"
+    },
+    {
+      "title": "文学批评为何失锐气?需重提文学批评的“内与外”",
+      "url": "http://baijiahao.baidu.com/s?id=1859444186919220910&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "昨天16:28",
+      "category": "meeting"
+    },
+    {
+      "title": "文学批评为何变“素描”?",
+      "url": "http://baijiahao.baidu.com/s?id=1859444213714245687&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "昨天16:29",
+      "category": "meeting"
+    },
+    {
+      "title": "以高质量文艺评论推动新时代文艺精品创作",
+      "url": "https://baijiahao.baidu.com/s?id=1859420057624123181&wfr=spider&for=pc",
+      "source": "光明网",
+      "time": "昨天09:46",
+      "category": "meeting"
+    },
+    {
+      "title": "水清石见君所知,此是吾家秘密藏——黄庭坚声色嗅味的英译与再现",
+      "url": "https://baijiahao.baidu.com/s?id=1859403065238762121&wfr=spider&for=pc",
+      "source": "光明网",
+      "time": "昨天04:55",
+      "category": "other"
+    },
+    {
+      "title": "20岁西宁小伙拿下全国写作大赛一等奖",
+      "url": "https://baijiahao.baidu.com/s?id=1859499556227720703&wfr=spider&for=pc",
+      "source": "青海新闻网",
+      "time": "昨天19:27",
+      "category": "other"
     }
   ]
 };
