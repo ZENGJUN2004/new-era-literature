7b24bd9804e7031045ea3e942c06866f1083bd95
GitHub Action <action@github.com>
2026-03-13 13:52:09 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-13 20:31",
+  "update_time": "2026-03-13 21:52",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,10 +23,80 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "“你只管努力,剩下的交给时间!” 他是曲园学子杨浩然",
+      "url": "https://baijiahao.baidu.com/s?id=1859294217842455751&wfr=spider&for=pc",
+      "source": "曲阜师范大学",
+      "time": "前天00:02",
+      "category": "other"
+    },
+    {
+      "title": "文学双周记|22家刊物答原创之问,邓紫棋入选银河奖引热议",
+      "url": "https://baijiahao.baidu.com/s?id=1853442602393722283&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "1月5日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "新加坡著名慈善家、作家卓顺发《太阳出来了,露水不见了》新书发布",
+      "url": "https://cul.sohu.com/a/976352761_121192997",
+      "source": "搜狐网",
+      "time": "1月15日",
+      "category": "activity"
+    },
+    {
+      "title": "盘州本土作家创作的长篇历史纪实小说《乌蒙铁壁》新书发布",
+      "url": "https://baijiahao.baidu.com/s?id=1854571604814312489&wfr=spider&for=pc",
+      "source": "天眼新闻",
+      "time": "1月17日",
+      "category": "activity"
+    },
+    {
+      "title": "长篇纪实文学《烽火》新书发布暨座谈会在德州举行",
+      "url": "https://baijiahao.baidu.com/s?id=1854984906459423165&wfr=spider&for=pc",
+      "source": "大众日报",
+      "time": "1月22日",
+      "category": "activity"
+    },
+    {
+      "title": "铜川作家陈继清小说《村雁》出版",
+      "url": "http://app.myzaker.com/news/article.php?pk=69b0c9781bc8e0ab7c000001",
+      "source": "ZAKER",
+      "time": "前天09:46",
+      "category": "activity"
+    },
+    {
+      "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
+      "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "1月10日",
+      "category": "activity"
+    },
+    {
+      "title": "共话“永远在场”的爱 长篇小说《爱不落下》推出五周年纪念版",
+      "url": "https://baijiahao.baidu.com/s?id=1859523807674276060&wfr=spider&for=pc",
+      "source": "中工网",
+      "time": "11小时前",
+      "category": "activity"
+    },
+    {
+      "title": "丛新强|《芝镇说》中的“芝镇史”是怎么说的",
+      "url": "https://baijiahao.baidu.com/s?id=1859444999622968640&wfr=spider&for=pc",
+      "source": "齐鲁壹点",
+      "time": "昨天16:32",
+      "category": "other"
+    },
+    {
+      "title": "中国出版代表团亮相2026年伦敦书展",
+      "url": "http://world.people.com.cn/n1/2026/0312/c1002-40680114.html",
+      "source": "人民网",
+      "time": "昨天06:23",
       "category": "activity"
     }
   ]
