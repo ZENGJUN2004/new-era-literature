79e49ecb5e3eb632184f3532a2cbdfbb5dc3a690
GitHub Action <action@github.com>
2026-03-14 13:38:58 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-14 20:24",
+  "update_time": "2026-03-14 21:38",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,13 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "20岁西宁小伙拿下全国写作大赛一等奖",
+      "url": "https://baijiahao.baidu.com/s?id=1859499556227720703&wfr=spider&for=pc",
+      "source": "青海新闻网",
+      "time": "前天19:27",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -35,6 +42,27 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
+    },
+    {
+      "title": "一杯咖啡,品出“书乡海淀”的别样温度",
+      "url": "https://baijiahao.baidu.com/s?id=1859633394282970208&wfr=spider&for=pc",
+      "source": "北京海淀官方发布",
+      "time": "3小时前",
+      "category": "other"
+    },
+    {
+      "title": "蔡骏、天蚕土豆等网文“大神”即将抵达文学“县”场广东开平,开启...",
+      "url": "https://baijiahao.baidu.com/s?id=1859605732523535840&wfr=spider&for=pc",
+      "source": "潇湘晨报",
+      "time": "10小时前",
+      "category": "other"
+    },
+    {
+      "title": "刘慈欣:我们社会的真正问题是科学精神严重缺失",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32751587",
+      "source": "澎湃新闻",
+      "time": "昨天11:54",
+      "category": "other"
     }
   ]
 };
