4e0fa2e62115e23e10e92300134254c65a74d7ee
GitHub Action <action@github.com>
2026-03-14 20:15:41 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-15 03:19",
+  "update_time": "2026-03-15 04:15",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,34 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "刘慈欣:我们社会的真正问题是科学精神严重缺失",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32751587",
+      "source": "澎湃新闻",
+      "time": "前天11:54",
+      "category": "other"
+    },
+    {
+      "title": "千帆竞发走在前 大潮奔腾开新局 ——构建中国哲学社会科学自主...",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32760138",
+      "source": "澎湃新闻",
+      "time": "前天13:08",
+      "category": "other"
+    },
+    {
+      "title": "沅水文脉的当代延续——马珂散文随笔与文学评论审美观察",
+      "url": "https://baijiahao.baidu.com/s?id=1858377455122319929&wfr=spider&for=pc",
+      "source": "潇湘晨报",
+      "time": "2月28日",
+      "category": "meeting"
+    },
+    {
+      "title": "“新南方写作”的生成与展开",
+      "url": "https://baijiahao.baidu.com/s?id=1859500141310144689&wfr=spider&for=pc",
+      "source": "千龙网",
+      "time": "前天07:18",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -35,6 +63,13 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
+    },
+    {
+      "title": "赣鄱故事如何走向“中国童年”?",
+      "url": "https://baijiahao.baidu.com/s?id=1859656022370457444&wfr=spider&for=pc",
+      "source": "大江网",
+      "time": "昨天11:21",
+      "category": "other"
     }
   ]
 };
