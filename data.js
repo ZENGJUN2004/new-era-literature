009f6b727eada96ff38f2ea34a53b3aa0e51672d
GitHub Action <action@github.com>
2026-03-15 05:59:47 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-15 12:23",
+  "update_time": "2026-03-15 13:59",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,27 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
+      "url": "https://m.jiemian.com/article/3573232.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "《金瓶梅》电影中的女性命运:被欲望与权力裹挟的悲剧史诗",
+      "url": "https://cul.sohu.com/a/996216101_120020441",
+      "source": "搜狐网",
+      "time": "前天22:14",
+      "category": "other"
+    },
+    {
+      "title": "媒体眼中海大的2025年!",
+      "url": "https://baijiahao.baidu.com/s?id=1856159417055495057&wfr=spider&for=pc",
+      "source": "中国海洋大学",
+      "time": "2月4日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -35,6 +56,13 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
+    },
+    {
+      "title": "第35次改编,《呼啸山庄》争议不少",
+      "url": "https://baijiahao.baidu.com/s?id=1859532705359042756&wfr=spider&for=pc",
+      "source": "齐鲁壹点",
+      "time": "昨天21:44",
+      "category": "voice"
     }
   ]
 };
