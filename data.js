c292eebc71e6dd52cee8104c1c4caf79fcfbb840
GitHub Action <action@github.com>
2026-03-15 07:38:07 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-15 14:54",
+  "update_time": "2026-03-15 15:38",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,18 +23,39 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "“读过《呼啸山庄》再看电影,会为小说骄傲”|文学花边",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32761067",
+      "source": "澎湃新闻",
+      "time": "前天15:20",
+      "category": "activity"
+    },
+    {
+      "title": "长篇小说《爱不落下》推出五周年纪念版",
+      "url": "https://baijiahao.baidu.com/s?id=1859543396031147501&wfr=spider&for=pc",
+      "source": "人民资讯",
+      "time": "前天18:35",
+      "category": "activity"
+    },
+    {
+      "title": "新时代主题创作新标杆 电视剧《生命树》创作座谈会举行",
+      "url": "https://www.chinanews.com.cn/cul/2026/03-13/10586390.shtml",
+      "source": "中国新闻网",
+      "time": "前天17:58",
+      "category": "other"
+    },
+    {
+      "title": "长篇小说《爱不落下》推出五周年纪念版,用人文精神抚慰患者心灵痛苦",
+      "url": "https://baijiahao.baidu.com/s?id=1859544289198840653&wfr=spider&for=pc",
+      "source": "北晚在线",
+      "time": "前天18:44",
+      "category": "activity"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "两会受权发布丨中华人民共和国国民经济和社会发展第十五个五年...",
-      "url": "https://baijiahao.baidu.com/s?id=1859558064116696132&wfr=spider&for=pc",
-      "source": "新华网",
-      "time": "前天22:38",
-      "category": "other"
     },
     {
       "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
