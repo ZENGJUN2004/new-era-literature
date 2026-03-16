c0ef60e16357a804b45d1f59c0625dbb97da30f7
GitHub Action <action@github.com>
2026-03-16 06:14:01 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-16 12:30",
+  "update_time": "2026-03-16 14:14",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,41 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "文学双周记|22家刊物答原创之问,邓紫棋入选银河奖引热议",
-      "url": "https://baijiahao.baidu.com/s?id=1853442602393722283&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "1月5日",
-      "category": "other"
-    },
-    {
-      "title": "沅水文脉的当代延续——马珂散文随笔与文学评论审美观察",
-      "url": "https://baijiahao.baidu.com/s?id=1858377455122319929&wfr=spider&for=pc",
-      "source": "潇湘晨报",
-      "time": "2月28日",
-      "category": "meeting"
-    },
-    {
-      "title": "2026伦敦书展:书香之间见中国 - 要闻 - 中国产业经济信息网",
-      "url": "https://www.cinic.org.cn/whys/zx/1627246.html",
-      "source": "中国产业经济信息网",
-      "time": "今天",
-      "category": "other"
-    },
-    {
-      "title": "典籍中的“天府繁花”绽放 四川花卉文化文献展成都展出",
-      "url": "https://scnews.newssc.org/system/20260314/001597603.html",
-      "source": "四川新闻网",
-      "time": "前天23:49",
-      "category": "other"
-    },
-    {
-      "title": "中国作家麦家作品译介展亮相2026伦敦书展",
-      "url": "https://baijiahao.baidu.com/s?id=1859604684436750731&wfr=spider&for=pc",
-      "source": "中国新闻网",
-      "time": "前天10:52",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -70,97 +35,6 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
-    },
-    {
-      "title": "《她走过无数人间》:一部萧红阅读史,一种文学力量的确证",
-      "url": "https://baijiahao.baidu.com/s?id=1859784022524666440&wfr=spider&for=pc",
-      "source": "北京日报客户端",
-      "time": "1小时前",
-      "category": "other"
-    },
-    {
-      "title": "《再造南方》:“新南方写作”的生成与展开",
-      "url": "https://baijiahao.baidu.com/s?id=1859782735672303379&wfr=spider&for=pc",
-      "source": "北京日报客户端",
-      "time": "2小时前",
-      "category": "other"
-    },
-    {
-      "title": "何为新女性写作?重读萧红,洞见百年女性文学精神源头",
-      "url": "https://baijiahao.baidu.com/s?id=1859782735731332842&wfr=spider&for=pc",
-      "source": "封面新闻",
-      "time": "2小时前",
-      "category": "voice"
-    },
-    {
-      "title": "东君《谁是曹操》:一部曹操、孙悟空、贾宝玉、杨志等“熟人”的...",
-      "url": "https://baijiahao.baidu.com/s?id=1859782300844786552&wfr=spider&for=pc",
-      "source": "浙江日报",
-      "time": "2小时前",
-      "category": "other"
-    },
-    {
-      "title": "“新华荐书”第三十期推荐图书发布",
-      "url": "https://baijiahao.baidu.com/s?id=1859780592009494655&wfr=spider&for=pc",
-      "source": "参考消息",
-      "time": "3小时前",
-      "category": "other"
-    },
-    {
-      "title": "“新华荐书”第三十一期推荐图书发布",
-      "url": "https://baijiahao.baidu.com/s?id=1859780592008288045&wfr=spider&for=pc",
-      "source": "参考消息",
-      "time": "3小时前",
-      "category": "other"
-    },
-    {
-      "title": "放假通知!连休3天!不调休|4月1日起,跨境电商退货不再“绕路...",
-      "url": "http://www.thepaper.cn/newsDetail_forward_32775735",
-      "source": "澎湃新闻",
-      "time": "6小时前",
-      "category": "other"
-    },
-    {
-      "title": "她们是你的镜子",
-      "url": "https://baijiahao.baidu.com/s?id=1859764793808718253&wfr=spider&for=pc",
-      "source": "文汇报",
-      "time": "7小时前",
-      "category": "other"
-    },
-    {
-      "title": "急头白脸看一篇构石论文,是年轻人的解压方式",
-      "url": "https://baijiahao.baidu.com/s?id=1859736806439720319&wfr=spider&for=pc",
-      "source": "虎嗅",
-      "time": "昨天12:00",
-      "category": "meeting"
-    },
-    {
-      "title": "张莉:新女性写作之“新”,不在面孔,在视角",
-      "url": "https://baijiahao.baidu.com/s?id=1859711296080429540&wfr=spider&for=pc",
-      "source": "潮新闻客户端",
-      "time": "昨天15:12",
-      "category": "voice"
-    },
-    {
-      "title": "西湖讲坛第2讲——张莉解读萧红,探寻新女性写作传统的源流",
-      "url": "https://baijiahao.baidu.com/s?id=1859709488761011576&wfr=spider&for=pc",
-      "source": "青瞳视角",
-      "time": "昨天14:45",
-      "category": "voice"
-    },
-    {
-      "title": "「视线」把藏在案卷里的法治故事讲给更多人听",
-      "url": "https://baijiahao.baidu.com/s?id=1859668949135914447&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "昨天04:01",
-      "category": "other"
-    },
-    {
-      "title": "“准点”相约茅盾文学奖得主乔叶共赴一场精神归乡之旅",
-      "url": "http://www.thepaper.cn/newsDetail_forward_32772177",
-      "source": "澎湃新闻",
-      "time": "昨天09:03",
-      "category": "other"
     }
   ]
 };
