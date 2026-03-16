d577f9d629b819a02b958f0239845628f0e8573e
GitHub Action <action@github.com>
2026-03-16 10:06:49 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-16 17:00",
+  "update_time": "2026-03-16 18:06",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,13 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "陈丹燕眼中的黄浦江,是母亲河,也是父亲河",
+      "url": "https://www.jfdaily.com/wx/detail.do?id=1081220",
+      "source": "上观",
+      "time": "前天07:40",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -35,6 +42,20 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
+    },
+    {
+      "title": "认识21世纪|青年视角中的ACG文化_澎湃号·湃客_澎湃新闻-The Paper",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32774496",
+      "source": "澎湃新闻",
+      "time": "4小时前",
+      "category": "other"
+    },
+    {
+      "title": "雅安作家入选鲁迅文学院中青年作家高级研讨班",
+      "url": "https://baijiahao.baidu.com/s?id=1859694275053739600&wfr=spider&for=pc",
+      "source": "湘湘带你看社会",
+      "time": "昨天10:44",
+      "category": "meeting"
     }
   ]
 };
