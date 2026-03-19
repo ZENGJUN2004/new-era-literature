0f26cd6e82be0c6c36573e68a085ba7686951203
GitHub Action <action@github.com>
2026-03-19 09:32:57 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-19 16:30",
+  "update_time": "2026-03-19 17:32",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,20 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "活动回顾 | Z世代文学,在越来越卷的赛道中“解放自己”:《十月...",
+      "url": "https://cul.sohu.com/a/997503741_121124729",
+      "source": "搜狐网",
+      "time": "前天09:52",
+      "category": "other"
+    },
+    {
+      "title": "“Z世代文学,向何处”,《十月》创作分享会尝试作答",
+      "url": "https://baijiahao.baidu.com/s?id=1859898043995873557&wfr=spider&for=pc",
+      "source": "北晚在线",
+      "time": "前天16:28",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -35,6 +49,41 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
+    },
+    {
+      "title": "《十月》杂志举办青年作家创作分享会",
+      "url": "https://baijiahao.baidu.com/s?id=1860050202536776799&wfr=spider&for=pc",
+      "source": "人民网",
+      "time": "15小时前",
+      "category": "other"
+    },
+    {
+      "title": "授权发布丨中国女诗人诗选(甘肃篇):听桃花一瓣一瓣/落入废弃的陶罐",
+      "url": "https://baijiahao.baidu.com/s?id=1859990593342155728&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "昨天17:13",
+      "category": "other"
+    },
+    {
+      "title": "美文欣赏|桑植美食系列散文",
+      "url": "https://baijiahao.baidu.com/s?id=1859962280318316562&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "昨天09:42",
+      "category": "other"
+    },
+    {
+      "title": "东西问·人物丨安德烈·马尔罗的“中国情结”与文化贡献",
+      "url": "https://baijiahao.baidu.com/s?id=1859965834614804815&wfr=spider&for=pc",
+      "source": "中国侨网",
+      "time": "昨天10:37",
+      "category": "other"
+    },
+    {
+      "title": "Z世代的文学:情感表达内敛,但需跳出私人化表达",
+      "url": "https://baijiahao.baidu.com/s?id=1859970680653734922&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "昨天11:56",
+      "category": "other"
     }
   ]
 };
