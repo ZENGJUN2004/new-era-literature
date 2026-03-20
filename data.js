a3b944c204b0aff3c6a97c96cf670861fb65ba56
GitHub Action <action@github.com>
2026-03-20 06:40:15 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-20 13:44",
+  "update_time": "2026-03-20 14:40",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,10 +23,31 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "文学期刊《岁月》打假:假杂志恶意仿冒版式,非法收取高额费用",
+      "url": "https://baijiahao.baidu.com/s?id=1860011448675082580&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "前天22:44",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
+      "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "1月10日",
+      "category": "activity"
+    },
+    {
+      "title": "七部门联合发布新版《鼓励进口服务目录》",
+      "url": "https://stock.10jqka.com.cn/20260319/c675400664.shtml",
+      "source": "同花顺",
+      "time": "昨天10:27",
       "category": "activity"
     }
   ]
