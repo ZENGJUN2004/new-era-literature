60d39bbf42c8d131f3e9ea0d735d5b6993f06427
GitHub Action <action@github.com>
2026-03-20 12:31:55 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-20 19:23",
+  "update_time": "2026-03-20 20:31",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,17 +23,31 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
-      "url": "https://m.jiemian.com/article/3573232.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "voice"
+      "title": "文学期刊《岁月》打假:假杂志恶意仿冒版式,非法收取高额费用",
+      "url": "https://baijiahao.baidu.com/s?id=1860011448675082580&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "前天22:44",
+      "category": "other"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "盘州本土作家创作的长篇历史纪实小说《乌蒙铁壁》新书发布",
+      "url": "https://baijiahao.baidu.com/s?id=1854571604814312489&wfr=spider&for=pc",
+      "source": "天眼新闻",
+      "time": "1月17日",
+      "category": "activity"
+    },
+    {
+      "title": "长篇纪实文学《烽火》新书发布暨座谈会在德州举行",
+      "url": "https://baijiahao.baidu.com/s?id=1854984906459423165&wfr=spider&for=pc",
+      "source": "大众日报",
+      "time": "1月22日",
       "category": "activity"
     },
     {
@@ -44,10 +58,52 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
+      "title": "诗心映山川文脉传古今《寻踪莲影》新书首发暨作品研讨会在杭举行",
+      "url": "https://baijiahao.baidu.com/s?id=1860170181749075639&wfr=spider&for=pc",
+      "source": "湘湘带你看社会",
+      "time": "3小时前",
+      "category": "activity"
+    },
+    {
+      "title": "诗心映山川 文脉传古今《寻踪莲影》新书首发暨作品研讨会在杭举行",
+      "url": "https://baijiahao.baidu.com/s?id=1860152219602776131&wfr=spider&for=pc",
+      "source": "杭州网",
+      "time": "8小时前",
+      "category": "activity"
+    },
+    {
+      "title": "一周文化讲座|春日不须尽",
+      "url": "https://baijiahao.baidu.com/s?id=1860148477368750651&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "9小时前",
+      "category": "meeting"
+    },
+    {
       "title": "七部门联合发布新版《鼓励进口服务目录》",
       "url": "https://stock.10jqka.com.cn/20260319/c675400664.shtml",
       "source": "同花顺",
       "time": "昨天10:27",
+      "category": "activity"
+    },
+    {
+      "title": "CCG举办研究员佐丁新书发布会 - 20多位国际嘉宾踊跃参加分享长期...",
+      "url": "http://www.thepaper.cn/newsDetail_forward_32793698",
+      "source": "澎湃新闻",
+      "time": "昨天11:19",
+      "category": "activity"
+    },
+    {
+      "title": "祝勇长篇小说《国宝》三部曲亮相 为故宫人塑时代雕像",
+      "url": "https://baijiahao.baidu.com/s?id=1860085895408379681&wfr=spider&for=pc",
+      "source": "人民资讯",
+      "time": "昨天17:53",
+      "category": "activity"
+    },
+    {
+      "title": "新书速览|祝勇《国宝》三部曲:故宫人的时代雕像",
+      "url": "https://baijiahao.baidu.com/s?id=1860056087434715305&wfr=spider&for=pc",
+      "source": "极目新闻",
+      "time": "昨天10:34",
       "category": "activity"
     }
   ]
