58cc020819a8676bf48f7d3ef7c0a48ae8473786
GitHub Action <action@github.com>
2026-03-20 17:28:48 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-21 00:30",
+  "update_time": "2026-03-21 01:28",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,20 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "豆瓣高分、评论高赞,不出门的日子里,这6部纪录片治愈了我",
-      "url": "http://baijiahao.baidu.com/s?id=1730506789958714144&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "",
-      "category": "meeting"
-    },
-    {
-      "title": "《小说评论》年度论坛:聚焦“现象级文本与文学史书写”",
-      "url": "http://baijiahao.baidu.com/s?id=1751357796338047975&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "",
-      "category": "activity"
-    },
-    {
       "title": "七部门联合发布新版《鼓励进口服务目录》",
       "url": "https://stock.10jqka.com.cn/20260319/c675400664.shtml",
       "source": "同花顺",
@@ -55,20 +41,6 @@ window.LIT_DATA = {
       "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
       "source": "红网",
       "time": "1月10日",
-      "category": "activity"
-    },
-    {
-      "title": "...即远行丨超星2026年4月全民阅读活动周系列书展_评分_在线_豆瓣",
-      "url": "https://cul.sohu.com/a/998851839_121106908",
-      "source": "搜狐网",
-      "time": "昨天10:49",
-      "category": "other"
-    },
-    {
-      "title": "【春分专辑】精品新书速递 | 做春天的读者,而非过客",
-      "url": "https://cul.sohu.com/a/999014823_121106869",
-      "source": "搜狐网",
-      "time": "昨天16:23",
       "category": "activity"
     }
   ]
