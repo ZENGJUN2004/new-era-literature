7c14931d110b5cf91e8cccabf7927ecb6176ab8a
GitHub Action <action@github.com>
2026-03-20 20:20:18 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-21 03:30",
+  "update_time": "2026-03-21 04:20",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,55 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "女性为何写作?她们用文字打破边界",
-      "url": "https://baijiahao.baidu.com/s?id=1855001128564567122&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "1月22日",
-      "category": "other"
-    },
-    {
-      "title": "三妇有异见 故事要新编",
-      "url": "https://www.bjnews.com.cn/detail/1773870456189497.html",
-      "source": "新京报网",
-      "time": "前天05:41",
-      "category": "other"
-    },
-    {
-      "title": "不被定义的她享有一切!_女性_埃莱娜·西苏_语言",
-      "url": "https://cul.sohu.com/a/994015289_472681",
-      "source": "搜狐网",
-      "time": "3月8日",
-      "category": "other"
-    },
-    {
-      "title": "长篇小说《国宝》三部曲出版,刻画故宫人的时代雕像",
-      "url": "https://baijiahao.baidu.com/s?id=1860089811797330188&wfr=spider&for=pc",
-      "source": "京报网",
-      "time": "前天19:16",
-      "category": "activity"
-    },
-    {
-      "title": "主题书展 | 三月 ·茅盾文学奖书展",
-      "url": "https://cul.sohu.com/a/998399439_121106994",
-      "source": "搜狐网",
-      "time": "前天10:08",
-      "category": "other"
-    },
-    {
-      "title": "于家族叙事中洞悉生命传承奥秘——评刘亮程长篇小说《长命》",
-      "url": "https://baijiahao.baidu.com/s?id=1860055991176551826&wfr=spider&for=pc",
-      "source": "中国青年网",
-      "time": "前天10:22",
-      "category": "activity"
-    },
-    {
-      "title": "祝勇长篇小说《国宝》三部曲亮相 为故宫人塑时代雕像",
-      "url": "https://baijiahao.baidu.com/s?id=1860085895408379681&wfr=spider&for=pc",
-      "source": "人民资讯",
-      "time": "前天17:53",
-      "category": "activity"
-    },
-    {
       "title": "七部门联合发布新版《鼓励进口服务目录》",
       "url": "https://stock.10jqka.com.cn/20260319/c675400664.shtml",
       "source": "同花顺",
@@ -91,55 +42,6 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
-    },
-    {
-      "title": "一周文化讲座|春日不须尽",
-      "url": "https://baijiahao.baidu.com/s?id=1860148477368750651&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "昨天11:02",
-      "category": "meeting"
-    },
-    {
-      "title": "《太平年》出海,11种语言覆盖全球73个国家和地区",
-      "url": "https://baijiahao.baidu.com/s?id=1860173735417024260&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "昨天17:43",
-      "category": "other"
-    },
-    {
-      "title": "“童眼”客观存在不然何来儿童文学",
-      "url": "https://baijiahao.baidu.com/s?id=1860147846335363548&wfr=spider&for=pc",
-      "source": "湘湘带你看社会",
-      "time": "昨天10:53",
-      "category": "other"
-    },
-    {
-      "title": "历史剧《太平年》观感:一杯热酒读“太平”",
-      "url": "https://baijiahao.baidu.com/s?id=1860135578473587484&wfr=spider&for=pc",
-      "source": "潮新闻客户端",
-      "time": "昨天07:37",
-      "category": "other"
-    },
-    {
-      "title": "书摘|宋遂良与茅奖读书班",
-      "url": "https://baijiahao.baidu.com/s?id=1860143570786130939&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "昨天09:45",
-      "category": "other"
-    },
-    {
-      "title": "“童眼”客观存在 不然何来儿童文学",
-      "url": "https://baijiahao.baidu.com/s?id=1860120510282427353&wfr=spider&for=pc",
-      "source": "光明网",
-      "time": "昨天02:45",
-      "category": "other"
-    },
-    {
-      "title": "“回家去!”抵达国宝守护者的内心",
-      "url": "https://baijiahao.baidu.com/s?id=1860167444763268363&wfr=spider&for=pc",
-      "source": "中工网",
-      "time": "昨天14:23",
-      "category": "other"
     }
   ]
 };
