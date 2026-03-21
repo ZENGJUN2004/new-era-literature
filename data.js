126c3884580b0137a9e3ff38398164e4883559c5
GitHub Action <action@github.com>
2026-03-21 15:16:09 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-21 22:17",
+  "update_time": "2026-03-21 23:16",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,48 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "项飙董宇辉周轶君对谈:个人IP的情感逻辑与消费的双重性",
-      "url": "https://stock.10jqka.com.cn/20260107/c673816413.shtml",
-      "source": "同花顺",
-      "time": "1月7日",
-      "category": "other"
-    },
-    {
-      "title": "她说|戴锦华:做现实主义者,求不可能之事",
-      "url": "https://baijiahao.baidu.com/s?id=1855465476132764191&wfr=spider&for=pc",
-      "source": "新闻晨报",
-      "time": "1月27日",
-      "category": "other"
-    },
-    {
-      "title": "四川省十四届人大五次会议“代表通道”集体采访活动(第二场)",
-      "url": "http://www.scio.gov.cn/xwfb/dfxwfb/gssfbh/sc_13848/202602/t20260210_949661.html",
-      "source": "中华人民共和国国务院新闻办公室",
-      "time": "2月4日",
-      "category": "other"
-    },
-    {
-      "title": "加快培养高端复合人才——访天津大学党委书记杨贤金代表",
-      "url": "https://news.gmw.cn/2026-03/05/content_38628359.htm",
-      "source": "光明网新闻中心",
-      "time": "3月5日",
-      "category": "other"
-    },
-    {
-      "title": "大法官访谈| 贵州高院院长茆荣华:以“案件质量提升工程”推动...",
-      "url": "https://www.court.gov.cn/zixun/xiangqing/493091.html",
-      "source": "中华人民共和国最高人民法院",
-      "time": "3月9日",
-      "category": "other"
-    },
-    {
-      "title": "以规立企 以数驭变|今元集团数智时代人力资源服务创新论坛圆满举办",
-      "url": "http://industry.caijing.com.cn/20260319/5148014.shtml",
-      "source": "财经网",
-      "time": "前天11:09",
-      "category": "meeting"
-    },
-    {
       "title": "七部门联合发布新版《鼓励进口服务目录》",
       "url": "https://stock.10jqka.com.cn/20260319/c675400664.shtml",
       "source": "同花顺",
@@ -77,27 +35,6 @@ window.LIT_DATA = {
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
-      "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "1月10日",
-      "category": "activity"
-    },
-    {
-      "title": "AI破局·智启新程-中国OPC联盟·成都AI破局年会 共话AI行业新机遇",
-      "url": "https://hea.china.com/articles/20260320/202603201829106.html",
-      "source": "中华网",
-      "time": "昨天11:35",
-      "category": "other"
-    },
-    {
-      "title": "一群中学生,开始关心家人的历史",
-      "url": "https://www.infzm.com/contents/317332",
-      "source": "南方周末",
-      "time": "昨天09:00",
-      "category": "other"
     }
   ]
 };
