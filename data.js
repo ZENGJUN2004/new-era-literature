c30abf4738be338deba453ee49087b40010f9913
GitHub Action <action@github.com>
2026-03-21 08:19:28 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-21 15:26",
+  "update_time": "2026-03-21 16:19",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,27 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "文艺评论|从地域文字叙事到跨媒介声画传播——贵州文学的影视转化...",
-      "url": "https://baijiahao.baidu.com/s?id=1860100191569572245&wfr=spider&for=pc",
-      "source": "天眼新闻",
-      "time": "前天21:05",
-      "category": "meeting"
-    },
-    {
-      "title": "文艺评论|文学与影视创作的双重交响",
-      "url": "https://baijiahao.baidu.com/s?id=1860100191569425598&wfr=spider&for=pc",
-      "source": "天眼新闻",
-      "time": "前天21:26",
-      "category": "meeting"
-    },
-    {
-      "title": "沅水文脉的当代延续——马珂散文随笔与文学评论审美观察",
-      "url": "https://baijiahao.baidu.com/s?id=1858377455122319929&wfr=spider&for=pc",
-      "source": "潇湘晨报",
-      "time": "2月28日",
-      "category": "meeting"
-    },
-    {
       "title": "七部门联合发布新版《鼓励进口服务目录》",
       "url": "https://stock.10jqka.com.cn/20260319/c675400664.shtml",
       "source": "同花顺",
@@ -58,18 +37,11 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "商伟|八十年代的大学:有关文学的回忆与随想",
-      "url": "https://baijiahao.baidu.com/s?id=1860234984683409667&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "5小时前",
-      "category": "other"
-    },
-    {
-      "title": "用文字为时代代言,为普通大众代言——专访著名作家、鲁迅文学奖...",
-      "url": "https://baijiahao.baidu.com/s?id=1860183141392853454&wfr=spider&for=pc",
-      "source": "湘湘带你看社会",
-      "time": "昨天20:14",
-      "category": "other"
+      "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
+      "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "1月10日",
+      "category": "activity"
     }
   ]
 };
