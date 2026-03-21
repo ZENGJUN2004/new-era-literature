1e57863834c1cfe2c2f62a39d2d645569e9ddd0f
GitHub Action <action@github.com>
2026-03-21 18:20:34 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-22 01:17",
+  "update_time": "2026-03-22 02:20",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,27 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "媒体眼中海大的2025年!",
+      "url": "https://baijiahao.baidu.com/s?id=1856159417055495057&wfr=spider&for=pc",
+      "source": "中国海洋大学",
+      "time": "2月4日",
+      "category": "other"
+    },
+    {
+      "title": "林森:每日绕行明昌塔 | 《花城》短篇小说",
+      "url": "http://www.thepaper.cn/newsDetail_forward_32799274",
+      "source": "澎湃新闻",
+      "time": "前天12:51",
+      "category": "activity"
+    },
+    {
+      "title": "写出属于自己的独特体验,Z世代文学向何处?",
+      "url": "http://baijiahao.baidu.com/s?id=1860149001545091201&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "前天11:11",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -35,6 +56,13 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
+    },
+    {
+      "title": "黄克武:《新民说》的创作背景与影响",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32733389",
+      "source": "澎湃新闻",
+      "time": "昨天09:28",
+      "category": "other"
     }
   ]
 };
