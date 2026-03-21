1119df8d82038ba7d15ab131a1c2efb2023d60ca
GitHub Action <action@github.com>
2026-03-21 11:14:52 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-21 18:16",
+  "update_time": "2026-03-21 19:14",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,20 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "罗伟章:大多数走上文学道路的人,生命里都留有一块伤疤",
-      "url": "http://baijiahao.baidu.com/s?id=1860074387067412034&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "前天15:25",
-      "category": "other"
-    },
-    {
-      "title": "《脐带纪事》新书首发:我们知道奶奶/外婆的名字,却不了解她的一生",
-      "url": "https://baijiahao.baidu.com/s?id=1860080496984888698&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "前天17:01",
-      "category": "activity"
-    },
-    {
       "title": "七部门联合发布新版《鼓励进口服务目录》",
       "url": "https://stock.10jqka.com.cn/20260319/c675400664.shtml",
       "source": "同花顺",
@@ -56,20 +42,6 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
-    },
-    {
-      "title": "熊若皙:从金融跨界新闻,用真诚对话建立真实的联结",
-      "url": "https://www.huxiu.com/article/4843862.html",
-      "source": "虎嗅APP",
-      "time": "昨天14:40",
-      "category": "other"
-    },
-    {
-      "title": "真实自有万钧之力|“澎湃·镜相”第三届非虚构写作大赛进入复审阶段",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32777272",
-      "source": "澎湃新闻",
-      "time": "昨天07:52",
-      "category": "voice"
     }
   ]
 };
