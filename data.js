16bfdb70d32ef760b47c20b92257ff892a47ff0a
GitHub Action <action@github.com>
2026-03-21 13:35:50 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-21 20:23",
+  "update_time": "2026-03-21 21:35",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,20 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "《十月》杂志举办青年作家创作分享会",
-      "url": "https://baijiahao.baidu.com/s?id=1860050202536776799&wfr=spider&for=pc",
-      "source": "人民网",
-      "time": "前天02:21",
-      "category": "other"
-    },
-    {
-      "title": "【钛晨报】国家医保局:医保药品目录调整专家评审工作已结束;把...",
-      "url": "https://www.tmtpost.com/6283071.html",
-      "source": "钛媒体官方网站",
-      "time": "",
-      "category": "activity"
-    },
-    {
       "title": "七部门联合发布新版《鼓励进口服务目录》",
       "url": "https://stock.10jqka.com.cn/20260319/c675400664.shtml",
       "source": "同花顺",
@@ -49,13 +35,6 @@ window.LIT_DATA = {
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "在华外企看中国“十五五”规划——与中国同行,共享发展机遇",
-      "url": "https://baijiahao.baidu.com/s?id=1860141240616188432&wfr=spider&for=pc",
-      "source": "国际在线",
-      "time": "昨天09:05",
-      "category": "other"
     }
   ]
 };
