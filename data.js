7fede6563e9294278ceef273295e87b22ee90933
GitHub Action <action@github.com>
2026-03-23 07:55:21 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-23 14:04",
+  "update_time": "2026-03-23 15:55",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,32 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "沅水文脉的当代延续——马珂散文随笔与文学评论审美观察",
-      "url": "https://baijiahao.baidu.com/s?id=1858377455122319929&wfr=spider&for=pc",
-      "source": "潇湘晨报",
-      "time": "2月28日",
-      "category": "meeting"
-    },
-    {
-      "title": "“非虚构”和“虚构”的界限",
-      "url": "http://baijiahao.baidu.com/s?id=1740112764190450044&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "如何把一个想法写成畅销书?美国顶尖非虚构作家的写作课",
-      "url": "https://www.lifeweek.com.cn/article/13495",
-      "source": "三联生活周刊",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
-      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "",
-      "category": "voice"
+      "title": "2026闽商年度盛典在福建福州举办",
+      "url": "https://www.chinanews.com.cn/cj/2026/03-21/10590413.shtml",
+      "source": "中国新闻网",
+      "time": "前天08:56",
+      "category": "other"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
@@ -65,10 +44,10 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "从文学批点到弹幕文化,大众文艺的古今共鸣",
-      "url": "http://news.youth.cn/gn/202603/t20260323_16569837.htm",
-      "source": "中国青年网",
-      "time": "7小时前",
+      "title": "“点赞”的暴政、智能体的忧伤以及人民为何需要“品味”",
+      "url": "https://www.huxiu.com/article/4844362.html",
+      "source": "虎嗅APP",
+      "time": "6小时前",
       "category": "other"
     },
     {
@@ -76,27 +55,6 @@ window.LIT_DATA = {
       "url": "https://www.bjnews.com.cn/detail/1774176071019173.html",
       "source": "新京报网",
       "time": "昨天18:41",
-      "category": "other"
-    },
-    {
-      "title": "英国当代作家如何讲述战争?",
-      "url": "https://cul.sohu.com/a/999762500_228930",
-      "source": "搜狐网",
-      "time": "昨天13:54",
-      "category": "other"
-    },
-    {
-      "title": "广东如何让新大众文艺创作“声量”远播?",
-      "url": "https://baijiahao.baidu.com/s?id=1860331326310923627&wfr=spider&for=pc",
-      "source": "南方网",
-      "time": "昨天11:30",
-      "category": "other"
-    },
-    {
-      "title": "当素人写作也出现“异曲同工”|编辑部聊天室",
-      "url": "https://baijiahao.baidu.com/s?id=1860329575687595744&wfr=spider&for=pc",
-      "source": "界面新闻",
-      "time": "昨天11:00",
       "category": "other"
     }
   ]
