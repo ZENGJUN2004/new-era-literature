52781fae95b92f2304e5868c59408c15b671f609
GitHub Action <action@github.com>
2026-03-23 16:44:22 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-23 23:50",
+  "update_time": "2026-03-24 00:44",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,20 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "如何把一个想法写成畅销书?美国顶尖非虚构作家的写作课",
-      "url": "https://www.lifeweek.com.cn/article/13495",
-      "source": "三联生活周刊",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
-      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "",
-      "category": "voice"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -44,101 +30,10 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "《光荣亭》新书发布活动在福建上杭举行",
-      "url": "http://www.mzyfz.com/html/2203/2026-01-12/content-1669350.html",
-      "source": "民主与法制网",
-      "time": "1月12日",
-      "category": "activity"
-    },
-    {
-      "title": "盘州本土作家创作的长篇历史纪实小说《乌蒙铁壁》新书发布",
-      "url": "https://baijiahao.baidu.com/s?id=1854571604814312489&wfr=spider&for=pc",
-      "source": "天眼新闻",
-      "time": "1月17日",
-      "category": "activity"
-    },
-    {
-      "title": "长篇纪实文学《烽火》新书发布暨座谈会在德州举行",
-      "url": "https://baijiahao.baidu.com/s?id=1854984906459423165&wfr=spider&for=pc",
-      "source": "大众日报",
-      "time": "1月22日",
-      "category": "activity"
-    },
-    {
-      "title": "4000余种图书亮相 举办40余场活动 中国书香浸润2026伦敦书展",
-      "url": "https://www.cinic.org.cn/whys/ds/1628219.html",
-      "source": "中国产业经济信息网",
-      "time": "今天",
-      "category": "other"
-    },
-    {
       "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
       "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
       "source": "红网",
       "time": "1月10日",
-      "category": "activity"
-    },
-    {
-      "title": "春暖三湘 阅启新程,2026岳麓书会暨长沙图书交易会即将启幕",
-      "url": "https://baijiahao.baidu.com/s?id=1860456882519775221&wfr=spider&for=pc",
-      "source": "潇湘晨报",
-      "time": "3小时前",
-      "category": "other"
-    },
-    {
-      "title": "张炜《陶渊明的遗产》|农事与健康(三):孤云",
-      "url": "https://baijiahao.baidu.com/s?id=1860444020198385858&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "6小时前",
-      "category": "other"
-    },
-    {
-      "title": "张炜《陶渊明的遗产》|农事与健康(四):“劳心”和“劳力”",
-      "url": "https://baijiahao.baidu.com/s?id=1860444051260339454&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "6小时前",
-      "category": "other"
-    },
-    {
-      "title": "新书速览|刘诗伟《创作十二疑难》:写给未来杰出作家的谏言",
-      "url": "https://baijiahao.baidu.com/s?id=1860443389440302999&wfr=spider&for=pc",
-      "source": "极目新闻",
-      "time": "6小时前",
-      "category": "activity"
-    },
-    {
-      "title": "多位名家将走进四川马边 让文学抵达“县”场",
-      "url": "https://baijiahao.baidu.com/s?id=1860431910434555076&wfr=spider&for=pc",
-      "source": "封面新闻",
-      "time": "9小时前",
-      "category": "other"
-    },
-    {
-      "title": "商南作家杨富安新作《最美的风景》分享会举行",
-      "url": "https://baijiahao.baidu.com/s?id=1860434422862197784&wfr=spider&for=pc",
-      "source": "三秦都市报",
-      "time": "9小时前",
-      "category": "other"
-    },
-    {
-      "title": "岳麓书会前瞻丨马伯庸赴约长沙,以《历史中的大与小》见微探史",
-      "url": "https://baijiahao.baidu.com/s?id=1860419386371262629&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "13小时前",
-      "category": "other"
-    },
-    {
-      "title": "茅盾文学奖得主阿来携新作《东坡在人间》亮相江城",
-      "url": "http://m.cnhubei.com/content/2026-03/22/content_19881928.html",
-      "source": "荆楚网",
-      "time": "昨天19:36",
-      "category": "other"
-    },
-    {
-      "title": "全国新书发布厅第340期,海飞携“迷城”新作《剧院》亮相上海书城",
-      "url": "https://baijiahao.baidu.com/s?id=1860347224925766401&wfr=spider&for=pc",
-      "source": "新闻晨报",
-      "time": "昨天15:42",
       "category": "activity"
     }
   ]
