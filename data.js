7a0366abfbb36e57b4df45c7179a4fba9dc6c2c4
GitHub Action <action@github.com>
2026-03-23 12:38:37 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-23 19:36",
+  "update_time": "2026-03-23 20:38",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,27 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "李敬泽《引回风:随笔自选集》:自我追溯的“时间之书”",
+      "url": "https://baijiahao.baidu.com/s?id=1854627724132683640&wfr=spider&for=pc",
+      "source": "中国青年网",
+      "time": "1月18日",
+      "category": "other"
+    },
+    {
+      "title": "泸州老窖封藏大典举行 各界名家共话浓香传承与“专注”精神",
+      "url": "http://news.10jqka.com.cn/20260321/c675459593.shtml",
+      "source": "同花顺财经",
+      "time": "前天20:34",
+      "category": "other"
+    },
+    {
+      "title": "生命中的那坛酒|李敬泽、刘擎:专注、纯粹、极致,是人类共同的精神...",
+      "url": "https://www.dzwww.com/xinwen/jishixinwen/202603/t20260321_17562182.htm",
+      "source": "大众网",
+      "time": "前天",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -35,6 +56,20 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
+    },
+    {
+      "title": "李敬泽、刘擎泸州对谈:专注、纯粹、极致是人类对抗时代焦虑的精神...",
+      "url": "https://stock.10jqka.com.cn/20260323/c675485358.shtml",
+      "source": "同花顺",
+      "time": "1小时前",
+      "category": "other"
+    },
+    {
+      "title": "李敬泽、刘擎、许正宏等齐聚泸州,畅谈“生命中的那坛酒”",
+      "url": "https://baijiahao.baidu.com/s?id=1860367832768674447&wfr=spider&for=pc",
+      "source": "中国新闻网",
+      "time": "昨天20:29",
+      "category": "other"
     }
   ]
 };
