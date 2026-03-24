a15c1d078fa2318c75d1f1dc5d07bd131910cff4
GitHub Action <action@github.com>
2026-03-24 11:37:41 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-24 18:39",
+  "update_time": "2026-03-24 19:37",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,13 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "文学双周记|22家刊物答原创之问,邓紫棋入选银河奖引热议",
+      "url": "https://baijiahao.baidu.com/s?id=1853442602393722283&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "1月5日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -35,6 +42,27 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
+    },
+    {
+      "title": "花城文学院成立三周年,探索“文学+”的无限可能",
+      "url": "https://culture.ycwb.com/ikimvkhtio/content_54026563.htm",
+      "source": "金羊网",
+      "time": "2小时前",
+      "category": "other"
+    },
+    {
+      "title": "破壁出圈,文学何为?花城文学院三周年交出“融合”答卷",
+      "url": "https://news.dayoo.com/gzrbrmt/202603/24/170636_54939999.htm",
+      "source": "大洋网",
+      "time": "2小时前",
+      "category": "other"
+    },
+    {
+      "title": "从“有风自南”到“去辽阔处”:花城文学院三周年,让文学走向大众...",
+      "url": "https://baijiahao.baidu.com/s?id=1860516336860401204&wfr=spider&for=pc",
+      "source": "扬子晚报",
+      "time": "7小时前",
+      "category": "other"
     }
   ]
 };
