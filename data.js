6d32ca60f47f1d4f78b69b5115e159c73d03088e
GitHub Action <action@github.com>
2026-03-24 09:44:07 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-24 16:38",
+  "update_time": "2026-03-24 17:44",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,20 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "李敬泽《引回风:随笔自选集》:自我追溯的“时间之书”",
-      "url": "https://baijiahao.baidu.com/s?id=1854627724132683640&wfr=spider&for=pc",
-      "source": "中国青年网",
-      "time": "1月18日",
-      "category": "other"
-    },
-    {
-      "title": "李敬泽、刘擎、许正宏等齐聚泸州,畅谈“生命中的那坛酒”",
-      "url": "https://baijiahao.baidu.com/s?id=1860367832768674447&wfr=spider&for=pc",
-      "source": "中国新闻网",
-      "time": "前天20:29",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -49,13 +35,6 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
-    },
-    {
-      "title": "李敬泽、刘擎泸州对谈:专注、纯粹、极致是人类对抗时代焦虑的精神...",
-      "url": "https://stock.10jqka.com.cn/20260323/c675485358.shtml",
-      "source": "同花顺",
-      "time": "昨天18:40",
-      "category": "other"
     }
   ]
 };
