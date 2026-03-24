a7bed9b00022b69cf8d6fb2e9e7e08372447f7e0
GitHub Action <action@github.com>
2026-03-24 15:55:23 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-24 22:11",
+  "update_time": "2026-03-24 23:55",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,20 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "《人民文学》葡文版首刊正式发行",
+      "url": "https://baijiahao.baidu.com/s?id=1856707364268271148&wfr=spider&for=pc",
+      "source": "海外网",
+      "time": "2月10日",
+      "category": "other"
+    },
+    {
+      "title": "向葡语国家传播中国文学《人民文学》葡文版首刊正式发行",
+      "url": "https://baijiahao.baidu.com/s?id=1857935327681773426&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "2月24日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -34,6 +48,20 @@ window.LIT_DATA = {
       "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
       "source": "红网",
       "time": "1月10日",
+      "category": "activity"
+    },
+    {
+      "title": "萧红以她“越轨的笔致”写出了“无数人间”并跨过了时空",
+      "url": "https://www.81.cn/yd_208600/16450808.html",
+      "source": "中国军网",
+      "time": "8小时前",
+      "category": "other"
+    },
+    {
+      "title": "《沉睡的人》:这本60年前的书写出了当下人的精神状态丨新书",
+      "url": "https://baijiahao.baidu.com/s?id=1860521184549915572&wfr=spider&for=pc",
+      "source": "湘湘带你看社会",
+      "time": "10小时前",
       "category": "activity"
     }
   ]
