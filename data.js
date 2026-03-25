fc919dfc1bd7efeb182e6a46416e92fc2dea52c1
GitHub Action <action@github.com>
2026-03-25 04:09:12 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-25 09:07",
+  "update_time": "2026-03-25 12:09",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,25 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
-      "url": "https://m.jiemian.com/article/3573232.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "voice"
+      "title": "收获文学榜在上海揭晓!苏童《好天气》领衔长篇榜首",
+      "url": "http://baijiahao.baidu.com/s?id=1856101673487226555&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "2月3日",
+      "category": "other"
+    },
+    {
+      "title": "苏童毕飞宇吴真全勇先,2025收获文学榜揭晓,榜首作家这样说",
+      "url": "http://baijiahao.baidu.com/s?id=1856104193950555898&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "2月3日",
+      "category": "other"
+    },
+    {
+      "title": "《收获》文学榜揭晓,榜单映射中国文坛迎来“丰年”",
+      "url": "https://baijiahao.baidu.com/s?id=1856184355598057672&wfr=spider&for=pc",
+      "source": "新华网",
+      "time": "2月4日",
+      "category": "other"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
@@ -42,13 +56,6 @@ window.LIT_DATA = {
       "source": "红网",
       "time": "1月10日",
       "category": "activity"
-    },
-    {
-      "title": "苏轼与“一色杏花三十里”",
-      "url": "https://baijiahao.baidu.com/s?id=1860501553296733593&wfr=spider&for=pc",
-      "source": "光明网",
-      "time": "昨天08:29",
-      "category": "other"
     }
   ]
 };
