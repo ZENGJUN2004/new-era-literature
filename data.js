f78adc0a861150d9036dc4dbe5ceefafd98cc9b8
GitHub Action <action@github.com>
2026-03-25 16:58:40 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-25 23:58",
+  "update_time": "2026-03-26 00:58",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,13 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "《沉睡的人》:这本60年前的书写出了当下人的精神状态丨新书",
+      "url": "https://baijiahao.baidu.com/s?id=1860521184549915572&wfr=spider&for=pc",
+      "source": "湘湘带你看社会",
+      "time": "前天13:47",
+      "category": "activity"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -30,10 +37,52 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
+      "title": "《光荣亭》新书发布活动在福建上杭举行",
+      "url": "http://www.mzyfz.com/html/2203/2026-01-12/content-1669350.html",
+      "source": "民主与法制网",
+      "time": "1月12日",
+      "category": "activity"
+    },
+    {
+      "title": "盘州本土作家创作的长篇历史纪实小说《乌蒙铁壁》新书发布",
+      "url": "https://baijiahao.baidu.com/s?id=1854571604814312489&wfr=spider&for=pc",
+      "source": "天眼新闻",
+      "time": "1月17日",
+      "category": "activity"
+    },
+    {
+      "title": "长篇纪实文学《烽火》新书发布暨座谈会在德州举行",
+      "url": "https://baijiahao.baidu.com/s?id=1854984906459423165&wfr=spider&for=pc",
+      "source": "大众日报",
+      "time": "1月22日",
+      "category": "activity"
+    },
+    {
       "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
       "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
       "source": "红网",
       "time": "1月10日",
+      "category": "activity"
+    },
+    {
+      "title": "南阳作家鲁钊获得全国“志愿文学奖”",
+      "url": "https://baijiahao.baidu.com/s?id=1860597561447257643&wfr=spider&for=pc",
+      "source": "大象新闻",
+      "time": "昨天10:00",
+      "category": "other"
+    },
+    {
+      "title": "电视剧《冬去春来》热播,同名新书即将上市",
+      "url": "https://baijiahao.baidu.com/s?id=1860624047932002560&wfr=spider&for=pc",
+      "source": "大众日报",
+      "time": "昨天17:02",
+      "category": "activity"
+    },
+    {
+      "title": "《洪炉:川菜有个徐老师》新书发布会在蓉举行",
+      "url": "https://baijiahao.baidu.com/s?id=1860595140015704806&wfr=spider&for=pc",
+      "source": "封面新闻",
+      "time": "昨天09:23",
       "category": "activity"
     }
   ]
