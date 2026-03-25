799852298cb0ee78ad48aa4c3e73ac9a007595ee
GitHub Action <action@github.com>
2026-03-25 08:37:05 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-25 15:45",
+  "update_time": "2026-03-25 16:37",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,13 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "豆瓣高分、评论高赞,不出门的日子里,这6部纪录片治愈了我",
+      "url": "http://baijiahao.baidu.com/s?id=1730506789958714144&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "meeting"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -34,6 +41,13 @@ window.LIT_DATA = {
       "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
       "source": "红网",
       "time": "1月10日",
+      "category": "activity"
+    },
+    {
+      "title": "《沉睡的人》:这本60年前的书写出了当下人的精神状态丨新书",
+      "url": "https://baijiahao.baidu.com/s?id=1860521184549915572&wfr=spider&for=pc",
+      "source": "湘湘带你看社会",
+      "time": "昨天13:47",
       "category": "activity"
     }
   ]
