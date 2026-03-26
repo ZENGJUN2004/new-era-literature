988d9e3cd221c127d0d3139a326419b7bd446a1e
GitHub Action <action@github.com>
2026-03-26 04:23:30 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-26 09:12",
+  "update_time": "2026-03-26 12:23",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,20 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "王文东:我有两块“责任田”,苹果和写作都是甜蜜的事业|新大众文艺...",
+      "url": "http://baijiahao.baidu.com/s?id=1860503697156732528&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "前天09:09",
+      "category": "other"
+    },
+    {
+      "title": "以写作介入现实,安妮·埃尔诺为何执着于书写记忆?",
+      "url": "http://www.thepaper.cn/newsDetail_forward_32816862",
+      "source": "澎湃新闻",
+      "time": "前天15:04",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -30,11 +44,25 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
-      "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "1月10日",
-      "category": "activity"
+      "title": "花城文学院成立三周年,探索“文学+”的无限可能",
+      "url": "https://culture.ycwb.com/ikimvkhtio/content_54026563.htm",
+      "source": "金羊网",
+      "time": "前天16:49",
+      "category": "other"
+    },
+    {
+      "title": "记录许多家庭隐痛 让孩子的世界里有“光”",
+      "url": "http://news.cnhubei.com/content/2026-03/26/content_19888237.html",
+      "source": "荆楚网新闻",
+      "time": "2小时前",
+      "category": "other"
+    },
+    {
+      "title": "报告文学如何写?黔西南这个讲座干货满满",
+      "url": "https://baijiahao.baidu.com/s?id=1860626154753374962&wfr=spider&for=pc",
+      "source": "天眼新闻",
+      "time": "昨天16:24",
+      "category": "meeting"
     }
   ]
 };
