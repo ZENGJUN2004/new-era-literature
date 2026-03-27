8dca9f7c3fb1909ad359abcd035c91edf4027e3d
GitHub Action <action@github.com>
2026-03-27 23:21:57 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-28 06:20",
+  "update_time": "2026-03-28 07:21",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,13 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "《寻找金蔷薇》在沪举办分享会,探寻文学创作的“珍贵尘埃”",
+      "url": "https://baijiahao.baidu.com/s?id=1860698065736126750&wfr=spider&for=pc",
+      "source": "文汇报",
+      "time": "前天12:38",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -30,11 +37,46 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
+      "title": "期刊导航站 | 2026年线上新刊推荐第四期: 藏山河于纸页,纳万象于...",
+      "url": "https://cul.sohu.com/a/1001477195_121106991",
+      "source": "搜狐网",
+      "time": "前天17:05",
+      "category": "other"
+    },
+    {
       "title": "《巴黎评论》首发余华小说新作",
       "url": "http://baijiahao.baidu.com/s?id=1860705385276777429&wfr=spider&for=pc",
       "source": "上观新闻",
       "time": "前天14:35",
       "category": "activity"
+    },
+    {
+      "title": "这场青年批评家研讨会,探讨数智时代如何重拾文学性",
+      "url": "https://baijiahao.baidu.com/s?id=1860797408873943273&wfr=spider&for=pc",
+      "source": "青瞳视角",
+      "time": "昨天14:56",
+      "category": "meeting"
+    },
+    {
+      "title": "广东省作协主席、中山大学教授谢有顺:城市赋美之路,在于美不美...",
+      "url": "https://news.ycwb.com/ikimvkhtil/content_54031500.htm",
+      "source": "金羊网",
+      "time": "昨天06:49",
+      "category": "other"
+    },
+    {
+      "title": "理论与批评| 郭冰茹:如何传统,怎样现代(节选)",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32830743",
+      "source": "澎湃新闻",
+      "time": "昨天13:03",
+      "category": "meeting"
+    },
+    {
+      "title": "安徽省国民经济和社会发展第十五个五年规划纲要",
+      "url": "http://ah.anhuinews.com/szxw/202603/t20260327_9180834.html",
+      "source": "中安在线网站",
+      "time": "昨天07:23",
+      "category": "other"
     }
   ]
 };
