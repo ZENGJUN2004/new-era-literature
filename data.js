4e72f8583ea610c0589da118e2365637fd2c2448
GitHub Action <action@github.com>
2026-03-27 08:38:34 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-27 15:53",
+  "update_time": "2026-03-27 16:38",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,67 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "李敬泽《引回风:随笔自选集》:自我追溯的“时间之书”",
-      "url": "https://baijiahao.baidu.com/s?id=1854627724132683640&wfr=spider&for=pc",
-      "source": "中国青年网",
-      "time": "1月18日",
-      "category": "other"
-    },
-    {
-      "title": "李敬泽做客西湖讲坛谈“情感考古”:在文学中探寻中国人情感的源头",
-      "url": "https://baijiahao.baidu.com/s?id=1856159539287434750&wfr=spider&for=pc",
-      "source": "浙江日报",
-      "time": "2月4日",
-      "category": "other"
-    },
-    {
-      "title": "2026中国科幻产业报告:总营收1261亿元",
-      "url": "https://www.cast.org.cn/xw/MTBD/art/2026/art_adacc783aaa84e21b0324a62493bdc8f.html",
-      "source": "中国科学技术协会",
-      "time": "今天",
-      "category": "voice"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "理论与批评 | 郭冰茹:如何传统,怎样现代(节选)",
-      "url": "http://www.thepaper.cn/newsDetail_forward_32830743",
-      "source": "澎湃新闻",
-      "time": "2小时前",
-      "category": "meeting"
-    },
-    {
-      "title": "一周文化讲座|诗歌、往事和City Walk",
-      "url": "https://baijiahao.baidu.com/s?id=1860785990636755651&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "3小时前",
-      "category": "meeting"
-    },
-    {
-      "title": "锐评|中国科幻何以迎来黄金时代",
-      "url": "https://baijiahao.baidu.com/s?id=1860779666238452869&wfr=spider&for=pc",
-      "source": "京报网",
-      "time": "6小时前",
-      "category": "voice"
-    },
-    {
-      "title": "《沙丘3》导演称其为惊悚片,风格转变引期待",
-      "url": "https://cul.sohu.com/a/1001463306_122255128",
-      "source": "搜狐网",
-      "time": "昨天16:41",
-      "category": "other"
-    },
-    {
-      "title": "世界科幻版图崛起新地标——写在2026中国科幻大会召开前夕",
-      "url": "https://baijiahao.baidu.com/s?id=1860721627511643793&wfr=spider&for=pc",
-      "source": "参考消息",
-      "time": "昨天18:50",
-      "category": "voice"
     },
     {
       "title": "《巴黎评论》首发余华小说新作",
