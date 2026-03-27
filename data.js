80531526f891f84392452653f74569eaa4063c17
GitHub Action <action@github.com>
2026-03-27 11:34:24 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-27 18:37",
+  "update_time": "2026-03-27 19:34",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,39 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "文学双周记|22家刊物答原创之问,邓紫棋入选银河奖引热议",
+      "url": "https://baijiahao.baidu.com/s?id=1853442602393722283&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "1月5日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "聆听文字回响,20种“年度好书”、5种“年度最美的书”荣膺2025...",
+      "url": "https://baijiahao.baidu.com/s?id=1860787056334517766&wfr=spider&for=pc",
+      "source": "潇湘晨报",
+      "time": "7小时前",
+      "category": "other"
+    },
+    {
+      "title": "一周文化讲座|诗歌、往事和City Walk",
+      "url": "https://baijiahao.baidu.com/s?id=1860785990636755651&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "7小时前",
+      "category": "meeting"
+    },
+    {
+      "title": "以书为渡,在阅读里安顿自己|北京实体书店活动预告(3月28日~4月3日)",
+      "url": "https://baijiahao.baidu.com/s?id=1860781066072953273&wfr=spider&for=pc",
+      "source": "北晚在线",
+      "time": "8小时前",
+      "category": "other"
     },
     {
       "title": "《巴黎评论》首发余华小说新作",
