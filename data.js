8a5838960c703b2684d97f74a407efc3d625303d
GitHub Action <action@github.com>
2026-03-29 15:21:18 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-29 22:24",
+  "update_time": "2026-03-29 23:21",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,10 +23,52 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "在AI时代,何为艺术?何为人?我们为何创作?",
+      "url": "https://baijiahao.baidu.com/s?id=1860771486600074242&wfr=spider&for=pc",
+      "source": "北青网",
+      "time": "前天08:04",
+      "category": "other"
+    },
+    {
+      "title": "媒体眼中海大的2025年!",
+      "url": "https://baijiahao.baidu.com/s?id=1856159417055495057&wfr=spider&for=pc",
+      "source": "中国海洋大学",
+      "time": "2月4日",
+      "category": "other"
+    },
+    {
+      "title": "光明文化周末:期刊看台",
+      "url": "https://baijiahao.baidu.com/s?id=1860760484269396945&wfr=spider&for=pc",
+      "source": "光明网",
+      "time": "前天04:35",
+      "category": "other"
+    },
+    {
+      "title": "“Z世代文学,向何处”,《十月》创作分享会尝试作答",
+      "url": "https://baijiahao.baidu.com/s?id=1859897163056459211&wfr=spider&for=pc",
+      "source": "北京日报客户端",
+      "time": "3月17日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "让诗歌语言重新发光,吴撇诗集《我在我里面放了什么》作品研讨会举办",
+      "url": "https://baijiahao.baidu.com/s?id=1860994775058579632&wfr=spider&for=pc",
+      "source": "北京日报客户端",
+      "time": "4小时前",
+      "category": "meeting"
+    },
+    {
+      "title": "长篇小说《群马》昆明研讨:从“边地文学”走向“中国文学”",
+      "url": "https://baijiahao.baidu.com/s?id=1860907387730358534&wfr=spider&for=pc",
+      "source": "中国新闻网",
+      "time": "昨天20:05",
       "category": "activity"
     }
   ]
