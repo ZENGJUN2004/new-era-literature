f62758f4c5f2486be2f0f5224d4b60b8e629c990
GitHub Action <action@github.com>
2026-03-29 07:44:39 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-29 14:06",
+  "update_time": "2026-03-29 15:44",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,52 +23,10 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "“Z世代文学,向何处”,《十月》创作分享会尝试作答",
-      "url": "https://baijiahao.baidu.com/s?id=1859897163056459211&wfr=spider&for=pc",
-      "source": "北京日报客户端",
-      "time": "3月17日",
-      "category": "other"
-    },
-    {
-      "title": "【钛晨报】国家医保局:医保药品目录调整专家评审工作已结束;把...",
-      "url": "https://www.tmtpost.com/6283071.html",
-      "source": "钛媒体官方网站",
-      "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "收获文学榜在上海揭晓!苏童《好天气》领衔长篇榜首",
-      "url": "https://www.jfdaily.com/news/detail?id=1062380",
-      "source": "上观",
-      "time": "2月3日",
-      "category": "other"
-    },
-    {
-      "title": "包头:一“马”当先揽英才",
-      "url": "https://baijiahao.baidu.com/s?id=1860805967753549795&wfr=spider&for=pc",
-      "source": "央广网",
-      "time": "前天17:12",
-      "category": "other"
-    },
-    {
-      "title": "万邦德深度报告:神经及罕见病领域转型新星",
-      "url": "https://baijiahao.baidu.com/s?id=1860815341539132130&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "前天19:43",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "孔学堂书局学术编辑部副主任王紫玥:光是目录都核对了10多遍|...",
-      "url": "https://baijiahao.baidu.com/s?id=1860870988075425260&wfr=spider&for=pc",
-      "source": "天眼新闻",
-      "time": "昨天09:40",
       "category": "activity"
     }
   ]
