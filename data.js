2d5f16dbbb16a597e02dcaa87780da32893f2be4
GitHub Action <action@github.com>
2026-03-30 12:48:55 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-30 19:45",
+  "update_time": "2026-03-30 20:48",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,74 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "“我每天都要写作啊!”苏菲·玛索携《暗河》亮相北京大学",
-      "url": "https://baijiahao.baidu.com/s?id=1860953217686731919&wfr=spider&for=pc",
-      "source": "中国青年报",
-      "time": "前天04:51",
-      "category": "other"
-    },
-    {
-      "title": "一蓑烟雨 江城相逢阿来携新作与李修文共话东坡精神",
-      "url": "http://news.cnhubei.com/content/2026-03/28/content_19892467.html",
-      "source": "荆楚网",
-      "time": "前天09:06",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "侧记|93岁宋遂良日记出版,何以清气郁如兰",
-      "url": "https://baijiahao.baidu.com/s?id=1861084020105453250&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "52分钟前",
-      "category": "activity"
-    },
-    {
-      "title": "话剧《平凡的世界》亮相泉州",
-      "url": "https://baijiahao.baidu.com/s?id=1861083198542396323&wfr=spider&for=pc",
-      "source": "湘湘带你看社会",
-      "time": "1小时前",
-      "category": "other"
-    },
-    {
-      "title": "董宇辉的话,其实预示过张雪峰的结局。此前他在直播中被问及张雪峰...",
-      "url": "https://baijiahao.baidu.com/s?id=1861079157452075192&wfr=spider&for=pc",
-      "source": "台海网",
-      "time": "1小时前",
-      "category": "other"
-    },
-    {
-      "title": "阿来专题讲座走进乐山 畅谈世界双遗产保护之道",
-      "url": "https://scnews.newssc.org/system/20260330/001601220.html",
-      "source": "四川新闻网",
-      "time": "3小时前",
-      "category": "meeting"
-    },
-    {
-      "title": "《等到青蝉坠落》长沙签售 丁墨与读者共赴“悬爱与救赎”之约",
-      "url": "https://baijiahao.baidu.com/s?id=1860961270014354466&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "昨天10:18",
-      "category": "other"
-    },
-    {
-      "title": "上三鲜 书圈顶流还是苏东坡",
-      "url": "https://baijiahao.baidu.com/s?id=1860954698460070572&wfr=spider&for=pc",
-      "source": "华声在线",
-      "time": "昨天08:08",
-      "category": "other"
-    },
-    {
-      "title": "以文学赋能乡村发展 文学大咖齐聚四川马边",
-      "url": "https://baijiahao.baidu.com/s?id=1861000814467442074&wfr=spider&for=pc",
-      "source": "中国新闻网",
-      "time": "昨天20:48",
-      "category": "other"
     }
   ]
 };
