e4fb2e7e7aed746d4ef47c4a42e8ee8f7e540fd9
GitHub Action <action@github.com>
2026-03-30 21:32:43 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-31 04:33",
+  "update_time": "2026-03-31 05:32",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,10 +23,31 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "李敬泽《引回风:随笔自选集》:自我追溯的“时间之书”",
+      "url": "https://baijiahao.baidu.com/s?id=1854627724132683640&wfr=spider&for=pc",
+      "source": "中国青年网",
+      "time": "1月18日",
+      "category": "other"
+    },
+    {
+      "title": "李敬泽做客西湖讲坛谈“情感考古”:在文学中探寻中国人情感的源头",
+      "url": "https://baijiahao.baidu.com/s?id=1856159539287434750&wfr=spider&for=pc",
+      "source": "浙江日报",
+      "time": "2月4日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "苏菲·玛索携新书《暗河》对话李敬泽张莉黄荭:作品被理解,远比...",
+      "url": "https://baijiahao.baidu.com/s?id=1861061115598253254&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "昨天12:47",
       "category": "activity"
     }
   ]
