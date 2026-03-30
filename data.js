da3757848c246df9947f520aa99a5bcf160158aa
GitHub Action <action@github.com>
2026-03-30 04:35:32 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-30 09:16",
+  "update_time": "2026-03-30 12:35",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,46 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "“你只管努力,剩下的交给时间!” 他是曲园学子杨浩然",
-      "url": "https://baijiahao.baidu.com/s?id=1859294217842455751&wfr=spider&for=pc",
-      "source": "曲阜师范大学",
-      "time": "3月11日",
-      "category": "other"
-    },
-    {
-      "title": "文学双周记|22家刊物答原创之问,邓紫棋入选银河奖引热议",
-      "url": "https://baijiahao.baidu.com/s?id=1853442602393722283&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "1月5日",
-      "category": "other"
-    },
-    {
-      "title": "中国当代文学研究会年度榜单发布",
-      "url": "https://baijiahao.baidu.com/s?id=1855942095675059485&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "2月2日",
-      "category": "other"
-    },
-    {
-      "title": "文学京彩季2026精品研推工程在京启幕",
-      "url": "https://baijiahao.baidu.com/s?id=1860917638173859418&wfr=spider&for=pc",
-      "source": "京报网",
-      "time": "前天22:33",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "以《诗歌的肖像》开启2026“文学京彩季”",
-      "url": "https://baijiahao.baidu.com/s?id=1860953249007262620&wfr=spider&for=pc",
-      "source": "中国青年报",
-      "time": "昨天03:09",
-      "category": "other"
     }
   ]
 };
