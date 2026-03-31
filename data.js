7a30f1736cdd9c08355da4cf5ec8602eb4db395f
GitHub Action <action@github.com>
2026-03-31 19:46:20 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-01 02:44",
+  "update_time": "2026-04-01 03:46",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,10 +23,59 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "虹书舫“超级文学邮轮”盛装启航,北外滩再添都市文化新地标",
+      "url": "http://baijiahao.baidu.com/s?id=1861099537713986256&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "前天22:11",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "...融合文商旅体展 “虹书舫”将打造国内首家以世界文学奖项为...",
+      "url": "https://www.shanghai.gov.cn/nw4411/20260331/36b884260cd6480c9b3097872d6df79f.html",
+      "source": "上海市人民政府",
+      "time": "昨天",
+      "category": "other"
+    },
+    {
+      "title": "水上世界文学之舟 “虹书舫”正式启航",
+      "url": "https://baijiahao.baidu.com/s?id=1861170051665326416&wfr=spider&for=pc",
+      "source": "新民晚报",
+      "time": "昨天17:39",
+      "category": "other"
+    },
+    {
+      "title": "这是一艘“文学邮轮”,虹书舫北外滩“起航”",
+      "url": "https://baijiahao.baidu.com/s?id=1861136172712551764&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "昨天08:41",
+      "category": "other"
+    },
+    {
+      "title": "海河遇珠江橘香润文脉",
+      "url": "https://baijiahao.baidu.com/s?id=1861153757912027478&wfr=spider&for=pc",
+      "source": "湘湘带你看社会",
+      "time": "昨天13:22",
+      "category": "other"
+    },
+    {
+      "title": "“海河之子,珠江寻根”:王松《橘红》新书分享会温情举行",
+      "url": "https://baijiahao.baidu.com/s?id=1861171750917200576&wfr=spider&for=pc",
+      "source": "扬子晚报",
+      "time": "昨天17:39",
+      "category": "activity"
+    },
+    {
+      "title": "“海河之子,珠江寻根”:王松《橘红》新书分享会在天津广东会馆举行",
+      "url": "https://baijiahao.baidu.com/s?id=1861176152784918954&wfr=spider&for=pc",
+      "source": "青瞳视角",
+      "time": "昨天19:16",
       "category": "activity"
     }
   ]
