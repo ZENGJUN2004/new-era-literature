cb6883d077592365c248c925be08f6f7fd7484e0
GitHub Action <action@github.com>
2026-03-31 22:24:31 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-01 05:28",
+  "update_time": "2026-04-01 06:24",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,10 +23,45 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "2026北京科幻之夜暨科幻星球盛典举办",
+      "url": "https://baijiahao.baidu.com/s?id=1861081311920752204&wfr=spider&for=pc",
+      "source": "央广网",
+      "time": "前天18:07",
+      "category": "voice"
+    },
+    {
+      "title": "2026北京科幻之夜暨科幻星球盛典举办 发布多项新品力作",
+      "url": "https://baijiahao.baidu.com/s?id=1861094554359550721&wfr=spider&for=pc",
+      "source": "中国新闻网",
+      "time": "前天21:39",
+      "category": "voice"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "陈思和:在斑斓驳杂的民间叙事间,构建严肃的文学旨意|新批评",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32854383",
+      "source": "澎湃新闻",
+      "time": "昨天12:22",
+      "category": "meeting"
+    },
+    {
+      "title": "《寻找金蔷薇》:保持“白心”,探寻文学创作的“珍贵尘埃”",
+      "url": "https://baijiahao.baidu.com/s?id=1861163950350834814&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "昨天16:02",
+      "category": "other"
+    },
+    {
+      "title": "解锁女性科幻的多元表达,《陌生的女孩》新书分享会举办",
+      "url": "https://baijiahao.baidu.com/s?id=1861147058602880524&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "昨天11:34",
       "category": "activity"
     }
   ]
