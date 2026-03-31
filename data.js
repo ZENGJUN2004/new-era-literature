c5d8f91edbfc7ea196de367e6a681864eec0d6e5
GitHub Action <action@github.com>
2026-03-31 10:43:36 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-31 17:02",
+  "update_time": "2026-03-31 18:43",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -27,6 +27,41 @@ window.LIT_DATA = {
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "《光荣亭》新书发布活动在福建上杭举行",
+      "url": "http://www.mzyfz.com/html/2203/2026-01-12/content-1669350.html",
+      "source": "民主与法制网",
+      "time": "1月12日",
+      "category": "activity"
+    },
+    {
+      "title": "寻找AI迷宫中的心灵坐标,老藤长篇小说《自愈者》新书发布会在京举行",
+      "url": "https://baijiahao.baidu.com/s?id=1853935548042845532&wfr=spider&for=pc",
+      "source": "潇湘晨报",
+      "time": "1月10日",
+      "category": "activity"
+    },
+    {
+      "title": "盘州本土作家创作的长篇历史纪实小说《乌蒙铁壁》新书发布",
+      "url": "https://baijiahao.baidu.com/s?id=1854571604814312489&wfr=spider&for=pc",
+      "source": "天眼新闻",
+      "time": "1月17日",
+      "category": "activity"
+    },
+    {
+      "title": "峡江文韵丨人民文学奖获奖作品《城防图》首发式在宜举行",
+      "url": "https://cul.sohu.com/a/1003344055_121106908",
+      "source": "搜狐网",
+      "time": "7小时前",
+      "category": "activity"
+    },
+    {
+      "title": "花城出版社引进出版苏菲·玛索《暗河》中译本",
+      "url": "https://baijiahao.baidu.com/s?id=1861089206748603775&wfr=spider&for=pc",
+      "source": "中国新闻网",
+      "time": "昨天20:14",
       "category": "activity"
     }
   ]
