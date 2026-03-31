1e9c7a9b7cdba2a9381ad433da119d29b9beeb78
GitHub Action <action@github.com>
2026-03-31 01:13:28 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-31 07:25",
+  "update_time": "2026-03-31 09:13",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,60 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "李敬泽《引回风:随笔自选集》:自我追溯的“时间之书”",
+      "url": "https://baijiahao.baidu.com/s?id=1854627724132683640&wfr=spider&for=pc",
+      "source": "中国青年网",
+      "time": "1月18日",
+      "category": "other"
+    },
+    {
+      "title": "李敬泽做客西湖讲坛谈“情感考古”:在文学中探寻中国人情感的源头",
+      "url": "https://baijiahao.baidu.com/s?id=1856159539287434750&wfr=spider&for=pc",
+      "source": "浙江日报",
+      "time": "2月4日",
+      "category": "other"
+    },
+    {
+      "title": "豆瓣高分、评论高赞,不出门的日子里,这6部纪录片治愈了我",
+      "url": "http://baijiahao.baidu.com/s?id=1730506789958714144&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "meeting"
+    },
+    {
+      "title": "到底谁把这本书读完了呀?",
+      "url": "https://cul.sohu.com/a/1002643464_701608",
+      "source": "搜狐网",
+      "time": "前天19:56",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "苏菲·玛索携新书《暗河》对话李敬泽张莉黄荭:作品被理解,远比...",
+      "url": "https://baijiahao.baidu.com/s?id=1861061115598253254&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "昨天12:47",
+      "category": "activity"
+    },
+    {
+      "title": "这才是中国人的底气",
+      "url": "https://baijiahao.baidu.com/s?id=1861042673731057365&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "昨天07:56",
+      "category": "other"
+    },
+    {
+      "title": "豆瓣9.4,这本火了30年的入门书,终于来了!",
+      "url": "http://app.myzaker.com/news/article.php?pk=69ca0e227f780b1939000003",
+      "source": "ZAKER",
+      "time": "昨天13:46",
+      "category": "other"
     }
   ]
 };
