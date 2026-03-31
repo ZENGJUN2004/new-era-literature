6f662f71db9bc41581e43e68755e9555cc36909c
GitHub Action <action@github.com>
2026-03-31 12:47:28 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-03-31 19:40",
+  "update_time": "2026-03-31 20:47",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,74 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "李易特 | 承拟汉武与从“经史”转向“文采”的大明时代",
+      "url": "https://history.sohu.com/a/998707494_121119368",
+      "source": "搜狐网",
+      "time": "3月19日",
+      "category": "other"
+    },
+    {
+      "title": "女性为何写作?她们用文字打破边界",
+      "url": "https://baijiahao.baidu.com/s?id=1855001128564567122&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "1月22日",
+      "category": "other"
+    },
+    {
+      "title": "被揭开的女性困境,100多年前伍尔夫就写过了",
+      "url": "https://www.lifeweek.com.cn/h5/article/detail.do?artId=264179",
+      "source": "三联生活周刊",
+      "time": "",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "余华小说新作首发《巴黎评论》,托宾谈爱尔兰文学被低估作家|3月...",
+      "url": "http://www.thepaper.cn/newsDetail_forward_32854661",
+      "source": "澎湃新闻",
+      "time": "8小时前",
+      "category": "activity"
+    },
+    {
+      "title": "作家苏菲·玛索:我的职业是演员,我把“虚构”当成“现实”",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32858964",
+      "source": "澎湃新闻",
+      "time": "昨天08:21",
+      "category": "other"
+    },
+    {
+      "title": "在“陌生且熟悉的她们”里寻找中国声音,女性科幻分享会在京举办",
+      "url": "https://news.ycwb.com/ikimvkhthk/content_54037263.htm",
+      "source": "金羊网",
+      "time": "昨天14:14",
+      "category": "voice"
+    },
+    {
+      "title": "贺桂梅|阅读的本事全在“笨功夫”里",
+      "url": "http://www.thepaper.cn/newsDetail_forward_32855333",
+      "source": "澎湃新闻",
+      "time": "昨天12:54",
+      "category": "other"
+    },
+    {
+      "title": "文学双周记|读书博主被曝“不读书”?苏菲·玛索也是作家",
+      "url": "https://baijiahao.baidu.com/s?id=1861057623019264261&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "昨天11:52",
+      "category": "other"
+    },
+    {
+      "title": "跨越时空看女性的困境与力量——王鹤携《冷香》《芳尘》对话洁尘...",
+      "url": "https://baijiahao.baidu.com/s?id=1861073510348422583&wfr=spider&for=pc",
+      "source": "文汇报",
+      "time": "昨天16:05",
+      "category": "other"
     }
   ]
 };
