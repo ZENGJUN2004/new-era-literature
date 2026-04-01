7b835d2fbee9f0893216077956663d0f18014bdb
GitHub Action <action@github.com>
2026-04-01 14:24:26 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-01 20:50",
+  "update_time": "2026-04-01 22:24",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,80 +23,10 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "“你只管努力,剩下的交给时间!” 他是曲园学子杨浩然",
-      "url": "https://baijiahao.baidu.com/s?id=1859294217842455751&wfr=spider&for=pc",
-      "source": "曲阜师范大学",
-      "time": "3月11日",
-      "category": "other"
-    },
-    {
       "title": "文学双周记|22家刊物答原创之问,邓紫棋入选银河奖引热议",
       "url": "https://baijiahao.baidu.com/s?id=1853442602393722283&wfr=spider&for=pc",
       "source": "澎湃新闻",
       "time": "1月5日",
-      "category": "other"
-    },
-    {
-      "title": "中国当代文学研究会年度榜单发布",
-      "url": "https://baijiahao.baidu.com/s?id=1855942095675059485&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "2月2日",
-      "category": "other"
-    },
-    {
-      "title": "李敬泽《引回风:随笔自选集》:自我追溯的“时间之书”",
-      "url": "https://baijiahao.baidu.com/s?id=1854627724132683640&wfr=spider&for=pc",
-      "source": "中国青年网",
-      "time": "1月18日",
-      "category": "other"
-    },
-    {
-      "title": "李敬泽做客西湖讲坛谈“情感考古”:在文学中探寻中国人情感的源头",
-      "url": "https://baijiahao.baidu.com/s?id=1856159539287434750&wfr=spider&for=pc",
-      "source": "浙江日报",
-      "time": "2月4日",
-      "category": "other"
-    },
-    {
-      "title": "“被理解,是文学最美丽的部分”——苏菲·玛索《暗河》新书分享会...",
-      "url": "https://cul.sohu.com/a/1002950152_826924",
-      "source": "搜狐网",
-      "time": "前天14:38",
-      "category": "activity"
-    },
-    {
-      "title": "苏菲·玛索携新书《暗河》对话李敬泽张莉黄荭:作品被理解,远比...",
-      "url": "https://baijiahao.baidu.com/s?id=1861061115598253254&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "前天12:47",
-      "category": "activity"
-    },
-    {
-      "title": "澎湃思想周报丨出版与书评的危机;社交媒体的“滑铁卢时刻”",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32856043",
-      "source": "澎湃新闻",
-      "time": "前天13:16",
-      "category": "activity"
-    },
-    {
-      "title": "【钛晨报】国家医保局:医保药品目录调整专家评审工作已结束;把...",
-      "url": "https://www.tmtpost.com/6283071.html",
-      "source": "钛媒体官方网站",
-      "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "收获文学榜在上海揭晓!苏童《好天气》领衔长篇榜首",
-      "url": "https://www.jfdaily.com/news/detail?id=1062380",
-      "source": "上观",
-      "time": "2月3日",
-      "category": "other"
-    },
-    {
-      "title": "科技赋能让甘肃草原绿富同兴",
-      "url": "https://www.forestry.gov.cn/lyj/1/dfdt/20260401/665594.html",
-      "source": "国家林业和草原局",
-      "time": "今天",
       "category": "other"
     },
     {
@@ -107,95 +37,11 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "春风月榜|三月,审视、沉思与女性力量",
-      "url": "https://baijiahao.baidu.com/s?id=1861258919977331553&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "3小时前",
-      "category": "other"
-    },
-    {
-      "title": "宋遂良先生文献与研究“新集两种”阅后感:在文献中看见“人”",
-      "url": "https://baijiahao.baidu.com/s?id=1861238788743424644&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "8小时前",
-      "category": "other"
-    },
-    {
-      "title": "吉峰科技(300022):2025年度向特定对象发行股票募集说明书(申报稿)",
-      "url": "https://sj.cfi.cn/p20260401001966.html",
-      "source": "中财网",
-      "time": "8小时前",
-      "category": "other"
-    },
-    {
-      "title": "小逄观星丨吾爱吾师,究竟怎么爱",
-      "url": "https://baijiahao.baidu.com/s?id=1861220383711302239&wfr=spider&for=pc",
-      "source": "大众日报",
-      "time": "13小时前",
-      "category": "other"
-    },
-    {
-      "title": "扎根烟火 以文共情",
-      "url": "https://baijiahao.baidu.com/s?id=1861230952541248208&wfr=spider&for=pc",
-      "source": "三秦都市报",
-      "time": "13小时前",
-      "category": "other"
-    },
-    {
-      "title": "中共中国作家协会党组关于二十届中央第四轮巡视整改进展情况的通报",
-      "url": "https://baijiahao.baidu.com/s?id=1861183640247618807&wfr=spider&for=pc",
-      "source": "北京日报客户端",
-      "time": "昨天21:16",
-      "category": "other"
-    },
-    {
-      "title": "从“读书时代”到“文献时代”_理论·学术_澎湃新闻-The Paper",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32866443",
-      "source": "澎湃新闻",
-      "time": "昨天15:22",
-      "category": "meeting"
-    },
-    {
-      "title": "生活在别处|远方与身旁——我们的别处,我们的成长",
-      "url": "https://www.thepaper.cn/newsDetail_forward_32858828",
-      "source": "澎湃新闻",
-      "time": "昨天12:21",
-      "category": "other"
-    },
-    {
-      "title": "创新地方司法档案的“打开方式”",
-      "url": "http://www.legaldaily.com.cn/fxjy/content/2026-03/31/content_9365663.html",
-      "source": "法治网",
-      "time": "昨天15:09",
-      "category": "other"
-    },
-    {
-      "title": "盐城市高中语文学科中心“非虚构写作教学”主题研讨活动举办",
-      "url": "http://www.jyb.cn/rmtxwwyyq/jyxx1306/202603/t20260331_2111461526.html",
-      "source": "中国教育新闻网",
-      "time": "昨天",
-      "category": "voice"
-    },
-    {
-      "title": "赛分科技大涨超10%,大手笔回购!科创创新药ETF汇添富(589120)昨日...",
-      "url": "https://www.jiemian.com/article/14188074.html",
-      "source": "界面新闻",
-      "time": "昨天10:55",
-      "category": "other"
-    },
-    {
-      "title": "128株罂粟混种菜地,只为一口“美味”...",
-      "url": "http://www.thepaper.cn/newsDetail_forward_32868061",
-      "source": "澎湃新闻",
-      "time": "昨天18:54",
-      "category": "other"
-    },
-    {
-      "title": "解码亚盛医药2025年财报:商业双引擎提速,全球化创新即将收获",
-      "url": "https://www.jiemian.com/article/14187704.html",
-      "source": "界面新闻",
-      "time": "昨天10:41",
-      "category": "other"
+      "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
+      "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "1月10日",
+      "category": "activity"
     },
     {
       "title": "余华小说新作首发《巴黎评论》,托宾谈爱尔兰文学被低估作家|3月...",
