629e7ccd3da68a0155ab0c80f9bdb77086bf2d44
GitHub Action <action@github.com>
2026-04-02 14:15:36 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-02 20:44",
+  "update_time": "2026-04-02 22:15",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,10 +23,66 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "李易特 | 承拟汉武与从“经史”转向“文采”的大明时代",
+      "url": "https://history.sohu.com/a/998707494_121119368",
+      "source": "搜狐网",
+      "time": "3月19日",
+      "category": "other"
+    },
+    {
+      "title": "在AI时代,何为艺术?何为人?我们为何创作?",
+      "url": "https://baijiahao.baidu.com/s?id=1861175304513678141&wfr=spider&for=pc",
+      "source": "千龙网",
+      "time": "前天19:04",
+      "category": "other"
+    },
+    {
+      "title": "陈思和:在斑斓驳杂的民间叙事间,构建严肃的文学旨意|新批评",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32854383",
+      "source": "澎湃新闻",
+      "time": "前天12:22",
+      "category": "meeting"
+    },
+    {
+      "title": "文学双周记|22家刊物答原创之问,邓紫棋入选银河奖引热议",
+      "url": "https://baijiahao.baidu.com/s?id=1853442602393722283&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "1月5日",
+      "category": "other"
+    },
+    {
+      "title": "盐城市高中语文学科中心“非虚构写作教学”主题研讨活动举办",
+      "url": "http://www.jyb.cn/rmtxwwyyq/jyxx1306/202603/t20260331_2111461526_wap.html",
+      "source": "中国教育新闻网",
+      "time": "前天",
+      "category": "voice"
+    },
+    {
+      "title": "什么样的日记可以为历史作注?",
+      "url": "https://baijiahao.baidu.com/s?id=1861186755962675805&wfr=spider&for=pc",
+      "source": "大众日报",
+      "time": "前天22:06",
+      "category": "other"
+    },
+    {
+      "title": "刘亮程受聘南师大驻(地)作家,作家驻校延伸至中小学",
+      "url": "http://baijiahao.baidu.com/s?id=1861164758290821218&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "前天16:16",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
+      "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "1月10日",
       "category": "activity"
     },
     {
@@ -35,6 +91,83 @@ window.LIT_DATA = {
       "source": "澎湃新闻",
       "time": "前天12:22",
       "category": "activity"
+    },
+    {
+      "title": "丁墨与读者共赴“悬爱与救赎”之约,《等到青蝉坠落》新书签售会...",
+      "url": "https://baijiahao.baidu.com/s?id=1861358915398985391&wfr=spider&for=pc",
+      "source": "潇湘晨报",
+      "time": "2小时前",
+      "category": "activity"
+    },
+    {
+      "title": "茅奖得主刘亮程打假,从AI盗脸到AI伪文,伪文化工业当休!",
+      "url": "http://baijiahao.baidu.com/s?id=1861356719528746622&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "3小时前",
+      "category": "other"
+    },
+    {
+      "title": "在世界与文学之间——读季进新著《世界中的当代文学》",
+      "url": "http://baijiahao.baidu.com/s?id=1861351411612897509&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "4小时前",
+      "category": "other"
+    },
+    {
+      "title": "一位享誉全球的文学批评家的悲剧研究力作,梳理悲剧艺术的历史脉络",
+      "url": "https://baijiahao.baidu.com/s?id=1861346023561590171&wfr=spider&for=pc",
+      "source": "青瞳视角",
+      "time": "5小时前",
+      "category": "meeting"
+    },
+    {
+      "title": "茅盾文学奖得主刘亮程在线打假,称被AI仿写文章还假冒署名差点编入...",
+      "url": "https://baijiahao.baidu.com/s?id=1861346841052876577&wfr=spider&for=pc",
+      "source": "大象新闻",
+      "time": "6小时前",
+      "category": "other"
+    },
+    {
+      "title": "大山里的学习时光:关于河池高中的个人记忆",
+      "url": "http://www.thepaper.cn/newsDetail_forward_32871047",
+      "source": "澎湃新闻",
+      "time": "9小时前",
+      "category": "other"
+    },
+    {
+      "title": "真正的文学创作,是心怀家国、肩担道义的神圣使命",
+      "url": "https://www.81.cn/yd_208600/16452748.html",
+      "source": "中国军网",
+      "time": "9小时前",
+      "category": "other"
+    },
+    {
+      "title": "近代化的目的是为妇女争取权利,但过程中又反复牺牲她们的权利|专访",
+      "url": "https://baijiahao.baidu.com/s?id=1861326112224854816&wfr=spider&for=pc",
+      "source": "界面新闻",
+      "time": "11小时前",
+      "category": "other"
+    },
+    {
+      "title": "宋遂良先生文献与研究“新集两种”阅后感:在文献中看见“人”",
+      "url": "https://baijiahao.baidu.com/s?id=1861238788743424644&wfr=spider&for=pc",
+      "source": "齐鲁壹点",
+      "time": "昨天11:53",
+      "category": "other"
+    },
+    {
+      "title": "春风月榜|三月,审视、沉思与女性力量",
+      "url": "https://baijiahao.baidu.com/s?id=1861258919977331553&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "昨天17:12",
+      "category": "other"
+    },
+    {
+      "title": "河声丨刘震云获国际大奖:以市井烟火绘铸就文学丰碑",
+      "url": "https://baijiahao.baidu.com/s?id=1861242341914266969&wfr=spider&for=pc",
+      "source": "大河网",
+      "time": "昨天11:45",
+      "category": "other"
     }
   ]
 };
