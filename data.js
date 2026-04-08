be79ca8bde6a10bc4e0ea1c75e4c6470838837bf
GitHub Action <action@github.com>
2026-04-08 08:05:52 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-08 14:14",
+  "update_time": "2026-04-08 16:05",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,38 +23,10 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "豆瓣2025年度图书榜单出炉你打卡了几本?",
-      "url": "https://cul.sohu.com/a/973411847_121106994",
-      "source": "搜狐网",
-      "time": "1月7日",
-      "category": "other"
-    },
-    {
-      "title": "长篇小说《大海风》入选中国当代文学研究会年度榜单作者赵德发...",
-      "url": "https://baijiahao.baidu.com/s?id=1853815024647126329&wfr=spider&for=pc",
-      "source": "闪电新闻",
-      "time": "1月9日",
-      "category": "activity"
-    },
-    {
-      "title": "2025豆瓣年度榜单:从经典到先锋,这10部作品是时代的精神注脚",
-      "url": "https://cul.sohu.com/a/976892771_121123844",
-      "source": "搜狐网",
-      "time": "1月16日",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "佳作力推|禹风:抵押出去的好房子(中篇小说)",
-      "url": "https://fashion.sohu.com/a/1006140529_121119387",
-      "source": "搜狐时尚频道",
-      "time": "昨天08:33",
       "category": "activity"
     }
   ]
