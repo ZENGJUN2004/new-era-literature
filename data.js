258ae835e76096fdac682b4eaa4773186469f931
GitHub Action <action@github.com>
2026-04-10 16:39:24 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-10 23:40",
+  "update_time": "2026-04-11 00:39",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,20 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "济南优势工业产品目录公布 天鹅股份核心农机装备入选",
-      "url": "https://stock.10jqka.com.cn/20260408/c675834775.shtml",
-      "source": "同花顺",
-      "time": "前天16:33",
-      "category": "activity"
-    },
-    {
-      "title": "...开展《2026年度上海市智能机器人标杆企业与应用场景推荐目录...",
-      "url": "http://baijiahao.baidu.com/s?id=1861907357138835619&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "前天20:24",
-      "category": "activity"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -44,17 +30,10 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "小说《好天气》拍了5集AI微电影,苏童:我支持",
-      "url": "https://baijiahao.baidu.com/s?id=1861874978356591035&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "前天12:24",
-      "category": "activity"
-    },
-    {
       "title": "杨年华新书《文成公主民间故事集》《神奇八宿》在拉萨首发",
       "url": "http://www.tibet.cn/cn/culture/202604/t20260409_7958655.html",
       "source": "中国西藏网",
-      "time": "昨天09:37",
+      "time": "前天09:37",
       "category": "activity"
     }
   ]
