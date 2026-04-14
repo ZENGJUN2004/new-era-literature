55a47b670bb4867ef82299199872a2654b78784b
GitHub Action <action@github.com>
2026-04-14 08:20:06 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-14 14:25",
+  "update_time": "2026-04-14 16:20",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,158 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "中国当代文学研究会年度榜单发布",
+      "url": "https://baijiahao.baidu.com/s?id=1855942095675059485&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "2月2日",
+      "category": "other"
+    },
+    {
+      "title": "【专访】范湉湉:每个人都想成为幸运儿,这件事让我觉得可笑",
+      "url": "https://m.jiemian.com/video/AGQCNghuB2EBO1Vl.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "other"
+    },
+    {
+      "title": "在AI时代,何为艺术?何为人?我们为何创作?",
+      "url": "https://baijiahao.baidu.com/s?id=1860771486600074242&wfr=spider&for=pc",
+      "source": "北青网",
+      "time": "3月27日",
+      "category": "other"
+    },
+    {
+      "title": "视频播客:缝纫机与雨伞在解剖台上的相",
+      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1090713",
+      "source": "上观",
+      "time": "4月3日",
+      "category": "other"
+    },
+    {
+      "title": "沅水文脉的当代延续——马珂散文随笔与文学评论审美观察",
+      "url": "https://baijiahao.baidu.com/s?id=1858377455122319929&wfr=spider&for=pc",
+      "source": "潇湘晨报",
+      "time": "2月28日",
+      "category": "meeting"
+    },
+    {
+      "title": "中国科幻全球搜索热度5年间增长3.29倍 整体呈加速上升态势",
+      "url": "https://baijiahao.baidu.com/s?id=1860958564566605987&wfr=spider&for=pc",
+      "source": "中国新闻网",
+      "time": "3月29日",
+      "category": "voice"
+    },
+    {
+      "title": "在工业遗址聆听“普通人的故事” 邱兵《鲟鱼》分享会走进株洲",
+      "url": "https://baijiahao.baidu.com/s?id=1862258889942476840&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "前天18:03",
+      "category": "other"
+    },
+    {
+      "title": "《鲟鱼》洄游至株洲!邱兵分享书里书外动人故事",
+      "url": "https://baijiahao.baidu.com/s?id=1862270377114310990&wfr=spider&for=pc",
+      "source": "株洲新闻网",
+      "time": "前天20:32",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "写青年|龙秀梅 张文昊:一朵不凋谢的花:丝线缠绕的青春",
+      "url": "http://www.thepaper.cn/newsDetail_forward_32958546",
+      "source": "澎湃新闻",
+      "time": "4小时前",
+      "category": "other"
+    },
+    {
+      "title": "散皮诗集《置身于别处》学术研讨会在济南举行",
+      "url": "https://baijiahao.baidu.com/s?id=1862338313704780184&wfr=spider&for=pc",
+      "source": "济南时报",
+      "time": "昨天13:50",
+      "category": "meeting"
+    },
+    {
+      "title": "书写西部大地的四时轮回,80后代表作家王选散文集《梨花消息》出版",
+      "url": "https://baijiahao.baidu.com/s?id=1862335274786467663&wfr=spider&for=pc",
+      "source": "潇湘晨报",
+      "time": "昨天14:22",
+      "category": "activity"
+    },
+    {
+      "title": "莫言自曝戒不掉刷短视频,以此为灵感创作新书《人呐》,收录作品最...",
+      "url": "https://baijiahao.baidu.com/s?id=1862340451767899591&wfr=spider&for=pc",
+      "source": "21世纪经济报道",
+      "time": "昨天15:43",
+      "category": "activity"
+    },
+    {
+      "title": "网络文艺创作与情感动力机制",
+      "url": "https://baijiahao.baidu.com/s?id=1862316102423725985&wfr=spider&for=pc",
+      "source": "环球网",
+      "time": "昨天08:25",
+      "category": "other"
+    },
+    {
+      "title": "“快乐阅读”不够,有些学问必须苦读",
+      "url": "https://pinglun.youth.cn/youthtopics/202604/t20260413_16604293.htm",
+      "source": "中国青年网",
+      "time": "昨天09:58",
+      "category": "other"
+    },
+    {
+      "title": "《2025中国网络文学发展研究报告》发布,网络文学成“新大众文艺...",
+      "url": "https://baijiahao.baidu.com/s?id=1862362450957887866&wfr=spider&for=pc",
+      "source": "潇湘晨报",
+      "time": "昨天21:33",
+      "category": "other"
+    },
+    {
+      "title": "网文作者规模超3200万,社科院发布《2025中国网络文学发展研究报告》",
+      "url": "http://baijiahao.baidu.com/s?id=1862359841537680152&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "昨天20:51",
+      "category": "other"
+    },
+    {
+      "title": "网络文学成新大众文艺重要支点,2025网文IP改编市场规模达3676.1亿元",
+      "url": "https://baijiahao.baidu.com/s?id=1862353859845326367&wfr=spider&for=pc",
+      "source": "京报网",
+      "time": "昨天18:59",
+      "category": "other"
+    },
+    {
+      "title": "《2025中国网络文学发展研究报告》发布 网文成为新大众文艺重要支点",
+      "url": "https://baijiahao.baidu.com/s?id=1862366308850339405&wfr=spider&for=pc",
+      "source": "新民晚报",
+      "time": "昨天22:32",
+      "category": "other"
+    },
+    {
+      "title": "中国社会科学院:网络文学成“新大众文艺”重要支点",
+      "url": "https://baijiahao.baidu.com/s?id=1862348638458157110&wfr=spider&for=pc",
+      "source": "三秦都市报",
+      "time": "昨天17:52",
+      "category": "other"
+    },
+    {
+      "title": "网文 IP 改编爆发式增长 2025年中国网络文学市场多项数据创新高",
+      "url": "https://baijiahao.baidu.com/s?id=1862359715315277138&wfr=spider&for=pc",
+      "source": "东方网",
+      "time": "昨天19:44",
+      "category": "other"
+    },
+    {
+      "title": "烟火落笔成珍,大众书写立心",
+      "url": "https://baijiahao.baidu.com/s?id=1862326927168149150&wfr=spider&for=pc",
+      "source": "湘湘带你看社会",
+      "time": "昨天12:08",
+      "category": "other"
     }
   ]
 };
