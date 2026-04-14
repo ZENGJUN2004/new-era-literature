a0d132d8a574de0c3c68a201a0eb7f3c386f2bd1
GitHub Action <action@github.com>
2026-04-14 11:50:20 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-14 18:06",
+  "update_time": "2026-04-14 19:50",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,115 +23,10 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "沅水文脉的当代延续——马珂散文随笔与文学评论审美观察",
-      "url": "https://baijiahao.baidu.com/s?id=1858377455122319929&wfr=spider&for=pc",
-      "source": "潇湘晨报",
-      "time": "2月28日",
-      "category": "meeting"
-    },
-    {
-      "title": "茅盾文学奖得主刘亮程在线打假,称被AI仿写文章还假冒署名差点编入...",
-      "url": "https://baijiahao.baidu.com/s?id=1861346841052876577&wfr=spider&for=pc",
-      "source": "大象新闻",
-      "time": "4月2日",
-      "category": "other"
-    },
-    {
-      "title": "从《我的阿勒泰》到苏菲·玛索的新书《暗河》,花城文学院3岁了!",
-      "url": "https://baijiahao.baidu.com/s?id=1860805556134866933&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "3月27日",
-      "category": "activity"
-    },
-    {
-      "title": "媒体眼中海大的2025年!",
-      "url": "https://baijiahao.baidu.com/s?id=1856159417055495057&wfr=spider&for=pc",
-      "source": "中国海洋大学",
-      "time": "2月4日",
-      "category": "other"
-    },
-    {
-      "title": "《十月》杂志举办青年作家创作分享会",
-      "url": "https://baijiahao.baidu.com/s?id=1860050202536776799&wfr=spider&for=pc",
-      "source": "人民网",
-      "time": "3月19日",
-      "category": "other"
-    },
-    {
-      "title": "内塔尼亚胡腐败案定于12日恢复审理,若受贿罪名成立或面临最高10年...",
-      "url": "https://baijiahao.baidu.com/s?id=1862258481822606636&wfr=spider&for=pc",
-      "source": "九派新闻",
-      "time": "前天18:00",
-      "category": "other"
-    },
-    {
-      "title": "淘宝20年,漫长又短暂的季节",
-      "url": "https://36kr.com/p/2251449840480132",
-      "source": "36kr",
-      "time": "",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "经典常谈丨“必须从最过硬的事实出发”",
-      "url": "https://baijiahao.baidu.com/s?id=1862431008090334961&wfr=spider&for=pc",
-      "source": "中国纪检监察报",
-      "time": "2小时前",
-      "category": "other"
-    },
-    {
-      "title": "人民论坛每日推荐(5篇)",
-      "url": "https://baijiahao.baidu.com/s?id=1862405047914354936&wfr=spider&for=pc",
-      "source": "人民网",
-      "time": "10小时前",
-      "category": "meeting"
-    },
-    {
-      "title": "莫言自曝戒不掉刷短视频,以此为灵感创作新书《人呐》,收录作品最...",
-      "url": "https://baijiahao.baidu.com/s?id=1862340451767899591&wfr=spider&for=pc",
-      "source": "21世纪经济报道",
-      "time": "昨天15:43",
-      "category": "activity"
-    },
-    {
-      "title": "网络文艺创作与情感动力机制",
-      "url": "https://baijiahao.baidu.com/s?id=1862316102423725985&wfr=spider&for=pc",
-      "source": "环球网",
-      "time": "昨天08:25",
-      "category": "other"
-    },
-    {
-      "title": "“快乐阅读”不够,有些学问必须苦读",
-      "url": "https://pinglun.youth.cn/youthtopics/202604/t20260413_16604293.htm",
-      "source": "中国青年网",
-      "time": "昨天09:58",
-      "category": "other"
-    },
-    {
-      "title": "鲁奖得主周晓枫:电影以强画面感说话,写作也当具有这种力量",
-      "url": "https://baijiahao.baidu.com/s?id=1862369999585509389&wfr=spider&for=pc",
-      "source": "封面新闻",
-      "time": "昨天23:33",
-      "category": "other"
-    },
-    {
-      "title": "书写西部大地的四时轮回,80后代表作家王选散文集《梨花消息》出版",
-      "url": "https://baijiahao.baidu.com/s?id=1862335274786467663&wfr=spider&for=pc",
-      "source": "潇湘晨报",
-      "time": "昨天14:22",
-      "category": "activity"
-    },
-    {
-      "title": "《摇铃铛的人》新书分享会将于北京举行 李敬泽等嘉宾共话西部家族...",
-      "url": "http://news.10jqka.com.cn/20260413/c675946309.shtml",
-      "source": "同花顺财经",
-      "time": "昨天17:13",
       "category": "activity"
     }
   ]
