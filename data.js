89c7216e22aa1472382f03524ffe6126dbd7fa62
GitHub Action <action@github.com>
2026-04-15 20:36:57 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-16 03:07",
+  "update_time": "2026-04-16 04:36",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,13 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "中国锂电十年:欧美抢跑,日韩超车,中国企业后来居上",
+      "url": "https://36kr.com/p/1937106931305089",
+      "source": "36kr",
+      "time": "",
+      "category": "other"
+    },
+    {
       "title": "2026滋补行业精选,十佳灵芝孢子粉国货,品质过硬收获大众认可",
       "url": "https://baijiahao.baidu.com/s?id=1862435436165218453&wfr=spider&for=pc",
       "source": "中国农科新闻网",
@@ -41,6 +48,20 @@ window.LIT_DATA = {
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "一部小说的多重解读:关于《相对性》的讨论",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32891114",
+      "source": "澎湃新闻",
+      "time": "4月3日",
+      "category": "activity"
+    },
+    {
+      "title": "时隔六年莫言小说“上新”,喊话读者:像刷短视频那样刷新书吧",
+      "url": "https://baijiahao.baidu.com/s?id=1862414856918967976&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "前天11:14",
       "category": "activity"
     }
   ]
