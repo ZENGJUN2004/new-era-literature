8002518759bb9874015e38a35e21de4acf9f8e7f
GitHub Action <action@github.com>
2026-04-15 08:21:21 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-15 14:26",
+  "update_time": "2026-04-15 16:21",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,13 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "中国锂电十年:欧美抢跑,日韩超车,中国企业后来居上",
+      "url": "https://36kr.com/p/1937106931305089",
+      "source": "36kr",
+      "time": "",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -42,6 +49,13 @@ window.LIT_DATA = {
       "source": "顶端新闻",
       "time": "昨天12:47",
       "category": "other"
+    },
+    {
+      "title": "时隔六年莫言小说“上新”,喊话读者:像刷短视频那样刷新书吧",
+      "url": "https://baijiahao.baidu.com/s?id=1862414856918967976&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "昨天11:14",
+      "category": "activity"
     }
   ]
 };
