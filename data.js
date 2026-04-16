eadbd4b5692fe3bd143341da3f21d7acf1569102
GitHub Action <action@github.com>
2026-04-16 16:16:48 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-16 22:57",
+  "update_time": "2026-04-17 00:16",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,53 +23,11 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "迈向跨界融合的“大文学”时代",
-      "url": "http://www.xinhuanet.com/book/20260120/d3fa6fd5f93048fc88a2d3be4ef48224/c.html",
-      "source": "新华网",
-      "time": "1月20日",
-      "category": "other"
-    },
-    {
-      "title": "“文学里的虹口”作家访谈第三季活动举行",
-      "url": "https://m.thepaper.cn/newsDetail_forward_32966491",
-      "source": "澎湃新闻",
-      "time": "前天12:04",
-      "category": "other"
-    },
-    {
-      "title": "2026滋补行业精选,十佳灵芝孢子粉国货,品质过硬收获大众认可",
-      "url": "https://baijiahao.baidu.com/s?id=1862435436165218453&wfr=spider&for=pc",
-      "source": "中国农科新闻网",
-      "time": "前天16:54",
-      "category": "other"
-    },
-    {
-      "title": "《长城》主编张雅丽:不迎合潮流,只引领精神|顶端文学名刊访谈",
-      "url": "https://baijiahao.baidu.com/s?id=1862424072859094860&wfr=spider&for=pc",
-      "source": "顶端新闻",
-      "time": "前天12:47",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "《中原文学》主编李辉:破圈不破底,风依旧滚烫|顶端文学名刊访谈",
-      "url": "https://baijiahao.baidu.com/s?id=1862517996245963491&wfr=spider&for=pc",
-      "source": "顶端新闻",
-      "time": "昨天12:23",
-      "category": "other"
-    },
-    {
-      "title": "粤好书|橘红香中品粤韵",
-      "url": "https://news.dayoo.com/gzrbrmt/202604/15/170636_54948267.htm",
-      "source": "大洋网",
-      "time": "昨天16:05",
-      "category": "other"
     }
   ]
 };
