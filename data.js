590a2a81cd534f19a646db99b4b4e841c30f1c5d
GitHub Action <action@github.com>
2026-04-18 16:26:27 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-18 23:28",
+  "update_time": "2026-04-19 00:26",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,27 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "“Z世代文学,向何处”,《十月》创作分享会尝试作答",
-      "url": "https://baijiahao.baidu.com/s?id=1859897163056459211&wfr=spider&for=pc",
-      "source": "北京日报客户端",
-      "time": "3月17日",
-      "category": "other"
-    },
-    {
-      "title": "写出属于自己的独特体验,Z世代文学向何处?",
-      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1082705",
-      "source": "上观",
-      "time": "3月20日",
-      "category": "other"
-    },
-    {
-      "title": "《十月》杂志举办青年作家创作分享会",
-      "url": "https://baijiahao.baidu.com/s?id=1860050202536776799&wfr=spider&for=pc",
-      "source": "人民网",
-      "time": "3月19日",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -54,7 +33,7 @@ window.LIT_DATA = {
       "title": "一周文化讲座|世界上本没有爱情",
       "url": "https://www.thepaper.cn/newsDetail_forward_32985472",
       "source": "澎湃新闻",
-      "time": "昨天11:03",
+      "time": "前天11:03",
       "category": "meeting"
     }
   ]
