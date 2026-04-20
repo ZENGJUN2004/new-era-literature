fe2854b72021543e7b761555f12579bb164aa602
GitHub Action <action@github.com>
2026-04-20 15:08:02 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-20 21:01",
+  "update_time": "2026-04-20 23:08",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,34 +21,6 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
-    },
-    {
-      "title": "高司令新作全球票房破4.2亿刀!豆瓣8.5高分你看了吗",
-      "url": "https://www.gamersky.com/news/202604/2119265.shtml",
-      "source": "游民星空",
-      "time": "4月6日",
-      "category": "other"
-    },
-    {
-      "title": "收获文学榜在上海揭晓!苏童《好天气》领衔长篇榜首",
-      "url": "http://baijiahao.baidu.com/s?id=1856101673487226555&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "2月3日",
-      "category": "other"
-    },
-    {
-      "title": "苏童毕飞宇吴真全勇先,2025收获文学榜揭晓,榜首作家这样说",
-      "url": "http://baijiahao.baidu.com/s?id=1856104193950555898&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "2月3日",
-      "category": "other"
-    },
-    {
-      "title": "《收获》文学榜揭晓,榜单映射中国文坛迎来“丰年”",
-      "url": "https://baijiahao.baidu.com/s?id=1856184355598057672&wfr=spider&for=pc",
-      "source": "新华网",
-      "time": "2月4日",
-      "category": "other"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
