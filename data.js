a9cd69f25834f523ef89332ffbd9d8d42a149225
GitHub Action <action@github.com>
2026-04-20 20:35:10 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-21 03:53",
+  "update_time": "2026-04-21 04:35",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,6 +21,34 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
+    },
+    {
+      "title": "豆瓣2025年度图书榜单出炉你打卡了几本?",
+      "url": "https://cul.sohu.com/a/973411847_121106994",
+      "source": "搜狐网",
+      "time": "1月7日",
+      "category": "other"
+    },
+    {
+      "title": "长篇小说《大海风》入选中国当代文学研究会年度榜单作者赵德发...",
+      "url": "https://baijiahao.baidu.com/s?id=1853815024647126329&wfr=spider&for=pc",
+      "source": "闪电新闻",
+      "time": "1月9日",
+      "category": "activity"
+    },
+    {
+      "title": "2025豆瓣年度榜单:从经典到先锋,这10部作品是时代的精神注脚",
+      "url": "https://cul.sohu.com/a/978492959_121106832",
+      "source": "搜狐网",
+      "time": "1月21日",
+      "category": "other"
+    },
+    {
+      "title": "赵德发长篇小说《大海风》入选中国当代文学研究会年度榜单(2025)",
+      "url": "https://baijiahao.baidu.com/s?id=1853465599041956599&wfr=spider&for=pc",
+      "source": "闪电新闻",
+      "time": "1月5日",
+      "category": "activity"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
