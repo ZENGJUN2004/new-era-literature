30c87a4b89ae03bc1cee010235cee09fe4115ccf
GitHub Action <action@github.com>
2026-04-22 17:03:17 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-23 00:00",
+  "update_time": "2026-04-23 01:03",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,13 +21,6 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
-    },
-    {
-      "title": "苏童毕飞宇吴真全勇先,2025收获文学榜揭晓,榜首作家这样说",
-      "url": "http://baijiahao.baidu.com/s?id=1856104193950555898&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "2月3日",
-      "category": "other"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
