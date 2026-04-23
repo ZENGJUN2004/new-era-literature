2e5e1712d4d7eb5bf2cf56406374728733c71144
GitHub Action <action@github.com>
2026-04-23 20:00:49 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-24 02:53",
+  "update_time": "2026-04-24 04:00",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,108 +23,10 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "文学双周记|22家刊物答原创之问,邓紫棋入选银河奖引热议",
-      "url": "https://baijiahao.baidu.com/s?id=1853442602393722283&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "1月5日",
-      "category": "other"
-    },
-    {
-      "title": "中国当代文学研究会年度榜单发布",
-      "url": "https://baijiahao.baidu.com/s?id=1855942095675059485&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "2月2日",
-      "category": "other"
-    },
-    {
-      "title": "AI帮你速读文本,但阅读更需“主动狩猎”",
-      "url": "https://www.jfdaily.com/news/detail?id=1101185",
-      "source": "上观",
-      "time": "前天23:51",
-      "category": "other"
-    },
-    {
-      "title": "中国科幻产业走向“生态出海”",
-      "url": "https://baijiahao.baidu.com/s?id=1861855033203367752&wfr=spider&for=pc",
-      "source": "环球网",
-      "time": "4月8日",
-      "category": "voice"
-    },
-    {
-      "title": "从“单峰突起”走向“群峰并立”",
-      "url": "https://baijiahao.baidu.com/s?id=1863114992955541364&wfr=spider&for=pc",
-      "source": "光明网",
-      "time": "前天04:00",
-      "category": "other"
-    },
-    {
-      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
-      "url": "https://m.jiemian.com/article/3573232.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "媒体眼中海大的2025年!",
-      "url": "https://baijiahao.baidu.com/s?id=1856159417055495057&wfr=spider&for=pc",
-      "source": "中国海洋大学",
-      "time": "2月4日",
-      "category": "other"
-    },
-    {
-      "title": "《十月》杂志举办青年作家创作分享会",
-      "url": "https://baijiahao.baidu.com/s?id=1860050202536776799&wfr=spider&for=pc",
-      "source": "人民网",
-      "time": "3月19日",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "在“精读堂”里,对经典细嚼慢咽|世界读书日",
-      "url": "https://baijiahao.baidu.com/s?id=1863226382610776437&wfr=spider&for=pc",
-      "source": "天眼新闻",
-      "time": "昨天09:43",
-      "category": "other"
-    },
-    {
-      "title": "以轻喜剧书写新时代山乡巨变,张继长篇小说《喜上眉梢》研讨会在京...",
-      "url": "https://baijiahao.baidu.com/s?id=1863255890411536534&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "昨天18:14",
-      "category": "activity"
-    },
-    {
-      "title": "拍北京地铁上的读书人第八年:依然相信文字的力量",
-      "url": "https://baijiahao.baidu.com/s?id=1863233588100629875&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "昨天12:19",
-      "category": "other"
-    },
-    {
-      "title": "值得反复阅读的8本满分传记,真能改变人生",
-      "url": "http://www.thepaper.cn/newsDetail_forward_33016730",
-      "source": "澎湃新闻",
-      "time": "昨天12:21",
-      "category": "other"
-    },
-    {
-      "title": "网文领跑数字阅读出海",
-      "url": "https://baijiahao.baidu.com/s?id=1863226308179440803&wfr=spider&for=pc",
-      "source": "中工网",
-      "time": "昨天09:31",
-      "category": "other"
-    },
-    {
-      "title": "作家出版社推出“一页榆·轻阅读系列”,首批4本小说问世",
-      "url": "https://baijiahao.baidu.com/s?id=1863237393200488046&wfr=spider&for=pc",
-      "source": "济南时报",
-      "time": "昨天12:40",
       "category": "activity"
     }
   ]
