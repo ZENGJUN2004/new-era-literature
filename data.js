56b7fcd76eebf9228c17e8bc5419028dccf5168f
GitHub Action <action@github.com>
2026-04-23 11:52:58 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-23 18:12",
+  "update_time": "2026-04-23 19:52",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -28,6 +28,27 @@ window.LIT_DATA = {
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "原创图像小说《雪鹿》入选2025年度“中国好书”",
+      "url": "https://www.chinanews.com.cn/cul/2026/04-21/10608151.shtml",
+      "source": "中国新闻网",
+      "time": "前天22:56",
+      "category": "activity"
+    },
+    {
+      "title": "北大张颐武教授将在贵阳孔学堂开讲!",
+      "url": "https://baijiahao.baidu.com/s?id=1863245993418439041&wfr=spider&for=pc",
+      "source": "贵阳网",
+      "time": "4小时前",
+      "category": "other"
+    },
+    {
+      "title": "4月26日,北大教授张颐武将在孔学堂开讲“民族文化与当下生活”",
+      "url": "https://baijiahao.baidu.com/s?id=1863165830177270482&wfr=spider&for=pc",
+      "source": "天眼新闻",
+      "time": "昨天17:42",
+      "category": "other"
     }
   ]
 };
