453ec4305ba9d5e36a5c9cef39203a666998c16f
GitHub Action <action@github.com>
2026-04-23 08:27:54 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-23 14:54",
+  "update_time": "2026-04-23 16:27",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -27,13 +27,6 @@ window.LIT_DATA = {
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "时隔六年推出《人呐》 莫言请读者“刷”短小说",
-      "url": "http://news.yunnan.cn/system/2026/04/21/033974363.shtml",
-      "source": "云南网",
-      "time": "前天11:53",
       "category": "activity"
     }
   ]
