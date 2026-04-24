a0b9bea956e0316fab4e5db1e3abeb508ef7fdf1
GitHub Action <action@github.com>
2026-04-24 12:54:43 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-24 19:52",
+  "update_time": "2026-04-24 20:54",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,122 +23,10 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "中国当代文学研究会年度榜单(2025)发布",
-      "url": "http://www.thepaper.cn/newsDetail_forward_32317986",
-      "source": "澎湃新闻",
-      "time": "1月5日",
-      "category": "other"
-    },
-    {
-      "title": "深圳2部作品上榜!第八届城市文学排行榜榜单揭晓",
-      "url": "https://baijiahao.baidu.com/s?id=1863180819543190399&wfr=spider&for=pc",
-      "source": "深圳特区报",
-      "time": "前天21:02",
-      "category": "voice"
-    },
-    {
-      "title": "泛舟阅文脉、临岸品书香,邂逅虹口北外滩阅读之美",
-      "url": "https://www.shanghai.gov.cn/nw15343/20260424/969748f4688a4df882e89183b9c241ff.html",
-      "source": "上海市人民政府",
-      "time": "今天",
-      "category": "other"
-    },
-    {
-      "title": "从《我的阿勒泰》到苏菲·玛索的新书《暗河》,花城文学院3岁了!",
-      "url": "https://baijiahao.baidu.com/s?id=1860805556134866933&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "3月27日",
-      "category": "activity"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "接地气、有特色、具新意!2025年山东省全民阅读典型案例详情来了",
-      "url": "https://baijiahao.baidu.com/s?id=1863351489038713889&wfr=spider&for=pc",
-      "source": "大众日报",
-      "time": "17分钟前",
-      "category": "other"
-    },
-    {
-      "title": "30部好书推荐 北京西城区启动“阅读春天”系列活动",
-      "url": "https://baijiahao.baidu.com/s?id=1863270554281880215&wfr=spider&for=pc",
-      "source": "潮新闻客户端",
-      "time": "昨天22:07",
-      "category": "other"
-    },
-    {
-      "title": "作家出版社推出“一页榆·轻阅读系列”,首批4本小说问世",
-      "url": "https://baijiahao.baidu.com/s?id=1863237393200488046&wfr=spider&for=pc",
-      "source": "济南时报",
-      "time": "昨天12:40",
-      "category": "activity"
-    },
-    {
-      "title": "2025年度浙版好书TOP30榜单发布!你种草了哪一本?",
-      "url": "https://zjnews.zjol.com.cn/zjnews/202604/t20260423_31623735.shtml",
-      "source": "浙江在线·新闻",
-      "time": "昨天16:27",
-      "category": "other"
-    },
-    {
-      "title": "《毛泽东选集》成大学图书馆热搜关键词,沪上高校公布年度阅读...",
-      "url": "http://app.myzaker.com/news/article.php?pk=69ea14e18e9f09092b055746",
-      "source": "ZAKER",
-      "time": "昨天20:44",
-      "category": "other"
-    },
-    {
-      "title": "在“精读堂”里,对经典细嚼慢咽|世界读书日",
-      "url": "https://baijiahao.baidu.com/s?id=1863226382610776437&wfr=spider&for=pc",
-      "source": "天眼新闻",
-      "time": "昨天09:43",
-      "category": "other"
-    },
-    {
-      "title": "首个“全民阅读活动周” 晚报邀请知名作家分享案头好书",
-      "url": "https://baijiahao.baidu.com/s?id=1863225029531096504&wfr=spider&for=pc",
-      "source": "北青网",
-      "time": "昨天10:04",
-      "category": "other"
-    },
-    {
-      "title": "东港!最美人间四“阅”天!_澎湃号·政务_澎湃新闻-The Paper",
-      "url": "https://www.thepaper.cn/newsDetail_forward_33036856",
-      "source": "澎湃新闻",
-      "time": "昨天14:15",
-      "category": "other"
-    },
-    {
-      "title": "世界读书日 |“如果有天堂,那一定是图书馆的模样。”",
-      "url": "http://baijiahao.baidu.com/s?id=1863258564407690058&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "昨天18:04",
-      "category": "other"
-    },
-    {
-      "title": "文化十分丨阅读穿越古今 文学纵横南北 听茅盾文学奖得主徐则臣...",
-      "url": "http://news.cnhubei.com/content/2026-04/23/content_19940880.html",
-      "source": "荆楚网新闻",
-      "time": "昨天21:35",
-      "category": "other"
-    },
-    {
-      "title": "鲁迅文学奖得主亮相分享《AI时代的阅读》",
-      "url": "https://barb.sznews.com/PC/content/202604/23/content_3442774.html",
-      "source": "宝安日报数字报",
-      "time": "昨天",
-      "category": "other"
-    },
-    {
-      "title": "一部乡村振兴的文学样本——柏祥伟长篇小说《尼山脚下》研讨会在...",
-      "url": "https://baijiahao.baidu.com/s?id=1863256772969470715&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "昨天18:27",
       "category": "activity"
     }
   ]
