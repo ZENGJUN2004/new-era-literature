b7a6aaa2b90942060a93cf72a7db951da53366a7
GitHub Action <action@github.com>
2026-04-28 17:28:16 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-28 20:19",
+  "update_time": "2026-04-29 01:28",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,39 +23,32 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "收获文学榜在上海揭晓!苏童《好天气》领衔长篇榜首",
+      "url": "http://baijiahao.baidu.com/s?id=1856101673487226555&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "2月3日",
+      "category": "other"
+    },
+    {
+      "title": "苏童毕飞宇吴真全勇先,2025收获文学榜揭晓,榜首作家这样说",
+      "url": "http://baijiahao.baidu.com/s?id=1856104193950555898&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "2月3日",
+      "category": "other"
+    },
+    {
+      "title": "《收获》文学榜揭晓,榜单映射中国文坛迎来“丰年”",
+      "url": "https://baijiahao.baidu.com/s?id=1856184355598057672&wfr=spider&for=pc",
+      "source": "新华网",
+      "time": "2月4日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "埃安N60正式上市:10.68万起,把30万级智驾体验带给年轻人",
-      "url": "https://baijiahao.baidu.com/s?id=1863695441961167715&wfr=spider&for=pc",
-      "source": "潇湘晨报",
-      "time": "5小时前",
-      "category": "activity"
-    },
-    {
-      "title": "拟每10股派发现金红利5元 以岭药业2025年实现归母净利润12.86亿元",
-      "url": "https://www.cnstock.com/commonDetail/694039",
-      "source": "上海证券报·中国证券网",
-      "time": "6小时前",
-      "category": "other"
-    },
-    {
-      "title": "向高质量发展聚力前行(深观察)",
-      "url": "https://baijiahao.baidu.com/s?id=1863585413636206556&wfr=spider&for=pc",
-      "source": "人民网",
-      "time": "昨天03:13",
-      "category": "other"
-    },
-    {
-      "title": "读懂这部旷世神作,人生就少了99%的烦恼!",
-      "url": "https://www.thepaper.cn/newsDetail_forward_33045290",
-      "source": "澎湃新闻",
-      "time": "昨天15:05",
-      "category": "other"
     }
   ]
 };
