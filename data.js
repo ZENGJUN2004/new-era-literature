1061b3057afc68fe2513e6059bf5ad2772554071
GitHub Action <action@github.com>
2026-04-29 17:14:57 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-04-29 23:51",
+  "update_time": "2026-04-30 01:14",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,13 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "读懂这部旷世神作,人生就少了99%的烦恼!",
-      "url": "https://www.thepaper.cn/newsDetail_forward_33045290",
-      "source": "澎湃新闻",
-      "time": "前天15:05",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -40,7 +33,7 @@ window.LIT_DATA = {
       "title": "给时光以生命,请山河证青春——《棋盘山下》新书首发式在蓉举行",
       "url": "http://www.xinhuanet.com/book/20260428/ae1f4024236c432985062b8c4ab1fa88/c.html",
       "source": "新华网",
-      "time": "昨天17:48",
+      "time": "前天17:48",
       "category": "activity"
     }
   ]
