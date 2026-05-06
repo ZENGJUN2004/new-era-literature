b379bd982ae0ddb6abac516754c379f3076a2191
GitHub Action <action@github.com>
2026-05-06 15:20:24 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-05-06 20:22",
+  "update_time": "2026-05-06 23:20",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,24 +23,10 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "电影文学公开课:戴锦华、双雪涛谈文学与影像",
-      "url": "https://baijiahao.baidu.com/s?id=1863049371917277802&wfr=spider&for=pc",
-      "source": "青瞳视角",
-      "time": "4月21日",
-      "category": "other"
-    },
-    {
-      "title": "全都要!这届天津青年,凭什么这么“贪心”?",
-      "url": "https://baijiahao.baidu.com/s?id=1864225131691439425&wfr=spider&for=pc",
-      "source": "网信宝坻",
-      "time": "前天11:00",
-      "category": "other"
-    },
-    {
-      "title": "揭秘!《阳光倾城》邓煌访谈王勇:城品人物凭什么叫创业者村庄?",
-      "url": "https://yule.sohu.com/a/1018191337_121160111",
-      "source": "搜狐娱乐",
-      "time": "前天15:04",
+      "title": "「书目推荐」长安街读书会第20260404期干部学习书目博览",
+      "url": "http://m.bjnews.com.cn/detail/1776946963019820.html",
+      "source": "新京报",
+      "time": "4月23日",
       "category": "other"
     },
     {
@@ -49,20 +35,6 @@ window.LIT_DATA = {
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "焦点访谈丨实干笃行新征程 不负韶华书写青春答卷",
-      "url": "https://news.e23.cn/content/2026-05-05/2026050500086.html",
-      "source": "舜网",
-      "time": "昨天15:49",
-      "category": "other"
-    },
-    {
-      "title": "对话孙天澍:AI时代,企业家要从“管人”到“架构智能体”",
-      "url": "https://baijiahao.baidu.com/s?id=1864317067502087149&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "昨天11:21",
-      "category": "other"
     }
   ]
 };
