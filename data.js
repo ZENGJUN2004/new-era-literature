be9027ebf33f55dc04e3335f61ae2c596aa51e2d
GitHub Action <action@github.com>
2026-05-09 11:40:50 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-05-09 18:45",
+  "update_time": "2026-05-09 19:40",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,48 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
+      "url": "https://www.jfdaily.com/wx/detail.do?id=1050469",
+      "source": "上观",
+      "time": "1月12日",
+      "category": "activity"
+    },
+    {
+      "title": "迈向跨界融合的“大文学”时代",
+      "url": "http://www.xinhuanet.com/book/20260120/d3fa6fd5f93048fc88a2d3be4ef48224/c.html",
+      "source": "新华网",
+      "time": "1月20日",
+      "category": "other"
+    },
+    {
+      "title": "《中原文学》主编李辉:破圈不破底,风依旧滚烫|顶端文学名刊访谈",
+      "url": "https://baijiahao.baidu.com/s?id=1862517996245963491&wfr=spider&for=pc",
+      "source": "顶端新闻",
+      "time": "4月15日",
+      "category": "other"
+    },
+    {
+      "title": "特别策划 | 如何捍卫文字的“底线”?",
+      "url": "https://www.jiemian.com/article/14294626.html",
+      "source": "界面新闻",
+      "time": "4月22日",
+      "category": "other"
+    },
+    {
+      "title": "中青报刊文:写同人文、给历史名人上坟,从访古热看文化自信鲜活表达",
+      "url": "https://www.thepaper.cn/newsDetail_forward_33060057",
+      "source": "澎湃新闻",
+      "time": "4月27日",
+      "category": "other"
+    },
+    {
+      "title": "新华访谈丨对话南派三叔:文学不止一种模样",
+      "url": "http://www.zj.xinhuanet.com/20260507/72307188a6df4c8eadf1a6678d2e62dc/c.html",
+      "source": "新华网",
+      "time": "前天09:03",
+      "category": "other"
+    },
+    {
       "title": "爬格子的收获",
       "url": "https://whwb.cjn.cn/html/2026-05/07/content_48089_3406684.htm",
       "source": "武汉晚报",
@@ -42,13 +84,6 @@ window.LIT_DATA = {
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "一周文化讲座|聊聊那些年被妈妈打的故事",
-      "url": "https://cul.sohu.com/a/1019731501_260616",
-      "source": "搜狐网",
-      "time": "昨天11:33",
-      "category": "meeting"
     }
   ]
 };
