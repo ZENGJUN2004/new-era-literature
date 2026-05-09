c02acb9da37b039957d0fb8b7c6749f1e8d65105
GitHub Action <action@github.com>
2026-05-09 12:48:50 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-05-09 19:40",
+  "update_time": "2026-05-09 20:48",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,48 +21,6 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
-    },
-    {
-      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
-      "url": "https://www.jfdaily.com/wx/detail.do?id=1050469",
-      "source": "上观",
-      "time": "1月12日",
-      "category": "activity"
-    },
-    {
-      "title": "迈向跨界融合的“大文学”时代",
-      "url": "http://www.xinhuanet.com/book/20260120/d3fa6fd5f93048fc88a2d3be4ef48224/c.html",
-      "source": "新华网",
-      "time": "1月20日",
-      "category": "other"
-    },
-    {
-      "title": "《中原文学》主编李辉:破圈不破底,风依旧滚烫|顶端文学名刊访谈",
-      "url": "https://baijiahao.baidu.com/s?id=1862517996245963491&wfr=spider&for=pc",
-      "source": "顶端新闻",
-      "time": "4月15日",
-      "category": "other"
-    },
-    {
-      "title": "特别策划 | 如何捍卫文字的“底线”?",
-      "url": "https://www.jiemian.com/article/14294626.html",
-      "source": "界面新闻",
-      "time": "4月22日",
-      "category": "other"
-    },
-    {
-      "title": "中青报刊文:写同人文、给历史名人上坟,从访古热看文化自信鲜活表达",
-      "url": "https://www.thepaper.cn/newsDetail_forward_33060057",
-      "source": "澎湃新闻",
-      "time": "4月27日",
-      "category": "other"
-    },
-    {
-      "title": "新华访谈丨对话南派三叔:文学不止一种模样",
-      "url": "http://www.zj.xinhuanet.com/20260507/72307188a6df4c8eadf1a6678d2e62dc/c.html",
-      "source": "新华网",
-      "time": "前天09:03",
-      "category": "other"
     },
     {
       "title": "爬格子的收获",
