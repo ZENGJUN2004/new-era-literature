76d1e2585445c7fdd9268028805830741c2b70cc
GitHub Action <action@github.com>
2026-05-16 07:46:23 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-05-16 13:18",
+  "update_time": "2026-05-16 15:46",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,39 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "这一场文学跨年,作家带大家一起重温文学故乡,述说文学跋涉",
+      "url": "https://www.takefoto.cn/news/2026/01/02/11500412.shtml",
+      "source": "北晚在线",
+      "time": "1月2日",
+      "category": "other"
+    },
+    {
+      "title": "《中原文学》主编李辉:破圈不破底,风依旧滚烫|顶端文学名刊访谈",
+      "url": "https://baijiahao.baidu.com/s?id=1862517996245963491&wfr=spider&for=pc",
+      "source": "顶端新闻",
+      "time": "4月15日",
+      "category": "other"
+    },
+    {
+      "title": "特别策划 | 如何捍卫文字的“底线”?",
+      "url": "https://www.jiemian.com/article/14294626.html",
+      "source": "界面新闻",
+      "time": "4月22日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
+    },
+    {
+      "title": "从观看到阅读,影视与文学正“双向圈粉”",
+      "url": "http://baijiahao.baidu.com/s?id=1865223901538796508&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "昨天11:34",
+      "category": "other"
     }
   ]
 };
