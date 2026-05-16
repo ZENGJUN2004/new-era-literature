9220fa17a70487009e96410cf5bb46638f09d715
GitHub Action <action@github.com>
2026-05-16 01:48:07 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-05-16 07:43",
+  "update_time": "2026-05-16 09:48",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,18 +23,109 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "十部作品里的女性故事,汇聚成一支温良智慧的火把",
+      "url": "http://baijiahao.baidu.com/s?id=1726726766072973629&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "other"
+    },
+    {
+      "title": "女性写作浮出历史地表之后:当代中国文学应当如何书写拉姆们?",
+      "url": "https://www.jiemian.com/article/5128818_toutiao.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "《三体》之后,下一个“破圈”之作在哪里",
+      "url": "https://baijiahao.baidu.com/s?id=1863114992955488797&wfr=spider&for=pc",
+      "source": "光明网",
+      "time": "4月22日",
+      "category": "other"
+    },
+    {
+      "title": "中国科幻产业走向“生态出海”",
+      "url": "https://baijiahao.baidu.com/s?id=1861855033203367752&wfr=spider&for=pc",
+      "source": "环球网",
+      "time": "4月8日",
+      "category": "voice"
+    },
+    {
+      "title": "蔡崇达非虚构写作访谈录:人是最了不起的艺术品",
+      "url": "https://36kr.com/p/1723320942593",
+      "source": "36kr",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "“非虚构”和“虚构”的界限",
+      "url": "http://baijiahao.baidu.com/s?id=1740112764190450044&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "如何把一个想法写成畅销书?美国顶尖非虚构作家的写作课",
+      "url": "https://www.lifeweek.com.cn/article/13495",
+      "source": "三联生活周刊",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
+      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
+      "url": "https://m.jiemian.com/article/3573232.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "2025春风月榜半年报告:希望这22本佳作为你带来光",
+      "url": "https://baijiahao.baidu.com/s?id=1853802033490694012&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "1月9日",
+      "category": "other"
+    },
+    {
+      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
+      "url": "https://www.jfdaily.com/wx/detail.do?id=1050469",
+      "source": "上观",
+      "time": "1月12日",
+      "category": "activity"
+    },
+    {
+      "title": "2026年,这些新书值得我们期待(上)",
+      "url": "https://baijiahao.baidu.com/s?id=1854440677511475404&wfr=spider&for=pc",
+      "source": "界面新闻",
+      "time": "1月16日",
+      "category": "activity"
+    },
+    {
+      "title": "2026年,这些图书不要错过",
+      "url": "https://baijiahao.baidu.com/s?id=1857913561928553786&wfr=spider&for=pc",
+      "source": "扬子晚报",
+      "time": "2月23日",
+      "category": "other"
+    },
+    {
+      "title": "今年上海作家们有哪些新作品出版?这场文学创作座谈会有分享,十二...",
+      "url": "https://baijiahao.baidu.com/s?id=1863069065231049456&wfr=spider&for=pc",
+      "source": "新闻晨报",
+      "time": "4月21日",
+      "category": "activity"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "湃书单|澎湃新闻编辑们在读的13本书:玩以载道",
-      "url": "https://baijiahao.baidu.com/s?id=1865244871251419690&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "昨天17:07",
-      "category": "other"
     }
   ]
 };
