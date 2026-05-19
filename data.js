fc9c0f94dff5adfbcc5705f183ef4232e12786e7
GitHub Action <action@github.com>
2026-05-19 02:01:16 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-05-19 07:54",
+  "update_time": "2026-05-19 10:01",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,10 +23,157 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "十部作品里的女性故事,汇聚成一支温良智慧的火把",
+      "url": "http://baijiahao.baidu.com/s?id=1726726766072973629&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "other"
+    },
+    {
+      "title": "女性写作浮出历史地表之后:当代中国文学应当如何书写拉姆们?",
+      "url": "https://www.jiemian.com/article/5128818_toutiao.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "大众写科幻,带来了什么?",
+      "url": "http://baijiahao.baidu.com/s?id=1853855131427137219&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "1月9日",
+      "category": "voice"
+    },
+    {
+      "title": "《三体》之后,下一个“破圈”之作在哪里",
+      "url": "https://baijiahao.baidu.com/s?id=1863114992955488797&wfr=spider&for=pc",
+      "source": "光明网",
+      "time": "4月22日",
+      "category": "other"
+    },
+    {
+      "title": "中国科幻产业走向“生态出海”",
+      "url": "https://baijiahao.baidu.com/s?id=1861855033203367752&wfr=spider&for=pc",
+      "source": "环球网",
+      "time": "4月8日",
+      "category": "voice"
+    },
+    {
+      "title": "蔡崇达非虚构写作访谈录:人是最了不起的艺术品",
+      "url": "https://36kr.com/p/1723320942593",
+      "source": "36kr",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "“非虚构”和“虚构”的界限",
+      "url": "http://baijiahao.baidu.com/s?id=1740112764190450044&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
+      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
+      "url": "https://m.jiemian.com/article/3573232.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "这一场文学跨年,作家带大家一起重温文学故乡,述说文学跋涉",
+      "url": "https://www.takefoto.cn/news/2026/01/02/11500412.shtml",
+      "source": "北晚在线",
+      "time": "1月2日",
+      "category": "other"
+    },
+    {
+      "title": "迈向跨界融合的“大文学”时代",
+      "url": "http://www.xinhuanet.com/book/20260120/d3fa6fd5f93048fc88a2d3be4ef48224/c.html",
+      "source": "新华网",
+      "time": "1月20日",
+      "category": "other"
+    },
+    {
+      "title": "《中原文学》主编李辉:破圈不破底,风依旧滚烫|顶端文学名刊访谈",
+      "url": "https://baijiahao.baidu.com/s?id=1862517996245963491&wfr=spider&for=pc",
+      "source": "顶端新闻",
+      "time": "4月15日",
+      "category": "other"
+    },
+    {
+      "title": "特别策划 | 如何捍卫文字的“底线”?",
+      "url": "https://www.jiemian.com/article/14294626.html",
+      "source": "界面新闻",
+      "time": "4月22日",
+      "category": "other"
+    },
+    {
+      "title": "2025春风月榜半年报告:希望这22本佳作为你带来光",
+      "url": "https://baijiahao.baidu.com/s?id=1853802033490694012&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "1月9日",
+      "category": "other"
+    },
+    {
+      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
+      "url": "https://www.jfdaily.com/wx/detail.do?id=1050469",
+      "source": "上观",
+      "time": "1月12日",
+      "category": "activity"
+    },
+    {
+      "title": "2026年,这些新书值得我们期待(上)",
+      "url": "https://baijiahao.baidu.com/s?id=1854440677511475404&wfr=spider&for=pc",
+      "source": "界面新闻",
+      "time": "1月16日",
+      "category": "activity"
+    },
+    {
+      "title": "2026年,这些图书不要错过",
+      "url": "https://baijiahao.baidu.com/s?id=1857913561928553786&wfr=spider&for=pc",
+      "source": "扬子晚报",
+      "time": "2月23日",
+      "category": "other"
+    },
+    {
+      "title": "2025年度刀锋图书奖在泉州揭晓 年度主题聚焦“假如我是新的”",
+      "url": "http://news.10jqka.com.cn/20260421/c676158301.shtml",
+      "source": "同花顺财经",
+      "time": "4月21日",
+      "category": "other"
+    },
+    {
+      "title": "从“新”出发,重塑书写的河道|2025年度刀锋图书奖全榜单",
+      "url": "https://baijiahao.baidu.com/s?id=1862975324373258594&wfr=spider&for=pc",
+      "source": "新周刊",
+      "time": "4月20日",
+      "category": "other"
+    },
+    {
+      "title": "今年上海作家们有哪些新作品出版?这场文学创作座谈会有分享,十二...",
+      "url": "https://baijiahao.baidu.com/s?id=1863069065231049456&wfr=spider&for=pc",
+      "source": "新闻晨报",
+      "time": "4月21日",
+      "category": "activity"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "给时光以生命,请山河证青春——《棋盘山下》新书首发式在蓉举行",
+      "url": "http://www.xinhuanet.com/book/20260428/ae1f4024236c432985062b8c4ab1fa88/c.html",
+      "source": "新华网",
+      "time": "4月28日",
       "category": "activity"
     },
     {
