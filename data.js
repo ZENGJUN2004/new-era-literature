a1d54cfc2317e52bd1d92e0b4897aa3c7e156218
GitHub Action <action@github.com>
2026-05-20 21:29:10 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-05-21 03:11",
+  "update_time": "2026-05-21 05:29",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -30,10 +30,31 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "给时光以生命,请山河证青春——《棋盘山下》新书首发式在蓉举行",
-      "url": "http://www.xinhuanet.com/book/20260428/ae1f4024236c432985062b8c4ab1fa88/c.html",
-      "source": "新华网",
-      "time": "4月28日",
+      "title": "新刊推介|致青春 致奋斗 致家风绵长",
+      "url": "https://baijiahao.baidu.com/s?id=1865692102816627647&wfr=spider&for=pc",
+      "source": "中国妇女",
+      "time": "昨天15:21",
+      "category": "other"
+    },
+    {
+      "title": "程小莹长篇新作《原本》:寻找庸常岁月里的精神原乡 | 深读",
+      "url": "https://www.thepaper.cn/newsDetail_forward_33178883",
+      "source": "澎湃新闻",
+      "time": "昨天11:52",
+      "category": "other"
+    },
+    {
+      "title": "文耀岛城、薪火相传!首届刘知侠长篇小说奖颁奖,《青岛市中青年...",
+      "url": "https://baijiahao.baidu.com/s?id=1865698674387227669&wfr=spider&for=pc",
+      "source": "大众日报",
+      "time": "昨天17:21",
+      "category": "activity"
+    },
+    {
+      "title": "首届“刘知侠长篇小说奖”颁奖典礼暨《青岛市中青年作家文库...",
+      "url": "https://baijiahao.baidu.com/s?id=1865704053349245673&wfr=spider&for=pc",
+      "source": "半岛都市报",
+      "time": "昨天18:41",
       "category": "activity"
     }
   ]
