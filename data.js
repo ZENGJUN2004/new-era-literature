be1e1bbaa7e31270ba17076efc3cc3548626a221
GitHub Action <action@github.com>
2026-05-21 02:00:23 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-05-21 07:03",
+  "update_time": "2026-05-21 10:00",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,18 +23,137 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "大众写科幻,带来了什么?",
+      "url": "http://baijiahao.baidu.com/s?id=1853855131427137219&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "1月9日",
+      "category": "voice"
+    },
+    {
+      "title": "《三体》之后,下一个“破圈”之作在哪里",
+      "url": "https://baijiahao.baidu.com/s?id=1863114992955488797&wfr=spider&for=pc",
+      "source": "光明网",
+      "time": "4月22日",
+      "category": "other"
+    },
+    {
+      "title": "中国科幻产业走向“生态出海”",
+      "url": "https://baijiahao.baidu.com/s?id=1861855033203367752&wfr=spider&for=pc",
+      "source": "环球网",
+      "time": "4月8日",
+      "category": "voice"
+    },
+    {
+      "title": "项静:从博物到非虚构:自然生态写作的一条路径 | 生态文学与自然...",
+      "url": "http://baijiahao.baidu.com/s?id=1741290134711738687&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "蔡崇达非虚构写作访谈录:人是最了不起的艺术品",
+      "url": "https://36kr.com/p/1723320942593",
+      "source": "36kr",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
+      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
+      "url": "https://m.jiemian.com/article/3573232.html",
+      "source": "界面新闻",
+      "time": "",
+      "category": "voice"
+    },
+    {
+      "title": "这一场文学跨年,作家带大家一起重温文学故乡,述说文学跋涉",
+      "url": "https://www.takefoto.cn/news/2026/01/02/11500412.shtml",
+      "source": "北晚在线",
+      "time": "1月2日",
+      "category": "other"
+    },
+    {
+      "title": "迈向跨界融合的“大文学”时代",
+      "url": "http://www.xinhuanet.com/book/20260120/d3fa6fd5f93048fc88a2d3be4ef48224/c.html",
+      "source": "新华网",
+      "time": "1月20日",
+      "category": "other"
+    },
+    {
+      "title": "《中原文学》主编李辉:破圈不破底,风依旧滚烫|顶端文学名刊访谈",
+      "url": "https://baijiahao.baidu.com/s?id=1862517996245963491&wfr=spider&for=pc",
+      "source": "顶端新闻",
+      "time": "4月15日",
+      "category": "other"
+    },
+    {
+      "title": "特别策划 | 如何捍卫文字的“底线”?",
+      "url": "https://www.jiemian.com/article/14294626.html",
+      "source": "界面新闻",
+      "time": "4月22日",
+      "category": "other"
+    },
+    {
+      "title": "2025年度“中国人文学术十大热点”发布",
+      "url": "https://baijiahao.baidu.com/s?id=1864699811170864207&wfr=spider&for=pc",
+      "source": "中国新闻网",
+      "time": "5月9日",
+      "category": "voice"
+    },
+    {
+      "title": "2025年度“中国人文学术十大热点”正式发布",
+      "url": "https://baijiahao.baidu.com/s?id=1864768892164544944&wfr=spider&for=pc",
+      "source": "中国青年网",
+      "time": "5月10日",
+      "category": "voice"
+    },
+    {
+      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
+      "url": "https://www.jfdaily.com/wx/detail.do?id=1050469",
+      "source": "上观",
+      "time": "1月12日",
+      "category": "activity"
+    },
+    {
+      "title": "2026年,这些新书值得我们期待(上)",
+      "url": "https://baijiahao.baidu.com/s?id=1854440677511475404&wfr=spider&for=pc",
+      "source": "界面新闻",
+      "time": "1月16日",
+      "category": "activity"
+    },
+    {
+      "title": "2026年,这些图书不要错过",
+      "url": "https://baijiahao.baidu.com/s?id=1857913561928553786&wfr=spider&for=pc",
+      "source": "扬子晚报",
+      "time": "2月23日",
+      "category": "other"
+    },
+    {
+      "title": "今年上海作家们有哪些新作品出版?这场文学创作座谈会有分享,十二...",
+      "url": "https://baijiahao.baidu.com/s?id=1863069065231049456&wfr=spider&for=pc",
+      "source": "新闻晨报",
+      "time": "4月21日",
+      "category": "activity"
+    },
+    {
+      "title": "“Z世代文学,向何处”,《十月》创作分享会尝试作答",
+      "url": "https://baijiahao.baidu.com/s?id=1859897163056459211&wfr=spider&for=pc",
+      "source": "北京日报客户端",
+      "time": "3月17日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "新刊推介|致青春 致奋斗 致家风绵长",
-      "url": "https://baijiahao.baidu.com/s?id=1865692102816627647&wfr=spider&for=pc",
-      "source": "中国妇女",
-      "time": "昨天15:21",
-      "category": "other"
     },
     {
       "title": "程小莹长篇新作《原本》:寻找庸常岁月里的精神原乡 | 深读",
