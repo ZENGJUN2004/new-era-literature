8cd55c2551908aef689f67f6909cd1e6209106ba
GitHub Action <action@github.com>
2026-05-21 16:32:57 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-05-21 21:52",
+  "update_time": "2026-05-22 00:32",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,76 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "吉林文学馆三楼《作家》杂志专题展 折射中国文学期刊与文学思潮的...",
+      "url": "http://baijiahao.baidu.com/s?id=1865693572583542000&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "前天16:00",
+      "category": "voice"
+    },
+    {
+      "title": "国话原创话剧《燕食记》排练现场:于洋首度演绎粤菜名厨",
+      "url": "https://baijiahao.baidu.com/s?id=1865704431074791831&wfr=spider&for=pc",
+      "source": "北晚在线",
+      "time": "前天18:37",
+      "category": "other"
+    },
+    {
+      "title": "北京号最精彩|更便捷!京城这条道路全线通车",
+      "url": "https://baijiahao.baidu.com/s?id=1865696785994179720&wfr=spider&for=pc",
+      "source": "北京日报客户端",
+      "time": "前天16:51",
+      "category": "other"
+    },
+    {
+      "title": "专访《主角》原著作者陈彦:这是戏曲艺人的一生,也是大时代的变迁",
+      "url": "https://baijiahao.baidu.com/s?id=1865688984111079921&wfr=spider&for=pc",
+      "source": "北京日报客户端",
+      "time": "前天14:46",
+      "category": "other"
+    },
+    {
+      "title": "不交论文交作品!华东师大36名学生凭文学创作获硕士学位",
+      "url": "https://baijiahao.baidu.com/s?id=1865688418370627389&wfr=spider&for=pc",
+      "source": "澎湃新闻",
+      "time": "前天14:37",
+      "category": "meeting"
+    },
+    {
+      "title": "聚焦未来文学:西华大学创意写作中心成立,全国专家共议罗伟章...",
+      "url": "https://www.eol.cn/sichuan/scgd/202605/t20260520_2735974.shtml",
+      "source": "教育在线",
+      "time": "前天10:12",
+      "category": "other"
+    },
+    {
+      "title": "茅盾文学奖得主阿来来泉分享《东坡在人间》",
+      "url": "https://baijiahao.baidu.com/s?id=1865671024791625177&wfr=spider&for=pc",
+      "source": "泉州网",
+      "time": "前天08:59",
+      "category": "other"
+    },
+    {
+      "title": "茅盾文学奖得主王旭烽新作《茶事笔记》发布,在茶文化中读懂治愈...",
+      "url": "https://baijiahao.baidu.com/s?id=1865673129347195663&wfr=spider&for=pc",
+      "source": "闪电新闻",
+      "time": "前天10:30",
+      "category": "other"
+    },
+    {
+      "title": "茅奖作家王旭烽继“茶人四部曲”后又一力作,《茶事笔记》举办首发式",
+      "url": "https://baijiahao.baidu.com/s?id=1865694868062654446&wfr=spider&for=pc",
+      "source": "济南时报",
+      "time": "前天15:44",
+      "category": "activity"
+    },
+    {
+      "title": "新刊推介|致青春 致奋斗 致家风绵长",
+      "url": "https://baijiahao.baidu.com/s?id=1865692102816627647&wfr=spider&for=pc",
+      "source": "中国妇女",
+      "time": "前天15:21",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -33,22 +103,29 @@ window.LIT_DATA = {
       "title": "程小莹长篇新作《原本》:寻找庸常岁月里的精神原乡 | 深读",
       "url": "https://www.thepaper.cn/newsDetail_forward_33178883",
       "source": "澎湃新闻",
-      "time": "昨天11:52",
+      "time": "前天11:52",
       "category": "other"
     },
     {
       "title": "文耀岛城、薪火相传!首届刘知侠长篇小说奖颁奖,《青岛市中青年...",
       "url": "https://baijiahao.baidu.com/s?id=1865698674387227669&wfr=spider&for=pc",
       "source": "大众日报",
-      "time": "昨天17:21",
+      "time": "前天17:21",
       "category": "activity"
     },
     {
       "title": "首届“刘知侠长篇小说奖”颁奖典礼暨《青岛市中青年作家文库...",
       "url": "https://baijiahao.baidu.com/s?id=1865704053349245673&wfr=spider&for=pc",
       "source": "半岛都市报",
-      "time": "昨天18:41",
+      "time": "前天18:41",
       "category": "activity"
+    },
+    {
+      "title": "柞水凤凰古镇亮相央视电视剧《主角》",
+      "url": "https://baijiahao.baidu.com/s?id=1865759144337044618&wfr=spider&for=pc",
+      "source": "商洛新闻网",
+      "time": "昨天09:23",
+      "category": "other"
     }
   ]
 };
