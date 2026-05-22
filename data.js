5d4e4c3debabc5013594c49c33764c9ea8a8cbf7
GitHub Action <action@github.com>
2026-05-22 16:13:24 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-05-22 21:20",
+  "update_time": "2026-05-23 00:13",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,6 +23,13 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "山东省发布新版抗菌药物分级管理目录2026年5月起施行",
+      "url": "http://news.10jqka.com.cn/20260512/c676619814.shtml",
+      "source": "同花顺财经",
+      "time": "5月12日",
+      "category": "activity"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -30,31 +37,17 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "文耀岛城、薪火相传!首届刘知侠长篇小说奖颁奖,《青岛市中青年...",
-      "url": "https://baijiahao.baidu.com/s?id=1865698674387227669&wfr=spider&for=pc",
-      "source": "大众日报",
-      "time": "前天17:21",
-      "category": "activity"
-    },
-    {
-      "title": "首届“刘知侠长篇小说奖”颁奖典礼暨《青岛市中青年作家文库...",
-      "url": "https://baijiahao.baidu.com/s?id=1865704053349245673&wfr=spider&for=pc",
-      "source": "半岛都市报",
-      "time": "前天18:41",
-      "category": "activity"
-    },
-    {
       "title": "《芝镇说》获“刘知侠长篇小说奖”",
       "url": "https://paper.dzwww.com/ncdz/content/20260521/Articel02004MT.htm",
       "source": "大众日报数字报",
-      "time": "昨天",
+      "time": "前天",
       "category": "activity"
     },
     {
       "title": "《芝镇说》获“刘知侠长篇小说奖",
       "url": "http://www.sd.xinhuanet.com/20260521/99288cd565ca45b8aa461c551361c5f7/c.html",
       "source": "新华网山东频道",
-      "time": "昨天11:23",
+      "time": "前天11:23",
       "category": "activity"
     }
   ]
