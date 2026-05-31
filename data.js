f9133502c08be7028aa12c7dffce497d24266a61
GitHub Action <action@github.com>
2026-05-31 06:45:55 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-05-31 10:07",
+  "update_time": "2026-05-31 14:45",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,153 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "大众写科幻,带来了什么?",
-      "url": "http://baijiahao.baidu.com/s?id=1853855131427137219&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "1月9日",
-      "category": "voice"
-    },
-    {
-      "title": "《三体》之后,下一个“破圈”之作在哪里",
-      "url": "https://baijiahao.baidu.com/s?id=1863114992955488797&wfr=spider&for=pc",
-      "source": "光明网",
-      "time": "4月22日",
-      "category": "other"
-    },
-    {
-      "title": "中国科幻产业走向“生态出海”",
-      "url": "https://baijiahao.baidu.com/s?id=1861855033203367752&wfr=spider&for=pc",
-      "source": "环球网",
-      "time": "4月8日",
-      "category": "voice"
-    },
-    {
-      "title": "“非虚构”和“虚构”的界限",
-      "url": "http://baijiahao.baidu.com/s?id=1740112764190450044&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
-      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
-      "url": "https://m.jiemian.com/article/3573232.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "迈向跨界融合的“大文学”时代",
-      "url": "http://www.xinhuanet.com/book/20260120/d3fa6fd5f93048fc88a2d3be4ef48224/c.html",
-      "source": "新华网",
-      "time": "1月20日",
-      "category": "other"
-    },
-    {
-      "title": "《中原文学》主编李辉:破圈不破底,风依旧滚烫|顶端文学名刊访谈",
-      "url": "https://baijiahao.baidu.com/s?id=1862517996245963491&wfr=spider&for=pc",
-      "source": "顶端新闻",
-      "time": "4月15日",
-      "category": "other"
-    },
-    {
-      "title": "特别策划 | 如何捍卫文字的“底线”?",
-      "url": "https://www.jiemian.com/article/14294626.html",
-      "source": "界面新闻",
-      "time": "4月22日",
-      "category": "other"
-    },
-    {
-      "title": "2025年度“中国人文学术十大热点”发布",
-      "url": "https://baijiahao.baidu.com/s?id=1864699811170864207&wfr=spider&for=pc",
-      "source": "中国新闻网",
-      "time": "5月9日",
-      "category": "voice"
-    },
-    {
-      "title": "2025年度“中国人文学术十大热点”正式发布",
-      "url": "https://baijiahao.baidu.com/s?id=1864768892164544944&wfr=spider&for=pc",
-      "source": "中国青年网",
-      "time": "5月10日",
-      "category": "voice"
-    },
-    {
-      "title": "理性看待影视与文学“双向圈粉”",
-      "url": "https://baijiahao.baidu.com/s?id=1866460195984791736&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "前天03:05",
-      "category": "other"
-    },
-    {
-      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
-      "url": "https://www.jfdaily.com/wx/detail.do?id=1050469",
-      "source": "上观",
-      "time": "1月12日",
-      "category": "activity"
-    },
-    {
-      "title": "2026年,这些新书值得我们期待(上)",
-      "url": "https://baijiahao.baidu.com/s?id=1854440677511475404&wfr=spider&for=pc",
-      "source": "界面新闻",
-      "time": "1月16日",
-      "category": "activity"
-    },
-    {
-      "title": "2026年,这些图书不要错过",
-      "url": "https://baijiahao.baidu.com/s?id=1857913561928553786&wfr=spider&for=pc",
-      "source": "扬子晚报",
-      "time": "2月23日",
-      "category": "other"
-    },
-    {
-      "title": "从“新”出发,重塑书写的河道|2025年度刀锋图书奖全榜单",
-      "url": "https://baijiahao.baidu.com/s?id=1862975324373258594&wfr=spider&for=pc",
-      "source": "新周刊",
-      "time": "4月20日",
-      "category": "other"
-    },
-    {
-      "title": "马伯庸14万字新作《楚弓计划》等12部长篇体现上海文学老中青接力...",
-      "url": "http://baijiahao.baidu.com/s?id=1863050275289718289&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "4月21日",
-      "category": "other"
-    },
-    {
-      "title": "今年上海作家们有哪些新作品出版?这场文学创作座谈会有分享,十二...",
-      "url": "https://baijiahao.baidu.com/s?id=1863069065231049456&wfr=spider&for=pc",
-      "source": "新闻晨报",
-      "time": "4月21日",
-      "category": "activity"
-    },
-    {
-      "title": "“我书写上海的作品很快就会开花结果“!“今年12部长篇小说值得期待",
-      "url": "https://baijiahao.baidu.com/s?id=1863071926151298760&wfr=spider&for=pc",
-      "source": "新民晚报",
-      "time": "4月21日",
-      "category": "activity"
-    },
-    {
-      "title": "2025年度刀锋图书奖在泉州揭晓 年度主题聚焦“假如我是新的”",
-      "url": "http://news.10jqka.com.cn/20260421/c676158301.shtml",
-      "source": "同花顺财经",
-      "time": "4月21日",
-      "category": "other"
-    },
-    {
-      "title": "一周文艺:村上春树将出新长篇,《主角》开播,《给阿嬷的情书》口碑...",
-      "url": "http://baijiahao.baidu.com/s?id=1864855230078019269&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "5月11日",
-      "category": "other"
-    },
-    {
       "title": "2026年文学上新:长篇扩容、非虚构升温、技术语境探人心",
       "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1047392&sid=300",
       "source": "上观",
@@ -181,6 +34,13 @@ window.LIT_DATA = {
       "url": "https://baijiahao.baidu.com/s?id=1859897163056459211&wfr=spider&for=pc",
       "source": "北京日报客户端",
       "time": "3月17日",
+      "category": "other"
+    },
+    {
+      "title": "许子东:今年读过最意外的书,来自一位中国企业家",
+      "url": "https://baijiahao.baidu.com/s?id=1866517760913867169&wfr=spider&for=pc",
+      "source": "新周刊",
+      "time": "前天16:01",
       "category": "other"
     },
     {
