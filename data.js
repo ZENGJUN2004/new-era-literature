b9f705c8dfeee0d042caee03da146ebee477572a
GitHub Action <action@github.com>
2026-06-04 22:59:35 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-06-05 05:22",
+  "update_time": "2026-06-05 06:59",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,17 +23,66 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "周观天下丨日韩首脑会谈,矛盾依然突出",
-      "url": "https://sichuan.scol.com.cn/ggxw/202601/83190684.html",
-      "source": "四川在线",
-      "time": "1月17日",
+      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
+      "url": "https://www.jfdaily.com/wx/detail.do?id=1050469",
+      "source": "上观",
+      "time": "1月12日",
+      "category": "activity"
+    },
+    {
+      "title": "2026年,这些新书值得我们期待(上)",
+      "url": "https://baijiahao.baidu.com/s?id=1854440677511475404&wfr=spider&for=pc",
+      "source": "界面新闻",
+      "time": "1月16日",
+      "category": "activity"
+    },
+    {
+      "title": "2026年,这些图书不要错过",
+      "url": "https://baijiahao.baidu.com/s?id=1857913561928553786&wfr=spider&for=pc",
+      "source": "扬子晚报",
+      "time": "2月23日",
       "category": "other"
     },
     {
-      "title": "解析世界乱局下的中国抉择,“悦见人文”走进上应大",
-      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1088469",
-      "source": "上观",
-      "time": "3月29日",
+      "title": "从《我的阿勒泰》到苏菲·玛索的新书《暗河》,花城文学院3岁了!",
+      "url": "https://baijiahao.baidu.com/s?id=1860805556134866933&wfr=spider&for=pc",
+      "source": "钱江晚报",
+      "time": "3月27日",
+      "category": "activity"
+    },
+    {
+      "title": "从“新”出发,重塑书写的河道|2025年度刀锋图书奖全榜单",
+      "url": "https://baijiahao.baidu.com/s?id=1862975324373258594&wfr=spider&for=pc",
+      "source": "新周刊",
+      "time": "4月20日",
+      "category": "other"
+    },
+    {
+      "title": "今年上海作家们有哪些新作品出版?这场文学创作座谈会有分享,十二...",
+      "url": "https://baijiahao.baidu.com/s?id=1863069065231049456&wfr=spider&for=pc",
+      "source": "新闻晨报",
+      "time": "4月21日",
+      "category": "activity"
+    },
+    {
+      "title": "“我书写上海的作品很快就会开花结果“!“今年12部长篇小说值得期待",
+      "url": "https://baijiahao.baidu.com/s?id=1863071926151298760&wfr=spider&for=pc",
+      "source": "新民晚报",
+      "time": "4月21日",
+      "category": "activity"
+    },
+    {
+      "title": "2025年度刀锋图书奖在泉州揭晓 年度主题聚焦“假如我是新的”",
+      "url": "http://news.10jqka.com.cn/20260421/c676158301.shtml",
+      "source": "同花顺财经",
+      "time": "4月21日",
+      "category": "other"
+    },
+    {
+      "title": "一周文艺:村上春树将出新长篇,《主角》开播,《给阿嬷的情书》口碑...",
+      "url": "http://baijiahao.baidu.com/s?id=1864855230078019269&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "5月11日",
       "category": "other"
     },
     {
