b9cf466c536f794ab71bae7187933be1e8b6a255
GitHub Action <action@github.com>
2026-06-17 05:57:36 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-06-17 08:07",
+  "update_time": "2026-06-17 13:57",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,18 +23,25 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "苏童毕飞宇吴真全勇先,2025收获文学榜揭晓,榜首作家这样说",
-      "url": "http://baijiahao.baidu.com/s?id=1856104193950555898&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "2月3日",
-      "category": "other"
+      "title": "王玉玊:为幻想文学一辩,我们在虚构的故事当中寻求真实|新批评...",
+      "url": "http://baijiahao.baidu.com/s?id=1748448083899376729&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "meeting"
     },
     {
-      "title": "《收获》文学榜揭晓,榜单映射中国文坛迎来“丰年”",
-      "url": "https://www.news.cn/politics/20260204/87438f63cae0416ba7ab64906791eccb/c.html",
-      "source": "新华网",
-      "time": "2月4日",
-      "category": "other"
+      "title": "真善美的三重奏:文学批评的时代使命|新批评",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32417549",
+      "source": "澎湃新闻",
+      "time": "1月20日",
+      "category": "meeting"
+    },
+    {
+      "title": "广西发布首批软件产品与信息技术服务方案目录",
+      "url": "http://news.10jqka.com.cn/20260615/c677480743.shtml",
+      "source": "同花顺财经",
+      "time": "前天22:11",
+      "category": "activity"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
