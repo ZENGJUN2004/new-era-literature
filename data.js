4f74e7829ace086de8b1de824318b9477703f759
GitHub Action <action@github.com>
2026-06-18 07:24:19 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-06-18 10:16",
+  "update_time": "2026-06-18 15:24",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,27 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "大众写科幻,带来了什么?",
-      "url": "http://baijiahao.baidu.com/s?id=1853855131427137219&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "1月9日",
-      "category": "voice"
-    },
-    {
-      "title": "《三体》之后,下一个“破圈”之作在哪里",
-      "url": "https://baijiahao.baidu.com/s?id=1863114992955488797&wfr=spider&for=pc",
-      "source": "光明网",
-      "time": "4月22日",
-      "category": "other"
-    },
-    {
-      "title": "中国科幻产业走向“生态出海”",
-      "url": "https://baijiahao.baidu.com/s?id=1861855033203367752&wfr=spider&for=pc",
-      "source": "环球网",
-      "time": "4月8日",
-      "category": "voice"
-    },
-    {
       "title": "“非虚构”和“虚构”的界限",
       "url": "http://baijiahao.baidu.com/s?id=1740112764190450044&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -58,109 +37,11 @@ window.LIT_DATA = {
       "category": "voice"
     },
     {
-      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
-      "url": "https://m.jiemian.com/article/3573232.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "《中原文学》主编李辉:破圈不破底,风依旧滚烫|顶端文学名刊访谈",
-      "url": "https://baijiahao.baidu.com/s?id=1862517996245963491&wfr=spider&for=pc",
-      "source": "顶端新闻",
-      "time": "4月15日",
-      "category": "other"
-    },
-    {
-      "title": "2025年度中国人文学术十大热点发布",
-      "url": "http://yn.people.com.cn/n2/2026/0512/c372453-41577528.html",
-      "source": "人民网",
-      "time": "5月12日",
-      "category": "voice"
-    },
-    {
-      "title": "理性看待影视与文学“双向圈粉”",
-      "url": "https://baijiahao.baidu.com/s?id=1866460195984791736&wfr=spider&for=pc",
-      "source": "新浪财经",
-      "time": "5月29日",
-      "category": "other"
-    },
-    {
-      "title": "“文学游”流行,能否带动文学阅读复兴|封面评论",
-      "url": "https://baijiahao.baidu.com/s?id=1868150267274336716&wfr=spider&for=pc",
-      "source": "封面新闻",
-      "time": "前天18:48",
-      "category": "meeting"
-    },
-    {
-      "title": "2025春风月榜半年报告:希望这22本佳作为你带来光",
-      "url": "https://baijiahao.baidu.com/s?id=1853802033490694012&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "1月9日",
-      "category": "other"
-    },
-    {
-      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
-      "url": "https://www.jfdaily.com/wx/detail.do?id=1050469",
-      "source": "上观",
-      "time": "1月12日",
+      "title": "戴思杰暌违十年推出中文小说《唐卡画师》 同名电影改编筹备中",
+      "url": "https://stock.10jqka.com.cn/20260616/c677507678.shtml",
+      "source": "同花顺",
+      "time": "前天18:35",
       "category": "activity"
-    },
-    {
-      "title": "2026年,这些新书值得我们期待(上)",
-      "url": "https://baijiahao.baidu.com/s?id=1854440677511475404&wfr=spider&for=pc",
-      "source": "界面新闻",
-      "time": "1月16日",
-      "category": "activity"
-    },
-    {
-      "title": "2026年,这些图书不要错过",
-      "url": "https://baijiahao.baidu.com/s?id=1857913561928553786&wfr=spider&for=pc",
-      "source": "扬子晚报",
-      "time": "2月23日",
-      "category": "other"
-    },
-    {
-      "title": "从“新”出发,重塑书写的河道|2025年度刀锋图书奖全榜单",
-      "url": "https://baijiahao.baidu.com/s?id=1862975324373258594&wfr=spider&for=pc",
-      "source": "新周刊",
-      "time": "4月20日",
-      "category": "other"
-    },
-    {
-      "title": "今年上海作家们有哪些新作品出版?这场文学创作座谈会有分享,十二...",
-      "url": "https://baijiahao.baidu.com/s?id=1863069065231049456&wfr=spider&for=pc",
-      "source": "新闻晨报",
-      "time": "4月21日",
-      "category": "activity"
-    },
-    {
-      "title": "“我书写上海的作品很快就会开花结果“!“今年12部长篇小说值得期待",
-      "url": "https://baijiahao.baidu.com/s?id=1863071926151298760&wfr=spider&for=pc",
-      "source": "新民晚报",
-      "time": "4月21日",
-      "category": "activity"
-    },
-    {
-      "title": "2025年度刀锋图书奖在泉州揭晓 年度主题聚焦“假如我是新的”",
-      "url": "http://news.10jqka.com.cn/20260421/c676158301.shtml",
-      "source": "同花顺财经",
-      "time": "4月21日",
-      "category": "other"
-    },
-    {
-      "title": "2026年文学上新:长篇扩容、非虚构升温、技术语境探人心",
-      "url": "https://www.jfdaily.com/staticsg/res/html/web/newsDetail.html?id=1047392&sid=300",
-      "source": "上观",
-      "time": "1月6日",
-      "category": "voice"
-    },
-    {
-      "title": "“Z世代文学,向何处”,《十月》创作分享会尝试作答",
-      "url": "https://baijiahao.baidu.com/s?id=1859897163056459211&wfr=spider&for=pc",
-      "source": "北京日报客户端",
-      "time": "3月17日",
-      "category": "other"
     },
     {
       "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
