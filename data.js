a7a4bcd9aa6935c795d921863d077954afaffb54
GitHub Action <action@github.com>
2026-07-01 16:14:31 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-07-01 21:34",
+  "update_time": "2026-07-02 00:14",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,94 +23,10 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "2025春风月榜半年报告:希望这22本佳作为你带来光",
-      "url": "https://baijiahao.baidu.com/s?id=1853802033490694012&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "1月9日",
-      "category": "other"
-    },
-    {
-      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
-      "url": "https://www.jfdaily.com/news/detail?id=1050469",
-      "source": "上观",
-      "time": "1月12日",
-      "category": "activity"
-    },
-    {
-      "title": "2026年,这些新书值得我们期待(上)",
-      "url": "https://baijiahao.baidu.com/s?id=1854440677511475404&wfr=spider&for=pc",
-      "source": "界面新闻",
-      "time": "1月16日",
-      "category": "activity"
-    },
-    {
-      "title": "2026年,这些图书不要错过",
-      "url": "https://baijiahao.baidu.com/s?id=1857913561928553786&wfr=spider&for=pc",
-      "source": "扬子晚报",
-      "time": "2月23日",
-      "category": "other"
-    },
-    {
-      "title": "从《我的阿勒泰》到苏菲·玛索的新书《暗河》,花城文学院3岁了!",
-      "url": "https://baijiahao.baidu.com/s?id=1860805556134866933&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "3月27日",
-      "category": "activity"
-    },
-    {
-      "title": "从“新”出发,重塑书写的河道|2025年度刀锋图书奖全榜单",
-      "url": "https://baijiahao.baidu.com/s?id=1862975324373258594&wfr=spider&for=pc",
-      "source": "新周刊",
-      "time": "4月20日",
-      "category": "other"
-    },
-    {
-      "title": "今年上海作家们有哪些新作品出版?这场文学创作座谈会有分享,十二...",
-      "url": "https://baijiahao.baidu.com/s?id=1863069065231049456&wfr=spider&for=pc",
-      "source": "新闻晨报",
-      "time": "4月21日",
-      "category": "activity"
-    },
-    {
-      "title": "“我书写上海的作品很快就会开花结果“!“今年12部长篇小说值得期待",
-      "url": "https://baijiahao.baidu.com/s?id=1863071926151298760&wfr=spider&for=pc",
-      "source": "新民晚报",
-      "time": "4月21日",
-      "category": "activity"
-    },
-    {
-      "title": "一周文艺:村上春树将出新长篇《夏帆》,《主角》开播,2026年普利策...",
-      "url": "https://www.thepaper.cn/newsDetail_forward_33160865",
-      "source": "澎湃新闻",
-      "time": "5月20日",
-      "category": "other"
-    },
-    {
-      "title": "苏童毕飞宇吴真全勇先,2025收获文学榜揭晓,榜首作家这样说",
-      "url": "http://baijiahao.baidu.com/s?id=1856104193950555898&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "2月3日",
-      "category": "other"
-    },
-    {
-      "title": "《收获》文学榜揭晓,榜单映射中国文坛迎来“丰年”",
-      "url": "https://www.news.cn/politics/20260204/87438f63cae0416ba7ab64906791eccb/c.html",
-      "source": "新华网",
-      "time": "2月4日",
-      "category": "other"
-    },
-    {
       "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
       "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
       "source": "红网",
       "time": "1月10日",
-      "category": "activity"
-    },
-    {
-      "title": "长篇儿童小说《拾光乐队》在南京首发,作者杨娟分享幕后创作故事",
-      "url": "https://baijiahao.baidu.com/s?id=1869318336398988623&wfr=spider&for=pc",
-      "source": "扬子晚报",
-      "time": "前天15:46",
       "category": "activity"
     }
   ]
