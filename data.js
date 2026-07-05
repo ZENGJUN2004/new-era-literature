be37f8fbf230160073f229adc40eb41ba5754b7f
GitHub Action <action@github.com>
2026-07-05 15:59:35 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-07-05 22:24",
+  "update_time": "2026-07-05 23:59",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,11 +23,32 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "任见《牯岭秘纪-李德立传》(欧版)第三章 清凉荒山 012",
-      "url": "https://cul.sohu.com/a/1044529736_643334",
-      "source": "搜狐网",
-      "time": "前天05:20",
+      "title": "收获文学榜在上海揭晓!苏童《好天气》领衔长篇榜首",
+      "url": "http://baijiahao.baidu.com/s?id=1856101673487226555&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "2月3日",
       "category": "other"
+    },
+    {
+      "title": "苏童毕飞宇吴真全勇先,2025收获文学榜揭晓,榜首作家这样说",
+      "url": "http://baijiahao.baidu.com/s?id=1856104193950555898&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "2月3日",
+      "category": "other"
+    },
+    {
+      "title": "《收获》文学榜揭晓,榜单映射中国文坛迎来“丰年”",
+      "url": "https://www.news.cn/politics/20260204/87438f63cae0416ba7ab64906791eccb/c.html",
+      "source": "新华网",
+      "time": "2月4日",
+      "category": "other"
+    },
+    {
+      "title": "《收获长篇小说2026夏卷》:三部名家新作抢先读|新书",
+      "url": "https://baijiahao.baidu.com/s?id=1869679308074987804&wfr=spider&for=pc",
+      "source": "济南时报",
+      "time": "前天14:54",
+      "category": "activity"
     },
     {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
