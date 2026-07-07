236b74b980fc98cce098d98ce8dbd0548db48439
GitHub Action <action@github.com>
2026-07-07 06:34:05 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-07-07 10:02",
+  "update_time": "2026-07-07 14:34",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,153 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "大众写科幻,带来了什么?",
-      "url": "http://baijiahao.baidu.com/s?id=1853855131427137219&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "1月9日",
-      "category": "voice"
-    },
-    {
-      "title": "中国网络文学走向多样性繁荣",
-      "url": "https://baijiahao.baidu.com/s?id=1862950470829076947&wfr=spider&for=pc",
-      "source": "金台资讯",
-      "time": "4月20日",
-      "category": "other"
-    },
-    {
-      "title": "《三体》之后,下一个“破圈”之作在哪里",
-      "url": "https://baijiahao.baidu.com/s?id=1863114992955488797&wfr=spider&for=pc",
-      "source": "光明网",
-      "time": "4月22日",
-      "category": "other"
-    },
-    {
-      "title": "中国科幻产业走向“生态出海”",
-      "url": "https://baijiahao.baidu.com/s?id=1861855033203367752&wfr=spider&for=pc",
-      "source": "环球网",
-      "time": "4月8日",
-      "category": "voice"
-    },
-    {
-      "title": "贾樟柯、梁鸿谈非虚构写作和纪录电影",
-      "url": "http://baijiahao.baidu.com/s?id=1717824461237591660&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "【思想界】彼得·汉德克获诺奖惹争议:文学性能够用政治和道德衡量...",
-      "url": "https://m.jiemian.com/article/3573232.html",
-      "source": "界面新闻",
-      "time": "",
-      "category": "voice"
-    },
-    {
-      "title": "2026,出版的新圈子",
-      "url": "https://cul.sohu.com/a/975651938_121123863",
-      "source": "搜狐网",
-      "time": "1月13日",
-      "category": "activity"
-    },
-    {
-      "title": "《中原文学》主编李辉:破圈不破底,风依旧滚烫|顶端文学名刊访谈",
-      "url": "https://baijiahao.baidu.com/s?id=1862517996245963491&wfr=spider&for=pc",
-      "source": "顶端新闻",
-      "time": "4月15日",
-      "category": "other"
-    },
-    {
-      "title": "2025年度“中国人文学术十大热点”发布",
-      "url": "https://baijiahao.baidu.com/s?id=1864699811170864207&wfr=spider&for=pc",
-      "source": "中国新闻网",
-      "time": "5月9日",
-      "category": "voice"
-    },
-    {
-      "title": "2025年度中国人文学术十大热点发布",
-      "url": "http://yn.people.com.cn/n2/2026/0512/c372453-41577528.html",
-      "source": "人民网",
-      "time": "5月12日",
-      "category": "voice"
-    },
-    {
-      "title": "浮夸腰封渐散 口碑才是流量底色",
-      "url": "https://baijiahao.baidu.com/s?id=1868832604201302647&wfr=spider&for=pc",
-      "source": "青瞳视角",
-      "time": "6月24日",
-      "category": "other"
-    },
-    {
-      "title": "一周文艺:阿来推出非虚构新作,2026年新书出版计划,文学改编电视剧...",
-      "url": "https://www.jfdaily.com/news/detail?id=1050469",
-      "source": "上观",
-      "time": "1月12日",
-      "category": "activity"
-    },
-    {
-      "title": "2026年,这些新书值得我们期待(上)",
-      "url": "https://baijiahao.baidu.com/s?id=1854440677511475404&wfr=spider&for=pc",
-      "source": "界面新闻",
-      "time": "1月16日",
-      "category": "activity"
-    },
-    {
-      "title": "2026年,这些图书不要错过",
-      "url": "https://baijiahao.baidu.com/s?id=1857913561928553786&wfr=spider&for=pc",
-      "source": "扬子晚报",
-      "time": "2月23日",
-      "category": "other"
-    },
-    {
-      "title": "从《我的阿勒泰》到苏菲·玛索的新书《暗河》,花城文学院3岁了!",
-      "url": "https://baijiahao.baidu.com/s?id=1860805556134866933&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "3月27日",
-      "category": "activity"
-    },
-    {
-      "title": "从“新”出发,重塑书写的河道|2025年度刀锋图书奖全榜单",
-      "url": "https://baijiahao.baidu.com/s?id=1862975324373258594&wfr=spider&for=pc",
-      "source": "新周刊",
-      "time": "4月20日",
-      "category": "other"
-    },
-    {
-      "title": "马伯庸年底又出新小说!上海作家今年将出版这些文学新作",
-      "url": "https://baijiahao.baidu.com/s?id=1863055507131434401&wfr=spider&for=pc",
-      "source": "澎湃新闻",
-      "time": "4月21日",
-      "category": "activity"
-    },
-    {
-      "title": "今年上海作家们有哪些新作品出版?这场文学创作座谈会有分享,十二...",
-      "url": "https://baijiahao.baidu.com/s?id=1863069065231049456&wfr=spider&for=pc",
-      "source": "新闻晨报",
-      "time": "4月21日",
-      "category": "activity"
-    },
-    {
-      "title": "“我书写上海的作品很快就会开花结果“!“今年12部长篇小说值得期待",
-      "url": "https://baijiahao.baidu.com/s?id=1863071926151298760&wfr=spider&for=pc",
-      "source": "新民晚报",
-      "time": "4月21日",
-      "category": "activity"
-    },
-    {
-      "title": "2025年度刀锋图书奖在泉州揭晓 年度主题聚焦“假如我是新的”",
-      "url": "http://news.10jqka.com.cn/20260421/c676158301.shtml",
-      "source": "同花顺财经",
-      "time": "4月21日",
-      "category": "other"
-    },
-    {
-      "title": "“Z世代文学,向何处”,《十月》创作分享会尝试作答",
-      "url": "https://baijiahao.baidu.com/s?id=1859897163056459211&wfr=spider&for=pc",
-      "source": "北京日报客户端",
-      "time": "3月17日",
-      "category": "other"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
@@ -177,10 +30,31 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
+      "title": "现实主义长篇小说《呼吸》在京首发",
+      "url": "http://www.xinhuanet.com/politics/20260621/796fa78c25d14b0a99a347d38d29689f/c.html",
+      "source": "新华网",
+      "time": "6月21日",
+      "category": "activity"
+    },
+    {
       "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
       "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
       "source": "红网",
       "time": "1月10日",
+      "category": "activity"
+    },
+    {
+      "title": "再一次把老家请进文字,祁智长篇儿童小说《我们兄弟》在江苏书展首发",
+      "url": "https://baijiahao.baidu.com/s?id=1869945028176085222&wfr=spider&for=pc",
+      "source": "扬子晚报",
+      "time": "昨天13:44",
+      "category": "activity"
+    },
+    {
+      "title": "法治文学结硕果!干警长篇小说《雏菊》首发研讨会在漳县法院召开",
+      "url": "https://baijiahao.baidu.com/s?id=1869935057554337175&wfr=spider&for=pc",
+      "source": "每日甘肃",
+      "time": "昨天10:16",
       "category": "activity"
     }
   ]
