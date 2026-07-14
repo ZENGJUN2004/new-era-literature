1f2d2d8906ada1b47373bfa49a59fffe0c3b5dfc
GitHub Action <action@github.com>
2026-07-14 20:03:16 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-07-15 02:01",
+  "update_time": "2026-07-15 04:03",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,34 +21,6 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
-    },
-    {
-      "title": "大众写科幻,带来了什么?",
-      "url": "http://baijiahao.baidu.com/s?id=1853855131427137219&wfr=spider&for=pc",
-      "source": "上观新闻",
-      "time": "1月9日",
-      "category": "voice"
-    },
-    {
-      "title": "中国网络文学走向多样性繁荣",
-      "url": "https://baijiahao.baidu.com/s?id=1862950470829076947&wfr=spider&for=pc",
-      "source": "金台资讯",
-      "time": "4月20日",
-      "category": "other"
-    },
-    {
-      "title": "当刘慈欣的预言渐渐迫近,科幻作家们如何书写来自AI的威胁?",
-      "url": "https://baijiahao.baidu.com/s?id=1863355106623523352&wfr=spider&for=pc",
-      "source": "红星新闻",
-      "time": "4月24日",
-      "category": "voice"
-    },
-    {
-      "title": "中国科幻产业走向“生态出海”",
-      "url": "https://baijiahao.baidu.com/s?id=1861855033203367752&wfr=spider&for=pc",
-      "source": "环球网",
-      "time": "4月8日",
-      "category": "voice"
     },
     {
       "title": "创新驱动,低碳引领|盾安环境精彩亮相 2026 热泵行业年会",
