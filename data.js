cb13b84e4b0887df5c702439d36d498476fb19dc
GitHub Action <action@github.com>
2026-07-16 13:14:05 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-07-16 19:25",
+  "update_time": "2026-07-16 21:14",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,6 +21,69 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
+    },
+    {
+      "title": "豆瓣8.0+!高分人物传记,一书读罢头飞雪",
+      "url": "https://www.thepaper.cn/newsDetail_forward_33007780",
+      "source": "澎湃新闻",
+      "time": "4月21日",
+      "category": "other"
+    },
+    {
+      "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
+      "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "「期刊推荐」长安街读书会第20260303期干部学习核心期刊目录",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32793858",
+      "source": "澎湃新闻",
+      "time": "3月19日",
+      "category": "activity"
+    },
+    {
+      "title": "《光荣亭》新书发布活动在福建上杭举行",
+      "url": "http://www.mzyfz.com/html/2203/2026-01-12/content-1669350.html",
+      "source": "民主与法制网",
+      "time": "1月12日",
+      "category": "activity"
+    },
+    {
+      "title": "长篇纪实文学《烽火》新书发布暨座谈会在德州举行",
+      "url": "https://baijiahao.baidu.com/s?id=1854984906459423165&wfr=spider&for=pc",
+      "source": "大众日报",
+      "time": "1月22日",
+      "category": "activity"
+    },
+    {
+      "title": "《自愈者》新书在京发布作家老藤探讨AI时代的心灵救赎之路",
+      "url": "https://baijiahao.baidu.com/s?id=1854293051586245393&wfr=spider&for=pc",
+      "source": "央广网",
+      "time": "1月14日",
+      "category": "activity"
+    },
+    {
+      "title": "直播丨“作家天团”空降开课!《大作家的小作文》新书发布会,速来...",
+      "url": "http://www.thepaper.cn/newsDetail_forward_31405883",
+      "source": "澎湃新闻",
+      "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "书香喀什・新书发布暨现场签售活动举行",
+      "url": "http://baijiahao.baidu.com/s?id=1863459232450526300&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "4月25日",
+      "category": "activity"
+    },
+    {
+      "title": "青年作家徐鹏新作《未央·蜀道残阳》在重庆永川发布",
+      "url": "http://www.cq.xinhuanet.com/20260429/47022f852e3d4ac5a7f184dfcf56d340/c.html",
+      "source": "新华网重庆频道",
+      "time": "4月29日",
+      "category": "other"
     }
   ]
 };
