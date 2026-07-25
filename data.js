2e9fcf26682991bf14329d7b9feb82c59c09e685
GitHub Action <action@github.com>
2026-07-25 18:46:46 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-07-26 01:47",
+  "update_time": "2026-07-26 02:46",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -21,13 +21,6 @@ window.LIT_DATA = {
       "source": "CNKI",
       "time": "实时",
       "category": "meeting"
-    },
-    {
-      "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
-      "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "",
-      "category": "activity"
     },
     {
       "title": "山乡巨变中的文学守望:《太阳照在滹沱河上》新书发布会在正定塔元...",
