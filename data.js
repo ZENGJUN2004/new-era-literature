2984ad2211b89972b2f8863c698fc80ada7f5fa8
GitHub Action <action@github.com>
2026-07-27 23:53:14 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-07-28 06:04",
+  "update_time": "2026-07-28 07:53",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -30,20 +30,6 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "书香喀什・新书发布暨现场签售活动举行",
-      "url": "http://baijiahao.baidu.com/s?id=1863459232450526300&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "4月25日",
-      "category": "activity"
-    },
-    {
-      "title": "聚焦边疆书写 张者《天边》新书发布",
-      "url": "https://baijiahao.baidu.com/s?id=1862343135717648529&wfr=spider&for=pc",
-      "source": "中国新闻网",
-      "time": "4月13日",
-      "category": "activity"
-    },
-    {
       "title": "《郭永怀传》新书发布,“千里山海书香威海”全民阅读活动周启幕",
       "url": "https://baijiahao.baidu.com/s?id=1862883593361097132&wfr=spider&for=pc",
       "source": "齐鲁壹点",
@@ -54,6 +40,13 @@ window.LIT_DATA = {
       "title": "青年作家徐鹏新作《未央·蜀道残阳》在重庆永川发布",
       "url": "http://www.cq.xinhuanet.com/20260429/47022f852e3d4ac5a7f184dfcf56d340/c.html",
       "source": "新华网重庆频道",
+      "time": "4月29日",
+      "category": "other"
+    },
+    {
+      "title": "青年作家徐鹏新作《未央·蜀道残阳》在永川发布",
+      "url": "https://baijiahao.baidu.com/s?id=1863771851573387062&wfr=spider&for=pc",
+      "source": "金台资讯",
       "time": "4月29日",
       "category": "other"
     },
