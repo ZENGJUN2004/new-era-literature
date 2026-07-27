09bc3dab0586920d86975b424ca5234597342e0c
GitHub Action <action@github.com>
2026-07-27 17:27:42 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-07-27 23:35",
+  "update_time": "2026-07-28 01:27",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -30,17 +30,17 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
+      "title": "山乡巨变中的文学守望:《太阳照在滹沱河上》新书发布会在正定塔元...",
+      "url": "https://baijiahao.baidu.com/s?id=1863822847994328712&wfr=spider&for=pc",
+      "source": "河北青年报",
+      "time": "4月30日",
+      "category": "activity"
+    },
+    {
       "title": "《郭永怀传》新书发布,“千里山海书香威海”全民阅读活动周启幕",
       "url": "https://baijiahao.baidu.com/s?id=1862883593361097132&wfr=spider&for=pc",
       "source": "齐鲁壹点",
       "time": "4月19日",
-      "category": "activity"
-    },
-    {
-      "title": "书香喀什・新书发布暨现场签售活动举行",
-      "url": "http://baijiahao.baidu.com/s?id=1863459232450526300&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "4月25日",
       "category": "activity"
     },
     {
@@ -69,27 +69,6 @@ window.LIT_DATA = {
       "url": "https://baijiahao.baidu.com/s?id=1868331656322285113&wfr=spider&for=pc",
       "source": "人民资讯",
       "time": "6月18日",
-      "category": "activity"
-    },
-    {
-      "title": "第四届接力杯曹文轩儿童小说奖新书发布为读者提供安放情绪的精神...",
-      "url": "https://www.myzaker.com/article/6a647f8c8e9f0919e650249d",
-      "source": "ZAKER资讯",
-      "time": "前天17:04",
-      "category": "activity"
-    },
-    {
-      "title": "第四届接力杯曹文轩儿童小说奖新书发布暨作家对谈在杭举行",
-      "url": "https://baijiahao.baidu.com/s?id=1871683520804917732&wfr=spider&for=pc",
-      "source": "浙江日报",
-      "time": "前天18:48",
-      "category": "activity"
-    },
-    {
-      "title": "第四届接力杯曹文轩儿童小说奖新书发布暨作家对谈在全国书博会举行",
-      "url": "https://baijiahao.baidu.com/s?id=1871655928518036912&wfr=spider&for=pc",
-      "source": "钱江晚报",
-      "time": "前天11:27",
       "category": "activity"
     },
     {
