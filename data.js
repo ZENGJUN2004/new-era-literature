6b1ca67050f19e0ad2a8494e666a31ede1efccb1
GitHub Action <action@github.com>
2026-08-02 11:10:58 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-08-02 17:57",
+  "update_time": "2026-08-02 19:10",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -30,6 +30,13 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
+      "title": "书香喀什・新书发布暨现场签售活动举行",
+      "url": "http://baijiahao.baidu.com/s?id=1863459232450526300&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "4月25日",
+      "category": "activity"
+    },
+    {
       "title": "《郭永怀传》新书发布,“千里山海书香威海”全民阅读活动周启幕",
       "url": "https://baijiahao.baidu.com/s?id=1862883593361097132&wfr=spider&for=pc",
       "source": "齐鲁壹点",
@@ -55,13 +62,6 @@ window.LIT_DATA = {
       "url": "https://baijiahao.baidu.com/s?id=1863401191001013265&wfr=spider&for=pc",
       "source": "青瞳视角",
       "time": "4月25日",
-      "category": "activity"
-    },
-    {
-      "title": "《平居日札》新书发布暨王充闾作品研讨会举办",
-      "url": "https://baijiahao.baidu.com/s?id=1868331656322285113&wfr=spider&for=pc",
-      "source": "人民资讯",
-      "time": "6月18日",
       "category": "activity"
     },
     {
