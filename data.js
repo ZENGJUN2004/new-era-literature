fa44a576b887d3adf37687d6b80d10604200db7f
GitHub Action <action@github.com>
2026-08-06 10:24:00 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-08-06 15:30",
+  "update_time": "2026-08-06 18:24",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -30,10 +30,24 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "《郭永怀传》新书发布,“千里山海书香威海”全民阅读活动周启幕",
-      "url": "https://baijiahao.baidu.com/s?id=1862883593361097132&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "4月19日",
+      "title": "聚焦边疆书写 张者《天边》新书发布",
+      "url": "https://baijiahao.baidu.com/s?id=1862343135717648529&wfr=spider&for=pc",
+      "source": "中国新闻网",
+      "time": "4月13日",
+      "category": "activity"
+    },
+    {
+      "title": "书香喀什・新书发布暨现场签售活动举行",
+      "url": "http://baijiahao.baidu.com/s?id=1863459232450526300&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "4月25日",
+      "category": "activity"
+    },
+    {
+      "title": "纪念哈尔滨解放80周年文学作品集《绽放》首发式暨新书发布座谈会...",
+      "url": "https://baijiahao.baidu.com/s?id=1864519120020918740&wfr=spider&for=pc",
+      "source": "中国新闻网",
+      "time": "5月7日",
       "category": "activity"
     },
     {
@@ -44,11 +58,11 @@ window.LIT_DATA = {
       "category": "other"
     },
     {
-      "title": "青年作家徐鹏新作《未央·蜀道残阳》在永川发布",
-      "url": "https://baijiahao.baidu.com/s?id=1863771851573387062&wfr=spider&for=pc",
-      "source": "金台资讯",
-      "time": "4月29日",
-      "category": "other"
+      "title": "延边作者柳雪花《清明晚霞》新书发布会在书博会举行",
+      "url": "http://baijiahao.baidu.com/s?id=1865244738734904313&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "5月15日",
+      "category": "activity"
     },
     {
       "title": "作家邱华栋《敦煌变》新书发布会纪实",
@@ -58,16 +72,9 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "《平居日札》新书发布暨王充闾作品研讨会举办",
-      "url": "https://baijiahao.baidu.com/s?id=1868331656322285113&wfr=spider&for=pc",
-      "source": "人民资讯",
-      "time": "6月18日",
-      "category": "activity"
-    },
-    {
       "title": "第四届接力杯曹文轩儿童小说奖新书发布暨作家对谈在杭举行",
-      "url": "https://baijiahao.baidu.com/s?id=1871683520804917732&wfr=spider&for=pc",
-      "source": "浙江日报",
+      "url": "https://baijiahao.baidu.com/s?id=1871683673744035341&wfr=spider&for=pc",
+      "source": "潮新闻客户端",
       "time": "7月25日",
       "category": "activity"
     },
