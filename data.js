7390713df768bda8a2b119a59f371c32f4a17d91
GitHub Action <action@github.com>
2026-08-07 08:04:34 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-08-07 14:03",
+  "update_time": "2026-08-07 16:04",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,25 +23,102 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "燕赵书香,钱塘绽放丨河北出版传媒集团这份精品书单等您品鉴",
+      "url": "https://baijiahao.baidu.com/s?id=1871556630746665255&wfr=spider&for=pc",
+      "source": "河北青年报",
+      "time": "7月24日",
+      "category": "activity"
+    },
+    {
+      "title": "批评深描与游历之眼",
+      "url": "https://epaper.gmw.cn/zhdsb/html/content/202603/11/content_8748.html",
+      "source": "光明数字报",
+      "time": "3月11日",
+      "category": "meeting"
+    },
+    {
+      "title": "文艺批评·新书选读|宫立《中国现代文学史诀微》导言、后记及书评",
+      "url": "https://cul.sohu.com/a/1006593663_121124790",
+      "source": "搜狐网",
+      "time": "4月8日",
+      "category": "activity"
+    },
+    {
+      "title": "文艺批评·新书选读 | 韩松刚:《批评的抵达》",
+      "url": "https://cul.sohu.com/a/1015993578_121124790",
+      "source": "搜狐网",
+      "time": "4月29日",
+      "category": "activity"
+    },
+    {
+      "title": "真善美的三重奏:文学批评的时代使命|新批评",
+      "url": "https://www.thepaper.cn/newsDetail_forward_32417549",
+      "source": "澎湃新闻",
+      "time": "1月20日",
+      "category": "meeting"
+    },
+    {
+      "title": "文学批评的“正典”与“副典”,如何重拾利器锋芒?",
+      "url": "https://baijiahao.baidu.com/s?id=1859444206926581017&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "3月12日",
+      "category": "meeting"
+    },
+    {
+      "title": "文学批评为何变“素描”?",
+      "url": "http://baijiahao.baidu.com/s?id=1859444213714245687&wfr=spider&for=pc",
+      "source": "上观新闻",
+      "time": "3月12日",
+      "category": "meeting"
+    },
+    {
+      "title": "在流量时代,文学批评如何“抵达”人心?",
+      "url": "https://baijiahao.baidu.com/s?id=1865876878495342673&wfr=spider&for=pc",
+      "source": "济南时报",
+      "time": "5月22日",
+      "category": "meeting"
+    },
+    {
+      "title": "豆瓣8.0+!高分人物传记,一书读罢头飞雪",
+      "url": "https://www.thepaper.cn/newsDetail_forward_33007780",
+      "source": "澎湃新闻",
+      "time": "4月21日",
+      "category": "other"
+    },
+    {
+      "title": "央视热播、奥斯卡提名!这12部高分影视原著,承包你的夏日书单丨...",
+      "url": "https://cul.sohu.com/a/1051765790_121106832",
+      "source": "搜狐网",
+      "time": "",
+      "category": "other"
+    },
+    {
+      "title": "甘图云塾丨央视热播、奥斯卡提名!这12部高分影视原著,承包你的...",
+      "url": "https://yule.sohu.com/a/1059167225_121106869",
+      "source": "搜狐娱乐",
+      "time": "前天17:43",
+      "category": "other"
+    },
+    {
+      "title": "万人评分8.5的奇葩神作!哲学还能这么写?!",
+      "url": "https://baijiahao.baidu.com/s?id=1872698737039490057&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "前天23:44",
+      "category": "other"
+    },
+    {
+      "title": "《收获》文学榜揭晓,榜单映射中国文坛迎来“丰年”",
+      "url": "https://baijiahao.baidu.com/s?id=1856184355598057672&wfr=spider&for=pc",
+      "source": "新华网",
+      "time": "2月4日",
+      "category": "other"
+    },
+    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
       "category": "activity"
-    },
-    {
-      "title": "《郭永怀传》新书发布,“千里山海书香威海”全民阅读活动周启幕",
-      "url": "https://baijiahao.baidu.com/s?id=1862883593361097132&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "4月19日",
-      "category": "activity"
-    },
-    {
-      "title": "青年作家徐鹏新作《未央·蜀道残阳》在重庆永川发布",
-      "url": "http://www.cq.xinhuanet.com/20260429/47022f852e3d4ac5a7f184dfcf56d340/c.html",
-      "source": "新华网重庆频道",
-      "time": "4月29日",
-      "category": "other"
     },
     {
       "title": "青年作家徐鹏新作《未央·蜀道残阳》在永川发布",
@@ -86,31 +163,66 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "冉学东长篇小说《上寨》首发仪式暨作品讨论会在西安举行",
-      "url": "https://baijiahao.baidu.com/s?id=1855993505774569048&wfr=spider&for=pc",
-      "source": "三秦都市报",
-      "time": "2月2日",
-      "category": "activity"
-    },
-    {
-      "title": "聚焦个体命运与历史纵深 抗战题材长篇小说《地啸》首发",
-      "url": "https://baijiahao.baidu.com/s?id=1853803167951408475&wfr=spider&for=pc",
-      "source": "齐鲁壹点",
-      "time": "1月9日",
-      "category": "activity"
-    },
-    {
-      "title": "老藤长篇小说《自愈者》在京首发在“AI迷宫”中寻找心灵坐标",
-      "url": "https://baijiahao.baidu.com/s?id=1853940665722016960&wfr=spider&for=pc",
-      "source": "红网",
-      "time": "1月10日",
-      "category": "activity"
-    },
-    {
       "title": "抵达辽阔远方的多声部抗战叙事——长篇小说《地啸》首发实录",
       "url": "https://baijiahao.baidu.com/s?id=1854825795286531236&wfr=spider&for=pc",
       "source": "齐鲁壹点",
       "time": "1月20日",
+      "category": "activity"
+    },
+    {
+      "title": "把家乡长辈抗战故事写成书,长篇小说《长城1933》长沙首发",
+      "url": "https://baijiahao.baidu.com/s?id=1862171110712203201&wfr=spider&for=pc",
+      "source": "潇湘晨报",
+      "time": "4月11日",
+      "category": "activity"
+    },
+    {
+      "title": "聚焦普通人蜕变成长长篇小说《不虚此生》首发",
+      "url": "https://baijiahao.baidu.com/s?id=1868337403425152313&wfr=spider&for=pc",
+      "source": "中国新闻网",
+      "time": "6月18日",
+      "category": "activity"
+    },
+    {
+      "title": "再一次把老家请进文字,祁智长篇儿童小说《我们兄弟》在江苏书展首发",
+      "url": "https://baijiahao.baidu.com/s?id=1869945028176085222&wfr=spider&for=pc",
+      "source": "扬子晚报",
+      "time": "7月6日",
+      "category": "activity"
+    },
+    {
+      "title": "书评丨陈建辉:在地·共情·文心",
+      "url": "https://baijiahao.baidu.com/s?id=1872846049569463468&wfr=spider&for=pc",
+      "source": "红网",
+      "time": "1小时前",
+      "category": "other"
+    },
+    {
+      "title": "长篇科幻小说《拯救蓝水星》发布",
+      "url": "https://baijiahao.baidu.com/s?id=1872834109191332145&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "4小时前",
+      "category": "activity"
+    },
+    {
+      "title": "成年后想自学数学?5本书从入门到精通!",
+      "url": "https://baijiahao.baidu.com/s?id=1872790177240537180&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "昨天23:58",
+      "category": "other"
+    },
+    {
+      "title": "“新时代辽宁文学之星”新书发布会在沈举办",
+      "url": "https://baijiahao.baidu.com/s?id=1872717973189476790&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "昨天04:50",
+      "category": "activity"
+    },
+    {
+      "title": "...墨白年谱》出版——“中原作家群年谱丛书”新书发布暨研讨会...",
+      "url": "https://cul.sohu.com/a/1059589477_121124705",
+      "source": "搜狐网",
+      "time": "昨天16:48",
       "category": "activity"
     }
   ]
