8e18ec2a2c386cf827caad56ce549e2bd95497c8
GitHub Action <action@github.com>
2026-08-08 20:15:31 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-08-09 03:20",
+  "update_time": "2026-08-09 04:15",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -30,16 +30,16 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
+      "title": "《郭永怀传》新书发布,“千里山海书香威海”全民阅读活动周启幕",
+      "url": "https://baijiahao.baidu.com/s?id=1862883593361097132&wfr=spider&for=pc",
+      "source": "齐鲁壹点",
+      "time": "4月19日",
+      "category": "activity"
+    },
+    {
       "title": "青年作家徐鹏新作《未央·蜀道残阳》在重庆永川发布",
       "url": "http://www.cq.xinhuanet.com/20260429/47022f852e3d4ac5a7f184dfcf56d340/c.html",
       "source": "新华网重庆频道",
-      "time": "4月29日",
-      "category": "other"
-    },
-    {
-      "title": "青年作家徐鹏新作《未央·蜀道残阳》在永川发布",
-      "url": "https://baijiahao.baidu.com/s?id=1863771851573387062&wfr=spider&for=pc",
-      "source": "金台资讯",
       "time": "4月29日",
       "category": "other"
     },
