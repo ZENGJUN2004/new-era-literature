d64b1ae13a985fbb60328c4351ed2807891109d6
GitHub Action <action@github.com>
2026-08-15 08:13:45 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-08-15 15:20",
+  "update_time": "2026-08-15 16:13",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -54,6 +54,13 @@ window.LIT_DATA = {
       "title": "青年作家徐鹏新作《未央·蜀道残阳》在重庆永川发布",
       "url": "http://www.cq.xinhuanet.com/20260429/47022f852e3d4ac5a7f184dfcf56d340/c.html",
       "source": "新华网重庆频道",
+      "time": "4月29日",
+      "category": "other"
+    },
+    {
+      "title": "青年作家徐鹏新作《未央·蜀道残阳》在永川发布",
+      "url": "https://baijiahao.baidu.com/s?id=1863771851573387062&wfr=spider&for=pc",
+      "source": "金台资讯",
       "time": "4月29日",
       "category": "other"
     },
@@ -139,6 +146,13 @@ window.LIT_DATA = {
       "url": "https://baijiahao.baidu.com/s?id=1869945028176085222&wfr=spider&for=pc",
       "source": "扬子晚报",
       "time": "7月6日",
+      "category": "activity"
+    },
+    {
+      "title": "史笔通心——《平居日札》新书发布暨王充闾作品研讨会在沈阳成功...",
+      "url": "https://www.thepaper.cn/newsDetail_forward_33783689",
+      "source": "澎湃新闻",
+      "time": "昨天16:35",
       "category": "activity"
     }
   ]
