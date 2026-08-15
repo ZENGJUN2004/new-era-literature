3c679e835f38139c0caf73e1705330bd08405631
GitHub Action <action@github.com>
2026-08-15 20:09:48 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-08-16 03:12",
+  "update_time": "2026-08-16 04:09",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -37,13 +37,6 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "关仁山《太阳照在滹沱河上》新书发布会在正定举行",
-      "url": "https://cul.sohu.com/a/1020080228_122014422",
-      "source": "搜狐网",
-      "time": "5月9日",
-      "category": "activity"
-    },
-    {
       "title": "新加坡著名慈善家、作家卓顺发《太阳出来了,露水不见了》新书发布",
       "url": "https://baijiahao.baidu.com/s?id=1854366953074514692&wfr=spider&for=pc",
       "source": "曲靖珠江网",
@@ -51,10 +44,10 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "书香喀什・新书发布暨现场签售活动举行",
-      "url": "http://baijiahao.baidu.com/s?id=1863459232450526300&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "4月25日",
+      "title": "山乡巨变中的文学守望:《太阳照在滹沱河上》新书发布会在正定塔元...",
+      "url": "https://baijiahao.baidu.com/s?id=1863822847994328712&wfr=spider&for=pc",
+      "source": "河北青年报",
+      "time": "4月30日",
       "category": "activity"
     },
     {
