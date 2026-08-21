5d7f9eba2c2d8da18c0cef55b66370d37b981d2e
GitHub Action <action@github.com>
2026-08-21 06:23:25 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-08-21 13:22",
+  "update_time": "2026-08-21 14:23",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,13 +23,6 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "书香喀什・新书发布暨现场签售活动举行",
-      "url": "http://baijiahao.baidu.com/s?id=1863459232450526300&wfr=spider&for=pc",
-      "source": "澎湃新闻客户端",
-      "time": "4月25日",
-      "category": "activity"
-    },
-    {
       "title": "山乡巨变中的文学守望:《太阳照在滹沱河上》新书发布会在正定塔元...",
       "url": "https://baijiahao.baidu.com/s?id=1863822847994328712&wfr=spider&for=pc",
       "source": "河北青年报",
@@ -41,6 +34,13 @@ window.LIT_DATA = {
       "url": "https://www.thepaper.cn/newsDetail_forward_33783689",
       "source": "澎湃新闻",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "《平居日札》新书发布暨王充闾作品研讨会举办",
+      "url": "https://baijiahao.baidu.com/s?id=1868331656322285113&wfr=spider&for=pc",
+      "source": "人民资讯",
+      "time": "6月18日",
       "category": "activity"
     },
     {
