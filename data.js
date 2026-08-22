01f035efaa321c877ef87e7937f2b447f25aa8cd
GitHub Action <action@github.com>
2026-08-22 05:16:36 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-08-22 12:21",
+  "update_time": "2026-08-22 13:16",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,17 +23,17 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
+      "title": "书香喀什・新书发布暨现场签售活动举行",
+      "url": "http://baijiahao.baidu.com/s?id=1863459232450526300&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "4月25日",
+      "category": "activity"
+    },
+    {
       "title": "山乡巨变中的文学守望:《太阳照在滹沱河上》新书发布会在正定塔元...",
       "url": "https://baijiahao.baidu.com/s?id=1863822847994328712&wfr=spider&for=pc",
       "source": "河北青年报",
       "time": "4月30日",
-      "category": "activity"
-    },
-    {
-      "title": "史笔通心——《平居日札》新书发布暨王充闾作品研讨会在沈阳成功...",
-      "url": "https://www.thepaper.cn/newsDetail_forward_33783689",
-      "source": "澎湃新闻",
-      "time": "",
       "category": "activity"
     },
     {
@@ -47,6 +47,13 @@ window.LIT_DATA = {
       "title": "青年作家徐鹏新作《未央·蜀道残阳》在重庆永川发布",
       "url": "http://www.cq.xinhuanet.com/20260429/47022f852e3d4ac5a7f184dfcf56d340/c.html",
       "source": "新华网重庆频道",
+      "time": "4月29日",
+      "category": "other"
+    },
+    {
+      "title": "青年作家徐鹏新作《未央·蜀道残阳》在永川发布",
+      "url": "https://baijiahao.baidu.com/s?id=1863771851573387062&wfr=spider&for=pc",
+      "source": "金台资讯",
       "time": "4月29日",
       "category": "other"
     },
