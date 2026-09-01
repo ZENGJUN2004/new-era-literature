14472a25c421be93a67e4d8c917700f3f48e9ec5
GitHub Action <action@github.com>
2026-09-01 01:30:22 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-09-01 06:07",
+  "update_time": "2026-09-01 09:30",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -27,6 +27,20 @@ window.LIT_DATA = {
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
+      "category": "activity"
+    },
+    {
+      "title": "老藤长篇小说《自愈者》新书发布会举办",
+      "url": "https://baijiahao.baidu.com/s?id=1854092227377307964&wfr=spider&for=pc",
+      "source": "工人日报",
+      "time": "1月12日",
+      "category": "activity"
+    },
+    {
+      "title": "新加坡著名慈善家、作家卓顺发《太阳出来了,露水不见了》新书发布",
+      "url": "https://baijiahao.baidu.com/s?id=1854366953074514692&wfr=spider&for=pc",
+      "source": "曲靖珠江网",
+      "time": "1月15日",
       "category": "activity"
     },
     {
@@ -61,13 +75,6 @@ window.LIT_DATA = {
       "title": "青年作家徐鹏新作《未央·蜀道残阳》在重庆永川发布",
       "url": "http://www.cq.xinhuanet.com/20260429/47022f852e3d4ac5a7f184dfcf56d340/c.html",
       "source": "新华网重庆频道",
-      "time": "4月29日",
-      "category": "other"
-    },
-    {
-      "title": "青年作家徐鹏新作《未央·蜀道残阳》在永川发布",
-      "url": "https://baijiahao.baidu.com/s?id=1863771851573387062&wfr=spider&for=pc",
-      "source": "金台资讯",
       "time": "4月29日",
       "category": "other"
     },
@@ -107,13 +114,6 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "把家乡长辈抗战故事写成书,长篇小说《长城1933》长沙首发",
-      "url": "https://baijiahao.baidu.com/s?id=1862171110712203201&wfr=spider&for=pc",
-      "source": "潇湘晨报",
-      "time": "4月11日",
-      "category": "activity"
-    },
-    {
       "title": "首届“刘知侠长篇小说奖”颁奖典礼暨《青岛市中青年作家文库...",
       "url": "https://baijiahao.baidu.com/s?id=1865704053349245673&wfr=spider&for=pc",
       "source": "半岛都市报",
@@ -139,6 +139,13 @@ window.LIT_DATA = {
       "url": "https://baijiahao.baidu.com/s?id=1869945028176085222&wfr=spider&for=pc",
       "source": "扬子晚报",
       "time": "7月6日",
+      "category": "activity"
+    },
+    {
+      "title": "【新刊速递】| 《啄木鸟》2024年第2期目录",
+      "url": "https://bao.hvacr.cn/ArTicle/details/anan/97623261.shtml",
+      "source": "制冷快报",
+      "time": "昨天09:53",
       "category": "activity"
     }
   ]
