11d94f2bc92429bbf665e7506e71d7042e4ce931
GitHub Action <action@github.com>
2026-09-02 18:12:44 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-09-02 22:47",
+  "update_time": "2026-09-03 02:12",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -23,38 +23,10 @@ window.LIT_DATA = {
       "category": "meeting"
     },
     {
-      "title": "【新刊速递】| 《啄木鸟》2024年第2期目录",
-      "url": "https://bao.hvacr.cn/ArTicle/details/anan/97623261.shtml",
-      "source": "制冷快报",
-      "time": "前天09:53",
-      "category": "activity"
-    },
-    {
       "title": "C刊目录 | CSSCI来源期刊目录(2021-2022)",
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "老藤长篇小说《自愈者》新书发布会举办",
-      "url": "https://baijiahao.baidu.com/s?id=1854092227377307964&wfr=spider&for=pc",
-      "source": "工人日报",
-      "time": "1月12日",
-      "category": "activity"
-    },
-    {
-      "title": "新加坡著名慈善家、作家卓顺发《太阳出来了,露水不见了》新书发布",
-      "url": "https://baijiahao.baidu.com/s?id=1854366953074514692&wfr=spider&for=pc",
-      "source": "曲靖珠江网",
-      "time": "1月15日",
-      "category": "activity"
-    },
-    {
-      "title": "孔德林新书发布暨作品分享会在七星关区举行",
-      "url": "https://baijiahao.baidu.com/s?id=1862968999751818915&wfr=spider&for=pc",
-      "source": "天眼新闻",
-      "time": "4月20日",
       "category": "activity"
     },
     {
@@ -79,9 +51,23 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
+      "title": "《平居日札》新书发布暨王充闾作品研讨会举办",
+      "url": "https://baijiahao.baidu.com/s?id=1868331656322285113&wfr=spider&for=pc",
+      "source": "人民资讯",
+      "time": "6月18日",
+      "category": "activity"
+    },
+    {
       "title": "青年作家徐鹏新作《未央·蜀道残阳》在重庆永川发布",
       "url": "http://www.cq.xinhuanet.com/20260429/47022f852e3d4ac5a7f184dfcf56d340/c.html",
       "source": "新华网重庆频道",
+      "time": "4月29日",
+      "category": "other"
+    },
+    {
+      "title": "青年作家徐鹏新作《未央·蜀道残阳》在永川发布",
+      "url": "https://baijiahao.baidu.com/s?id=1863771851573387062&wfr=spider&for=pc",
+      "source": "金台资讯",
       "time": "4月29日",
       "category": "other"
     },
@@ -97,6 +83,13 @@ window.LIT_DATA = {
       "url": "https://baijiahao.baidu.com/s?id=1871655928518036912&wfr=spider&for=pc",
       "source": "钱江晚报",
       "time": "7月25日",
+      "category": "activity"
+    },
+    {
+      "title": "《于敏和氢弹》新书发布",
+      "url": "https://baijiahao.baidu.com/s?id=1873729907427440760&wfr=spider&for=pc",
+      "source": "新浪财经",
+      "time": "8月17日",
       "category": "activity"
     },
     {
@@ -128,6 +121,13 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
+      "title": "再一次把老家请进文字,祁智长篇儿童小说《我们兄弟》在江苏书展首发",
+      "url": "https://baijiahao.baidu.com/s?id=1869945028176085222&wfr=spider&for=pc",
+      "source": "扬子晚报",
+      "time": "7月6日",
+      "category": "activity"
+    },
+    {
       "title": "聚焦普通人蜕变成长长篇小说《不虚此生》首发",
       "url": "https://baijiahao.baidu.com/s?id=1868337403425152313&wfr=spider&for=pc",
       "source": "中国新闻网",
@@ -139,13 +139,6 @@ window.LIT_DATA = {
       "url": "http://www.xinhuanet.com/politics/20260619/50825e3ba9534491bf9c2ba5460739b3/c.html",
       "source": "新华网",
       "time": "6月19日",
-      "category": "activity"
-    },
-    {
-      "title": "再一次把老家请进文字,祁智长篇儿童小说《我们兄弟》在江苏书展首发",
-      "url": "https://baijiahao.baidu.com/s?id=1869945028176085222&wfr=spider&for=pc",
-      "source": "扬子晚报",
-      "time": "7月6日",
       "category": "activity"
     }
   ]
