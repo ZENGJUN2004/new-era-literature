62b537eb92224fbf06a9d42e4c964a549f404e26
GitHub Action <action@github.com>
2026-09-09 00:48:18 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-09-09 06:06",
+  "update_time": "2026-09-09 08:48",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -30,17 +30,17 @@ window.LIT_DATA = {
       "category": "activity"
     },
     {
-      "title": "新加坡著名慈善家、作家卓顺发《太阳出来了,露水不见了》新书发布",
-      "url": "https://baijiahao.baidu.com/s?id=1854366953074514692&wfr=spider&for=pc",
-      "source": "曲靖珠江网",
-      "time": "1月15日",
-      "category": "activity"
-    },
-    {
       "title": "聚焦边疆书写 张者《天边》新书发布",
       "url": "https://baijiahao.baidu.com/s?id=1862343135717648529&wfr=spider&for=pc",
       "source": "中国新闻网",
       "time": "4月13日",
+      "category": "activity"
+    },
+    {
+      "title": "书香喀什・新书发布暨现场签售活动举行",
+      "url": "http://baijiahao.baidu.com/s?id=1863459232450526300&wfr=spider&for=pc",
+      "source": "澎湃新闻客户端",
+      "time": "4月25日",
       "category": "activity"
     },
     {
