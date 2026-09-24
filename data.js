bb7decd5fcdaf94c930e851ac75bbd882aacdb59
GitHub Action <action@github.com>
2026-09-24 10:39:10 +0000

    Auto update

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.LIT_DATA = {
-  "update_time": "2026-09-24 13:37",
+  "update_time": "2026-09-24 18:39",
   "news": [
     {
       "title": "👉【微信深度】搜索“文学批评”公众号最新文章",
@@ -27,13 +27,6 @@ window.LIT_DATA = {
       "url": "http://baijiahao.baidu.com/s?id=1708225667557630537&wfr=spider&for=pc",
       "source": "澎湃新闻客户端",
       "time": "",
-      "category": "activity"
-    },
-    {
-      "title": "《自愈者》新书在京发布作家老藤探讨AI时代的心灵救赎之路",
-      "url": "https://baijiahao.baidu.com/s?id=1854293051586245393&wfr=spider&for=pc",
-      "source": "央广网",
-      "time": "1月14日",
       "category": "activity"
     },
     {
